b0804ce9694c83ab8cc3a1e349bf6b9e701868c6
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-02 23:48:37 +0300

    delete comments

--- frontend/src/pages/MainPage/ui/MainPage.tsx
+++ frontend/src/pages/MainPage/ui/MainPage.tsx
@@ -3,21 +3,15 @@ import {Navbar} from "@/widgets/Navbar";
 import cls from "./MainPage.module.scss";
 import {Button, ButtonSize, ButtonTheme} from "@/shared/ui/Button/Button";
 import {AppExternalLinks} from "@/shared/assets/appExternalLinks/appExternalLinks";
+import {Container} from "@/shared/ui/Container/ui/Container";
 
 const MainPage = () => {
     return (
         <>
             <Navbar overlayed marginTop={20} />
-            {/*<NavbarTouch overlayed marginTop={20} />*/}
-            {/*<Navbar overlayed marginTop={20} />*/}
+
             <HeroSection className={cls.heroSection} >
                 <Button theme={ButtonTheme.Graffiti} size={ButtonSize.XXXL} > <a href={AppExternalLinks.webgl} target="_blank">Pelaa netissa !</a></Button>
-            </HeroSection>
-
-            <br/>
-            <br/>
-
-            <HeroSection className={cls.heroSection}>
             </HeroSection>
         </>
     );
