f567875731c4620eef02c6e34b26ac4bb884fe00
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-02 23:45:45 +0300

    add comments

--- frontend/src/shared/ui/Sidebar/ui/Sidebar/Sidebar.tsx
+++ frontend/src/shared/ui/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
+/**
+ * @fileoverview A customizable sidebar component that can be expanded or collapsed.
+ */
+
 import {classNames} from "@/shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import {SidebarItemType} from "@/shared/ui/Sidebar/model/items";
-import {useEffect, useMemo, useRef, useState} from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import {SidebarItem} from "@/shared/ui/Sidebar/ui/SidebarItem/SidebarItem";
 
 
@@ -9,9 +13,10 @@ interface SidebarProps {
     buttonClassName?: string;
     sidebarItemsList: SidebarItemType[];
     side? : 'left'| 'right'
+    closeOnClickOutside?: boolean;
 }
 
-export const Sidebar = ({ buttonClassName = '',  sidebarItemsList , side = 'left'}: SidebarProps) => {
+export const Sidebar = ({ buttonClassName = '',  sidebarItemsList , side = 'left' ,closeOnClickOutside = false}: SidebarProps) => {
 
     const [isCollapsed, setIsCollapsed] = useState(true);
 
@@ -21,6 +26,13 @@ export const Sidebar = ({ buttonClassName = '',  sidebarItemsList , side = 'left
 
     const currentButton = isCollapsed ? '☰' : 'Х'
 
+    /**
+     * An object containing CSS classes to be applied to the Sidebar
+     * based on the component's state and properties. This is used to
+     * control the visual appearance of the Sidebar, such as whether
+     * it is expanded or collapsed, and if it is positioned on the left
+     * or right side of the screen.
+     */
     const mods = {
         [cls.collapsed]: isCollapsed,
         [cls.expanded]: !isCollapsed,
@@ -33,23 +45,53 @@ export const Sidebar = ({ buttonClassName = '',  sidebarItemsList , side = 'left
         [cls.expandedButton]: !isCollapsed
     }
 
+
+    /**
+     * A ref for the Sidebar component's root HTMLDivElement.
+     * It is used to check if a click event occurred inside or outside the Sidebar
+     * in the handleClickOutside function. If the click event happened outside
+     * the Sidebar and the closeOnClickOutside prop is set to true, the Sidebar
+     * will collapse.
+     *
+     */
     const sidebarRef = useRef<HTMLDivElement | null>(null);
 
+
+    /**
+     * Event handler for handling clicks outside the Sidebar component.
+     * If the click is outside the Sidebar and not on the toggle button(div with classname button),
+     * or some another button(e.g scrollUp Button),
+     * it will collapse the Sidebar if `closeOnClickOutside` prop is set to true.
+     */
     const handleClickOutside = (event: MouseEvent) => {
-        const clickedButton = (event.target as HTMLElement).classList.contains(cls.button);
-        if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)  && !clickedButton) {
+        const clickedButton = (event.target as HTMLElement).tagName === 'BUTTON' || (event.target as HTMLElement).classList.contains(cls.button);
+        if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node) && !clickedButton) {
             setIsCollapsed(true);
         }
     };
 
+    /**
+     * useEffect hook for managing the click outside event listener.
+     * If the `closeOnClickOutside` prop is true, the `handleClickOutside` event
+     * handler will be added to the document as a "mousedown" event listener.
+     * The event listener is removed when the component is unmounted or when
+     * the `closeOnClickOutside` prop changes.
+     */
     useEffect(() => {
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
+        if(closeOnClickOutside){
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }
+    }, [closeOnClickOutside]);
 
 
+    /**
+     * A memoized list of SidebarItem components based on the provided sidebarItemsList prop.
+     * The memoization ensures that the list is only recomputed when the `isCollapsed` state changes.
+     *
+     */
     const itemsList = useMemo(() => sidebarItemsList.map((item) => (
         <SidebarItem
             item={item}
