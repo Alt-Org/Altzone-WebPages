0e8804ef18d5d11e4bcb26c8634242607114c07a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-02 23:50:13 +0300

    add external links

--- frontend/src/shared/assets/appExternalLinks/appExternalLinks.ts
+++ frontend/src/shared/assets/appExternalLinks/appExternalLinks.ts
@@ -1,3 +1,7 @@
 export const AppExternalLinks = {
-    webgl : "https://alt-org.github.io/builds/webgl"
-}
+    webgl : "https://alt-org.github.io/builds/webgl",
+    discord: "https://discord.gg/58QVb7kn",
+    facebook : "https://www.facebook.com/GamingPsyche",
+    instagram : "https://www.instagram.com/gamingpsyche",
+    youtube : "https://www.youtube.com/channel/UCTgWpC9Hq7b-W1Lhmry97YA"
+} as const
