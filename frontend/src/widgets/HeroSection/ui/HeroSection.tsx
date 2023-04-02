1af24b5b749f3532e718d558db04d5bc64207807
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-02 23:49:13 +0300

    add container

--- frontend/src/widgets/HeroSection/ui/HeroSection.tsx
+++ frontend/src/widgets/HeroSection/ui/HeroSection.tsx
@@ -2,6 +2,7 @@ import {classNames} from "@/shared/lib/classNames/classNames";
 import {memo, ReactNode} from "react";
 import cls from "./HeroSection.module.scss";
 import backgroundImage from '@/shared/assets/images/introBackground.avif';
+import {Container} from "@/shared/ui/Container/ui/Container";
 
 interface HeroSectionProps {
     className?: string;
@@ -21,9 +22,13 @@ export const HeroSection = memo((props : HeroSectionProps) => {
 
     return (
         <section className={classNames(cls.HeroSection, mods, [className])} style={{backgroundImage : `url(${backgroundImage})`}}>
+
             <div className={classNames(cls.childrenWrapper)}>
+                <Container>
                 {children}
+                </Container>
             </div>
+
         </section>
     );
 });
