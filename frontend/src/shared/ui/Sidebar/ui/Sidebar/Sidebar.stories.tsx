729dda1d042cb9604d9f99d99fef9b2a52d8905a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-02 23:46:00 +0300

    add mock values

--- frontend/src/shared/ui/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ frontend/src/shared/ui/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -3,15 +3,33 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Sidebar } from './Sidebar';
 
 export default {
-    title: 'widget/Sidebar',
+    title: 'shared/Sidebar',
     component: Sidebar,
     argTypes: {
-        backgroundColor: { control: 'color' },
+        buttonClassName: { control: 'text' },
+        side: { control: { type: 'select', options: ['left', 'right'] } },
+        closeOnClickOutside: { control: 'boolean' },
     },
 } as ComponentMeta<typeof Sidebar>;
+
+const sidebarItemsList = [
+        {
+            name: 'Item 1',
+            path: '#item1',
+
+        },
+        {
+            name: 'Item 2',
+            path: '#item2',
+        },
+    ]
 
 const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
-
+Primary.args = {
+    buttonClassName: '',
+    sidebarItemsList: sidebarItemsList,
+    side: 'left',
+    closeOnClickOutside: false,
+};
