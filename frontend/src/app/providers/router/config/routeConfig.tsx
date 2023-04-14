5c1433a5aad9d54e9605af6e6de93ad4ddf8c206
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-14 16:01:49 +0300

    changed location of internal app routes

--- frontend/src/app/providers/router/config/routeConfig.tsx
+++ frontend/src/app/providers/router/config/routeConfig.tsx
@@ -1,47 +1,31 @@
-import { RouteProps, redirect, Navigate  } from "react-router-dom";
+import { RouteProps, Navigate  } from "react-router-dom";
 import { MainPage } from "@/pages/MainPage";
 import { AboutPage } from "@/pages/AboutPage";
 import { NotFoundPage } from "@/pages/NotFoundPage";
+import {RoutePaths,AppRoutesLinks} from "@/shared/appLinks/RoutePaths";
 
-export enum AppRoutes {
-    MAIN = "main",
-    ABOUT = "about",
-    FORUM = "forum",
-    NOT_FOUND = "notFound",
-    NOT_FOUND_CATCH = "notFoundCatch",
-}
-
-export const RoutePath: Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]: "/",
-    [AppRoutes.ABOUT]: "/about",
-    [AppRoutes.FORUM]: "/forum",
-    [AppRoutes.NOT_FOUND]: "/404",
-    // last one
-    [AppRoutes.NOT_FOUND_CATCH]: "*",
-};
-
-export const routeConfig: Record<AppRoutes, RouteProps> = {
-    [AppRoutes.MAIN]: {
-        path: RoutePath.main,
+export const routeConfig: Record<AppRoutesLinks, RouteProps> = {
+    [AppRoutesLinks.MAIN]: {
+        path: RoutePaths.MAIN,
         element: <MainPage />,
     },
-    [AppRoutes.ABOUT]: {
-        path: RoutePath.about,
+    [AppRoutesLinks.ABOUT]: {
+        path: RoutePaths.ABOUT,
         element: <AboutPage />,
     },
 
-    [AppRoutes.FORUM]: {
-        path: RoutePath.forum,
+    [AppRoutesLinks.FORUM]: {
+        path: RoutePaths.FORUM,
         element: <AboutPage/>
     },
 
-    [AppRoutes.NOT_FOUND]: {
-        path: RoutePath.notFound,
+    [AppRoutesLinks.NOT_FOUND]: {
+        path: RoutePaths.NOT_FOUND,
         element: <NotFoundPage/>
     },
 
-    [AppRoutes.NOT_FOUND_CATCH]: {
-        path: RoutePath.notFoundCatch,
-        element: <Navigate to="/404" />
+    [AppRoutesLinks.NOT_FOUND_CATCH]: {
+        path: RoutePaths.NOT_FOUND_CATCH,
+        element: <Navigate to={RoutePaths.NOT_FOUND} />
     },
 };
