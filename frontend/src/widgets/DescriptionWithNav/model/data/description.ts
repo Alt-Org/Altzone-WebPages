5efe336c12275af98736f649c26844988acd8f2a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-14 15:54:45 +0300

    add description with nav elements widget

---
+++ frontend/src/widgets/DescriptionWithNav/model/data/description.ts
@@ -0,0 +1,64 @@
+export const title = 'Projektin Kuvaus';
+
+export const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
+    '                    explicabo laudantium nihil\n' +
+    '                    nulla suscipit velit veniam.\n' +
+    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
+    '                    rem repellat?' +
+    ''
