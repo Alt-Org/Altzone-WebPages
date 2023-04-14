3558efedc6585fcfa76d59a35890c0f6d799445d
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-14 16:00:29 +0300

    moved to another location and added new links

---
+++ frontend/src/shared/appLinks/appExternalLinks.ts
@@ -0,0 +1,12 @@
+/**
+ * The values are used as externalLinks throughout the app,
+ */
+export const AppExternalLinks = {
+    webgl : "https://alt-org.github.io/builds/webgl",
+    discord: "https://discord.gg/58QVb7kn",
+    facebook : "https://www.facebook.com/GamingPsyche",
+    instagram : "https://www.instagram.com/gamingpsyche",
+    youtube : "https://www.youtube.com/channel/UCTgWpC9Hq7b-W1Lhmry97YA",
+    duunitori: "https://duunitori.fi/tyopaikat/tyo/koodari-vapaaehtoistyona-toteutettavaan-mobiilipelituotantoon-sdsuu-16480485",
+    stub: '#developerChangeThisLink'
+} as const
