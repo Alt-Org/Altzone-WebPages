b75b5938849d74793431381bc210e1e0d7127c16
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-14 15:59:32 +0300

    update main page

---
+++ frontend/src/pages/MainPage/ui/Main/MainPage.tsx
@@ -0,0 +1,30 @@
+import {HeroSection} from "@/widgets/HeroSection";
+import cls from "./MainPage.module.scss";
+import {Button, ButtonSize, ButtonTheme} from "@/shared/ui/Button/Button";
+import {AppExternalLinks} from "@/shared/appLinks/appExternalLinks";
+import {Container} from "@/shared/ui/Container";
+import {DescriptionWithNav} from "@/widgets/DescriptionWithNav";
+import {NewsSection} from "@/widgets/NewsSection";
+
+const MainPage = () => {
+    return (
+        <>
+
+            <HeroSection className={cls.heroSection} >
+                <Button withScalableLink theme={ButtonTheme.Graffiti} size={ButtonSize.XXXL} > <a href={AppExternalLinks.webgl} target="_blank">Pelaa netissa !</a></Button>
+            </HeroSection>
+
+            <Container>
+                <DescriptionWithNav/>
+            </Container>
+
+
+            <NewsSection/>
+        </>
+    );
+};
+
+export default MainPage;
+
+
+
