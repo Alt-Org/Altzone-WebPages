4b489acb53f1278aa865a44ecea07d179b8a3958
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-18 11:08:03 +0300

    move location

---
+++ frontend/src/entities/Worker/index.ts
@@ -0,0 +1,4 @@
+import {groupsWithWorkersLocally} from './model/data/workersLocally'
+export type {Workman,GroupWithWorkman} from './model/types/types'
+
+export {groupsWithWorkersLocally} from './model/data/workersLocally'
