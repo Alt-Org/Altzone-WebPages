ee49cf9513aac1bf279f4c0cbc847f3537714e8c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-23 23:22:42 +0300

    update view

--- frontend/src/widgets/WorkersSection/ui/WorkersSection.tsx
+++ frontend/src/widgets/WorkersSection/ui/WorkersSection.tsx
@@ -2,13 +2,18 @@ import cls from "./WorkersSection.module.scss";
 import {groupsWithWorkersLocally, GroupWithWorkman, Workman} from "@/entities/Worker";
 import {FC, memo} from "react";
 import {ScrollBottomButton} from "@/features/ScrollBottom";
+import {classNames} from "@/shared/lib/classNames/classNames";
 
+interface WorkersSectionProps  {
+    className?: string;
+}
 
-export const WorkersSection = () => {
+export const WorkersSection = ({className = ''}: WorkersSectionProps) => {
     return (
-        <div className={cls.WorkersSection}>
+        // <div className={cls.WorkersSection}>
+        <div className={classNames(cls.WorkersSection,{},[className])}>
 
-            <ScrollBottomButton/>
+            <ScrollBottomButton className={cls.scrollBottomButton}/>
             {
                 groupsWithWorkersLocally.map(group=>(
                     <GroupWithWorkmanComponent key={group.group} groupWithWorkman={group}/>
@@ -48,14 +53,13 @@ const WorkmanComponent: FC<WorkmanProps> = memo(({ workman }) => {
         <div className={cls.workmanComponent}>
             <h3>{workman.name}</h3>
             <ul>
-                {Object.entries(workman).map(([key, value]) => (
-                    key !== 'name' && key !== 'id' && (
-                        <li key={key}>
-                            <strong>{key}: </strong>
-                            {value}
-                        </li>
-                    )
-                ))}
+                 {workman.role &&  <li><strong>Rooli:</strong> {`${workman.role}`}</li> }
+                 {workman.status &&  <li><strong>Status:</strong> {`${workman.status}`}</li> }
+                 {workman.workPeriod &&  <li><strong>Työjakso:</strong> {`${workman.workPeriod}`}</li> }
+                 {workman.email &&  <li><strong>Email:</strong> {`${workman.email}`}</li> }
+                 {workman.phone &&  <li><strong>Puhelinnumero:</strong> {`${workman.phone}`}</li> }
+                 {workman.trello &&  <li><strong>Trello:</strong> {`${workman.trello}`}</li> }
+                 {workman.discord &&  <li><strong>Discord:</strong> {`${workman.discord}`}</li> }
             </ul>
         </div>
     );
