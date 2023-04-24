5fcbc1f7e2212e34d82cc4e8f0869f6ffff58691
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-24 17:34:33 +0300

    update description for project

--- frontend/src/widgets/DescriptionWithNav/model/data/description.ts
+++ frontend/src/widgets/DescriptionWithNav/model/data/description.ts
@@ -1,64 +1,12 @@
 export const title = 'Projektin Kuvaus';
 
-export const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum eveniet\n' +
-    '                    explicabo laudantium nihil\n' +
-    '                    nulla suscipit velit veniam.\n' +
-    '                    Ad aspernatur deserunt error ex in minima perferendis provident quam\n' +
-    '                    rem repellat?' +
+export const text = 'ALT Zone on yhteistoiminnallinen verkkomobiilipeli, joka on suunniteltu erilaisten pelaajien yhteiseksi ' +
+    'liittoumaksi. Pelin tarkoitus on yhdistää pelaajat ihmisyyden äärelle ja korostaa niiden vahvuuksia, joita kullakin pelaajalla ' +
+    'on. Pelissä pelaajat muodostavat 30-jäsenisen tiimin, jossa jokaisella pelaajatyypillä on oma tärkeä paikkansa. Pelaajat asuvat ' +
+    'planeetalla, jota he sisustavat pelin voitoilla ja pelikaupasta ostettavilla huonekaluilla ja muilla modauksilla. Pelin hahmojen ' +
+    'suojakilpinä toimivat elävästä elämästä tutut suojautumismekanismit, ja kukin hahmo kuuluu yhteen seitsemästä defenssiluokasta. ' +
+    'Peli sisältää kaksi erilaista ping pong -tyylistä matsimuotoa, jossa pelaajat voivat pelata joko botteja tai muita pelaajia vastaan.' +
+    ' Voittajajoukkue pääsee tekemään ryöstön vastustajan planeetalle. ALT Zone -pelissä vapaaehtoiset devaajat työskentelevät etänä ja ' +
+    'tavoitteena on tuottaa kohtuullinen palkkio tiimijäsenille sekä lahjoittaa osa pelin ideoineen pojan nimeä kantavaan säätiöön nuorten' +
+    ' unelmien tukemiseksi.' +
     ''
