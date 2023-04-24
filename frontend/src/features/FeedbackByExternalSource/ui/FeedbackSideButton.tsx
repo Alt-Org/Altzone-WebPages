b0a32eaa970b3b159353607ec880129356e40994
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-24 20:58:19 +0300

    fix css path

--- frontend/src/features/FeedbackByExternalSource/ui/FeedbackSideButton.tsx
+++ frontend/src/features/FeedbackByExternalSource/ui/FeedbackSideButton.tsx
@@ -1,5 +1,5 @@
 import {Button, ButtonTheme} from "@/shared/ui/Button/Button";
-import cls from "@/features/FeedbackByGoogleForms/ui/FeedbackSideButton.module.scss"
+import cls from "./FeedbackSideButton.module.scss"
 import {AppExternalLinks} from "@/shared/appLinks/appExternalLinks";
 import {openLinkInNewTab} from "@/shared/lib/openLinkInNewTab/openLinkInNewTab";
 
