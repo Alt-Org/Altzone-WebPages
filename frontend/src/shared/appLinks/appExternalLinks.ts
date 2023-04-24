87b125d5740c6e28769670cfec4ae513d6c9df21
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-04-24 18:02:04 +0300

    update link

--- frontend/src/shared/appLinks/appExternalLinks.ts
+++ frontend/src/shared/appLinks/appExternalLinks.ts
@@ -4,7 +4,7 @@
 export const AppExternalLinks = {
     readMore: "https://drive.google.com/file/d/16KAaOAQudDQ_PvGfebXiB51mtR_M42UW/view?usp=sharing",
     webgl : "https://alt-org.github.io/builds/webgl",
-    discord: "https://discord.gg/58QVb7kn",
+    discord: "https://discord.gg/k4YWG5Cf48",
     facebook : "https://www.facebook.com/GamingPsyche",
     instagram : "https://www.instagram.com/gamingpsyche",
     youtube : "https://www.youtube.com/channel/UCTgWpC9Hq7b-W1Lhmry97YA",
