705bf427f95864d06bfd26fec075ac20151ce79c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-05-08 12:04:23 +0300

    update workers

--- frontend/src/widgets/WorkersSection/ui/WorkersSection.tsx
+++ frontend/src/widgets/WorkersSection/ui/WorkersSection.tsx
@@ -3,6 +3,8 @@ import {groupsWithWorkersLocally, GroupWithWorkman, Workman} from "@/entities/Wo
 import {FC, memo} from "react";
 import {ScrollBottomButton} from "@/features/ScrollBottom";
 import {classNames} from "@/shared/lib/classNames/classNames";
+import githubLogo from "@/shared/assets/images/githubLogo.png"
+import {openLinkInNewTab} from "@/shared/lib/openLinkInNewTab/openLinkInNewTab";
 
 interface WorkersSectionProps  {
     className?: string;
@@ -10,7 +12,6 @@ interface WorkersSectionProps  {
 
 export const WorkersSection = ({className = ''}: WorkersSectionProps) => {
     return (
-        // <div className={cls.WorkersSection}>
         <div className={classNames(cls.WorkersSection,{},[className])}>
 
             <ScrollBottomButton className={cls.scrollBottomButton}/>
@@ -51,15 +52,17 @@ interface WorkmanProps {
 const WorkmanComponent: FC<WorkmanProps> = memo(({ workman }) => {
     return (
         <div className={cls.workmanComponent}>
-            <h3>{workman.name}</h3>
+            {workman.imgSrc && <img src={workman.imgSrc} alt={workman.name + ' logo' }/>}
+            <h3>{workman.name} </h3>
             <ul>
-                 {workman.role &&  <li><strong>Rooli:</strong> {`${workman.role}`}</li> }
-                 {workman.status &&  <li><strong>Status:</strong> {`${workman.status}`}</li> }
-                 {workman.workPeriod &&  <li><strong>Työjakso:</strong> {`${workman.workPeriod}`}</li> }
-                 {workman.email &&  <li><strong>Email:</strong> {`${workman.email}`}</li> }
-                 {workman.phone &&  <li><strong>Puhelinnumero:</strong> {`${workman.phone}`}</li> }
-                 {workman.trello &&  <li><strong>Trello:</strong> {`${workman.trello}`}</li> }
-                 {workman.discord &&  <li><strong>Discord:</strong> {`${workman.discord}`}</li> }
+                {workman.role &&  <li><strong>Rooli:</strong> {`${workman.role}`}</li> }
+                {workman.github &&  <li className={cls.githubLogo}> <img onClick={() => openLinkInNewTab(`https://github.com/${workman.github}`)} src={githubLogo} alt="githubLogo"/></li> }
+                {workman.status &&  <li><strong>Status:</strong> {`${workman.status}`}</li> }
+                {workman.workPeriod &&  <li><strong>Työjakso:</strong> {`${workman.workPeriod}`}</li> }
+                {workman.email &&  <li><strong>Email:</strong> {`${workman.email}`}</li> }
+                {workman.phone &&  <li><strong>Puhelinnumero:</strong> {`${workman.phone}`}</li> }
+                {workman.trello &&  <li><strong>Trello:</strong> {`${workman.trello}`}</li> }
+                {workman.discord &&  <li><strong>Discord:</strong> {`${workman.discord}`}</li> }
             </ul>
         </div>
     );
