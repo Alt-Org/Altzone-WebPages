2322fb1da8078dc9e719ecfdc35eb8b380155aae
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-05-18 17:40:26 +0300

    change css

--- frontend/src/pages/MembersPage/ui/MembersPage.tsx
+++ frontend/src/pages/MembersPage/ui/MembersPage.tsx
@@ -2,18 +2,25 @@ import {Navbar} from "@/widgets/Navbar";
 import cls from './MembersPage.module.scss'
 import {FeedbackSideButton} from "@/features/FeedbackByExternalSource";
 import {MembersSection} from "@/widgets/MembersSection";
+import {classNames} from "@/shared/lib/classNames/classNames";
 
 const MembersPage = () => {
     return (
-        <div>
+        <div className={classNames(cls.MembersPage)}>
             <FeedbackSideButton/>
-            <Navbar  className={cls.navbar} />
-
-            {/*<div style={{marginTop: '200px'}}>*/}
-
-            {/*</div>*/}
-           <MembersSection  className={cls.workersSection}/>
+            <Navbar className={cls.navbar}/>
+            <MembersSection  className={cls.workersSection}/>
         </div>
+
+        // <div>
+        //     <FeedbackSideButton/>
+        //     <Navbar  className={cls.navbar} />
+        //
+        //     {/*<div style={{marginTop: '200px'}}>*/}
+        //
+        //     {/*</div>*/}
+        //    {/*<MembersSection  className={cls.workersSection}/>*/}
+        // </div>
     );
 };
 
