896a4cb34153e0c7758480a8aa9292c55226cb80
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-09-09 19:45:31 +0300

    refactor; custom slider has now it's own component

--- frontend/src/pages/PictureGalleryPage/ui/PictureGalleryPage.tsx
+++ frontend/src/pages/PictureGalleryPage/ui/PictureGalleryPage.tsx
@@ -1,19 +1,31 @@
 import FsLightbox from "fslightbox-react";
 import {Navbar} from "@/widgets/Navbar";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 const PictureGalleryPage = () => {
+
+    const navigate = useNavigate();
+
+    const [toggler, setToggler] = useState(false);
+    useEffect(()=>{
+        setToggler(true)
+    },[])
+
    return (
        <div style={{minHeight: "100vh"}}>
            <Navbar overlayed marginTop={20} />
+           <FsLightbox
+               onClose={()=>navigate(-1)}
+               toggler={toggler}
+               sources={[
+                   'https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D&w=1000&q=80',
+                   'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+                   'https://ichef.bbci.co.uk/news/1024/branded_news/16755/production/_126898919_catnewproject.jpg'
+               ]}
+           />
        </div>
-       // <FsLightbox
-       //     toggler={true}
-       //     sources={[
-       //         'https://i.imgur.com/fsyrScY.jpg',
-       //         'https://www.youtube.com/watch?v=3nQNiWdeH2Q',
-       //         'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-       //     ]}
-       // />
+
    )
 }
 
--- frontend/src/widgets/NewsPreviewSection/ui/NewsCards/NewsCards.tsx
+++ frontend/src/widgets/NewsPreviewSection/ui/NewsCards/NewsCards.tsx
@@ -1,53 +1,19 @@
-import cls from "./NewsCards.module.scss";
-import {classNames} from "@/shared/lib/classNames/classNames";
-import {memo, useRef} from "react";
+import {memo} from "react";
 import {NewsCard} from "../NewsCard/NewsCard";
 import {INewsElement} from "@/entities/News";
+import {CustomSlider} from "@/shared/ui/CustomSlider/CustomSlider";
 
 interface NewsCardProps{
     className?: string;
-    news: INewsElement[]
+    news: INewsElement[];
 }
 
 export const NewsCards = memo(({className='', news}: NewsCardProps) => {
-    const scrollRef = useRef<HTMLDivElement>(null);
-
-    const handleScrollRight = () => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollBy({
-                left: 3000,
-                behavior: 'smooth'
-            });
-        }
-    };
-
-    const handleScrollLeft = () => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollBy({
-                left: -3000,
-                behavior: 'smooth'
-            });
-        }
-    };
-
     return(
-        <div className={cls.CardsWrapper}>
-
-            <div className={cls.scrollLeft} onClick={handleScrollLeft}>
-                <i>{'←'}</i>
-            </div>
-
-        <div className={classNames(cls.Cards, {},[className])} ref={scrollRef}>
-
-            {news.map(item=>(
-                <NewsCard  bodyLength={200} key={item.id} title={item.title} bodyPreview={item.bodyPreview} date={item.date} id={item.id}/>
-            ))}
-        </div>
-
-            <div className={cls.scrollRight} onClick={handleScrollRight}>
-                <i>{'→'}</i>
-            </div>
-
-        </div>
+            <CustomSlider className={className} >
+                {news.map(item=>(
+                    <NewsCard  bodyLength={200} key={item.id} title={item.title} bodyPreview={item.bodyPreview} date={item.date} id={item.id}/>
+                ))}
+            </CustomSlider>
     )
 })
