f208bd0fec11386ba2ec3a2b88a061a14c73e425
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-09-13 18:24:27 +0300

    refactor , make the component more dynamic

--- frontend/src/pages/PictureGalleryPage/ui/PictureGalleryPage.tsx
+++ frontend/src/pages/PictureGalleryPage/ui/PictureGalleryPage.tsx
@@ -2,47 +2,25 @@ import {Navbar} from "@/widgets/Navbar";
 import {Container} from "@/shared/ui/Container";
 import cls from "./PictureGalleryPage.module.scss";
 import {GalleryCategoriesWithModalSlider} from "@/shared/ui/GalleryCategoriesWithModalSlider";
-
-const galleryCatPaths = Object.values(import.meta.glob('@/shared/assets/images/gallery/cats/*', { eager: true, as: 'url' }));
-const galleryDogsPaths = Object.values(import.meta.glob('@/shared/assets/images/gallery/dogs/*', { eager: true, as: 'url' }));
-const galleryZooPaths = Object.values(import.meta.glob('@/shared/assets/images/gallery/zoo/*', { eager: true, as: 'url' }));
-const galleryCartoonsPaths = Object.values(import.meta.glob('@/shared/assets/images/gallery/cartoons/*', { eager: true, as: 'url' }));
+import {categories} from "../model";
 
 const PictureGalleryPage = () => {
+
+
     return (
        <div className={cls.Wrapper}>
            <Navbar   className={cls.Navbar}/>
            <Container fluid={true}>
                <div style={{display: "flex ", gap: "30px", justifyContent: "center" , flexWrap: "wrap"}}>
-                   <GalleryCategoriesWithModalSlider
-                       key={"Cats"}
-                       sources={galleryCatPaths}
-                       title={"Kissat"}
-                   />
-                   <GalleryCategoriesWithModalSlider
-                       key={"Dogs"}
-                       sources={galleryDogsPaths}
-                       title={"Koirat"}
-                   />
-
-                   <GalleryCategoriesWithModalSlider
-                       key={"Zoo"}
-                       sources={galleryZooPaths}
-                       title={"Zoo"}
-                   />
-
-                   <GalleryCategoriesWithModalSlider
-                       key={"Sarjakuvat"}
-                       sources={galleryCartoonsPaths}
-                       title={"Sarjakuvat"}
-                   />
-
-                   <GalleryCategoriesWithModalSlider
-                       key={"Dogs3"}
-                       sources={galleryDogsPaths}
-                       title={"Koirat"}
-                   />
-
+                   {
+                       categories.map((category) => (
+                           <GalleryCategoriesWithModalSlider
+                               key={category.key}
+                               sources={category.sources}
+                               title={category.title}
+                           />
+                       ))
+                   }
                </div>
            </Container>
            <br/>
