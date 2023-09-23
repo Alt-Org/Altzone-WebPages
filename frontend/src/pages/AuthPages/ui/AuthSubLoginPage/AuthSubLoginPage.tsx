270e9a1a37502b2070821904e8abb9bbe3a44150
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-09-23 14:48:50 +0300

    refactor logic for login/register, add push up messages, add logic for saving user credentials

--- frontend/src/pages/AuthPages/ui/AuthSubLoginPage/AuthSubLoginPage.tsx
+++ frontend/src/pages/AuthPages/ui/AuthSubLoginPage/AuthSubLoginPage.tsx
@@ -2,18 +2,26 @@ import {ComponentType} from "react";
 import {RoutePaths} from "@/shared/appLinks/RoutePaths";
 import {Modal} from "@/shared/ui/Modal";
 import {LoginForm} from "@/features/AuthByUsername";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     HasOutletChildren: ComponentType;
 }
 
 const AuthSubLoginPage = ({HasOutletChildren}: Props) => {
+
+    const navigate = useNavigate()
+
     return (
         <>
             <Modal
                 isOpen={true}
             >
-                <LoginForm toForgottenPwPage={""} toRegisterPage={RoutePaths.auth_register}/>
+                <LoginForm
+                    toForgottenPwPage={""}
+                    toRegisterPage={RoutePaths.auth_register}
+                    onSuccessLogin={()=> navigate(RoutePaths.MAIN)}
+                />
             </Modal>
             <HasOutletChildren/>
         </>
