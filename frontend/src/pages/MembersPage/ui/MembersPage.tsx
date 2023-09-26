80ac6c5044b5920e4bed6a371c21e31a2ad40201
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-09-26 20:30:56 +0300

    fix background image problem

--- frontend/src/pages/MembersPage/ui/MembersPage.tsx
+++ frontend/src/pages/MembersPage/ui/MembersPage.tsx
@@ -3,10 +3,11 @@ import cls from './MembersPage.module.scss'
 import {FeedbackSideButton} from "@/features/FeedbackByExternalSource";
 import {MembersSection} from "@/widgets/MembersSection";
 import {classNames} from "@/shared/lib/classNames/classNames";
+import backgroundImg from '@/shared/assets/images/colored-portal-pictures-1.jpg'
 
 const MembersPage = () => {
     return (
-        <div className={classNames(cls.MembersPage)}>
+        <div className={classNames(cls.MembersPage)} style={{ backgroundImage: `url(${backgroundImg})` }}>
             <div className={cls.backgroundOpacity}></div>
             <div className={cls.navbarWrapper}>
             <Navbar overlayed={true} className={cls.navbar}/>
