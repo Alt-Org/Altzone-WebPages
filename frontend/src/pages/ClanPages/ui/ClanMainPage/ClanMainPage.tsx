f16008640bf5129dad3b76b9d74fa5913c3edf35
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-10-01 21:59:42 +0300

    add logic for displaying/hiding add new clan / user clan

--- frontend/src/pages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend/src/pages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,10 +1,12 @@
-import {ComponentType, useEffect} from "react";
+import {ComponentType, useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 import {Navbar} from "@/widgets/Navbar";
 import {Container} from "@/shared/ui/Container";
 import backgroundImg from "@/shared/assets/images/clanBg/Optimized-Moon.jpg"
-import cls from "./ClanMainPage.module.scss";
-import {Link} from "react-router-dom";
 import {RoutePaths} from "@/shared/appLinks/RoutePaths";
+import {selectProfile} from "@/entities/Auth";
+import cls from "./ClanMainPage.module.scss";
 
 
 type Props = {
@@ -14,14 +16,33 @@ type Props = {
 
 const ClanMainPage = ({ HasOutletChildren}: Props) => {
 
+    const user = useSelector(selectProfile);
+
+    const [canShowAddNew, setCanShowAddNew] = useState(false);
+    const [canShowUserClan, setShowUserClan] = useState(false);
+
+    useEffect(() => {
+        if(!user) return;
+
+        if(!user.Player.clan_id){
+            setCanShowAddNew(true);
+            setShowUserClan(false);
+            return;
+        }
+
+        setShowUserClan(true);
+        setCanShowAddNew(false);
+
+    },[user]);
+
     return (
         <div className={cls.Wrapper} style={{ backgroundImage: `url(${backgroundImg})` }}>
             <Navbar className={cls.Navbar} />
             <Container className={cls.Container}>
-
                 <div style={{display: "flex" , gap: "10px"}}>
-                    <Link to={RoutePaths.clan_all}>All Clans</Link>
-                    <Link to={RoutePaths.clan_add_new}>Add Clan</Link>
+                    <Link to={RoutePaths.clan_all}>Kaikki Klaanit</Link>
+                    {canShowUserClan && <Link to={RoutePaths.clan + `/${user?.Player.clan_id}`}>Minun klaani</Link>}
+                    {canShowAddNew && <Link to={RoutePaths.clan_add_new}>Lisää Klaani</Link> }
                 </div>
 
                 <div className={cls.ClansViewMain}>
