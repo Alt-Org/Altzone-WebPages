3fb83ece97d7062dcc085ba4e31944cbabc02c9e
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-10-19 09:33:40 +0300

    fix blinking bug ; test changes for categories

--- frontend/src/pages/PictureGalleryPage/model/index.ts
+++ frontend/src/pages/PictureGalleryPage/model/index.ts
@@ -4,9 +4,9 @@ const galleryZooPaths = Object.values(import.meta.glob('@/shared/assets/images/g
 const galleryCartoonsPaths = Object.values(import.meta.glob('@/shared/assets/images/gallery/cartoons/*', { eager: true, as: 'url' }));
 
 export const categories = [
-    { key: "Cats", sources: galleryCatPaths, title: "Kissat" , followLastImage: true},
-    { key: "Dogs", sources: galleryDogsPaths, title: "Koirat" ,followLastImage: true},
-    { key: "Zoo", sources: galleryZooPaths, title: "Zoo" ,followLastImage: true},
-    { key: "Cartoons", sources: galleryCartoonsPaths, title: "Sarjakuvat" ,followLastImage: true},
+    { key: "Cats", sources: galleryCatPaths, title: "Kissat" , followLastImage: false},
+    { key: "Dogs", sources: galleryDogsPaths, title: "Koirat" ,followLastImage: false},
+    { key: "Zoo", sources: galleryZooPaths, title: "Zoo" ,followLastImage: false},
+    { key: "Cartoons", sources: galleryCartoonsPaths, title: "Sarjakuvat" ,followLastImage: false},
     { key: "Dogs2", sources: galleryDogsPaths, title: "Koirat", followLastImage: false},
 ];
