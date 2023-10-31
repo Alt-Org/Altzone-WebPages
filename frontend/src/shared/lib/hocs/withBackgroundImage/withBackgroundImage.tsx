b0d5efa7af9e2bcb2c9e425fc7ed4d52dc7d9d4f
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-10-31 19:04:40 +0200

    change css

--- frontend/src/shared/lib/hocs/withBackgroundImage/withBackgroundImage.tsx
+++ frontend/src/shared/lib/hocs/withBackgroundImage/withBackgroundImage.tsx
@@ -17,6 +17,8 @@ function withBackgroundImage<P extends object>(config: BackgroundImageConfig<P>)
     const { WrappedComponent, imagePath, placeHolderPath = '', alt = '', shouldBeLazyLoaded= false } = config;
 
     return class extends Component<P> {
+
+
         render() {
             return (
                 <>
@@ -41,83 +43,17 @@ function withBackgroundImage<P extends object>(config: BackgroundImageConfig<P>)
 
                                 </img>
                         }
-
-
                     </div>
                     <div className={cls.Content}>
                         <WrappedComponent {...this.props} />
                     </div>
+
                 </>
             );
         }
+
     };
 }
 
 export default withBackgroundImage;
-
-
-// import { Component, ComponentType } from 'react';
-// // @ts-ignore
-// import { LazyLoadImage } from 'react-lazy-load-image-component';
-// import 'react-lazy-load-image-component/src/effects/blur.css';
-// import cls from "./withBackgroundImage.module.scss";
-//
-// interface BackgroundImageConfig<P> {
-//     WrappedComponent: ComponentType<P>;
-//     imagePath: string;
-//     placeHolderPath?: string;
-//     alt?: string;
-// }
-//
-// function withBackgroundImage<P extends object>(config: BackgroundImageConfig<P>): ComponentType<P> {
-//     const { WrappedComponent, imagePath, placeHolderPath = '', alt = '' } = config;
-//
-//     return class extends Component<P, { isImageLoaded: boolean }> {
-//         imageLoadedKey = `loaded:${imagePath}`;
-//
-//         state = {
-//             isImageLoaded: localStorage.getItem(this.imageLoadedKey) === 'true'
-//         }
-//
-//         handleImageLoaded = () => {
-//             localStorage.setItem(this.imageLoadedKey, 'true');
-//             this.setState({ isImageLoaded: true });
-//         }
-//
-//         render() {
-//             if (this.state.isImageLoaded) {
-//                 return (
-//                     <>
-//                         <div className={cls.Background}>
-//                             <img src={imagePath} alt={alt}/>
-//                         </div>
-//                         <div className={cls.Content}>
-//                             <WrappedComponent {...this.props} />
-//                         </div>
-//                     </>
-//                 );
-//             }
-//
-//             return (
-//                 <>
-//                     <div className={cls.Background}>
-//                         <LazyLoadImage
-//                             src={imagePath}
-//                             alt={alt}
-//                             effect="blur"
-//                             placeholderSrc={placeHolderPath}
-//                             wrapperClassName={cls.ImageWrapper}
-//                             afterLoad={this.handleImageLoaded}
-//                         />
-//                     </div>
-//                     <div className={cls.Content}>
-//                         <WrappedComponent {...this.props} />
-//                     </div>
-//                 </>
-//             );
-//         }
-//     };
-// }
-//
-// export default withBackgroundImage;
 
