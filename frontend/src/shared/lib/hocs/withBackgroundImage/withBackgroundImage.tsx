06ef3e364a26db893f929161a100df8296743b44
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-10-31 13:13:09 +0200

    wrapper placeholder test

--- frontend/src/shared/lib/hocs/withBackgroundImage/withBackgroundImage.tsx
+++ frontend/src/shared/lib/hocs/withBackgroundImage/withBackgroundImage.tsx
@@ -1,94 +1,95 @@
-    // import {Component, ComponentType, createRef} from 'react';
-    // import cls from "./withBackgroundImage.module.scss";
-    //
-    //
-    // function withBackgroundImage<P extends object>(WrappedComponent: ComponentType<P>, imagePath: string): ComponentType<P> {
-    //     return class extends Component<P> {
-    //
-    //
-    //
-    //         render() {
-    //             return (
-    //                 <>
-    //                     <div className={cls.Background} style={{ backgroundImage: `url(${imagePath})` }} />
-    //                     <div className={cls.Content}>
-    //                         <WrappedComponent {...this.props} />
-    //                     </div>
-    //                 </>
-    //             );
-    //         }
-    //     };
-    // }
-    //
-    // export default withBackgroundImage;
-    //
+// import {ComponentType, useRef, useEffect, useState } from 'react';
+//     import cls from "./withBackgroundImage.module.scss";
+//
+//     function withBackgroundImage<P extends object>(WrappedComponent: ComponentType<P>, imagePath: string): ComponentType<P> {
+//         return function WithBgImageComponent(props: P) {
+//             const [isImageLoaded, setImageLoaded] = useState(false);
+//             const backgroundDiv = useRef<HTMLDivElement>(null);
+//
+//             useEffect(() => {
+//                 if (!backgroundDiv.current) return;
+//
+//                 const observer = new IntersectionObserver((entries) => {
+//                     entries.forEach(entry => {
+//                         if (entry.isIntersecting && !isImageLoaded) {
+//                             setImageLoaded(true);
+//                         }
+//                     });
+//                 });
+//
+//                 observer.observe(backgroundDiv.current);
+//
+//                 return () => {
+//                     observer.disconnect();
+//                 };
+//             }, [isImageLoaded]);
+//
+//             return (
+//                 <>
+//                     <div
+//                         className={cls.Background}
+//                         ref={backgroundDiv}
+//                         style={isImageLoaded ? { backgroundImage: `url(${imagePath})` } : {}}
+//                     />
+//                     <div className={cls.Content}>
+//                         <WrappedComponent {...props} />
+//                     </div>
+//                 </>
+//             );
+//         }
+//     }
+//
+//     export default withBackgroundImage;
 
-    //
-    // function withBackgroundImage<P extends object>(WrappedComponent: ComponentType<P>, imagePath: string): ComponentType<P> {
-    //     return class extends Component<P> {
-    //         private backgroundDiv = createRef<HTMLDivElement>();
-    //
-    //         componentDidMount() {
-    //             if (this.backgroundDiv.current) {
-    //                 this.backgroundDiv.current.style.backgroundImage = `url(${imagePath})`;
-    //             }
-    //
-    //         }
-    //
-    //         render() {
-    //             return (
-    //                 <>
-    //                     <div className={cls.Background} ref={this.backgroundDiv} />
-    //                     <div className={cls.Content}>
-    //                         <WrappedComponent {...this.props} />
-    //                     </div>
-    //                 </>
-    //             );
-    //         }
-    //     };
-    // }
+import React, { ComponentType, useRef, useEffect, useState } from 'react';
+import cls from "./withBackgroundImage.module.scss";
 
-    import React, { Component, ComponentType, useRef, useEffect, useState } from 'react';
-    import cls from "./withBackgroundImage.module.scss";
+function withBackgroundImage<P extends object>(
+    WrappedComponent: ComponentType<P>,
+    imagePath: string,
+    placeholderPath?: string
+): ComponentType<P> {
+    return function WithBgImageComponent(props: P) {
+        const [isImageLoaded, setImageLoaded] = useState(false);
+        const backgroundDiv = useRef<HTMLDivElement>(null);
 
-    function withBackgroundImage<P extends object>(WrappedComponent: ComponentType<P>, imagePath: string): ComponentType<P> {
-        return function WithBgImageComponent(props: P) {
-            const [isImageLoaded, setImageLoaded] = useState(false);
-            const backgroundDiv = useRef<HTMLDivElement>(null);
+        useEffect(() => {
+            if (!backgroundDiv.current) return;
 
-            useEffect(() => {
-                if (!backgroundDiv.current) return;
-
-                const observer = new IntersectionObserver((entries) => {
-                    entries.forEach(entry => {
-                        if (entry.isIntersecting && !isImageLoaded) {
-                            setImageLoaded(true);
-                            // На этом этапе можно дополнительно оптимизировать, загружая изображение через new Image() и устанавливая только после его загрузки.
-                        }
-                    });
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting && !isImageLoaded) {
+                        const img = new Image();
+                        img.src = imagePath;
+                        img.onload = () => setImageLoaded(true);
+                    }
                 });
+            });
 
-                observer.observe(backgroundDiv.current);
+            observer.observe(backgroundDiv.current);
 
-                return () => {
-                    observer.disconnect();
-                };
-            }, [isImageLoaded]);
+            return () => {
+                observer.disconnect();
+            };
+        }, [isImageLoaded]);
 
-            return (
-                <>
-                    <div
-                        className={cls.Background}
-                        ref={backgroundDiv}
-                        style={isImageLoaded ? { backgroundImage: `url(${imagePath})` } : {}}
-                    />
-                    <div className={cls.Content}>
-                        <WrappedComponent {...props} />
-                    </div>
-                </>
-            );
-        }
+        return (
+            <>
+                <div
+                    className={cls.Background}
+                    ref={backgroundDiv}
+                    style={{
+                        backgroundImage: `url(${isImageLoaded || !placeholderPath ? imagePath : placeholderPath})`
+                    }}
+                />
+                <div className={cls.Content}>
+                    <WrappedComponent {...props} />
+                </div>
+            </>
+        );
     }
+}
 
-    export default withBackgroundImage;
+export default withBackgroundImage;
+
 
