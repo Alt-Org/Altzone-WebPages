7c13e9fc6a40610be7decd6790617ba379c909e2
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-10-31 18:04:46 +0200

    remove feedback from memberspage

--- frontend/src/pages/MembersPage/ui/MembersPage.tsx
+++ frontend/src/pages/MembersPage/ui/MembersPage.tsx
@@ -1,6 +1,5 @@
 import {Navbar} from "@/widgets/Navbar";
 import cls from './MembersPage.module.scss'
-import {FeedbackSideButton} from "@/features/FeedbackByExternalSource";
 import {MembersSection} from "@/widgets/MembersSection";
 import {classNames} from "@/shared/lib/classNames/classNames";
 import backgroundImg from '@/shared/assets/images/clanBg/space.webp'
@@ -11,14 +10,11 @@ import {withBackgroundImage} from "@/shared/lib/hocs/withBackgroundImage";
 
 const MembersPage = () => {
     return (
-        // <div className={classNames(cls.MembersPage)} style={{ backgroundImage: `url(${backgroundImg})` }}>
-
         <div className={classNames(cls.MembersPage)}>
             {/*<div className={cls.backgroundOpacity}></div>*/}
             <div className={cls.navbarWrapper}>
             <Navbar overlayed={true} className={cls.navbar}/>
             </div>
-            <FeedbackSideButton/>
             <MembersSection  className={cls.workersSection}/>
         </div>
     );
