68617b101540d87f6f424eaef603eea898e21d13
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-11-06 17:39:29 +0200

    update next project

--- frontend-next-migration/src/pages/_app.tsx
+++ frontend-next-migration/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../app/styles/index.scss'
 import {Providers} from "@/app/providers/Providers";
-import {Head} from "next/document";
 
 // @ts-ignore
 export default function MyApp({ Component, pageProps }) {
