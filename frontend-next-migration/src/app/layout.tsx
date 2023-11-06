01236db0fd358b1ee8634805d2236f87b13554a1
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-11-06 22:13:21 +0200

    add test files for app router

--- frontend-next-migration/src/app/layout.tsx
+++ frontend-next-migration/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../preparedApp/styles/index.scss';
-import Head from "next/head";
 import {Providers} from "@/preparedApp/providers/Providers";
 import '../preparedApp/styles/index.scss'
 
