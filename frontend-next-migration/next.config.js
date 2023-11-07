1c28245614143d0a72444f7de2b787e8e11a92be
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-11-07 19:46:22 +0200

    delete i18 not server

--- frontend-next-migration/next.config.js
+++ frontend-next-migration/next.config.js
@@ -9,11 +9,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 
-const withNextIntl = require('next-intl/plugin')(
-    // This is the default (also the `src` folder is supported out of the box)
-    './i18n.ts'
-);
-
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -33,8 +28,7 @@ const nextConfig = {
 
 
 module.exports = withPlugins([
-    // withBundleAnalyzer,
-    withNextIntl
+    withBundleAnalyzer,
 ], nextConfig);
 
 
