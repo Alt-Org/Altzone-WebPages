b3897648ae47472063ffc459af946756bdf71bf0
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-11-07 20:35:56 +0200

    update i18n

--- frontend-next-migration/src/app/[lng]/layout.tsx
+++ frontend-next-migration/src/app/[lng]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../preparedApp/styles/index.scss';
+import '../../preparedApp/styles/index.scss';
 import {Providers} from "@/preparedApp/providers/Providers";
-import '../preparedApp/styles/index.scss'
+import '../../preparedApp/styles/index.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,13 +13,20 @@ export const metadata: Metadata = {
 
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+import { dir } from 'i18next'
+
+import { languages } from '../../shared/i18n/settings'
+
+export async function generateStaticParams() {
+    return languages.map((lng) => ({ lng }));
+}
+
+export default function RootLayout({children,params: {lng}}: {
+  children: React.ReactNode,
+  params: any
 }) {
   return (
-    <html lang="en">
+    <html lang={lng} dir={dir(lng)}>
       <body className={inter.className}>
       <Providers>
       {children}
--- frontend-next-migration/src/shared/i18n/index.ts
+++ frontend-next-migration/src/shared/i18n/index.ts
@@ -1,9 +1,27 @@
-import i18nConfig from "./lib/i18n";
-import { Language } from "./lib/types";
-import * as i18nForTests from "./tests/i18nForTests";
+import { createInstance } from 'i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import { initReactI18next } from 'react-i18next/initReactI18next'
+import { getOptions } from './settings'
 
 
-export {Language, i18nForTests};
+// @ts-ignore
+const initI18next = async (lng, ns) => {
+    // on server side we create a new instance for each render, because during compilation everything seems to be executed in parallel
+    const i18nInstance = createInstance()
+    await i18nInstance
+        .use(initReactI18next)
+        .use(resourcesToBackend((language: any, namespace: any) => import(`./locales/${language}/${namespace}.json`)))
+        .init(getOptions(lng, ns))
+    return i18nInstance
+}
 
-export default i18nConfig;
+// @ts-ignore
+export async function useTranslation(lng, ns, options = {}) {
+    const i18nextInstance = await initI18next(lng, ns)
+    return {
 
+// @ts-ignore
+        t: i18nextInstance.getFixedT(lng, Array.isArray(ns) ? ns[0] : ns, options?.keyPrefix),
+        i18n: i18nextInstance
+    }
+}
