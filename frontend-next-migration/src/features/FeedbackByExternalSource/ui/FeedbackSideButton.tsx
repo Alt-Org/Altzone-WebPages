50ab61945d696ceaa33090930e69ac5ad8210324
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-12-21 14:06:31 +0200

    update css , update translation

--- frontend-next-migration/src/features/FeedbackByExternalSource/ui/FeedbackSideButton.tsx
+++ frontend-next-migration/src/features/FeedbackByExternalSource/ui/FeedbackSideButton.tsx
@@ -3,17 +3,26 @@ import {Button, ButtonTheme} from "@/shared/ui/Button/Button";
 import cls from "./FeedbackSideButton.module.scss"
 import {AppExternalLinks} from "@/shared/appLinks/appExternalLinks";
 import {openLinkInNewTab} from "@/shared/lib/openLinkInNewTab/openLinkInNewTab";
+import {useClientTranslation} from "@/shared/i18n";
+import {useParams} from "next/navigation";
 
 
 export const FeedbackSideButton = () => {
+
+    const params = useParams();
+    const lng = params.lng as string;
+    const {t} = useClientTranslation(lng, "translation");
+
 
     const handleClick = () => {
         openLinkInNewTab(AppExternalLinks.googleFeedback);
     }
 
     return (
-        <Button theme={ButtonTheme.Graffiti} className={cls.SideButton} type='button'onClick={handleClick} >
-                Palaute
+        <Button theme={ButtonTheme.Graffiti} className={cls.SideButton} type='button' onClick={handleClick} >
+            {
+                t("feedback")
+            }
         </Button>
     )
 }
