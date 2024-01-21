af061914a0e9f961a8370ad62b8386f34327b207
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-01-21 19:58:56 +0200

    update

--- frontend-next-migration/next.config.js
+++ frontend-next-migration/next.config.js
@@ -1,15 +1,52 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const path = require('path');
+// const withPlugins = require('next-compose-plugins');
+//
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === true,
+//     // enabled: process.env.NODE_ENV === 'development',
+//     openAnalyzer: false
+// });
+//
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             { hostname: process.env.NEXT_PUBLIC_API_DOMAIN },
+//             { hostname: "hips.hearstapps.com" },
+//             { hostname: "www.thesprucepets.com" },
+//             { hostname: "images.unsplash.com" },
+//             { hostname: "raw.githubusercontent.com" },
+//         ],
+//     },
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, 'styles')],
+//         // prependData: `@import "src/app/styles/variables/_mixins.scss";`,
+//         prependData: `@import "src/preparedApp/styles/variables/_mixins.scss";`,
+//     },
+//     output: 'standalone',
+//
+//     experimental: {
+//         webpackBuildWorker: true
+//     },
+//
+//
+// }
+//
+//
+//
+// module.exports = withPlugins([
+//     withBundleAnalyzer,
+// ], nextConfig);
+
+
+// module.exports = nextConfig;
+
+
 const path = require('path');
-const withPlugins = require('next-compose-plugins');
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === true,
-    // enabled: process.env.NODE_ENV === 'development',
-    openAnalyzer: false
-});
-
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+
     images: {
         remotePatterns: [
             { hostname: process.env.NEXT_PUBLIC_API_DOMAIN },
@@ -24,15 +61,10 @@ const nextConfig = {
         // prependData: `@import "src/app/styles/variables/_mixins.scss";`,
         prependData: `@import "src/preparedApp/styles/variables/_mixins.scss";`,
     },
+    output: 'standalone',
+    // experimental: {
+    //     webpackBuildWorker: true
+    // },
 }
 
-
-module.exports = withPlugins([
-    withBundleAnalyzer,
-], nextConfig);
-
-
-// module.exports = nextConfig;
-
-
-
+module.exports = nextConfig
