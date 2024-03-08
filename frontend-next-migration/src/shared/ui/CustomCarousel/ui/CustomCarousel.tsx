686c9db540873a9111ee8a8b9811544d2e67afee
Unai1080 <upelaezla22wg@ikzubirimanteo.com>
2024-03-08 12:00:38 +0100

    Slider custom arrows added

--- frontend-next-migration/src/shared/ui/CustomCarousel/ui/CustomCarousel.tsx
+++ frontend-next-migration/src/shared/ui/CustomCarousel/ui/CustomCarousel.tsx
@@ -17,17 +17,17 @@ type Props = {
 const defaultSettings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 3,
+    speed: 200,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
-    autoplaySpeed: 1000,
+    //autoplay: true,
+    autoplaySpeed: 10000,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
