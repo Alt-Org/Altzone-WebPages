77f943ee8b9f880757d03545c15d0104b7a3ede4
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-03-28 20:53:19 +0200

    add cypress for e2e

---
+++ frontend-next-migration/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+require('dotenv').config({ path: '.env.local' });
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.env.LOCAL_HOST = process.env.NEXT_PUBLIC_LOCAL_HOST
+      return config
+    },
+  },
+})
---
+++ frontend-next-migration/cypress/e2e/env-check/env-check.cy.js
@@ -0,0 +1,15 @@
+describe('Environment variable set in plugin', () => {
+    let localHost;
+
+    before(() => {
+        localHost = Cypress.env('LOCAL_HOST');
+    });
+
+    it.only('can be accessed within test.', () => {
+        cy.log(`LOCAL_HOST is ${localHost}`);
+
+        // expect(localHost).to.exist;
+        //
+        // expect(localHost).to.equal('http://localhost:5173/');
+    });
+});
