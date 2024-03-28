3656f1cafff9a9fdaa3fe0c6478b230b88192a18
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-03-28 23:08:10 +0200

    add cypress testes

--- frontend-next-migration/cypress/e2e/navigation/navigation.cy.js
+++ frontend-next-migration/cypress/e2e/navigation/navigation.cy.js
@@ -1,15 +1,67 @@
+import {RoutePaths} from "../../../src/shared/appLinks/RoutePaths";
+
 describe('Navigation', () => {
-    it('should navigate to the about page', () => {
-        // Start from the index page
-        cy.visit('http://localhost:3000/')
+    let localHost;
 
-        // Find a link with an href attribute containing "about" and click it
-        cy.get('a[href*="about"]').click()
+    before(() => {
+        localHost = Cypress.env('LOCAL_HOST');
 
-        // The new url should include "/about"
-        cy.url().should('include', '/about')
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            if (err.message.includes('NEXT_NOT_FOUND')) {
+                return false;
+            }
+        });
+    });
 
-        // The new page should contain an h1 with "About"
-        cy.get('h1').contains('About')
-    })
-})
+    it('should navigate to the Clan All page', () => {
+        cy.visit(`${localHost}/`);
+        cy.get('nav').find(`a[href*="${RoutePaths.clan_all}"]`).click({ force: true });
+        cy.url().should('include', RoutePaths.clan_all);
+        cy.contains('All clans');
+    });
+
+    it('should navigate to the Members page', () => {
+        cy.visit(`${localHost}/`);
+        cy.get('nav').find(`a[href*="${RoutePaths.MEMBERS}"]`).click({ force: true });
+        cy.url().should('include', RoutePaths.MEMBERS);
+        cy.contains('Game design');
+    });
+
+    it('should handle Not Found page', () => {
+        cy.visit(`${localHost}${RoutePaths.NOT_FOUND}`, { failOnStatusCode: false });
+        cy.url().should('include', RoutePaths.NOT_FOUND);
+        cy.contains('404');
+    });
+
+    it('should navigate to the Picture Gallery page', () => {
+        cy.visit(`${localHost}/`);
+        cy.get('nav').find(`a[href*="${RoutePaths.PICTURE_GALLERY}"]`).click({ force: true });
+        cy.url().should('include', RoutePaths.PICTURE_GALLERY);
+        cy.contains('Galleries');
+    });
+
+    it('should navigate to the Comics Gallery page', () => {
+        cy.visit(`${localHost}/`);
+        cy.get('nav').find(`a[href*="${RoutePaths.COMICS_GALLERY}"]`).click({ force: true });
+        cy.url().should('include', RoutePaths.COMICS_GALLERY);
+        cy.contains('Comics');
+    });
+
+    it('should navigate to the main page when clicking on the logo image', () => {
+        cy.visit(`${localHost}/${RoutePaths.COMICS_GALLERY}`);
+        cy.get('nav').find('img[alt="main"]').parent('a').click();
+        cy.url().should('include', `${localHost}/`);
+    });
+
+    it('should navigate to the auth login and go to auth register', () => {
+        cy.visit(`${localHost}/`);
+        cy.get('nav').find(`a[href*="${RoutePaths.auth_login}"]`).click({ force: true });
+        cy.url().should('include', RoutePaths.auth_login);
+        cy.get('h1').contains('Log In');
+
+        cy.get(`a[href*="${RoutePaths.auth_register}"]`).click();
+        cy.url().should('include', RoutePaths.auth_register);
+        cy.get('h1').contains('Register');
+    });
+
+});
