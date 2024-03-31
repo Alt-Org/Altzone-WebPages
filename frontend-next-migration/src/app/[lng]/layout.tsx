27139046009fe4248eab604b183f63b98c503b97
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-03-31 16:12:08 +0300

    refactor

--- frontend-next-migration/src/app/[lng]/layout.tsx
+++ frontend-next-migration/src/app/[lng]/layout.tsx
@@ -2,12 +2,6 @@ import type { Metadata } from 'next'
 import {Open_Sans, Urbanist, Rubik} from 'next/font/google'
 import '../../preparedApp/styles/index.scss';
 import {Providers} from "@/preparedApp/providers/Providers";
-import '../../preparedApp/styles/index.scss'
-
-
-
-
-
 
 
 const openSans = Open_Sans({
