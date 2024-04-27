72c768dfaf22b737e92622c2b8efe2fdeb7bb8bd
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-04-27 19:26:04 +0300

    fix naming

--- frontend-next-migration/src/preparedPages/MainPage/ui/MainPage.tsx
+++ frontend-next-migration/src/preparedPages/MainPage/ui/MainPage.tsx
@@ -1,28 +1,16 @@
+import cls from "./page.module.scss"
 import { DescriptionWithNav } from "@/widgets/DescriptionWithNav";
 import { Navbar } from "@/widgets/Navbar";
 import { Footer } from "@/widgets/Footer";
 import { FeedbackSideButton } from "@/features/FeedbackByExternalSource";
-import { Button, ButtonSize, ButtonTheme } from "@/shared/ui/Button/Button";
-import { AppExternalLinks } from "@/shared/appLinks/appExternalLinks";
-import { Container } from "@/shared/ui/Container";
-import backgroundImage from '@/shared/assets/images/introBackground-transformed2.png';
-import { withBackgroundImage } from "@/shared/lib/hocs/withBackgroundImage";
-import cls from "./page.module.scss"
 import { ScrollTop } from "@/features/ScrollTop";
 import { useServerTranslation } from "@/shared/i18n";
-import Link from "next/link";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import Image from "next/image";
-import bookImg from "@/shared/assets/images/mainpage/book.webp";
 import { RoutePaths } from "@/shared/appLinks/RoutePaths";
-import VideoContentYoutube from "@/shared/ui/VideoContent/ui/VideoContentYoutube";
 import { SectionVideoAndGalleries } from "@/widgets/SectionVideoAndGalleries";
-import { SectionNewsPreview } from "@/widgets/SectionNewsPreview";
 import { SectionGetToKnowComics } from "@/widgets/SectionGetToKnowComics";
-import { SectionHeroes } from "@/widgets/SectionHeroes";
 import { SectionPlayWithUs } from "@/widgets/SectionPlayWithUs";
 import { SectionRanking } from "@/widgets/SectionRanking";
-import { SectionHeroes2 } from "@/widgets/SectionHeroes2";
+import { SectionHeroesBlocks } from "@/widgets/SectionHeroesBlocks";
 import { HorizontalLines } from "@/shared/ui/HorizontalLines";
 
 type Props = {
@@ -53,7 +41,7 @@ const MainPage = async ({ lng }: Props) => {
             <HorizontalLines />
             
              {/*<SectionHeroes />*/}
-            <SectionHeroes2 />
+            <SectionHeroesBlocks />
             <HorizontalLines />
             
             <SectionGetToKnowComics buttonParams={{ innerText: t("getToKnowComics"), href: RoutePaths.COMICS_GALLERY }} />
