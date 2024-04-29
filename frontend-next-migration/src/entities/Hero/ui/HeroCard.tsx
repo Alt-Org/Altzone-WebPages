605c95151cb4e353c8394e70aa60e7b89c758431
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-04-29 14:52:48 +0300

    update styles and fsd logic

--- frontend-next-migration/src/entities/Hero/ui/HeroCard.tsx
+++ frontend-next-migration/src/entities/Hero/ui/HeroCard.tsx
@@ -1,27 +1,32 @@
+'use client'
 import cls from "./HeroCard.module.scss";
 import Image from "next/image";
-import Link from "next/link";
-import heroBorder from "@/shared/assets/images/heros/hero-border/hero-border.webp";
 import { AppRoutesLinks, RoutePaths } from "@/shared/appLinks/RoutePaths";
+import {ClickableBorder} from "@/shared/ui/ClickableBorder";
+import {AppLink} from "@/shared/ui/AppLink/AppLink";
+import {classNames} from "@/shared/lib/classNames/classNames";
 
 type Props = {
     id: string,
     imageSrc: any,
     imageAlt: string,
+    className?: string
 };
 
 export const HeroCard = (props: Props) => {
-    const { id, imageSrc, imageAlt } = props;
+    const { id, imageSrc, imageAlt, className = "" } = props;
 
     return (
-        <div className={cls.Border}>
-            <Link className={cls.HeroBorder} href={RoutePaths[AppRoutesLinks.HEROES].replace(":id", id.toString())}>
-                <Image src={heroBorder} alt="heroBorder"  />
-            </Link>
-            <div className={cls.HeroDiv}>
-                <Image src={imageSrc} alt={imageAlt} className={cls.HeroImg} />
-            </div>
+        <ClickableBorder
+            borderImageSource={"/images/hero-border.webp"}
+            className={classNames(cls.Wrapper, {}, [className])}
+        >
+        <div className={cls.HeroDiv}>
+            <AppLink to={RoutePaths[AppRoutesLinks.HEROES].replace(":id", id.toString())}>
+            <Image src={imageSrc} alt={imageAlt} className={cls.HeroImg} />
+            </AppLink>
         </div>
+        </ClickableBorder>
     );
 };
 
