a6b8ba339c9e284037e42b4d1cd92aa486a7d389
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-04-30 20:19:32 +0300

    add animation

--- frontend-next-migration/src/widgets/SectionHeroesBlocks/ui/SectionHeroesBlocks.tsx
+++ frontend-next-migration/src/widgets/SectionHeroesBlocks/ui/SectionHeroesBlocks.tsx
@@ -1,23 +1,21 @@
-import cls from "./SectionHeroesBlocks.module.scss"
+'use client'
 import Image from "next/image";
 import bgPicture from "@/shared/assets/images/mainpage/background.webp";
 import { HeroCard, heroes } from "@/entities/Hero";
+import cls from "./SectionHeroesBlocks.module.scss";
+import {useInView} from "react-intersection-observer";
 
-type Props = {
-
-}
-
-const SectionHeroes2 = (props: Props) => {
-
-    const {
-
-    } = props;
-
+const SectionHeroes2 = () => {
+    const { ref, inView } = useInView({
+        rootMargin: '-150px 0px',
+        triggerOnce: true
+    });
 
     return (
         <section
             className={cls.SectionHeroes2}
             style={{ backgroundImage: `url(${bgPicture})`}}
+            ref={ref}
         >
 
             <div className={cls.backgroundImageWrapper}>
@@ -26,10 +24,10 @@ const SectionHeroes2 = (props: Props) => {
                        objectFit="cover"/>
             </div>
 
-            <div className={cls.Content}>
+            <div className={cls.Content} >
                 {heroes.map((item, index) => (
                     <HeroCard
-                        className={cls.HeroCard}
+                        className={`${cls.HeroCard} ${inView ? cls.Visible : ''}`}
                         key={item.title}
                         id={item.title}
                         imageSrc={item.src}
@@ -37,6 +35,9 @@ const SectionHeroes2 = (props: Props) => {
                     />
                 ))}
             </div>
+
+
+
         </section>
     );
 };
