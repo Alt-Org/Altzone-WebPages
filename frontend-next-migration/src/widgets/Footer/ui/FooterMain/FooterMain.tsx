517b859236a7da691c51e37bcaf5d2e39e114b8c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-05-01 21:21:12 +0300

    fix padding problem

--- frontend-next-migration/src/widgets/Footer/ui/FooterMain/FooterMain.tsx
+++ frontend-next-migration/src/widgets/Footer/ui/FooterMain/FooterMain.tsx
@@ -1,8 +1,14 @@
 import FooterDesktop from "../FooterDesktop/FooterDesktop";
+import useIsMobileSize from "@/shared/lib/hooks/useIsMobileSize";
 
 
 export const Footer = ()  => {
+
+    const {isMobileSize} = useIsMobileSize()
+
     return (
+        <div style={ !isMobileSize ? {paddingTop: "20px"} : {}}>
         <FooterDesktop/>
+        </div>
     )
 }
