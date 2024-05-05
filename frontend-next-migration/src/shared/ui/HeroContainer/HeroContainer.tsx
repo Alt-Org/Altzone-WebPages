1bda41c936744f1c086433d8147f8992201b518f
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-05-05 14:07:37 +0300

    add left, right ecs keyboard support

--- frontend-next-migration/src/shared/ui/HeroContainer/HeroContainer.tsx
+++ frontend-next-migration/src/shared/ui/HeroContainer/HeroContainer.tsx
@@ -8,6 +8,8 @@ import { AppRoutesLinks, RoutePaths } from "@/shared/appLinks/RoutePaths";
 import infoBg from "@/shared/assets/images/heros/hero-container/info-bg.svg";
 import rightArrow from "@/shared/assets/images/heros/hero-container/right-arrow.png";
 import leftArrow from "@/shared/assets/images/heros/hero-container/left-arrow.png";
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
 
 
 
@@ -20,6 +22,7 @@ type Props = {
     heroDescription: string,
     leftArrowLink: any,
     rightArrowLink: any
+    xLink: any
 }
 
 //todo fix that component, may be even create new duplication with another approach
@@ -32,10 +35,35 @@ const HeroContainer = (props: Props) => {
         heroDescription,
         rightArrowLink,
         leftArrowLink,
+        xLink,
         heroGif
     } = props;
 
     const { isMobileSize } = useIsMobileSize();
+
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleKeyDown = (event: any) => {
+            if (event.keyCode === 37) {
+                router.push(leftArrowLink);
+            }
+            else if (event.keyCode === 39) {
+                router.push(rightArrowLink);
+            }
+            else if (event.keyCode === 27) {
+                router.push(xLink);
+            }
+        };
+
+        // Add the event listener
+        document.addEventListener('keydown', handleKeyDown);
+
+        // Remove the event listener on component unmount
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [router, leftArrowLink, rightArrowLink]);
 
     return (
         <section className={cls.HeroContainer}>
@@ -63,7 +91,7 @@ const HeroContainer = (props: Props) => {
                         <h2 >{heroName}</h2>
                         <hr></hr>
                     </div>
-                    <Link href="/" className={cls.XButton}>
+                    <Link href={xLink} className={cls.XButton}>
                         <h1>X</h1>
                     </Link>
                     <div className={cls.HeroInfoMain}>
