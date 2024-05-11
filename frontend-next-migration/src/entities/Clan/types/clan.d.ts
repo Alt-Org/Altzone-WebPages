8c3cdbfeafdb690c8415f7d3dc2fd54b77fc2c74
Skoivumaki <sakari.koivumaki@gmail.com>
2024-05-11 18:29:52 +0300

    cleanup

--- frontend-next-migration/src/entities/Clan/types/clan.d.ts
+++ frontend-next-migration/src/entities/Clan/types/clan.d.ts
@@ -6,13 +6,14 @@ export type IClan = {
     name: string,
     gameCoins: number,
     playerCount: number,
-    furnitureCount: number,
-    raidRoomCount: number,
+    stockCount: number,
+    itemCount: number,
     tag: string,
     admin_ids: string[],
     Player: Array[],
+    isOpen: boolean,
 };
 
-export type IClanCreateDto = Pick<IClan, "name" | "tag" | "gameCoins">;
+export type IClanCreateDto = Pick<IClan, "name" | "tag" | "gameCoins" | "isOpen">;
 
-export type IClanUpdateDto = Pick<IClan, "_id" | "name" | "tag" | "gameCoins">;
+export type IClanUpdateDto = Pick<IClan, "_id" | "name" | "tag" | "gameCoins" | "isOpen">;
