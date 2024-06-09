bc00ed0276d056f7f7c2f9a8460e2f58c01f2199
Joni Roine <joni.roine@outlook.com>
2024-06-09 13:39:07 +0300

    popup conf

--- frontend-next-migration/src/entities/Hero/ui/HeroCard.tsx
+++ frontend-next-migration/src/entities/Hero/ui/HeroCard.tsx
@@ -1,48 +1,65 @@
-'use client'
-import cls from "./HeroCard.module.scss";
-import Image from "next/image";
-import { AppRoutesLinks, RoutePaths } from "@/shared/appLinks/RoutePaths";
-import { ClickableBorder } from "@/shared/ui/ClickableBorder";
-import { AppLink } from "@/shared/ui/AppLink/AppLink";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import { useCallback, useRef } from "react";
-import useResizeObserver, { ResizeCallback } from "@/shared/lib/hooks/useResizeObserver";
-
+'use client';
+import React, { useState, useCallback, useRef } from 'react'; // Lisätty useState
+import cls from './HeroCard.module.scss';
+import Image from 'next/image';
+import { AppRoutesLinks, RoutePaths } from '@/shared/appLinks/RoutePaths';
+import { ClickableBorder } from '@/shared/ui/ClickableBorder';
+import { AppLink } from '@/shared/ui/AppLink/AppLink';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import useResizeObserver, {
+  ResizeCallback,
+} from '@/shared/lib/hooks/useResizeObserver';
+import Popup from '@/shared/ui/Popup/Popup';
 
 type Props = {
-    id: string,
-    imageSrc: any,
-    imageAlt: string,
-    className?: string,
-    backgroundColor?: string,
+  id: string;
+  imageSrc: any;
+  imageAlt: string;
+  className?: string;
+  backgroundColor?: string;
 };
-
 
 export const HeroCard = (props: Props) => {
-    const { id, imageSrc, imageAlt, className = "", backgroundColor } = props;
+  const { id, imageSrc, imageAlt, className = '', backgroundColor } = props;
+  const [isPopupOpen, setIsPopupOpen] = useState(false); // Lisätty tilamuuttuja
 
-    const elementRef = useRef(null);
+  const elementRef = useRef(null);
 
-    const handleCardSizeUpdate: ResizeCallback<HTMLDivElement> = useCallback((refCurrent) => {
-        const width = refCurrent.clientWidth;
-        refCurrent.style.setProperty("--cardWidthLocal", `${width}px`);
-    }, []);
+  const handleCardSizeUpdate: ResizeCallback<HTMLDivElement> = useCallback(
+    (refCurrent) => {
+      const width = refCurrent.clientWidth;
+      refCurrent.style.setProperty('--cardWidthLocal', `${width}px`);
+    },
+    [],
+  );
 
-    useResizeObserver({ elementRef, callback: handleCardSizeUpdate })
+  useResizeObserver({ elementRef, callback: handleCardSizeUpdate });
 
-    return (
-        <ClickableBorder
-            ref={elementRef}
-            borderImageSource={"/images/hero-border3.png"}
-            className={classNames(cls.Wrapper, {}, [className])}
-        >
-            <div className={cls.HeroDiv} style={{ backgroundColor }}> 
-                <AppLink to={RoutePaths[AppRoutesLinks.HEROES_ONE].replace(":id", id.toString())}>
-                    <Image src={imageSrc} alt={imageAlt} className={cls.HeroImg} />
-                </AppLink>
-            </div>
-        </ClickableBorder>
-    );
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  return (
+    <>
+      <div className={cls.HeroDiv} style={{ backgroundColor }}>
+        <button onClick={openPopup}>
+          <Image src={imageSrc} alt={imageAlt} className={cls.HeroImg} />
+        </button>
+      </div>
+      <Popup isOpen={isPopupOpen} onClose={closePopup}>
+        {' '}
+        {/* Lisätty isOpen ja onClose propsit */}
+        {/* Popup-sisältö tässä */}
+        <div>
+          <p>Tähän voit lisätä popup-ikkunan sisällön</p>
+        </div>
+      </Popup>
+    </>
+  );
 };
 
-HeroCard.displayName = "HeroCard";
+HeroCard.displayName = 'HeroCard';
