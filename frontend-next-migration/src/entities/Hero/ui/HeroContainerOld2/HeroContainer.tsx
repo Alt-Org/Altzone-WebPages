b58f1fcd1182666705aee54a17179e526c6e355b
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-10 22:11:27 +0300

    Create new HeroContainer component and styles
    A new HeroContainer component has been created, reworking how hero entities are rendered on the page. A new SCSS file was also added to match this new structure. The old HeroContainer component is commented out in the export index, and replaced with our newly created one. A new SVG file was also included in the assets directory.

---
+++ frontend-next-migration/src/entities/Hero/ui/HeroContainerOld2/HeroContainer.tsx
@@ -0,0 +1,93 @@
+'use client'
+import {RefObject, useEffect, useRef, useState} from 'react';
+import bgBox from "@/shared/assets/images/heros/hero-container/readyContainer.png";
+import Image from 'next/image';
+import cls from "./HeroContainer.module.scss";
+
+type Props = {
+    heroImg: string,
+    heroDescription: string,
+    heroColor: string,
+}
+
+const HeroContainer = (props: Props) => {
+    const {
+        heroImg,
+        heroDescription,
+        heroColor
+    } = props;
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const imageRef = useRef<HTMLImageElement>(null);
+    const [distanceToBottom, setDistanceToBottom] = useState(0);
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+
+    const calculateDistance = (
+        containerRef:  RefObject<HTMLDivElement>,
+        imageRef:  RefObject<HTMLDivElement>,
+    ) => {
+        if (containerRef.current && imageRef.current) {
+            const containerRect = containerRef.current.getBoundingClientRect();
+            const imageRect = imageRef.current.getBoundingClientRect();
+            const distanceToBottom = containerRect.bottom - imageRect.bottom;
+            setDistanceToBottom(distanceToBottom);
+        }
+    };
+
+    const handleImageLoad = () => {
+        if (containerRef.current && imageRef.current) {
+            setImagesLoaded(true);
+        }
+    };
+
+    useEffect(() => {
+        if (imagesLoaded) {
+            calculateDistance(
+                containerRef,
+                imageRef
+            );
+        }
+    }, [imagesLoaded]);
+
+    useEffect(() => {
+        const handleResize = () => calculateDistance(containerRef, imageRef);
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <div className={cls.wrapper}>
+            <div className={cls.container} ref={containerRef}>
+                <Image className={cls.bgImg} src={bgBox} alt="hero" width={400} height={400} onLoad={handleImageLoad} />
+                <div className={cls.contentWrapper}>
+                    <div className={cls.content}>
+                        <div className={cls.heroImgWrapper} style={{backgroundColor: heroColor}}>
+                            <Image
+                                className={cls.heroImg}
+                                src={heroImg}
+                                alt="hero"
+                                width={500}
+                                height={500}
+                                ref={imageRef}
+                                onLoad={handleImageLoad}
+                            />
+                        </div>
+                        <div className={cls.heroDescription} style={{maxHeight: distanceToBottom}}>
+                            <p>
+                                {heroDescription}
+                                {heroDescription}
+                                {heroDescription}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default HeroContainer;
