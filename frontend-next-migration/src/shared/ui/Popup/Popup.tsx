fddb181d1d7b9a1358b65598ce5a017570e3be46
Joni Roine <joni.roine@outlook.com>
2024-06-10 16:18:26 +0300

    hero container popup

--- frontend-next-migration/src/shared/ui/Popup/Popup.tsx
+++ frontend-next-migration/src/shared/ui/Popup/Popup.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import cls from './Popup.module.scss'; // Tarkista tämä polku
+import React, { useEffect, useState } from 'react';
+import cls from './popup.module.scss';
 
 type Props = {
-  isOpen: boolean; // Lisätty isOpen props
+  isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  className?: string;
 };
 
 const Popup = ({ isOpen, onClose, children }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        event.target &&
+        !(event.target as HTMLElement).closest(`.${cls.popupContent}`)
+      ) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [isOpen, onClose]);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className={cls.popupOverlay}>
+    <div
+      className={cls.popupOverlay}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
       <div className={cls.popupContent}>
-        <button className={cls.closeButton} onClick={onClose}>
-          X
-        </button>
-        {children}
+        <div className={`${cls.HeroInfoDiv} ${isHovered ? cls.hovered : ''}`}>
+          {children}
+        </div>
       </div>
     </div>
   );
