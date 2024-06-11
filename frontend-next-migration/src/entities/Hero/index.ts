5c2cf79240e7e25475177c16fa1be5230e1fb38b
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-11 20:06:32 +0300

    Update HeroContainer import
    The HeroContainer import in the Hero entity has been updated. It previously imported from "HeroContainerOld2", but has now been changed to fetch from "HeroContainerNewByLeo". This update reflects the latest changes made to the HeroContainer component.

--- frontend-next-migration/src/entities/Hero/index.ts
+++ frontend-next-migration/src/entities/Hero/index.ts
@@ -2,7 +2,7 @@ export {HeroCard} from "./ui/HeroCard";
 // export {default as HeroContainer} from "./ui/HeroContainer/HeroContainer";
 
 
-export {default as HeroContainer} from "./ui/HeroContainerOld2/HeroContainer";
+export {default as HeroContainer} from "./ui/HeroContainerNewByLeo/HeroContainer";
 
 
 export {default as heroes} from "./model/heroes";
