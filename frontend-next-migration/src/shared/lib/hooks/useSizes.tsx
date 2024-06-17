e30a85e2eb8fad8cc3bfb435352056fb31ca2ff4
Joni Roine <joni.roine@outlook.com>
2024-06-17 15:22:58 +0300

    adjusting use sizes components

--- frontend-next-migration/src/shared/lib/hooks/useSizes.tsx
+++ frontend-next-migration/src/shared/lib/hooks/useSizes.tsx
@@ -1,16 +1,29 @@
+'use client';
+import { useEffect, useState } from 'react';
+
 import useIsDesktopSize from './useIsDesktopSize';
 import useIsMobileSize from './useIsMobileSize';
 import useIsTabletSize from './useIsTabletSize';
+import useIsWidescreenSize from './useIsWidescreenSize';
 
 const useSizes = () => {
   const { isDesktopSize } = useIsDesktopSize();
   const { isMobileSize } = useIsMobileSize();
   const { isTabletSize } = useIsTabletSize();
+  const { isWidescreenSize } = useIsWidescreenSize();
+
+  useEffect(() => {
+    console.log('isMobileSize', isMobileSize);
+    console.log('isTabletSize', isTabletSize);
+    console.log('isDesktopSize', isDesktopSize);
+    console.log('isWidescreenSize', isWidescreenSize);
+  });
 
   return {
     isDesktopSize,
     isMobileSize,
     isTabletSize,
+    isWidescreenSize,
   };
 };
 
