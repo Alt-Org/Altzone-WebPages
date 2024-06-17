6c9069311ae9941dadc5a2869be78a1ccb7c7654
Joni Roine <joni.roine@outlook.com>
2024-06-17 11:33:48 +0300

    made useSizes which takes three sizing hooks for more responsive pages + desktopSize

---
+++ frontend-next-migration/src/shared/lib/hooks/useIsDesktopSize.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const useIsDesktopSize = () => {
+  const checkForDevice = () =>
+    typeof window !== 'undefined' ? window.innerWidth >= 1024 : false;
+
+  const [isDesktopSize, setIsDesktopSize] = useState(checkForDevice());
+
+  useEffect(() => {
+    const handlePageResized = () => {
+      setIsDesktopSize(checkForDevice());
+    };
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handlePageResized);
+      window.addEventListener('orientationchange', handlePageResized);
+      window.addEventListener('load', handlePageResized);
+      window.addEventListener('reload', handlePageResized);
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handlePageResized);
+        window.removeEventListener('orientationchange', handlePageResized);
+        window.removeEventListener('load', handlePageResized);
+        window.removeEventListener('reload', handlePageResized);
+      }
+    };
+  }, []);
+
+  return {
+    isDesktopSize,
+  };
+};
+
+export default useIsDesktopSize;
