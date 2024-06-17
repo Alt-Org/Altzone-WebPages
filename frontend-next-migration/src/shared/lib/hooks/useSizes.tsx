914de6c375b780b3d8072e4f1019939213da6c91
Joni Roine <joni.roine@outlook.com>
2024-06-17 11:33:48 +0300

    made useSizes which takes three sizing hooks for more responsive pages + desktopSize

---
+++ frontend-next-migration/src/shared/lib/hooks/useSizes.tsx
@@ -0,0 +1,19 @@
+// useSizes.tsx
+import { useEffect, useState } from 'react';
+import useIsDesktopSize from './useIsDesktopSize';
+import useIsMobileSize from './useIsMobileSize';
+import useIsTabletSize from './useIsTabletSize';
+
+const useSizes = () => {
+  const { isDesktopSize } = useIsDesktopSize();
+  const { isMobileSize } = useIsMobileSize();
+  const { isTabletSize } = useIsTabletSize();
+
+  return {
+    isDesktopSize,
+    isMobileSize,
+    isTabletSize,
+  };
+};
+
+export default useSizes;
