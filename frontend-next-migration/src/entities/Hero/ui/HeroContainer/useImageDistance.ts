e6fad1c4febeff578f2eefea42159cdacbe01d34
Joni Roine <joni.roine@outlook.com>
2024-06-18 08:50:15 +0300

    added otherElement for text handling

--- frontend-next-migration/src/entities/Hero/ui/HeroContainer/useImageDistance.ts
+++ frontend-next-migration/src/entities/Hero/ui/HeroContainer/useImageDistance.ts
@@ -1,51 +1,58 @@
 import { useRef, useState, useEffect } from 'react';
 
-// be very careful with refactoring
 const useImageDistance = () => {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const imageRef = useRef<HTMLImageElement>(null);
-    const [distanceToBottom, setDistanceToBottom] = useState(0);
-    const [imagesLoaded, setImagesLoaded] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const otherElementRef = useRef<HTMLDivElement>(null);
+  const [distanceToBottom, setDistanceToBottom] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
-    const calculateDistance = () => {
-        if (containerRef.current && imageRef.current) {
-            const containerRect = containerRef.current.getBoundingClientRect();
-            const imageRect = imageRef.current.getBoundingClientRect();
-            const distance = containerRect.bottom - imageRect.bottom;
-            setDistanceToBottom(distance);
-        }
+  const calculateDistance = () => {
+    if (containerRef.current && imageRef.current && otherElementRef.current) {
+      const containerRect = containerRef.current.getBoundingClientRect();
+      const imageRect = imageRef.current.getBoundingClientRect();
+      const otherElementRect = otherElementRef.current.getBoundingClientRect();
+      const distance =
+        containerRect.bottom -
+        Math.max(imageRect.bottom, otherElementRect.bottom);
+      setDistanceToBottom(distance);
+    }
+  };
+
+  useEffect(() => {
+    if (imagesLoaded) {
+      calculateDistance();
+      setTimeout(() => {
+        calculateDistance();
+      }, 100);
+    }
+  }, [imagesLoaded]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      calculateDistance();
+      setTimeout(() => {
+        calculateDistance();
+      }, 100);
     };
 
-    useEffect(() => {
-        if (imagesLoaded) {
-            // be very careful with refactoring, first time for good internet second time for slow
-            calculateDistance();
-            setTimeout(() => {
-                calculateDistance();
-            },100)
-        }
-    }, [imagesLoaded]);
+    window.addEventListener('resize', handleResize);
 
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-    useEffect(() => {
-        const handleResize = () => {
-            calculateDistance();
-            // it is very important when we switch fullscreen we should make that macrotask
-            setTimeout(() => {
-                calculateDistance();
-            }, 100);
-        };
+  const handleImageLoad = () => setImagesLoaded(true);
 
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const handleImageLoad = () => setImagesLoaded(true);
-
-    return { containerRef, imageRef, distanceToBottom, handleImageLoad, imagesLoaded };
+  return {
+    containerRef,
+    imageRef,
+    otherElementRef,
+    distanceToBottom,
+    handleImageLoad,
+    imagesLoaded,
+  };
 };
 
 export default useImageDistance;
