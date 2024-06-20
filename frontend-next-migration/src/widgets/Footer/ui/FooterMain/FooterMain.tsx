8cf98992a7c7c9c061599a5de7bc653337099246
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-20 18:11:33 +0300

    Add responsive padding to Footer component
    Updated the FooterMain.tsx file within the Footer component to improve responsiveness. Extra padding is now conditionally added on larger (non-mobile) screen sizes for better visual separation and aesthetics. This change enhances the desktop user experience.

--- frontend-next-migration/src/widgets/Footer/ui/FooterMain/FooterMain.tsx
+++ frontend-next-migration/src/widgets/Footer/ui/FooterMain/FooterMain.tsx
@@ -1,10 +1,14 @@
-import FooterDesktop from '../FooterDesktop/FooterDesktop';
-import useIsMobileSize from '@/shared/lib/hooks/useIsMobileSize';
+import FooterDesktop from "../FooterDesktop/FooterDesktop";
+import useIsMobileSize from "@/shared/lib/hooks/useIsMobileSize";
 
-export const Footer = () => {
-  return (
-    <div>
-      <FooterDesktop />
-    </div>
-  );
-};
+
+export const Footer = ()  => {
+
+    const {isMobileSize} = useIsMobileSize()
+
+    return (
+        <div style={ !isMobileSize ? {paddingTop: "20px"} : {}}>
+            <FooterDesktop/>
+        </div>
+    )
+}
