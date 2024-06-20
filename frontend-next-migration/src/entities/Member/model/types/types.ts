183a0c8ca25ee84665dbc4bc4da5f60fc9eddc15
Joni Roine <142219973+Jonroi@users.noreply.github.com>
2024-06-20 19:40:54 +0300

    updated names and categorys (#20)

--- frontend-next-migration/src/entities/Member/model/types/types.ts
+++ frontend-next-migration/src/entities/Member/model/types/types.ts
@@ -1,43 +1,50 @@
 export enum GroupType {
-    GameDesign = 'GameDesign',
-    Graphic = "Graphic",
-    LogoGraphic = 'LogoGraphic',
-    GameGraphic = 'GameGraphic',
-    GameAnimation= 'GameAnimation',
-    GameCode = 'GameCode',
-    APICode = 'APICode',
-    WebPagesCode = 'WebPagesCode',
-    Sound = 'Sound',
-    Comics = 'Comics',
-    Prod = 'Prod',
-    OthersGame = 'OthersGame',
-    OthersComics = 'OthersComics',
-    Others = 'Others',
+  GameDesign = 'GameDesign',
+  Graphic = 'Graphics',
+  LogoGraphic = 'LogoGraphic',
+  GameGraphic = 'GameGraphic',
+  GameAnimation = 'GameAnimation',
+  GameCode = 'GameCode',
+  APICode = 'APICode',
+  WebPagesCode = 'WebPagesCode',
+  Sound = 'Sounds',
+  Comics = 'Comics',
+  OthersGame = 'OthersGame',
+  OthersComics = 'OthersComics',
+  Others = 'Others',
+  UserInterface = 'User Interface',
+  EducationalDesigner = 'Educational Designer',
+  Concept = 'Original game concept',
+  Production = 'Production',
+  ProductionConsultant = 'Production consultant',
+  SocialMedia = 'Social media',
+  TechnicalConsultant = 'Technical consultant',
+  ArtisticDirector = 'Artistic director',
 }
 
-export enum MemberStatus{
-    voluntary = 'vapaaehtoinen',
-    worker = 'työntekijä',
-    intern = 'työharjoittelija'
+export enum MemberStatus {
+  voluntary = 'vapaaehtoinen',
+  worker = 'työntekijä',
+  intern = 'työharjoittelija',
 }
 
 export interface GroupWithMember {
-    group: GroupType,
-    workers: Member[]
+  group: GroupType;
+  workers: Member[];
 }
 
 export interface Member {
-    id: number;
-    name: string;
-    imgSrc?: string;
-    role?: string;
-    email?: string;
-    phone?: string;
-    github?: string;
-    discord?: string;
-    trello?: string;
-    status?: MemberStatus;
-    workPeriod?: string;
-    site?: string;
-    linkedin?: string
+  id: number;
+  name: string;
+  imgSrc?: string;
+  role?: string;
+  email?: string;
+  phone?: string;
+  github?: string;
+  discord?: string;
+  trello?: string;
+  status?: MemberStatus;
+  workPeriod?: string;
+  site?: string;
+  linkedin?: string;
 }
