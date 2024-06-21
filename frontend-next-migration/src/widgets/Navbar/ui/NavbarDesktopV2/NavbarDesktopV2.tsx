e368cbd040e7cb3052b295c11175f6bb9d9c9012
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-21 19:18:18 +0300

    Extract NavItem component to its own file
    This commit migrates the NavItem component to its own file for modularity. Previously, the NavItem component's logic was located inside the NavbarDesktopV2 file. By extracting it into its own file, the code becomes easier to maintain and improve readability. This change does not impact the functionality of the NavBar component.

--- frontend-next-migration/src/widgets/Navbar/ui/NavbarDesktopV2/NavbarDesktopV2.tsx
+++ frontend-next-migration/src/widgets/Navbar/ui/NavbarDesktopV2/NavbarDesktopV2.tsx
@@ -1,15 +1,14 @@
 import React, { CSSProperties, memo } from "react";
 import cls from "./NavbarDesktopV2.module.scss";
-import { NavbarBuild, NavbarMenuItem } from "../../model/types/types";
+import { NavbarBuild } from "../../model/types/types";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink/AppLink";
 import { useParams } from "next/navigation";
 import { useClientTranslation } from "@/shared/i18n";
-import { DropdownWrapper } from "@/shared/ui/DropdownWrapper";
-import Image from "next/image";
 import { Container } from "@/shared/ui/Container";
 import { LangSwitcher } from "@/features/LangSwitcher";
 import { useLogoutMutation, useUserPermissions } from "@/entities/Auth";
+import NavItem from "./NavItem";
 
 
 
@@ -104,94 +103,3 @@ export default NavbarDesktopV2;
 
 NavbarDesktopV2.displayName = "NavbarDesktopV2"
 
-
-
-
-type NavItemProps = {
-    item: NavbarMenuItem;
-    className?: string;
-    navbarBuild: NavbarBuild
-}
-
-const NavItem = memo((props: NavItemProps) => {
-    const { item, className = '', navbarBuild } = props;
-    const { type: itemType } = item
-
-    const params = useParams();
-    const lng = params.lng as string;
-    const { t } = useClientTranslation(lng, "navbar");
-
-
-    if (itemType === "navLink") {
-        return (
-            <li
-                key={item.path}
-                className=
-                {classNames(cls.navItem, {}, [className])} >
-                <AppLink
-                    theme={AppLinkTheme.PRIMARY}
-                    to={item.path}
-                    key={item.path}
-                >
-                    <span>{t(`${item.name}`)}</span>
-                </AppLink>
-
-            </li>
-        )
-    }
-
-
-    if (itemType === "navDropDown") {
-
-        const localizedElements = item.elements.map((element) => ({
-            ...element,
-            elementText: t(`${element.elementText}`),
-        }));
-
-
-        return (
-            <li
-                key={item.name}
-                className=
-                {classNames(cls.navItem, {}, [className])} >
-
-                <DropdownWrapper
-                    elements={localizedElements}
-                    contentAbsolute={true}
-                    mouseOverLeaveMode={true}
-                    contentClassName={cls.itemNavbarDropDownContent}
-                >
-                    <div>{t(`${item.name}`)}</div>
-                </DropdownWrapper>
-            </li>
-        )
-    }
-
-
-    if (itemType === "navLogo") {
-        return (
-            <li
-                key={item.src}
-            >
-                <AppLink
-                    theme={AppLinkTheme.PRIMARY}
-                    to={item.path}
-                    className={classNames(cls.appLink, {}, [cls.appLinkLogo])}
-                >
-
-                    <Image
-                        loading={"eager"}
-                        alt={navbarBuild?.namedMenu?.navLogo?.name || ''}
-                        src={navbarBuild?.namedMenu?.navLogo?.src || ''}
-                        width={120}
-                        className={cls.itemLogoImg}
-                    />
-                </AppLink>
-            </li>
-
-        );
-    }
-
-})
-
-NavItem.displayName = "NavItem"
