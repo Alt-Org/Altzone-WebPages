440bb2d1bcba72ba71271ed9c031efe6890bc8e8
Joni Roine <joni.roine@outlook.com>
2024-06-24 15:20:15 +0300

    adjusting mobile view

--- frontend-next-migration/src/preparedPages/TeachingPackagePage/ui/TeachingPackagePage.tsx
+++ frontend-next-migration/src/preparedPages/TeachingPackagePage/ui/TeachingPackagePage.tsx
@@ -13,6 +13,9 @@ import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
 import NavbarSide from '@/widgets/NavbarSide/ui/NavbarSide';
 import backgroundImage from '@/shared/assets/images/clanBg/Moon.webp';
 import pageLogo from '@/shared/assets/images/teachingPackage/pageLogo.png';
+import useSizes from '@/shared/lib/hooks/useSizes';
+import { Mods } from '@/shared/lib/classNames/classNames';
+import { classNames } from '../../../shared/lib/classNames/classNames';
 
 interface Section {
   id: string;
@@ -24,6 +27,14 @@ type Props = {
 };
 
 const TeachingPackagePage: React.FC<Props> = ({ lng }) => {
+  const { isMobileSize, isTabletSize, isDesktopSize, isWidescreenSize } =
+    useSizes();
+  const combinedModCss: Mods = {
+    [cls.isMobile]: isMobileSize,
+    [cls.isTablet]: isTabletSize,
+    [cls.isDesktop]: isDesktopSize,
+    [cls.isWidescreen]: isWidescreenSize,
+  };
   const sections: Section[] = [
     { id: 'section1', label: 'Esittely' },
     { id: 'section2', label: 'Toteutus' },
@@ -41,22 +52,26 @@ const TeachingPackagePage: React.FC<Props> = ({ lng }) => {
   ];
 
   return (
-    <div className={cls.pageContainer}>
+    <div className={classNames(cls.pageContainer, combinedModCss)}>
       <Navbar overlaid={true} />
-      <div className={cls.mainContent}>
-        <div className={cls.pageLogo}>
-          <Image
-            src={pageLogo}
-            className={cls.backgroundImage}
-            alt='pageLogo'
-            height={600}
-            width={600}
-          />
-        </div>
-        <div className={cls.navbarSide}>
-          <NavbarSide sections={sections} containerId='content' />
-        </div>
-        <div className={cls.content} id='content'>
+      <div className={classNames(cls.mainContent, combinedModCss)}>
+        {!isMobileSize && (
+          <div className={cls.pageLogo}>
+            <Image
+              src={pageLogo}
+              className={cls.backgroundImage}
+              alt='pageLogo'
+              height={600}
+              width={600}
+            />
+          </div>
+        )}
+        {!isMobileSize && (
+          <div className={classNames(cls.navbarSide, combinedModCss)}>
+            <NavbarSide sections={sections} containerId='content' />
+          </div>
+        )}
+        <div className={classNames(cls.content, combinedModCss)} id='content'>
           {sections.map((section) => (
             <div id={section.id} key={section.id} className={cls.section}>
               <h2>{section.label}</h2>
