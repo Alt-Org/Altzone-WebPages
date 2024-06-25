23ec8a16426b659cf0af6e958c7ea5895178b865
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-25 20:59:59 +0300

    Remove commented code in teachingPackage page
    The commit removes a significant amount of commented code, including definitions of sections, from the teachingPackage page. Commented code was not required and was unnecessarily cluttering the file, making it harder to read and maintain.

--- frontend-next-migration/src/app/[lng]/teachingPackage/page.tsx
+++ frontend-next-migration/src/app/[lng]/teachingPackage/page.tsx
@@ -25,29 +25,6 @@ export default async function DefaultPage({ params }: Props){
 
     const sections = makeTeachingSectionsWithI18n(t);
 
-
-    // const sections =  [
-    //     { id: 'section1', ["t-label"]: t('demo-title')  , description: t('demo-description')},
-    //     { id: 'section2', label: t('implementation-title') , description: t('implementation-description')},
-    // ]
-    //
-    // const sections =  [
-    //     { id: 'section1', label: t('demo-title')  , description: t('demo-description')},
-    //     { id: 'section2', label: t('implementation-title') , description: t('implementation-description')},
-    //     { id: 'section3', label: 'Mitä Pelitaide on?' },
-    //     { id: 'section4', label: 'Visuaalinen vaikuttuvuus' },
-    //     { id: 'section5', label: 'Toiminnallinen vaikuttavuus' },
-    //     { id: 'section6', label: 'Yhteydet pelin ulkopuolelle' },
-    //     { id: 'section7', label: 'ALT Zone -verkkopeli' },
-    //     { id: 'section8', label: 'Erilaisten pelaajien yhteinen liittouma' },
-    //     { id: 'section9', label: 'Pelihahmot & niiden kehittäminen' },
-    //     { id: 'section10', label: 'Pelimekaniikka' },
-    //     { id: 'section11', label: 'Nuorisotyö & pelitaiteen opetus kouluissa' },
-    //     { id: 'section12', label: 'Pelitaiteen kirjallisuutta' },
-    //     { id: 'section13', label: 'PRG - Psyche’s Royale Gaming ry' },
-    // ]
-
-
     return(
                 <TeachingPackagePage
                     sections = {sections}
