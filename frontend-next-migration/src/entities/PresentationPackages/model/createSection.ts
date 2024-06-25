0b0de8894c20f078323c4dcd9780d69da94983a5
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-25 21:09:27 +0300

    Add documentation to createSection function
    In the PresentationPackages model, comments have been added to the createSection function

--- frontend-next-migration/src/entities/PresentationPackages/model/createSection.ts
+++ frontend-next-migration/src/entities/PresentationPackages/model/createSection.ts
@@ -1,5 +1,12 @@
 import {PresentationSection} from "../types";
 
+/**
+ * Creates a new presentation section.
+ *
+ * @param {string} prefix - The prefix for the section titles and descriptions.
+ * @param {number} index - The index of the section.
+ * @return {PresentationSection} - The newly created section.
+ */
 export function createSection(prefix: string, index: number): PresentationSection {
     return {
         id: `section${index + 1}`,
