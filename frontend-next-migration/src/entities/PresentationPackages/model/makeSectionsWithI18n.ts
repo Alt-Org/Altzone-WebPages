b359cb738c8e9eba699b3fb2a7295af8d1bbca21
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-25 21:02:04 +0300

    Remove commented code in teachingPackage page
    The commit removes a significant amount of commented code, including definitions of sections, from the teachingPackage page. Commented code was not required and was unnecessarily cluttering the file, making it harder to read and maintain.

--- frontend-next-migration/src/entities/PresentationPackages/model/makeSectionsWithI18n.ts
+++ frontend-next-migration/src/entities/PresentationPackages/model/makeSectionsWithI18n.ts
@@ -1,6 +1,14 @@
 import {PresentationSection} from "@/entities/PresentationPackages/types";
 import {TeachingSections} from "./data/teachingSections";
 
+/**
+ * Takes an array of PresentationSections and returns a higher-order function that takes a translation function
+ * and returns an array of PresentationSections with localized labels and descriptions.
+ *
+ * @param {PresentationSection[]} sections - The array of PresentationSections to localize.
+ * @returns {Function} A higher-order function that takes a translation function.
+ * @return {PresentationSection[]} An array of localized PresentationSections.
+ */
 const makeSectionsWithI18n = (sections: PresentationSection[]): ((t: (key: string) => string) => PresentationSection[]) => {
     return (t: (key: string) => string): PresentationSection[] => {
         return sections.map(section => ({
@@ -11,4 +19,10 @@ const makeSectionsWithI18n = (sections: PresentationSection[]): ((t: (key: strin
     };
 };
 
+/**
+ * Creates teaching sections with internationalization support.
+ *
+ * @param {Array} TeachingSections - The array of teaching sections.
+ * @returns {Array} - The array of teaching sections with internationalization support.
+ */
 export const makeTeachingSectionsWithI18n =  makeSectionsWithI18n(TeachingSections);
