35d179e35f838899e5acb0600957a52929575bb1
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-25 17:21:52 +0300

    Update Navbar export in frontend migration
    The code previously exported an older version of the Navbar component for use in the frontend migration. This change updates the export to the current Navbar version, indicating that it's now ready for use in this context. Old export command has been commented out for reference.

--- frontend-next-migration/src/widgets/Navbar/index.ts
+++ frontend-next-migration/src/widgets/Navbar/index.ts
@@ -1,5 +1,8 @@
-export { NavbarMainOld as Navbar } from './ui/NavbarMain/NavbarMainOld';
+// export { NavbarMainOld as Navbar } from './ui/NavbarMain/NavbarMainOld';
 export { NavbarClanMain as NavbarClan } from './ui/NavbarMain/NavbarMainOld';
 export { NavbarTeachingMain as NavbarTeaching } from './ui/NavbarMain/NavbarMainOld';
 export { NavbarGameArtMain as NavbarGameArt } from './ui/NavbarMain/NavbarMainOld';
 // export { NavbarMainAsync as Navbar} from "./ui/NavbarMain/NavbarMain.async";
+
+
+export { NavbarMain as Navbar } from './ui/NavbarMain/NavbarMain';
