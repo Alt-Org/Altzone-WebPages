0d396dafe71f46de94abbd2d28ab1c433fdeb97f
Joni Roine <joni.roine@outlook.com>
2024-06-25 09:07:16 +0300

    added eeva's icons and made barrel file

---
+++ frontend-next-migration/src/shared/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import discordIcon from './discordIcon.svg';
+import facebookIcon from './facebookIcon.svg';
+import instagramIcon from './instagramIcon.svg';
+import internetIcon from './internetIcon.svg';
+import qrcodeIcon from './qrcodeIcon.svg';
+import youtubeIcon from './youtubeIcon.svg';
+
+const icons = {
+  discordIcon,
+  facebookIcon,
+  instagramIcon,
+  internetIcon,
+  qrcodeIcon,
+  youtubeIcon,
+};
+
+export default icons;
