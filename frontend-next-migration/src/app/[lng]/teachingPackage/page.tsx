399946b968e64e9f9dc1224466ef0feb89b92ae9
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-06-25 16:27:44 +0300

    Refactor TeachingPackagePage to receive sections from props
    The TeachingPackagePage now receives sections from props instead of declaring them internally. This was done to improve modularity and facilitate content updates. Additionally, we removed the useServerTranslation hook as the new setup allows translated text to be passed directly via props.

--- frontend-next-migration/src/app/[lng]/teachingPackage/page.tsx
+++ frontend-next-migration/src/app/[lng]/teachingPackage/page.tsx
@@ -11,8 +11,6 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
     const { t } = await useServerTranslation(params.lng, 'teachingPackage');
 
-    console.log()
-
     return {
         title: t("head-title"),
         description: t("head-description"),
@@ -22,8 +20,28 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function DefaultPage({ params }: Props){
 
+    const { t } = await useServerTranslation(params.lng, 'teachingPackage');
+
+    const sections =  [
+        { id: 'section1', label: t('demo-title')  , description: t('demo-description')},
+        { id: 'section2', label: t('implementation-title') , description: t('implementation-description')},
+        { id: 'section3', label: 'Mitä Pelitaide on?' },
+        { id: 'section4', label: 'Visuaalinen vaikuttuvuus' },
+        { id: 'section5', label: 'Toiminnallinen vaikuttavuus' },
+        { id: 'section6', label: 'Yhteydet pelin ulkopuolelle' },
+        { id: 'section7', label: 'ALT Zone -verkkopeli' },
+        { id: 'section8', label: 'Erilaisten pelaajien yhteinen liittouma' },
+        { id: 'section9', label: 'Pelihahmot & niiden kehittäminen' },
+        { id: 'section10', label: 'Pelimekaniikka' },
+        { id: 'section11', label: 'Nuorisotyö & pelitaiteen opetus kouluissa' },
+        { id: 'section12', label: 'Pelitaiteen kirjallisuutta' },
+        { id: 'section13', label: 'PRG - Psyche’s Royale Gaming ry' },
+    ]
+
+
     return(
-                // @ts-ignore
-                <TeachingPackagePage lng={params.lng}  />
+                <TeachingPackagePage
+                    sections = {sections}
+                />
             )
 };
