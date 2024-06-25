7e01b30868bec91e2fe0efebd88b339447e5ef72
Joni Roine <joni.roine@outlook.com>
2024-06-25 12:57:56 +0300

    right links for sidebar

--- frontend-next-migration/src/widgets/Navbar/model/data/gameArtSections.ts
+++ frontend-next-migration/src/widgets/Navbar/model/data/gameArtSections.ts
@@ -4,17 +4,13 @@ export interface sections {
 }
 
 export const ArtSections: sections[] = [
-  { id: 'section1', label: 'Esittely' },
-  { id: 'section2', label: 'Toteutus' },
-  { id: 'section3', label: 'Mitä Pelitaide on?' },
-  { id: 'section4', label: 'Visuaalinen vaikuttuvuus' },
-  { id: 'section5', label: 'Toiminnallinen vaikuttavuus' },
-  { id: 'section6', label: 'Yhteydet pelin ulkopuolelle' },
-  { id: 'section7', label: 'ALT Zone -verkkopeli' },
-  { id: 'section8', label: 'Erilaisten pelaajien yhteinen liittouma' },
-  { id: 'section9', label: 'Pelihahmot & niiden kehittäminen' },
-  { id: 'section10', label: 'Pelimekaniikka' },
-  { id: 'section11', label: 'Nuorisotyö & pelitaiteen opetus kouluissa' },
-  { id: 'section12', label: 'Pelitaiteen kirjallisuutta' },
-  { id: 'section13', label: 'PRG - Psyche’s Royale Gaming ry' },
+  { id: 'section1', label: 'Mitä on pelitaide?' },
+  { id: 'section2', label: 'Pelitaiteen pieni infopaketti' },
+  { id: 'section3', label: 'Taiteellinen ilmaisu' },
+  { id: 'section4', label: 'Tarinan kerronta' },
+  { id: 'section5', label: 'Luominen' },
+  { id: 'section6', label: 'Kulttuuri & taide' },
+  { id: 'section7', label: 'Kirjallisuutta' },
+  { id: 'section8', label: 'Lähteet' },
+  { id: 'section9', label: 'PRG - Psyche’s Royale Gaming ry' },
 ];
