e6b3e64756ae6940c4a36bfd56f7446297a48c71
Joni Roine <joni.roine@outlook.com>
2024-06-26 23:07:47 +0300

    add gameart for section maker

--- frontend-next-migration/src/entities/PresentationPackages/model/makeSectionsWithI18n.ts
+++ frontend-next-migration/src/entities/PresentationPackages/model/makeSectionsWithI18n.ts
@@ -1,5 +1,6 @@
-import {PresentationSection} from "@/entities/PresentationPackages/types";
-import {TeachingSections} from "./data/teachingSections";
+import { PresentationSection } from '@/entities/PresentationPackages/types';
+import { TeachingSections } from './data/teachingSections';
+import { ArtGameSections } from './data/artGameSections';
 
 /**
  * Takes an array of PresentationSections and returns a higher-order function that takes a translation function
@@ -9,14 +10,16 @@ import {TeachingSections} from "./data/teachingSections";
  * @returns {Function} A higher-order function that takes a translation function.
  * @return {PresentationSection[]} An array of localized PresentationSections.
  */
-const makeSectionsWithI18n = (sections: PresentationSection[]): ((t: (key: string) => string) => PresentationSection[]) => {
-    return (t: (key: string) => string): PresentationSection[] => {
-        return sections.map(section => ({
-            ...section,
-            label: t(section.label),
-            description: t(section.description)
-        }));
-    };
+const makeSectionsWithI18n = (
+  sections: PresentationSection[],
+): ((t: (key: string) => string) => PresentationSection[]) => {
+  return (t: (key: string) => string): PresentationSection[] => {
+    return sections.map((section) => ({
+      ...section,
+      label: t(section.label),
+      description: t(section.description),
+    }));
+  };
 };
 
 /**
@@ -25,4 +28,14 @@ const makeSectionsWithI18n = (sections: PresentationSection[]): ((t: (key: strin
  * @param {Array} TeachingSections - The array of teaching sections.
  * @returns {Array} - The array of teaching sections with internationalization support.
  */
-export const makeTeachingSectionsWithI18n =  makeSectionsWithI18n(TeachingSections);
+export const makeTeachingSectionsWithI18n =
+  makeSectionsWithI18n(TeachingSections);
+
+/**
+ * Creates art game sections with internationalization support.
+ *
+ * @param {Array} ArtGameSections - The array of art game sections.
+ * @returns {Array} - The array of art game sections with internationalization support.
+ */
+export const makeArtGameSectionsWithI18n =
+  makeSectionsWithI18n(ArtGameSections);
