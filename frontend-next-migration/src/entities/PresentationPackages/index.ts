f3c9eebc7cda5c1033d23afd2283d6d4243b39bb
Joni Roine <joni.roine@outlook.com>
2024-06-26 23:07:08 +0300

    arts for I18n

--- frontend-next-migration/src/entities/PresentationPackages/index.ts
+++ frontend-next-migration/src/entities/PresentationPackages/index.ts
@@ -1,2 +1,5 @@
-export {TeachingSections} from "./model/data/teachingSections";
-export {makeTeachingSectionsWithI18n} from "./model/makeSectionsWithI18n";
+export { TeachingSections } from './model/data/teachingSections';
+export { makeTeachingSectionsWithI18n } from './model/makeSectionsWithI18n';
+
+export { ArtGameSections } from './model/data/artGameSections';
+export { makeArtGameSectionsWithI18n } from './model/makeSectionsWithI18n';
