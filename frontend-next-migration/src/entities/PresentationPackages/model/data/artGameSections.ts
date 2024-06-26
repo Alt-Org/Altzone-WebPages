51fadac7e2507446c307fda0f94a4239e4b831e8
Joni Roine <joni.roine@outlook.com>
2024-06-26 23:05:57 +0300

    sections for arts

--- frontend-next-migration/src/entities/PresentationPackages/model/data/artGameSections.ts
+++ frontend-next-migration/src/entities/PresentationPackages/model/data/artGameSections.ts
@@ -1,6 +1,15 @@
-import {  } from "../../types";
+import { createSection } from '../createSection';
 
-
-export const ArtGameSections = {
-
-}
+const i18nKeyPrefixes = [
+  'info',
+  'artistic',
+  'storytelling',
+  'creation',
+  'culture',
+  'literature',
+  'sources',
+  'prg',
+];
+export const ArtGameSections = i18nKeyPrefixes.map((key, index) =>
+  createSection(key, index),
+);
