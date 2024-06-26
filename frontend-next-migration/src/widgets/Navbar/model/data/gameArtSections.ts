73899a33ff476257c67ceccb7ecb6397b491424a
Joni Roine <joni.roine@outlook.com>
2024-06-26 23:34:08 +0300

    import sections for sidebar

--- frontend-next-migration/src/widgets/Navbar/model/data/gameArtSections.ts
+++ frontend-next-migration/src/widgets/Navbar/model/data/gameArtSections.ts
@@ -1,16 +1,5 @@
-export interface sections {
-  id: string;
-  label: string;
-}
+import { ArtGameSections } from '@/entities/PresentationPackages';
 
-export const ArtSections: sections[] = [
-  { id: 'section1', label: 'Mitä on pelitaide?' },
-  { id: 'section2', label: 'Pelitaiteen pieni infopaketti' },
-  { id: 'section3', label: 'Taiteellinen ilmaisu' },
-  { id: 'section4', label: 'Tarinan kerronta' },
-  { id: 'section5', label: 'Luominen' },
-  { id: 'section6', label: 'Kulttuuri & taide' },
-  { id: 'section7', label: 'Kirjallisuutta' },
-  { id: 'section8', label: 'Lähteet' },
-  { id: 'section9', label: 'PRG - Psyche’s Royale Gaming ry' },
-];
+// todo find a problem.There is some bug with module import order, so without this helper we cannot see the menu
+
+export const ArtGameSectionsPrepared = ArtGameSections;
