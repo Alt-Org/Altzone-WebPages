4b824272a6c9e34de6c075926fcd3751d59ab169
Joni Roine <joni.roine@outlook.com>
2024-06-27 14:47:13 +0300

    export bug re import

--- frontend-next-migration/src/entities/PresentationPackages/index.ts
+++ frontend-next-migration/src/entities/PresentationPackages/index.ts
@@ -1,5 +1,5 @@
-export { TeachingSections } from './model/data/teachingSections';
-export { makeTeachingSectionsWithI18n } from './model/makeSectionsWithI18n';
+export { TeachingSections } from '@/entities/PresentationPackages/model/data/teachingSections';
+export { makeTeachingSectionsWithI18n } from '@/entities/PresentationPackages/model/makeSectionsWithI18n';
 
-export { ArtGameSections } from './model/data/artGameSections';
-export { makeArtGameSectionsWithI18n } from './model/makeSectionsWithI18n';
+export { ArtGameSections } from '@/entities/PresentationPackages/model/data/artGameSections';
+export { makeArtGameSectionsWithI18n } from '@/entities/PresentationPackages/model/makeSectionsWithI18n';
