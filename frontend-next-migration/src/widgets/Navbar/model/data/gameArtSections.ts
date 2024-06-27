d32f6d1c22613367d7377900fd147c7be3a1e30a
Joni Roine <joni.roine@outlook.com>
2024-06-27 14:24:43 +0300

    fixed import order bug

--- frontend-next-migration/src/widgets/Navbar/model/data/gameArtSections.ts
+++
@@ -1,5 +0,0 @@
-import { ArtGameSections } from '@/entities/PresentationPackages';
-
-// todo find a problem.There is some bug with module import order, so without this helper we cannot see the menu
-
-export const ArtGameSectionsPrepared = ArtGameSections;
