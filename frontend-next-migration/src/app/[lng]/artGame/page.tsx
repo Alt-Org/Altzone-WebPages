5ee2323b7dc53f70854ba4d1e3a8f5116c6b65d7
Joni Roine <joni.roine@outlook.com>
2024-07-04 11:29:35 +0300

    gameArt translation & sections fixes

--- frontend-next-migration/src/app/[lng]/artGame/page.tsx
+++ frontend-next-migration/src/app/[lng]/artGame/page.tsx
@@ -1,29 +1,26 @@
-import GameArtPage from "@/preparedPages/GameArtPage/ui/GameArtPage";
-import { useServerTranslation } from "@/shared/i18n";
-import { Metadata } from "next";
+import GameArtPage from '@/preparedPages/GameArtPage/ui/GameArtPage';
+import { useServerTranslation } from '@/shared/i18n';
+import { Metadata } from 'next';
+import { makeArtGameSectionsWithI18n } from '@/entities/PresentationPackages';
 
 type Props = {
-    params: { lng: string }
-}
-
+  params: { lng: string };
+};
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { t } = await useServerTranslation(params.lng, 'artGame');
 
-    const { t } = await useServerTranslation(params.lng, 'artGame');
-
-    return {
-        title: t("head-title"),
-        description: t("head-description"),
-        keywords: t("head-keywords"),
-    }
+  return {
+    title: t('head-title'),
+    description: t('head-description'),
+    keywords: t('head-keywords'),
+  };
 }
 
+export default async function DefaultPage({ params }: Props) {
+  const { t } = await useServerTranslation(params.lng, 'artGame');
 
-export default async function GameArt({ params }: Props) {
-    return (
-        // @ts-ignore
-        <GameArtPage lng={params.lng}
-        />
-    )
+  const sections = makeArtGameSectionsWithI18n(t);
+
+  return <GameArtPage sections={sections} />;
 }
-
