5300fe891ff54e92e6e2826ff011c1676661361f
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-06 14:06:00 +0300

    Refactor MainPage component and improve translation handling
    The MainPage component has been refactored from an async function to a standard function, removing the necessity to use the useServerTranslation hook within this component. The translation functionality is now handled at a higher level in the page component which calls MainPage. The MainPage's properties type (Props) has also been modified and exported to sync with these alterations.

--- frontend-next-migration/src/preparedPages/MainPage/ui/MainPage.async.tsx
+++ frontend-next-migration/src/preparedPages/MainPage/ui/MainPage.async.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import {Props} from "./MainPage";
 
-export const MainPage = dynamic(() => import('./MainPage'));
+export const MainPage = dynamic<Props>(() => import('./MainPage'));
 
