78ada4fa6680cf7eecea6ff0e9b739f0e72eff18
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-06 15:40:28 +0300

    Add optional className prop to Paragraph component
    This commit allows the addition of a custom className to the Paragraph component. A default empty string value is assigned to ensure backward compatibility. This provides more flexibility in styling individual instances of the component.

--- frontend-next-migration/src/shared/ui/Paragraph/ui/Paragraph.tsx
+++ frontend-next-migration/src/shared/ui/Paragraph/ui/Paragraph.tsx
@@ -3,20 +3,22 @@ import cls from "./Paragraph.module.scss";
 
 
 type Props = {
+    className?: string;
     title?: string,
     text?: string,
     footer?: string;
 }
 
 const Paragraph = (props: Props) => {
-    const { 
+    const {
+        className="",
         title, 
         text ,
         footer
     } = props;
 
     return (
-        <div className={classNames(cls.Paragraph)}>
+        <div className={classNames(cls.Paragraph, {}, [className])}>
             {title && <h2>{title}</h2>}
             {text && <p>{text}</p>}
             {footer && <h2>{footer}</h2>}
