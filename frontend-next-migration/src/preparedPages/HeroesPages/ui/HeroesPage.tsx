1f50f243b916f9c7a3bf743ad6414ddb563f257d
Joni Roine <joni.roine@outlook.com>
2024-07-07 18:44:36 +0300

    -remodel herocontainer page -moved unused to assets to shared folder

--- frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroesPage.tsx
+++ frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroesPage.tsx
@@ -1,38 +1,31 @@
-import { FeedbackSideButton } from "@/features/FeedbackByExternalSource";
-import cls from "./HeroPage.module.scss"
-import bgPicture from "@/shared/assets/images/backgrounds/background.webp";
-import {withBackgroundImage} from "@/shared/lib/hocs/withBackgroundImage";
-import { SectionHeroesBlocks } from "@/widgets/SectionHeroesBlocks";
-import { HorizontalLines } from "@/shared/ui/HorizontalLines";
-
-
+import { FeedbackSideButton } from '@/features/FeedbackByExternalSource';
+import cls from './HeroPage.module.scss';
+import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
+import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
+import { SectionHeroesBlocks } from '@/widgets/SectionHeroesBlocks';
+import { HorizontalLines } from '@/shared/ui/HorizontalLines';
 
 const HeroesPage = () => {
+  const sameBg = undefined;
 
-    const sameBg = undefined;
-
-    return (
-        <>
-        <div className={cls.main}>
-            <FeedbackSideButton disableMobile={true} />
-            <SectionHeroesBlocks
-                backgroundImageSrc={sameBg}
-            />
-            <HorizontalLines/>
-            <SectionHeroesBlocks
-                backgroundImageSrc={sameBg}
-            />
-            <HorizontalLines/>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className={cls.main}>
+        <FeedbackSideButton disableMobile={true} />
+        <SectionHeroesBlocks backgroundImageSrc={sameBg} />
+        <HorizontalLines />
+        <SectionHeroesBlocks backgroundImageSrc={sameBg} />
+        <HorizontalLines />
+      </div>
+    </>
+  );
 };
 
 export default withBackgroundImage({
-    alt: "ClassifiedHeroesPage underground style background",
-    imagePath: bgPicture as unknown as string,
-    className: cls.wholePageBG
-    // @ts-ignore
+  alt: 'ClassifiedHeroesPage underground style background',
+  imagePath: bgPicture as unknown as string,
+  className: cls.wholePageBG,
+  // @ts-ignore
 })(HeroesPage);
 
 // export default GameArtPage;
