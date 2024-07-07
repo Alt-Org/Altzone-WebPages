b56ccdcd511cce5e175bccccb03092466339e766
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-07 18:15:11 +0300

    Refactor SectionPlayWithUs component and associated styles
    The SectionPlayWithUs component in the frontend migration has been dramatically restructured. Code related to the rendering of the webGl and the googlePlay button in the component has been simplified and refactored. This includes changes to both the TypeScript and SCSS files. Old, unnecessary code has been removed, and new, cleaner code has replaced it to improve readability and maintainability.

--- frontend-next-migration/src/widgets/SectionPlayWithUs/ui/SectionPlayWithUs.tsx
+++ frontend-next-migration/src/widgets/SectionPlayWithUs/ui/SectionPlayWithUs.tsx
@@ -2,30 +2,42 @@ import cls from "./SectionPlayWithUs.module.scss"
 import Image from "next/image";
 import googlePLay from "@/shared/assets/images/media/googleplay.png";
 import sideImg from "@/shared/assets/images/heros/einstein/professori.webp";
-import { Button, ButtonTheme } from "@/shared/ui/Button/Button";
 import Link from "next/link";
 import { AppExternalLinks } from "@/shared/appLinks/appExternalLinks";
-import { Navs } from "../model/data/navs";
 import {classNames} from "@/shared/lib/classNames/classNames";
-import {NavElement} from "@/widgets/SectionPlayWithUs/ui/NavElement/NavElement";
+import {NavElement, NavItem} from "./NavElement/NavElement";
 
 
 type WebGl = {
     title: string;
     link: string;
 }
+ const Navs: NavItem[] = [
+    {
+        title: 'seeOpenPositions',
+        body: 'comeWith',
+        link: AppExternalLinks.duunitori,
+        isExternal: true
+    },
+    {
+        title: 'becomeATester',
+        body: 'makeTheAppBetter',
+        link: AppExternalLinks.discord,
+        isExternal: true
+    },
+]
 
 
 type Props = {
     webGl: WebGl;
+    googlePLayLink: string;
 }
-
-
 
 const SectionPlayWithUs = (props: Props) => {
 
     const {
-        webGl
+        webGl,
+        googlePLayLink= AppExternalLinks.downloadAndroid
     } = props;
 
     return (
@@ -39,25 +51,22 @@ const SectionPlayWithUs = (props: Props) => {
             <div className={cls.Content}>
                 <div className={cls.ContentWithNav}>
                     <div className={cls.Buttons}>
-                        <Button
-                            key={"webgl"}
-                            withScalableLink
-                            theme={ButtonTheme.Graffiti}
-                            // size={ButtonSize.}
-                            className={cls.BtnGame}
-                        >
-                            {/*<Link target={"_blank"} href={AppExternalLinks.webgl}>*/}
-                            <Link target={"_blank"} href={webGl.link}>
-                                {webGl.title}
-                                {/*{webGlButtonText}*/}
-                            </Link>
-                        </Button>
 
-                        <Link href={AppExternalLinks.downloadAndroid} target={"_blank"}>
+                        <NavElement
+                            className={cls.webGl}
+                            navElem={{
+                            isExternal: true,
+                            title: webGl.title,
+                            link: webGl.link,
+                        }} key={webGl.title}/>
+
+                        <Link href={googlePLayLink} target={"_blank"}>
                             <Image src={googlePLay} alt={"google play button"}
                                    className={cls.BtnDownload}
                             />
                         </Link>
+
+
                     </div>
 
 
