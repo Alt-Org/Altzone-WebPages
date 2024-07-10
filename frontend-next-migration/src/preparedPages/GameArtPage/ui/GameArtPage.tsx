d93b3180b9f513afc6c5d0acb31a9e3e9c4a983d
Joni Roine <joni.roine@outlook.com>
2024-07-10 13:18:06 +0300

    - updated artpage to use wikiContentSidebar widget

--- frontend-next-migration/src/preparedPages/GameArtPage/ui/GameArtPage.tsx
+++ frontend-next-migration/src/preparedPages/GameArtPage/ui/GameArtPage.tsx
@@ -1,19 +1,17 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar } from '@/widgets/Navbar';
 import { Footer } from '@/widgets/Footer';
 import { HorizontalLines } from '@/shared/ui/HorizontalLines';
 import { FeedbackSideButton } from '@/features/FeedbackByExternalSource';
 import cls from './GameArtPage.module.scss';
 import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
-import Image from 'next/image';
 import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
-import NavbarSide from '@/widgets/NavbarSide/ui/NavbarSide';
-import pageLogo from '@/shared/assets/images/teachingPackage/pageLogo.png';
 import useSizes from '@/shared/lib/hooks/useSizes';
 import { Mods } from '@/shared/lib/classNames/classNames';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { ScrollTop } from '@/features/ScrollTop';
+import { WikiContentWithSidebar } from '@/widgets/WikiContentWithSidebar';
 
 interface Section {
   id: string;
@@ -21,6 +19,7 @@ interface Section {
   description: string;
   image: string;
   imageAlt: string;
+  sidebarLogo: string;
 }
 
 export type Props = {
@@ -31,7 +30,6 @@ const GameArtPackagePage = (props: Props) => {
   const { sections = [] } = props;
   const { isMobileSize, isTabletSize, isDesktopSize, isWidescreenSize } =
     useSizes();
-  console.log(sections);
 
   const combinedModCss: Mods = {
     [cls.isMobile]: isMobileSize,
@@ -43,54 +41,7 @@ const GameArtPackagePage = (props: Props) => {
   return (
     <div className={classNames(cls.pageContainer, combinedModCss)}>
       <Navbar overlaid={true} navBarType={'GameArt'} />
-      <div className={classNames(cls.mainContent, combinedModCss)}>
-        {!isMobileSize && (
-          <div className={classNames(cls.navbarSide, combinedModCss)}>
-            <div className={cls.pageLogo}>
-              <Image
-                src={pageLogo}
-                className={cls.backgroundImage}
-                alt='pageLogo'
-                height={600}
-                width={600}
-              />
-            </div>
-            <NavbarSide sections={sections} />
-          </div>
-        )}
-        <div className={classNames(cls.content, combinedModCss)} id='content'>
-          {sections.length > 0 ? (
-            sections.map((section) => {
-              const [imageError, setImageError] = useState(false);
-
-              return (
-                <div id={section.id} key={section.id} className={cls.section}>
-                  <h2>{section.label}</h2>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: section.description,
-                    }}></p>
-                  {section.image && !imageError && (
-                    <div
-                      className={classNames(cls.contentImage, combinedModCss)}>
-                      <Image
-                        src={section.image}
-                        className={cls.sectionImage}
-                        alt={section.imageAlt}
-                        height={300}
-                        width={600}
-                        onError={() => setImageError(true)}
-                      />
-                    </div>
-                  )}
-                </div>
-              );
-            })
-          ) : (
-            <p>No sections available.</p>
-          )}
-        </div>
-      </div>
+      <WikiContentWithSidebar sections={sections} sidebarLogo={''} />
       <div>
         <FeedbackSideButton />
         <HorizontalLines />
