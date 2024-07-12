001b128713a9e146eaecf31aed82c88d2b6f71d3
Joni Roine <joni.roine@outlook.com>
2024-07-12 16:27:04 +0300

    added privacy and cookies link for footer section

--- frontend-next-migration/src/widgets/Footer/ui/Rights/Rights.tsx
+++ frontend-next-migration/src/widgets/Footer/ui/Rights/Rights.tsx
@@ -1,19 +1,30 @@
-import {classNames} from "@/shared/lib/classNames/classNames";
-import cls from './Rights.module.scss'
-import {CurrentYear, CompanyName} from "../../model/data/text";
-import {memo} from "react";
+import { memo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './Rights.module.scss';
+import { AppExternalLinks } from '@/shared/appLinks/appExternalLinks';
+import {
+  CurrentYear,
+  CompanyName,
+  Cookies,
+  Privacy,
+} from '../../model/data/text';
 
-
-interface TitleProps{
-    className?: string;
+interface RightsProps {
+  className?: string;
 }
 
-export const Rights = memo(({className=''}:TitleProps)=>{
-    return (
-        <p className={classNames(cls.Rights,{},[className])}>
-            <span className={cls.copySymbol}>&copy;</span> {CurrentYear} {CompanyName}
-        </p>
-    )
-})
+export const Rights = memo(({ className = '' }: RightsProps) => {
+  return (
+    <p className={classNames(cls.Rights, {}, [className])}>
+      <span className={cls.copySymbol}>&copy;</span> {CurrentYear} {CompanyName}{' '}
+      <a href={AppExternalLinks.cookies} className={cls.link}>
+        {Cookies}
+      </a>{' '}
+      <a href={AppExternalLinks.privacy} className={cls.link}>
+        {Privacy}
+      </a>
+    </p>
+  );
+});
 
-Rights.displayName = "Footer-Rights";
+Rights.displayName = 'Footer-Rights';
