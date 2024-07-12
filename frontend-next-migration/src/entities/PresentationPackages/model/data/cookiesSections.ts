391b9a07333b4a60d6ddad42e7540b6878478569
Joni Roine <joni.roine@outlook.com>
2024-07-12 14:14:56 +0300

    deleted routes for logo images for better scrolling

--- frontend-next-migration/src/entities/PresentationPackages/model/data/cookiesSections.ts
+++ frontend-next-migration/src/entities/PresentationPackages/model/data/cookiesSections.ts
@@ -16,7 +16,7 @@ const i18nKeyPrefixes = [
 // next takes path : \public\images\
 const imageSrcs = [''];
 
-const logoSrc = '/images/cookies/cookies.webp';
+const logoSrc = '';
 
 export const CookiesSections = i18nKeyPrefixes.map((key, index) =>
   createSection(
