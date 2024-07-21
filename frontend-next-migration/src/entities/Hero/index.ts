fb14ab3ee9a6baec7bf5fde8df73c95235620ff3
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-21 14:50:15 +0300

    Update HeroCard import path
    The import path for HeroCard in the Hero entity index file has been corrected. This change addresses a faulty import statement, ensuring that the file path correctly leads to the HeroCard component.

--- frontend-next-migration/src/entities/Hero/index.ts
+++ frontend-next-migration/src/entities/Hero/index.ts
@@ -1,4 +1,4 @@
-export { HeroCard } from './ui/HeroCard';
+export { HeroCard } from './ui/HeroCard/HeroCard';
 export { default as HeroContainer } from './ui/HeroContainerV3/HeroContainer';
 
 // export {default as HeroContainer} from "./deprecated/ui/HeroContainerV1/HeroContainer.tsx";
