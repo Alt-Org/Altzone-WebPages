941b0e9976c2b0d0c284dc6b38014d88e9b0aa4a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-21 16:58:26 +0300

    Remove deprecated NavbarTeachingMain export
    The unused export `NavbarTeachingMain` from the deprecated ui `NavbarMainOld` has been removed to streamline the code. This change should not impact functionalities as it was not in use prior to this commit.

--- frontend-next-migration/src/widgets/Navbar/index.ts
+++ frontend-next-migration/src/widgets/Navbar/index.ts
@@ -1,6 +1,5 @@
 // export { NavbarMainOld as Navbar } from './ui/NavbarMain/NavbarMainOld';
 export { NavbarClanMain as NavbarClan } from './~deprecated/ui/NavbarMainOld';
-export { NavbarTeachingMain as NavbarTeaching } from './~deprecated/ui/NavbarMainOld';
 export { NavbarGameArtMain as NavbarGameArt } from './~deprecated/ui/NavbarMainOld';
 // export { NavbarMainAsync as Navbar} from "./ui/NavbarMain/NavbarMain.async";
 
