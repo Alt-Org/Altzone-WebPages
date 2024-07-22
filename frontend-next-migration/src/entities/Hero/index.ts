e9c9a2c89ba903479c4b0e198a9a2c6a6b61c769
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-22 11:10:03 +0300

    Update HeroContainer import path
    Switched from a deprecated HeroContainer version to use the latest one. The 'HeroContainerV

--- frontend-next-migration/src/entities/Hero/index.ts
+++ frontend-next-migration/src/entities/Hero/index.ts
@@ -1,8 +1,8 @@
 export { HeroCard } from './ui/HeroCard/HeroCard';
 
 
-// export { default as HeroContainer } from './ui/HeroContainerV4/HeroContainer';
-export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV3/HeroContainer";
+export { default as HeroContainer } from './ui/HeroContainerV4/HeroContainer';
+// export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV3/HeroContainer";
 // export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV1/HeroContainer";
 // export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV2/HeroContainer";
 
