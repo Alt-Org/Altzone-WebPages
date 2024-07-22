f564fe4cc393e17e2229839a0fab1bee5676547a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-22 11:10:08 +0300

    Reorganize imports in ThemeSwitcher.tsx
    The import statement in the ThemeSwitcher.tsx file has been rearranged. The Theme and useTheme functions from ThemeProvider have

--- frontend-next-migration/src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ frontend-next-migration/src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,8 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
-import { Theme, useTheme } from "@/preparedApp/providers/ThemeProvider";
 import LightIcon from "@/shared/assets/icons/theme-light.svg";
 import DarkIcon from "@/shared/assets/icons/theme-dark.svg";
 import { Button, ButtonTheme } from "@/shared/ui/Button/Button";
+import {useTheme,Theme} from "@/app/_providers/ThemeProvider";
 
 interface ThemeSwitcherProps {
     className?: string;
