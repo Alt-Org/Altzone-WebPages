24c0755e4d6c5119392a8ec6179d3243ab7cbeb6
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-07-22 11:12:38 +0300

    Remove 'TeachingPackage' case from navBarType switch
    The changes made in this commit involve removing the 'TeachingPackage' case from the switch statement in the navBarType function in the 'defineNs.ts' file. This specific case is no longer needed inside the Navbar model.

--- frontend-next-migration/src/widgets/Navbar/model/defineNs.ts
+++ frontend-next-migration/src/widgets/Navbar/model/defineNs.ts
@@ -4,8 +4,6 @@ export const defineNs = (navBarType: NavBarType) => {
   switch (navBarType) {
     case 'GameArt':
       return 'artGame';
-    case 'TeachingPackage':
-      return 'teachingPackage';
     case 'Clan':
       return 'clan';
     case 'Cookies':
