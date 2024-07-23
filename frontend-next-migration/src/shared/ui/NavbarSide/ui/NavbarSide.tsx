10532f9b881367ee1d57cc1ca50aa3c126b6851e
Joni Roine <joni.roine@outlook.com>
2024-07-23 15:10:23 +0300

    fixing problem with first section not activating

--- frontend-next-migration/src/shared/ui/NavbarSide/ui/NavbarSide.tsx
+++ frontend-next-migration/src/shared/ui/NavbarSide/ui/NavbarSide.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import cls from './NavbarSide.module.scss';
 
 interface Section {
@@ -9,11 +9,15 @@ interface Section {
 
 interface NavbarSideProps {
   sections: Section[];
+  activeSection: string;
+  setActiveSection: (id: string) => void;
 }
 
-const NavbarSide: React.FC<NavbarSideProps> = ({ sections }) => {
-  const [activeSection, setActiveSection] = useState<string>('');
-
+const NavbarSide: React.FC<NavbarSideProps> = ({
+  sections,
+  activeSection,
+  setActiveSection,
+}) => {
   useEffect(() => {
     const handleScroll = () => {
       const sectionOffsets = sections.map((section) => {
@@ -21,40 +25,54 @@ const NavbarSide: React.FC<NavbarSideProps> = ({ sections }) => {
         return { id: section.id, offsetTop: element ? element.offsetTop : 0 };
       });
 
-      const currentScrollPosition = window.scrollY + window.innerHeight / 2;
+      const currentScrollPosition = window.scrollY + window.innerHeight / 2; // Center of the viewport
 
+      // Determine the current section based on the scroll position
       const currentSection = sectionOffsets.find((section, index) => {
         const nextSection = sectionOffsets[index + 1];
+        if (
+          index === 0 &&
+          currentScrollPosition < (sectionOffsets[1]?.offsetTop || Infinity)
+        ) {
+          // Special case for the first section
+          return currentScrollPosition >= section.offsetTop;
+        }
         return (
           currentScrollPosition >= section.offsetTop &&
           (!nextSection || currentScrollPosition < nextSection.offsetTop)
         );
       });
 
-      if (currentSection && currentSection.id !== activeSection) {
+      const firstSection = sectionOffsets[0];
+      if (
+        currentScrollPosition <
+        firstSection.offsetTop + window.innerHeight / 2
+      ) {
+        if (firstSection.id !== activeSection) {
+          setActiveSection(firstSection.id);
+        }
+      } else if (currentSection && currentSection.id !== activeSection) {
         setActiveSection(currentSection.id);
       }
     };
 
     window.addEventListener('scroll', handleScroll);
-
-    // Initial check to set the active section when the component mounts
     handleScroll();
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [sections, activeSection]);
+  }, [sections, activeSection, setActiveSection]);
 
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
     if (element) {
-      const offsetPosition = element.offsetTop - 50; // offset up
+      const offsetPosition = element.offsetTop - 10; // offset up
       window.scrollTo({
         top: offsetPosition,
         behavior: 'smooth',
       });
-      setActiveSection(id); // Update the active section
+      setActiveSection(id);
     }
   };
 
