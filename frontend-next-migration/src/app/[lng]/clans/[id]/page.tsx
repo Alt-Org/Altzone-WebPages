ee936d14457f91528d8adf1e5e17ae98b71516b0
Skoivumaki <sakari.koivumaki@gmail.com>
2024-08-03 17:49:18 +0300

    update

--- frontend-next-migration/src/app/[lng]/clans/[id]/page.tsx
+++ frontend-next-migration/src/app/[lng]/clans/[id]/page.tsx
@@ -1,2 +1,27 @@
-"use client"
-export {ClanRoomSubPage as default} from "@/preparedPages/ClanPages";
+import { ClanRoomSubPage as PreparedPage } from "@/preparedPages/ClanPages";
+import { useServerTranslation } from "@/shared/i18n";
+import { Metadata } from "next";
+
+type Props = {
+    params: { lng: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+
+    const { t } = await useServerTranslation(params.lng, 'clan');
+
+    return {
+        title: t("head-title"),
+        description: t("head-description"),
+        keywords: t("head-keywords"),
+    }
+}
+
+export default async function ClanRoomSubPage({ params }: Props) {
+
+    return (
+        <PreparedPage
+        />
+    );
+
+}
