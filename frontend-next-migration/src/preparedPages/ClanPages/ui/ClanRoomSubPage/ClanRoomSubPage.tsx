f41dcd71eb156889fdd46cf5c0e248974148cd4f
Skoivumaki <sakari.koivumaki@gmail.com>
2024-08-04 11:42:56 +0300

    clan admins now shown on memberlist

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanRoomSubPage/ClanRoomSubPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanRoomSubPage/ClanRoomSubPage.tsx
@@ -15,6 +15,7 @@ import { envHelper } from "@/shared/const/envHelper";
 import { RoutePaths } from "@/shared/appLinks/RoutePaths";
 import { toast } from "react-toastify";
 import { useClientTranslation } from "@/shared/i18n";
+import useIsMobileSize from "@/shared/lib/hooks/useIsMobileSize";
 
 const ClanRoomSubPage = () => {
     // @ts-ignore
@@ -22,6 +23,7 @@ const ClanRoomSubPage = () => {
     const user = useSelector(selectProfile);
     const lng2 = lng as string;
     const { t } = useClientTranslation(lng2, "clan");
+    const { isMobileSize } = useIsMobileSize();
 
     const playerId = user?.Player._id;
     const [isAdmin, setIsAdmin] = useState(false);
@@ -30,6 +32,8 @@ const ClanRoomSubPage = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const { data: clan, error, isLoading } = useGetClanByIdWithPlayersQuery(id as string);
+
+    const adminIds = clan?.data?.Clan?.admin_ids || [];
     const players = clan?.data.Clan.Player || [];
 
     useEffect(() => {
@@ -39,7 +43,7 @@ const ClanRoomSubPage = () => {
         if (playerId) {
             setLoggedIn(true);
 
-            if (clan?.data.Clan.admin_ids.includes(playerId)) {
+            if (clan?.data?.Clan?.admin_ids.includes(playerId)) {
                 setIsAdmin(true);
             }
 
@@ -50,122 +54,120 @@ const ClanRoomSubPage = () => {
             setLoggedIn(false);
             setIsInClan(false);
         }
-    }, [isLoading, clan?.data.Clan.admin_ids, playerId, clan?.data.Clan.Player, clan?.data.Clan.isOpen]);
+    }, [isLoading, clan?.data?.Clan?.admin_ids, playerId, clan?.data.Clan.Player, clan?.data.Clan.isOpen]);
 
     const { handleDelete } = useDeleteClan();
 
-    return (
-        <>
-            {
-                isLoading ? (
-                    <Loader className={cls.Loader} />
-                )
-                    :
-                    error
-                        ?
-                        (
-                            <div>Error: {JSON.stringify(error)}</div>
-                        )
-                        :
-                        (
-                            <>
+    if (isLoading) return <Loader className={cls.Loader} />
 
-                                <div className={cls.parent}>
-                                    <div className={cls.div1}>
-                                        <Image
-                                            src={clanLogo}
-                                            alt={"clan logo"}
-                                            className={cls.clanLogo} />
-                                    </div>
-                                    <div className={cls.div2}>
-                                        <span className={cls.clanName}>{clan?.data?.Clan?.name}</span>
-                                        <a className={cls.number} href="/leaderboardjne">♛12</a>
-                                    </div>
-                                    <div className={cls.div3}>{t("clan_list")}
-                                    </div>
-                                    <div className={cls.div4}>
-                                        <Image
-                                            src={clanLogo}
-                                            alt={"clan logo"}
-                                            className={cls.clanLogo} />
-                                        (sielunkoti kuva)
-                                    </div>
-                                    <div className={cls.div5}>
-                                        {!isLoggedIn ? (
-                                            <Button
-                                                className={cls.JoinClanBtn}
-                                                theme={ButtonTheme.Graffiti}
-                                                size={ButtonSize.L}
-                                                onClick={() => toast.error(t("toast_notloggedin"))} >
-                                                {t("join_clan_btn")}
-                                            </Button>
-                                        ) : isLoggedIn && !isInClan ? (
-                                            <Button
-                                                className={cls.JoinClanBtn}
-                                                theme={ButtonTheme.Graffiti}
-                                                size={ButtonSize.L}
-                                                onClick={() => toast.success(t("toast_join_success"))} >
-                                                {t("join_clan_btn")}
-                                            </Button>
-                                        ) : isLoggedIn && !isInClan && !isOpen ? (
-                                            <Button
-                                                className={cls.JoinClanBtn}
-                                                theme={ButtonTheme.Graffiti}
-                                                size={ButtonSize.L}
-                                                onClick={() => toast.error('Clan is not open')} >
-                                                {t("join_clan_btn")}
-                                            </Button>
-                                        ) : (
-                                            <Button
-                                                className={cls.DeleteClanBtn}
-                                                theme={ButtonTheme.Graffiti}
-                                                size={ButtonSize.L}
-                                                onClick={() => toast.success('You left the Clan')} >
-                                                {t("leave_clan_btn")}
-                                            </Button>
-                                        )
-                                        }
-                                        {isAdmin && (
-                                            <Button
-                                                className={cls.EditClanBtn}
-                                                theme={ButtonTheme.Graffiti}
-                                                size={ButtonSize.L}
-                                                onClick={() => toast.success('Editing mode')} >
-                                                {t("edit_clan_btn")}
-                                            </Button>
-                                        )
-                                        }
-                                    </div>
-                                    <div className={cls.div6}>{t("member_list")}
-                                        <div className={cls.membersList}>
-                                            {players.map(player => (
-                                                <div key={player._id} className={cls.memberItem}>
-                                                    {player.name}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </div>
-                                    <div className={cls.div7}>
-                                        {t("motto")}
-                                        <p className={cls.mottoText}>Unite, Conquer, Prevail!</p>
-                                    </div>
-                                    <div className={cls.div8}>{t("info")}
-                                        < div className={cls.clanInfo} >
-                                            <p className={cls.infoItem}>{t("assets")}: {clan?.data?.Clan?.gameCoins}</p>
-                                            <p className={cls.infoItem}>{t("members_count")}: {players.length}/10</p>
-                                            <p className={cls.infoItem}>{t("language")}: {clan?.data?.Clan?.name}</p>
-                                            <p className={cls.infoItem}>{t("goal")}: {clan?.data?.Clan?.name}</p>
-                                            <p className={cls.infoItem}>{t("age_limit")}: {clan?.data?.Clan?.name}</p>
-                                            <p className={cls.infoItem}>{t("wins")}: {clan?.data?.Clan?.name}</p>
-                                            <p className={cls.infoItem}>{t("losses")}: {clan?.data?.Clan?.name}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </>
-                        )
-            }
+    if (error) {
+        return (
+            <>
+                <h1 style={{ textAlign: "center", marginBottom: "20px" }}>KLAANIT</h1>
+                {isMobileSize}
+                <h2 style={{ textAlign: "left", marginBottom: "20px" }}>No results</h2>
+            </>
+        );
+    }
+
+    if (clan) {
+        return (<>
+
+
+            <div className={cls.parent}>
+                <div className={cls.div1}>
+                    <Image
+                        src={clanLogo}
+                        alt={"clan logo"}
+                        className={cls.clanLogo} />
+                </div>
+                <div className={cls.div2}>
+                    <span className={cls.clanName}>{clan?.data?.Clan?.name}</span>
+                    <a className={cls.number} href="/leaderboardjne">♛12</a>
+                </div>
+                <div className={cls.div3}>{t("clan_list")}
+                </div>
+                <div className={cls.div4}>
+                    <Image
+                        src={clanLogo}
+                        alt={"clan logo"}
+                        className={cls.clanLogo} />
+                    (sielunkoti kuva)
+                </div>
+                <div className={cls.div5}>
+                    {!isLoggedIn ? (
+                        <Button
+                            className={cls.JoinClanBtn}
+                            theme={ButtonTheme.Graffiti}
+                            size={ButtonSize.L}
+                            onClick={() => toast.error(t("toast_notloggedin"))} >
+                            {t("join_clan_btn")}
+                        </Button>
+                    ) : isLoggedIn && !isInClan ? (
+                        <Button
+                            className={cls.JoinClanBtn}
+                            theme={ButtonTheme.Graffiti}
+                            size={ButtonSize.L}
+                            onClick={() => toast.success(t("toast_join_success"))} >
+                            {t("join_clan_btn")}
+                        </Button>
+                    ) : isLoggedIn && !isInClan && !isOpen ? (
+                        <Button
+                            className={cls.JoinClanBtn}
+                            theme={ButtonTheme.Graffiti}
+                            size={ButtonSize.L}
+                            onClick={() => toast.error('Clan is not open')} >
+                            {t("join_clan_btn")}
+                        </Button>
+                    ) : (
+                        <Button
+                            className={cls.DeleteClanBtn}
+                            theme={ButtonTheme.Graffiti}
+                            size={ButtonSize.L}
+                            onClick={() => toast.success('You left the Clan')} >
+                            {t("leave_clan_btn")}
+                        </Button>
+                    )
+                    }
+                    {isAdmin && (
+                        <Button
+                            className={cls.EditClanBtn}
+                            theme={ButtonTheme.Graffiti}
+                            size={ButtonSize.L}
+                            onClick={() => toast.success('Editing mode')} >
+                            {t("edit_clan_btn")}
+                        </Button>
+                    )
+                    }
+                </div>
+                <div className={cls.div6}>{t("member_list")}
+                    <div className={cls.membersList}>
+                        {players.map(player => (
+                            <div key={player._id} className={adminIds.includes(player._id) ? cls.adminItem : cls.memberItem}>
+                                {player.name}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={cls.div7}>
+                    {t("motto")}
+                    <p className={cls.mottoText}>Unite, Conquer, Prevail!</p>
+                </div>
+                <div className={cls.div8}>{t("info")}
+                    < div className={cls.clanInfo} >
+                        <p className={cls.infoItem}>{t("assets")}: {clan?.data?.Clan?.gameCoins}</p>
+                        <p className={cls.infoItem}>{t("members_count")}: {players.length}/10</p>
+                        <p className={cls.infoItem}>{t("language")}: {clan?.data?.Clan?.name}</p>
+                        <p className={cls.infoItem}>{t("goal")}: {clan?.data?.Clan?.name}</p>
+                        <p className={cls.infoItem}>{t("age_limit")}: {clan?.data?.Clan?.name}</p>
+                        <p className={cls.infoItem}>{t("wins")}: {clan?.data?.Clan?.name}</p>
+                        <p className={cls.infoItem}>{t("losses")}: {clan?.data?.Clan?.name}</p>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+        )
+    }
 }
 
 export default ClanRoomSubPage;
