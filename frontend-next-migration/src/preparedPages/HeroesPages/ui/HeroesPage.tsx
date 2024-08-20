172e7d8b44545fc718ca2713e74d6001014ea2bd
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-20 10:52:28 +0300

    Refactor HeroesPage structure and layout
    Converted container from `div` to `main`, ensuring semantic HTML usage. Removed redundant imports, extracted CSS into a dedicated SCSS file, and disabled duplicate components for cleaner layout and maintainability.

--- frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroesPage.tsx
+++ frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroesPage.tsx
@@ -1,21 +1,21 @@
+import { SectionHeroesBlocks } from '@/widgets/SectionHeroesBlocks';
 import { FeedbackSideButton } from '@/features/FeedbackByExternalSource';
-import cls from './HeroPage.module.scss';
 import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
 import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
-import { SectionHeroesBlocks } from '@/widgets/SectionHeroesBlocks';
 import { HorizontalLines } from '@/shared/ui/HorizontalLines';
+
+import cls from './HeroesPage.module.scss';
 
 const HeroesPage = () => {
   const sameBg = undefined;
 
   return (
-      <div className={cls.main}>
+      <main className={cls.main}>
         <FeedbackSideButton disableMobile={true} />
         <SectionHeroesBlocks backgroundImageSrc={sameBg} />
-        <HorizontalLines />
-        <SectionHeroesBlocks backgroundImageSrc={sameBg} />
-        <HorizontalLines />
-      </div>
+        {/*<HorizontalLines />*/}
+        {/*<SectionHeroesBlocks backgroundImageSrc={sameBg} />*/}
+      </main>
   );
 };
 
