23c704f64c44884aea3a72a19d4737b1b4c85845
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 19:19:31 +0300

    Add JSDoc comments to ParsedPostBody component
    Enhanced the ParsedPostBody component with JSDoc comments to improve code documentation and readability. The added comments provide information about the prop types and usage examples for better developer understanding.

--- frontend-next-migration/src/shared/ui/Post/ui/ParsedPostBody/ParsedPostBody.tsx
+++ frontend-next-migration/src/shared/ui/Post/ui/ParsedPostBody/ParsedPostBody.tsx
@@ -1,12 +1,26 @@
-import { FC, ReactNode } from 'react';
-import { IPostBodyElement } from "../../model/types";
+import {FC, ReactNode} from 'react';
+import {IPostBodyElement} from "../../model/types";
 import Image from "next/image";
 
 interface ParsedPostBodyProps {
     jsonData: IPostBodyElement[];
 }
 
-export const ParsedPostBody: FC<ParsedPostBodyProps> = ({ jsonData }) => {
+/**
+ * Component to parse and render JSON data for post body elements.
+ *
+ * @param props - The properties for the ParsedPostBody component.
+ * @param props.jsonData - The JSON data to be parsed and rendered.
+ *
+ * @example
+ * const jsonData = [
+ *   { type: 'text', content: 'This is a text element.' },
+ *   { type: 'image', url: '/path/to/image.jpg', alt: 'An image', widthPx: 500, heightPx: 300 }
+ * ];
+ *
+ * <ParsedPostBody jsonData={jsonData} />
+ */
+export const ParsedPostBody: FC<ParsedPostBodyProps> = ({jsonData}) => {
     const parseJSON = (json: IPostBodyElement[]): ReactNode[] => {
         return json.map((item, index) => {
             switch (item.type) {
@@ -34,13 +48,7 @@ export const ParsedPostBody: FC<ParsedPostBodyProps> = ({ jsonData }) => {
                         `<a href="${match}" target="_blank" rel="noopener noreferrer"><b>${match}</b></a>`
                     ));
 
-
-                    // const boldRegex = /\*\*(.*?)\*\*/g;
-                    // const contentWithBold = item.content.replace(boldRegex, (match, p1) => (
-                    //     `<b>${p1}</b>`
-                    // ));
-                    // return <p key={index} dangerouslySetInnerHTML={{ __html: contentWithBold }} />;
-                    return <div key={index} dangerouslySetInnerHTML={{__html: content}} />;
+                    return <div key={index} dangerouslySetInnerHTML={{__html: content}}/>;
                 default:
                     return null;
             }
