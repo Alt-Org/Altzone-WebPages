f470418c83660158b627a73a5ee0d28d3c8bf425
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 19:20:56 +0300

    Add JSDoc comments to SidebarItem component
    Enhanced the SidebarItem component with JSDoc comments to improve code documentation. This includes descriptions for props and examples for rendering different types of sidebar items.

--- frontend-next-migration/src/shared/ui/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ frontend-next-migration/src/shared/ui/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -5,19 +5,45 @@ import cls from './SidebarItem.module.scss';
 import {ISidebarItem, sidebarItemType} from '../../model/items';
 import {DropdownWrapper} from "@/shared/ui/DropdownWrapper";
 
+
 interface SidebarItemProps {
     item: ISidebarItem;
     collapsed: boolean;
 }
 
-export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
+/**
+ * Renders a sidebar item, which can either be a basic item or a dropdown.
+ *
+ * @param {SidebarItemProps} props - The props for the component.
+ * @param {ISidebarItem} props.item - The item to display in the sidebar.
+ * @param {boolean} props.collapsed - Whether the sidebar is collapsed or not.
+ *
+ * @example
+ * // Rendering a basic sidebar item:
+ * <SidebarItem
+ *    item={{ type: sidebarItemType.ISidebarItemBasic, path: '/home', name: 'Home' }}
+ *    collapsed={false}
+ * />
+ *
+ * @example
+ * // Rendering a dropdown sidebar item:
+ * <SidebarItem
+ *    item={{
+ *        type: sidebarItemType.ISidebarItemDropDown,
+ *        name: 'Settings',
+ *        elements: [{ elementText: 'Profile', link: {path: '/profile'} }, { elementText: 'Logout', onClickCallback: handleLogout }]
+ *    }}
+ *    collapsed={true}
+ * />
+ */
+export const SidebarItem = memo(({item, collapsed}: SidebarItemProps) => {
 
     if (item.type === sidebarItemType.ISidebarItemBasic) {
         return (
             <AppLink
                 theme={AppLinkTheme.PRIMARY}
                 to={item.path}
-                className={classNames(cls.item, { [cls.collapsed]: collapsed }, [cls.itemBasic])}
+                className={classNames(cls.item, {[cls.collapsed]: collapsed}, [cls.itemBasic])}
             >
                 <span className={cls.link}>{item.name}</span>
             </AppLink>
@@ -28,7 +54,7 @@ export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
         return (
             <DropdownWrapper className={cls.itemDropDown}
                              contentItemClassName={cls.item}
-                             contentClassName={classNames(cls.link, { [cls.collapsed]: collapsed })}
+                             contentClassName={classNames(cls.link, {[cls.collapsed]: collapsed})}
                              elements={item.elements}>
                 {item.name}
             </DropdownWrapper>
