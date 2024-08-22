62b8559bdb9204ed165242944b4c9cf7baf0287a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 18:20:40 +0300

    Add documentation to useSizes hook
    This commit introduces JSDoc style comments to the `useSizes` hook, providing detailed explanations of its functionality and return values. Additionally, these comments include an example usage for better clarity.

--- frontend-next-migration/src/shared/lib/hooks/useSizes.tsx
+++ frontend-next-migration/src/shared/lib/hooks/useSizes.tsx
@@ -1,6 +1,25 @@
 'use client';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
+/**
+ * Custom hook to check and monitor window size changes.
+ *
+ * The hook returns an object with boolean values indicating if the window
+ * size falls within certain ranges (e.g., mobile, tablet, desktop, widescreen).
+ *
+ * @returns - Object containing the following properties:
+ *
+ * - isMobileSize: boolean - True if window width is <= 768px.
+ * - isTabletSize: boolean - True if window width is >= 768px and < 1024px.
+ * - isDesktopSize: boolean - True if window width is >= 1024px and < 1440px.
+ * - isWidescreenSize: boolean - True if window width is >= 1440px.
+ *
+ * @example
+ * const { isMobileSize, isTabletSize, isDesktopSize, isWidescreenSize } = useSizes();
+ * if(isMobileSize) {
+ *   // Perform actions related to mobile size.
+ * }
+ */
 const useSizes = () => {
   const checkSizes = () => {
     const width = window.innerWidth;
@@ -33,7 +52,8 @@ const useSizes = () => {
     };
   }, []);
 
-  useEffect(() => {}, [sizes]);
+  useEffect(() => {
+  }, [sizes]);
 
   return sizes;
 };
