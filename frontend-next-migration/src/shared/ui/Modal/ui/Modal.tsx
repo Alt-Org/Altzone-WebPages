c548514258b6999ca865c7664d8a85e924c92371
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 19:08:43 +0300

    Add JSDoc comments to Modal component
    Enhanced the documentation for the Modal component by adding JSDoc comments. This provides detailed descriptions of the props and an example of how to use the component.

--- frontend-next-migration/src/shared/ui/Modal/ui/Modal.tsx
+++ frontend-next-migration/src/shared/ui/Modal/ui/Modal.tsx
@@ -3,14 +3,28 @@ import {ReactNode} from "react";
 import cls from "./Modal.module.scss"
 
 type Props = {
-    isOpen : boolean,
-    onRequestClose? : () => void;
-    shouldCloseOnExternal? : boolean,
-    children : ReactNode;
+    isOpen: boolean,
+    onRequestClose?: () => void;
+    shouldCloseOnExternal?: boolean,
+    children: ReactNode;
 }
 
 ReactModal.setAppElement('#root');
 
+/**
+ * Modal component that uses ReactModal under the hood.
+ *
+ * @param {Object} props - The properties object.
+ * @param {boolean} props.isOpen - Determines if the modal is open or closed.
+ * @param {() => void} [props.onRequestClose] - Function to be called when the modal requests to be closed.
+ * @param {boolean} [props.shouldCloseOnExternal=true] - Determines if the modal should close when clicking outside of it or pressing the ESC key.
+ * @param {ReactNode} props.children - Content to be rendered inside the modal.
+ *
+ * @example
+ * <Modal isOpen={true} onRequestClose={() => console.log('Closed')} shouldCloseOnExternal={true}>
+ *   <div>Your content here</div>
+ * </Modal>
+ */
 export const Modal = (
     {
         isOpen,
