c7378f11ed146bfe7385e47bb97a3eabf1ee7089
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 17:57:29 +0300

    Add JSDoc comments to addEventListeners function
    This commit adds descriptive JSDoc comments to the addEventListeners function to clarify its purpose and usage. The comments detail the expected parameters, including eventNames and handler, and specify the function's return type as void.

--- frontend-next-migration/src/shared/lib/eventListeners/addEventListeners.ts
+++ frontend-next-migration/src/shared/lib/eventListeners/addEventListeners.ts
@@ -1,3 +1,10 @@
+/**
+ * Attaches event listeners to the window object.
+ *
+ * @param {Array<keyof WindowEventMap>} eventNames - The names of the events to listen for.
+ * @param {EventListenerOrEventListenerObject} handler - The event handler function or object.
+ * @return {void}
+ */
 export const addEventListeners = (eventNames: Array<keyof WindowEventMap>, handler: EventListenerOrEventListenerObject) => {
     eventNames.forEach(eventName => {
         window.addEventListener(eventName, handler);
