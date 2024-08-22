1d167fe5e5329be954fe82e8b8a3f035af8759e3
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 16:28:29 +0300

    Refactor ProfilePage layout and improve styling
    Consolidate redundant class names into a streamlined container setup. Replace nested divs with semantic HTML elements for improved readability and change CSS grid implementation to a flexible layout using "gap" and "place-content".

--- frontend-next-migration/src/preparedPages/ProfilePage/ui/ProfilePage.tsx
+++ frontend-next-migration/src/preparedPages/ProfilePage/ui/ProfilePage.tsx
@@ -1,6 +1,6 @@
+// // //import { useDeleteProfile } from "@/features/DeleteProfile";
+// // import { Button, ButtonSize, ButtonTheme } from "@/shared/ui/Button";
 import cls from "./ProfilePage.module.scss";
-// //import { useDeleteProfile } from "@/features/DeleteProfile";
-// import { Button, ButtonSize, ButtonTheme } from "@/shared/ui/Button";
 
 type Props = {
     title: string;
@@ -8,36 +8,18 @@ type Props = {
     profileDeletionInfoText: string;
 }
 
-const ProfilePage = (props: Props) => {
+const ProfilePage = ({ title, profileDeletionText, profileDeletionInfoText }: Props) => {
+    // const { handleDelete } = useDeleteProfile();
 
-    const {
-        title,
-        profileDeletionInfoText,
-        profileDeletionText
-    } = props;
-
-    //const { handleDelete } = useDeleteProfile();
     return (
-        <div className={cls.main}>
+        <main className={cls.main}>
             <div className={cls.container}>
-                <div className={cls.div1}>
-                    <h1>
-                        {title}
-                    </h1>
-                </div>
-                <div className={cls.div2}>
-                    <p>
-                        {profileDeletionText}
-                    </p>
-                </div>
-                <div className={cls.div3}>
-                    <p>
-                        {profileDeletionInfoText}
-                    </p>
-                </div>
+                <h1>{title}</h1>
+                <p>{profileDeletionText}</p>
+                <p>{profileDeletionInfoText}</p>
             </div>
-        </div>
-    );
-};
+        </main>
+    )
+}
 
 export default ProfilePage;
