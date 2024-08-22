4575f8c2f1afa5efbaf9c949716a748a6a849761
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 18:28:22 +0300

    Add documentation comments and reformat code
    Added JSDoc comments to the ClickableBorder component for better clarity and usability. Reformatted the code to improve readability and maintain consistent code style.

--- frontend-next-migration/src/shared/ui/ClickableBorder/ClickableBorder.tsx
+++ frontend-next-migration/src/shared/ui/ClickableBorder/ClickableBorder.tsx
@@ -1,47 +1,66 @@
 import React, {
-  ReactNode,
-  useState,
-  forwardRef,
-  ForwardedRef,
-  CSSProperties,
+    ReactNode,
+    useState,
+    forwardRef,
+    ForwardedRef,
+    CSSProperties,
 } from 'react';
 import cls from './ClickableBorder.module.scss';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import {classNames} from '@/shared/lib/classNames/classNames';
 
+/**
+ * Props for ClickableBorder component.
+ */
 type Props = {
-  children: ReactNode;
-  borderImageSource: any;
-  className?: string;
+    children: ReactNode;
+    borderImageSource: any;
+    className?: string;
 };
 
+/**
+ * A component that wraps its children with a border that changes on hover.
+ *
+ * @component
+ * @example
+ * // Usage example:
+ * import ClickableBorder from './ClickableBorder';
+ *
+ * const MyComponent = () => (
+ *   <ClickableBorder borderImageSource="/images/border-image.png">
+ *     <div>Content inside the border</div>
+ *   </ClickableBorder>
+ * );
+ */
 const ClickableBorder = forwardRef(
-  (props: Props, ref: ForwardedRef<HTMLDivElement>) => {
-    const { children, borderImageSource, className = '' } = props;
-    const [isHovered, setIsHovered] = useState(false);
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
+    (props: Props, ref: ForwardedRef<HTMLDivElement>) => {
+        const {children, borderImageSource, className = ''} = props;
+        const [isHovered, setIsHovered] = useState(false);
 
-    const borderImageStyle = isHovered
-      ? { borderImageSource: `url(${borderImageSource})` }
-      : { borderImageSource: 'none' };
+        const handleMouseEnter = () => {
+            setIsHovered(true);
+        };
 
-    const mods = { [cls.hovered]: isHovered };
+        const handleMouseLeave = () => {
+            setIsHovered(false);
+        };
 
-    return (
-      <div
-        ref={ref}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className={classNames(cls.content, mods, [className])}
-        style={borderImageStyle}>
-        {children}
-      </div>
-    );
-  },
+        const borderImageStyle = isHovered
+            ? {borderImageSource: `url(${borderImageSource})`}
+            : {borderImageSource: 'none'};
+
+        const mods = {[cls.hovered]: isHovered};
+
+        return (
+            <div
+                ref={ref}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                className={classNames(cls.content, mods, [className])}
+                style={borderImageStyle}>
+                {children}
+            </div>
+        );
+    },
 );
 
 export default ClickableBorder;
