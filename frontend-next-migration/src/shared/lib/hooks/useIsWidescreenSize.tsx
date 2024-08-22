e91c167233d774ac0a42f668bacf1658068e02cd
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 18:15:16 +0300

    Add JSDoc comments to useIsWidescreenSize hook
    Enhanced the `useIsWidescreenSize` custom hook with detailed JSDoc comments. This includes a description, parameter, return, and example usage to improve code readability and developer understanding.

--- frontend-next-migration/src/shared/lib/hooks/useIsWidescreenSize.tsx
+++ frontend-next-migration/src/shared/lib/hooks/useIsWidescreenSize.tsx
@@ -1,10 +1,28 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
+/**
+ * Custom Hook to determine if the viewport is widescreen size.
+ *
+ * @returns {Object} An object containing a boolean `isWidescreenSize` indicating whether the viewport is considered widescreen.
+ *
+ * @example
+ * import useIsWidescreenSize from 'path/to/useIsWidescreenSize';
+ *
+ * const MyComponent = () => {
+ *   const { isWidescreenSize } = useIsWidescreenSize();
+ *
+ *   return (
+ *     <div>
+ *       {isWidescreenSize ? 'Widescreen mode' : 'Not in widescreen mode'}
+ *     </div>
+ *   );
+ * };
+ */
 const useIsWidescreenSize = () => {
   const checkForDevice = () =>
-    typeof window !== 'undefined' ? window.innerWidth >= 1920 : false;
+      typeof window !== 'undefined' ? window.innerWidth >= 1920 : false;
 
   const [isWidescreenSize, setIsWidescreenSize] = useState(checkForDevice());
 
