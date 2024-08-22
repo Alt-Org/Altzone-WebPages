bfebd7b55a77c67fda27b457f3862e5dc76f4c56
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-22 17:49:46 +0300

    Add JSDoc comments to envHelper object
    This commit enhances the documentation of the envHelper object by adding JSDoc comments. These comments provide clarity on the purpose and structure of the object, including the properties isDevMode, apiLink, and appDomain.

--- frontend-next-migration/src/shared/const/envHelper.ts
+++ frontend-next-migration/src/shared/const/envHelper.ts
@@ -1,3 +1,11 @@
+/**
+ * Helper object for managing environment variables.
+ *
+ * @type {Object}
+ * @property {boolean} isDevMode - Indicates whether the application is in development mode.
+ * @property {string} apiLink - The public API link for the application. Defaults to an empty string if not provided.
+ * @property {string} appDomain - The public domain for the application. Defaults to an empty string if not provided.
+ */
 export const envHelper = {
     isDevMode: process.env.NODE_ENV === 'development',
     apiLink : process.env.NEXT_PUBLIC_API_LINK || '',
