a8a136313e013f5df8e2f1f4467ac15bea0eba04
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-23 18:28:52 +0300

    Add mock for SVGs in Jest config
    Created a mock component `svgMock.js` to handle SVG imports in tests. Updated `jest.config.ts` to map SVG files to this new mock, ensuring consistent behavior in all test environments.

--- frontend-next-migration/jest.config.ts
+++ frontend-next-migration/jest.config.ts
@@ -102,6 +102,7 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
+    '^.+\\.(svg)$': '<rootDir>/__mocks__/svgMock.js',
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 
