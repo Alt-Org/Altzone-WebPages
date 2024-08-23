e1cadcd32077db300c8393a320b9ddb1f36c4671
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-23 18:05:31 +0300

    Update tests for openLinkInNewTab function
    Refactored test descriptions for clarity and replaced undefined URL handling with 'about:blank' to ensure a blank tab is opened when no URL is provided. This improves the robustness and readability of the test cases.

--- frontend-next-migration/src/shared/lib/openLinkInNewTab/openLinkInNewTab.test.ts
+++ frontend-next-migration/src/shared/lib/openLinkInNewTab/openLinkInNewTab.test.ts
@@ -1,21 +1,24 @@
 import {openLinkInNewTab} from './openLinkInNewTab';
 
+import {jest} from '@jest/globals';
 // Mocking the window.open function
 global.window = Object.create(window);
 const url = "http://dummy.com";
 Object.defineProperty(window, 'open', {writable: true, configurable: true, value: jest.fn()});
 
-// Function description
+/*
+* Test suite for openLinkInNewTab function.
+*/
 describe("openLinkInNewTab", () => {
     // Single use case: When a link is passed, function opens it in a new tab
-    it("opens a new tab with the URL passed", () => {
+    it("should open a new tab with the URL passed", () => {
         openLinkInNewTab(url);
         expect(window.open).toBeCalledWith(url, '_blank');
     });
 
     // Single use case: Handle when no link is passed
-    it("opens a new blank tab when no URL passed", () => {
+    it("should open a new blank tab when no URL passed", () => {
         openLinkInNewTab();
-        expect(window.open).toBeCalledWith(undefined, '_blank');
+        expect(window.open).toBeCalledWith('about:blank', '_blank');
     });
 });
