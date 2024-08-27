ed4bccb54aada2bc7b86bd24d6c09a929fd6ad6d
Skoivumaki <sakari.koivumaki@gmail.com>
2024-08-27 17:41:51 +0300

    fix: now properly passing Translation through Props

--- frontend-next-migration/src/app/[lng]/clans/[id]/page.tsx
+++ frontend-next-migration/src/app/[lng]/clans/[id]/page.tsx
@@ -18,9 +18,28 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function ClanRoomSubPage({ params }: Props) {
+    const { t } = await useServerTranslation(params.lng, 'clan');
 
     return (
         <PreparedPage
+            toastError={t('toast_error')}
+            toastNotLoggedIn={t('toast_notloggedin')}
+            toastClanNotOpen={t('toast_clan_not_open')}
+            toastEditMode={t('toast_edit_mode')}
+            joinClanBtn={t('join_clan_btn')}
+            leaveClanBtn={t("leave_clan_btn")}
+            editClanBtn={t("edit_clan_btn")}
+            memberListTitle={t("member_list")}
+            mottoText={t("motto")}
+            infoText={t("info")}
+            assetsText={t("assets")}
+            memberCountText={t("members_count")}
+            languageText={t("language")}
+            goalText={t("goal")}
+            ageLimitText={t("age_limit")}
+            winsText={t("wins")}
+            lossesText={t("losses")}
+
         />
     );
 
