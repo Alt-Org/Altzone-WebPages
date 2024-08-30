ef457211faf904c760450f2147e277f2258ec876
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-30 21:07:55 +0300

    Remove unnecessary whitespace
    This commit removes extra blank lines in the `page.tsx` file. The adjustments streamline the code by eliminating redundant whitespace, improving readability.

--- frontend-next-migration/src/app/[lng]/(home)/page.tsx
+++ frontend-next-migration/src/app/[lng]/(home)/page.tsx
@@ -3,7 +3,5 @@ import {_getPage} from "./_getPage";
 import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
 import {withPageData} from "@/shared/lib/hocs/withPageData";
 
-
 export const generateMetadata = createMetadataGenerator(_getPage);
-
 export default withPageData(MainPage, _getPage);
