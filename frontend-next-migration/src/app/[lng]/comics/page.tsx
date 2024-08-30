5d179db4f8df1507077360bc51731211fff1c922
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-30 21:12:44 +0300

    Refactor comics page to use HOCs for data fetching
    Replaced inlined metadata and data fetching logic with higher-order components. This improves code readability and promotes reuse by leveraging `createMetadataGenerator` for metadata and `withPageData` for data handling.

--- frontend-next-migration/src/app/[lng]/comics/page.tsx
+++ frontend-next-migration/src/app/[lng]/comics/page.tsx
@@ -1,23 +1,27 @@
 import {ComicsGalleriesPage} from "@/preparedPages/ComicsGalleriesPages";
-import {Metadata} from "next";
 import {_getPage} from "./_getPage";
+import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
+import {withPageData} from "@/shared/lib/hocs/withPageData";
 
-type Props = {
-    params: { lng: string }
-}
+// type Props = {
+//     params: { lng: string }
+// }
+//
+// export async function generateMetadata({ params }: Props): Promise<Metadata>  {
+//     return await _getPage(params.lng).then(r => r.seo);
+// }
+//
+// export default async function Comics({ params }: Props) {
+//     const {page} = await  _getPage(params.lng);
+//
+//     return (
+//         <>
+//             <ComicsGalleriesPage
+//                 {...page}
+//             />
+//         </>
+//     )
+// }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata>  {
-    return await _getPage(params.lng).then(r => r.seo);
-}
-
-export default async function Comics({ params }: Props) {
-    const {page} = await  _getPage(params.lng);
-
-    return (
-        <>
-            <ComicsGalleriesPage
-                {...page}
-            />
-        </>
-    )
-}
+export const generateMetadata = createMetadataGenerator(_getPage);
+export default withPageData(ComicsGalleriesPage, _getPage);
