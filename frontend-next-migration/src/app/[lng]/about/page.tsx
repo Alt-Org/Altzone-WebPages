b5c9223ddb99b371d6acf926ed138fae463cc5ac
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-30 20:25:48 +0300

    Refactor About page to use higher-order components
    Consolidate metadata generation and main About page logic using higher-order components and imported utilities. This change improves code readability and maintainability by centralizing shared functionality.

--- frontend-next-migration/src/app/[lng]/about/page.tsx
+++ frontend-next-migration/src/app/[lng]/about/page.tsx
@@ -1,35 +1,7 @@
-import { useServerTranslation} from "@/shared/i18n";
-import {Metadata} from "next";
+import {AboutPage} from "@/preparedPages/AboutPage";
+import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
+import {withPageData} from "@/shared/lib/hocs/withPageData";
+import {_getPage} from "./_getPage";
 
-type Props = {
-    params: { lng: string }
-}
-
-// eslint-disable-line react-hooks/rules-of-hooks
-export async function generateMetadata({ params }: Props): Promise<Metadata>  {
-
-    // eslint-disable-line react-hooks/rules-of-hooks
-    const { t } = await useServerTranslation(params.lng, 'about');
-
-    return {
-        title: t("head-title"),
-        description: t("head-description"),
-        keywords: t("head-keywords"),
-    }
-}
-
-
-export default async function About({ params }: Props) {
-
-    const { t } = await useServerTranslation(params.lng, 'about');
-
-    return (
-            <main>
-                <h1>
-                    {
-                        t("title")
-                    }
-                </h1>
-            </main>
-    )
-}
+export const generateMetadata = createMetadataGenerator(_getPage);
+export default withPageData(AboutPage, _getPage);
