cd497ef1eb1e15ae236c912b48252f297b1344e0
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-30 20:43:35 +0300

    Remove unnecessary newline in login page
    This change removes an extra newline for cleaner code structure. It does not affect functionality but improves readability and consistency.

--- frontend-next-migration/src/app/[lng]/auth/login/page.tsx
+++ frontend-next-migration/src/app/[lng]/auth/login/page.tsx
@@ -1,7 +1,6 @@
 import {AuthSubLoginPage} from "@/preparedPages/AuthPages";
 import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
 import {withPageData} from "@/shared/lib/hocs/withPageData";
-
 import {_getPage} from "./_getPage";
 
 export const generateMetadata = createMetadataGenerator(_getPage);
