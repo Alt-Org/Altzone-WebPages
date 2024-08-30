ca94c60ee99b0e062add9a03d928f0187e3b8759
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-30 20:08:56 +0300

    Add JSDoc comments to withPageData HOC
    Enhanced the `withPageData` higher-order component with detailed JSDoc comments. These comments explain the purpose, usage, and functionality of the HOC, aiding in better developer understanding and documentation.

--- frontend-next-migration/src/shared/lib/hocs/withPageData/index.tsx
+++ frontend-next-migration/src/shared/lib/hocs/withPageData/index.tsx
@@ -1,11 +1,31 @@
 import {ComponentType} from "react";
 
+/**
+ * Higher-order component to fetch page data based on language parameter and pass it to the wrapped component.
+ *
+ * @template PageProps - Props used by the wrapped component
+ * @param PageComponent - The React component to wrap and provide data to
+ * @param getPage - Function that fetches the page data given a language string
+ *
+ * @returns A component that fetches data and renders the wrapped component with both fetched data and original props
+ *
+ * @example
+ *
+ *  // Assume MyPageComponent is a React component and fetchPageData is a function that returns a promise
+ *  // resolving to page data
+ *
+ *  const MyPageWithData = withPageData(MyPageComponent, fetchPageData);
+ *
+ *  // Usage in a React application
+ *  <MyPageWithData params={{ lng: 'en' }} someOtherProp="value" />
+ *
+ */
 export function withPageData<PageProps>(
     PageComponent: ComponentType<PageProps>,
     getPage: (lng: string) => Promise<{ page: PageProps }>
 ) {
     return async function PageWithHOC(props: DefaultAppRouterProps & PageProps) {
-        const { params } = props;
+        const {params} = props;
         const data = await getPage(params.lng);
 
         return <PageComponent {...data.page} {...props} />;
