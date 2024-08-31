826e5fe2060629668771481fdad5aa66d152d1c2
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 09:05:17 +0300

    Refactor MainPage to use generic createPage function
    Simplified the construction of the MainPage by using the createPage helper function, which unifies SEO and page content building. Additionally, re-exported MainPageProps type to enhance type safety and code clarity across the project.

--- frontend-next-migration/src/preparedPages/MainPage/index.ts
+++ frontend-next-migration/src/preparedPages/MainPage/index.ts
@@ -1,2 +1,4 @@
 // export {MainPage} from "./ui/MainPage.async"
-export {default as MainPage} from "./ui/MainPage"
+export {default as MainPage} from "./ui/MainPage";
+
+export type {Props as MainPageProps} from "./ui/MainPage"
