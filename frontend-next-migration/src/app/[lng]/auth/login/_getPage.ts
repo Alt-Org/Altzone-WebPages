6bb9f3c7a48948a1570a27f6484e0a64e1790d65
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 12:19:10 +0300

    Refactor login page creation using createPage helper
    Simplified the login page setup by utilizing the createPage helper function. This refactor reduces redundancy and improves readability by encapsulating SEO and page creation logic within the helper.

--- frontend-next-migration/src/app/[lng]/auth/login/_getPage.ts
+++ frontend-next-migration/src/app/[lng]/auth/login/_getPage.ts
@@ -1,19 +1,15 @@
+import {createPage} from "@/app/_helpers/_createPage";
 import {useServerTranslation} from "@/shared/i18n";
 
 export async function _getPage (lng: string){
     const {t} = await useServerTranslation(lng, 'auth');
-
-    const seo = {
-        title: t("auth-login-head-title"),
-        description: t("auth-login-head-description"),
-        keywords: t("auth-login-head-keywords"),
-    }
-
-    const page = {}
-
-    return {
-        page,
-        seo
-    }
+    return createPage({
+        buildPage: () => ({}),
+        buildSeo: () => ({
+            title: t("auth-login-head-title"),
+            description: t("auth-login-head-description"),
+            keywords: t("auth-login-head-keywords"),
+        })
+    });
 
 }
