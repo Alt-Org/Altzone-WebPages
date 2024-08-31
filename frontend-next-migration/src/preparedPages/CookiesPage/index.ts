780d117d6dabbbee78e55354db58bd7065691e19
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 12:45:37 +0300

    Update CookiesPage exports with new type definition
    Replaced the default export with an async export and added a new type definition for `CookiesPageProps`. This change likely improves the modularity and type safety of the CookiesPage component.

--- frontend-next-migration/src/preparedPages/CookiesPage/index.ts
+++ frontend-next-migration/src/preparedPages/CookiesPage/index.ts
@@ -1,3 +1,4 @@
 // export {default as CookiesPage} from "./ui/CookiesPage";
 
 export { CookiesPageAsync as CookiesPage } from './ui/CookiesPage.async';
+export type {Props as CookiesPageProps} from "./ui/CookiesPage";
