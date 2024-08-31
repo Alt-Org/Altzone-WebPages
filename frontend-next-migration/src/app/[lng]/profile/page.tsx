fc656656a3b49476691d2edb585649eb4a2f5c6c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 17:45:03 +0300

    Disable unused metadata generator
    Commented out the import and export for createMetadataGenerator in profile page. Added a TODO note to enable it once it is added to i18n locales.

--- frontend-next-migration/src/app/[lng]/profile/page.tsx
+++ frontend-next-migration/src/app/[lng]/profile/page.tsx
@@ -1,8 +1,8 @@
 import {ProfilePage} from "@/preparedPages/ProfilePage";
-import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
+// import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
 import {_getPage} from "./_getPage";
 import {withPageData} from "@/shared/lib/hocs/withPageData";
 
-// enable after it will added to the i18n locales;
+// todo enable after it will added to the i18n locales;
 // export const generateMetadata = createMetadataGenerator(_getPage);
 export default withPageData(ProfilePage, _getPage);
