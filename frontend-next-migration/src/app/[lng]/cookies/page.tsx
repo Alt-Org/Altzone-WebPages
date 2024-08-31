2c0e40a6157a0a769f8f93a77634c40e208026be
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 12:50:56 +0300

    Reorder import statements in cookies/page.tsx
    Move `_getPage` import below other imports to maintain consistent import ordering. This change does not affect functionality but improves code readability and organization.

--- frontend-next-migration/src/app/[lng]/cookies/page.tsx
+++ frontend-next-migration/src/app/[lng]/cookies/page.tsx
@@ -1,7 +1,7 @@
-import {_getPage} from "./_getPage";
 import { CookiesPage } from '@/preparedPages/CookiesPage';
 import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
 import {withPageData} from "@/shared/lib/hocs/withPageData";
+import {_getPage} from "./_getPage";
 
 export const generateMetadata = createMetadataGenerator(_getPage);
 export default withPageData(CookiesPage, _getPage);
