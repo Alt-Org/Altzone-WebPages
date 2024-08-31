6d0604a2950f7c49103d59ae59e9738a19b9a165
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 09:04:47 +0300

    Add createPage helper function in Next.js migration
    This commit introduces a new helper function, `createPage`, in the Next.js migration folder. The function abstracts the construction of page and SEO data, improving code modularity and reusability.

---
+++ frontend-next-migration/src/app/_helpers/_createPage.ts
@@ -0,0 +1,16 @@
+type PageFactoryParams<TPageData = any, TSeoData = DefaultSeo> = {
+    buildSeo: () => TSeoData;
+    buildPage: () => TPageData;
+};
+
+export async function createPage<TPageData = any, TSeoData = DefaultSeo>(
+    {buildSeo, buildPage}: PageFactoryParams<TPageData, TSeoData>)
+{
+    const seo: TSeoData = buildSeo();
+    const page: TPageData = buildPage();
+
+    return {
+        page,
+        seo,
+    };
+}
