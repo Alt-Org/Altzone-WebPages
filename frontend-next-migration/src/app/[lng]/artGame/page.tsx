adce99f96ff6bc11bca28e7441d0a57e550b3662
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 12:11:53 +0300

    Remove unnecessary newline in page.tsx
    This commit removes a superfluous newline for cleaner code formatting in the artGame page component. There are no functional changes, but it improves code readability and maintains consistency.

--- frontend-next-migration/src/app/[lng]/artGame/page.tsx
+++ frontend-next-migration/src/app/[lng]/artGame/page.tsx
@@ -3,7 +3,6 @@ import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
 import {_getPage} from "./_getPage";
 import {withPageData} from "@/shared/lib/hocs/withPageData";
 
-
 export const generateMetadata = createMetadataGenerator(_getPage);
 export default withPageData(GameArtPage, _getPage);
 
