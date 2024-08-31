5d06c5bff30dc842b39b5980c43bc8bdd95436bd
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 18:22:26 +0300

    Remove extraneous newline in page.tsx
    This commit removes an unnecessary newline in the page.tsx file. This change helps maintain cleaner code and adheres to code formatting standards.

--- frontend-next-migration/src/app/[lng]/(home)/page.tsx
+++ frontend-next-migration/src/app/[lng]/(home)/page.tsx
@@ -3,5 +3,4 @@ import {_getPage} from "./_getPage";
 import {withPageData,createMetadataGenerator} from "src/app/_helpers";
 
 export const generateMetadata = createMetadataGenerator(_getPage);
-
 export default withPageData(MainPage, _getPage);
