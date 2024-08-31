7bc1a70465ab59e98c8be730a8c59b8ee33feebf
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 17:57:20 +0300

    Refactor team page to use higher-order component
    Refactored the `team/page.ts` to incorporate the `withPageData` HOC for better code reuse and maintainability. Simplified metadata generation by replacing the manual function with `createMetadataGenerator`.

--- frontend-next-migration/src/app/[lng]/team/page.ts
+++ frontend-next-migration/src/app/[lng]/team/page.ts
@@ -1,18 +1,7 @@
-import {Metadata} from "next";
-import {useServerTranslation} from "@/shared/i18n";
-export {default} from "@/preparedPages/MembersPage";
+import {_getPage} from "./_getPage";
+import {MembersPage} from "@/preparedPages/MembersPage";
+import {withPageData} from "@/shared/lib/hocs/withPageData";
+import {createMetadataGenerator} from "@/shared/lib/createMetadataGenerator";
 
-type Props = {
-    params: { lng: string }
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata>  {
-
-    const { t } = await useServerTranslation(params.lng, 'members');
-
-    return {
-        title: t("head-title"),
-        description: t("head-description"),
-        keywords: t("head-keywords"),
-    }
-}
+export const generateMetadata = createMetadataGenerator(_getPage);
+export default withPageData(MembersPage, _getPage);
