c316b275af4a08f435c53f24dce88a0f23467698
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-08-31 12:27:37 +0300

    Refactor _getPage to use createPage helper
    Simplified the _getPage function by utilizing the createPage helper to construct SEO and page objects. This change promotes code reusability and reduces redundancy across the project.

--- frontend-next-migration/src/app/[lng]/clans/_getPage.ts
+++ frontend-next-migration/src/app/[lng]/clans/_getPage.ts
@@ -1,19 +1,15 @@
+import {createPage} from "@/app/_helpers/_createPage";
 import {useServerTranslation} from "@/shared/i18n";
 
 export async function _getPage (lng: string){
     const {t} = await useServerTranslation(lng, 'clan');
 
-    const seo = {
-        title: t("head-title"),
-        description: t("head-description"),
-        keywords: t("head-keywords"),
-    }
-
-    const page = {}
-
-    return {
-        page,
-        seo
-    }
-
+    return createPage({
+        buildPage: () => ({}),
+        buildSeo: () => ({
+            title: t("head-title"),
+            description: t("head-description"),
+            keywords: t("head-keywords"),
+        })
+    });
 }
