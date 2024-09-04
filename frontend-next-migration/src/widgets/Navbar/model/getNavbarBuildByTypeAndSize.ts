dc2aa41c5d5e2c664790a7ff7642097acf70b149
Joni Roine <joni.roine@outlook.com>
2024-09-04 12:38:21 +0300

    Refactor navbar component for shared mobile/tablet logic and separate desktop logic, add JSDoc documentation

--- frontend-next-migration/src/widgets/Navbar/model/getNavbarBuildByTypeAndSize.ts
+++ frontend-next-migration/src/widgets/Navbar/model/getNavbarBuildByTypeAndSize.ts
@@ -1,3 +1,18 @@
+/**
+ * The function `getNavbarBuildByTypeAndSize` returns the appropriate navbar menu based on the type and
+ * size specified.
+ * @param {NavBarType} type - The `type` parameter in the `getNavbarBuildByTypeAndSize` function
+ * represents the type of navigation bar you want to retrieve. It can have the following values:
+ * 'Default', 'Clan', 'GameArt', 'Cookies', or 'Privacy'.
+ * @param {'mobile' | 'tablet' | 'desktop'} size - The `size` parameter in the
+ * `getNavbarBuildByTypeAndSize` function can have three possible values: 'mobile', 'tablet', or
+ * 'desktop'. This parameter is used to determine which set of navigation bar items to return based on
+ * the specified type and size.
+ * @returns The `getNavbarBuildByTypeAndSize` function returns the appropriate navbar menu based on the
+ * provided `type` and `size` parameters. If the `size` is 'desktop', it returns the corresponding
+ * desktop menu based on the `type`. If the `size` is 'mobile' or 'tablet', it returns the
+ * corresponding mobile menu based on the `type`. If the `type`
+ */
 import { NavBarType } from './types';
 import {
   navbarClanMobile,
@@ -16,25 +31,9 @@ import {
 
 export const getNavbarBuildByTypeAndSize = (
   type: NavBarType,
-  isMobileSize: boolean,
-  isTabletSize: boolean,
+  size: 'mobile' | 'tablet' | 'desktop',
 ) => {
-  if (isMobileSize || isTabletSize) {
-    switch (type) {
-      case 'Default':
-        return navbarMenuMobile;
-      case 'Clan':
-        return navbarClanMobile;
-      case 'GameArt':
-        return navbarGameArtMobile;
-      case 'Cookies':
-        return navbarCookiesMobile;
-      case 'Privacy':
-        return navbarPrivacyMobile;
-      default:
-        return navbarMenuMobile;
-    }
-  } else {
+  if (size === 'desktop') {
     switch (type) {
       case 'Default':
         return navbarMenuDesktop2;
@@ -48,6 +47,21 @@ export const getNavbarBuildByTypeAndSize = (
         return navbarPrivacyDesktop;
       default:
         return navbarMenuDesktop2;
+    }
+  } else {
+    switch (type) {
+      case 'Default':
+        return navbarMenuMobile;
+      case 'Clan':
+        return navbarClanMobile;
+      case 'GameArt':
+        return navbarGameArtMobile;
+      case 'Cookies':
+        return navbarCookiesMobile;
+      case 'Privacy':
+        return navbarPrivacyMobile;
+      default:
+        return navbarMenuMobile;
     }
   }
 };
