b0a4f305b3624d34cb790e82c9e0e1843065080b
Joni Roine <joni.roine@outlook.com>
2024-09-05 14:52:28 +0300

    Add Coming Soon Page

---
+++ frontend-next-migration/src/app/[lng]/coming/page.tsx
@@ -0,0 +1,6 @@
+import {AboutPage} from "@/preparedPages/AboutPage";
+import {withPageData,createMetadataGenerator} from "@/app/_helpers";
+import {_getPage} from "./_getPage";
+
+export const generateMetadata = createMetadataGenerator(_getPage);
+export default withPageData(AboutPage, _getPage);
---
+++ frontend-next-migration/src/preparedPages/ComingPage/ui/ComingPage.tsx
@@ -0,0 +1,28 @@
+'use client';
+import cls from './CookiesPage.module.scss';
+import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
+import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
+import { ReactNode } from 'react';
+
+export type Props = {
+  title: string;
+  text: ReactNode;
+};
+
+const ComingPage = (props: Props) => {
+  const { title, text } = props;
+
+  return (
+    <main className={cls.main}>
+      <div className={cls.container}>
+        <h1>{title}</h1>
+        <div className={cls.text}>{text}</div>
+      </div>
+    </main>
+  );
+};
+
+export default withBackgroundImage<Props>({
+  alt: 'Tile bg image',
+  imagePath: bgPicture as unknown as string,
+})(ComingPage);
