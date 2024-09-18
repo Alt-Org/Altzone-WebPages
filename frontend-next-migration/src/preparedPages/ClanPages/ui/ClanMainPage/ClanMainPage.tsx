e12b7b4912a28aaa3a7f1ded409b956e5f88da45
Skoivumaki <sakari.koivumaki@gmail.com>
2024-09-18 18:49:43 +0300

    refactor: removed useless code and comments

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,49 +1,16 @@
 "use client"
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { useSelector } from "react-redux";
 import { Navbar } from "@/widgets/Navbar";
 import { Container } from "@/shared/ui/Container";
-import { selectProfile } from "@/entities/Auth";
 import cls from "./ClanMainPage.module.scss";
 import backgroundImage from "@/shared/assets/images/backgrounds/background.webp"
 import { withBackgroundImage } from "@/shared/lib/hocs/withBackgroundImage";
-import { RoutePaths } from "@/shared/appLinks/RoutePaths";
-import { useParams } from "next/navigation";
-import { useClientTranslation } from "@/shared/i18n";
 
 const ClanMainPage = ({ children }: any) => {
-
-    const user = useSelector(selectProfile);
-
-    const [canShowAddNew, setCanShowAddNew] = useState(false);
-    const [canShowUserClan, setShowUserClan] = useState(false);
-
-    useEffect(() => {
-        if (!user || !user.Player.clan_id) {
-            setShowUserClan(false);
-            // setCanShowAddNew(false);
-            return;
-        }
-
-        setShowUserClan(true);
-        setCanShowAddNew(false);
-
-    }, [user]);
-
-    const params = useParams();
-    const lng = params.lng as string;
-    const { t } = useClientTranslation(lng, "clan");
 
     return (
         <div className={cls.Wrapper}>
             <Navbar className={cls.Navbar} />
             <Container className={cls.Container}>
-                <div>
-                    {/*<Link href={RoutePaths.CLAN_ALL}>{t('all_clans')}</Link>*/}
-                    {/*canShowUserClan ? (<Link href={`${RoutePaths.clan}/${user?.Player.clan_id}`}>{t('my_clan')}</Link>) : (<p>Et ole liittynyt klaaniin <Link href={RoutePaths.clan_add_new}>{t('Luo oma klaani tästä')}</Link> tai liity klaaniin tästä!</p>)*/}
-                    {/*{canShowAddNew && <Link href={RoutePaths.clan_add_new}>{t('create_clan')}</Link>}*/}
-                </div>
                 <div className={cls.ClansViewMain}>
                     {children}
                 </div>
@@ -54,6 +21,6 @@ const ClanMainPage = ({ children }: any) => {
 };
 
 export default withBackgroundImage({
-    alt: "Clan-Page Japan sakura style background",
+    alt: "Clan-Page Brick style background",
     imagePath: backgroundImage as unknown as string,
 })(ClanMainPage);
