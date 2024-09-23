8c3feaaac5248787cffa2c2fe62f9b346c558894
Joni Roine <joni.roine@outlook.com>
2024-09-23 15:33:28 +0300

    Add envHelper for strapi

--- frontend-next-migration/src/shared/const/envHelper.ts
+++ frontend-next-migration/src/shared/const/envHelper.ts
@@ -7,7 +7,8 @@
  * @property {string} appDomain - The public domain for the application. Defaults to an empty string if not provided.
  */
 export const envHelper = {
-    isDevMode: process.env.NODE_ENV === 'development',
-    apiLink : process.env.NEXT_PUBLIC_API_LINK || '',
-    appDomain : process.env.NEXT_PUBLIC_APP_DOMAIN || ''
+  isDevMode: process.env.NODE_ENV === 'development',
+  apiLink: process.env.NEXT_PUBLIC_API_LINK || '',
+  appDomain: process.env.NEXT_PUBLIC_APP_DOMAIN || '',
+  strapiApiUrl: process.env.NEXT_PUBLIC_STRAPI_API_URL || '',
 } as const;
