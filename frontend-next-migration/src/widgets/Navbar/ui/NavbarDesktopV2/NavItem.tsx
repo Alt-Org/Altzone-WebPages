cb7034b784b6cf2efe95d7e01a02a14571a142ca
Skoivumaki <sakari.koivumaki@gmail.com>
2024-09-25 22:31:16 +0300

    feat: added a temporary function to navbuilder that check if user has joined a clan and detemines if button will be shown.

--- frontend-next-migration/src/widgets/Navbar/ui/NavbarDesktopV2/NavItem.tsx
+++ frontend-next-migration/src/widgets/Navbar/ui/NavbarDesktopV2/NavItem.tsx
@@ -1,12 +1,13 @@
 import { NavbarBuild, NavbarMenuItem } from "../../model/types";
-import  {memo} from "react";
-import {useParams} from "next/navigation";
-import {useClientTranslation} from "@/shared/i18n";
-import {classNames} from "@/shared/lib/classNames/classNames";
+import { memo } from "react";
+import { useParams } from "next/navigation";
+import { useClientTranslation } from "@/shared/i18n";
+import { classNames } from "@/shared/lib/classNames/classNames";
 import cls from "./NavbarDesktopV2.module.scss";
-import {AppLink, AppLinkTheme} from "@/shared/ui/AppLink/AppLink";
-import {DropdownWrapper} from "@/shared/ui/DropdownWrapper";
+import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink/AppLink";
+import { DropdownWrapper } from "@/shared/ui/DropdownWrapper";
 import Image from "next/image";
+import { useUserPermissions } from "@/entities/Auth";
 
 type NavItemProps = {
     item: NavbarMenuItem;
@@ -21,14 +22,14 @@ const NavItem = memo((props: NavItemProps) => {
     const params = useParams();
     const lng = params.lng as string;
     const { t } = useClientTranslation(lng, "navbar");
-
+    const { canI } = useUserPermissions();
 
     if (itemType === "navLink") {
         return (
             <li
                 key={item.path}
                 className=
-                    {classNames(cls.navItem, {}, [className])} >
+                {classNames(cls.navItem, {}, [className])} >
                 <AppLink
                     theme={AppLinkTheme.PRIMARY}
                     to={item.path}
@@ -44,17 +45,25 @@ const NavItem = memo((props: NavItemProps) => {
 
     if (itemType === "navDropDown") {
 
-        const localizedElements = item.elements.map((element) => ({
-            ...element,
-            elementText: t(`${element.elementText}`),
-        }));
+        const localizedElements = item.elements
+            .map((element) => {
+                if (element.elementText == "clanpage" && !canI("canISeeOwnClan")) {
+                    return null; // Skip this element if elementText is "clanpage"
+                }
+                return {
+                    ...element,
+                    elementText: t(`${element.elementText}`), // Localize elementText for other elements
+                };
+            })
+            .filter(element => element !== null); // Filter out the null elements
+
 
 
         return (
             <li
                 key={item.name}
                 className=
-                    {classNames(cls.navItem, {}, [className])} >
+                {classNames(cls.navItem, {}, [className])} >
 
                 <DropdownWrapper
                     elements={localizedElements}
