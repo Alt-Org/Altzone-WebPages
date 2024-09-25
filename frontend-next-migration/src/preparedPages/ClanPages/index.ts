09664d9cece0ad9a5c219515a9178e20e2938b21
Skoivumaki <sakari.koivumaki@gmail.com>
2024-09-25 22:23:26 +0300

    feat: initial files for myclanpage "redirect page"

--- frontend-next-migration/src/preparedPages/ClanPages/index.ts
+++ frontend-next-migration/src/preparedPages/ClanPages/index.ts
@@ -1,9 +1,10 @@
-export {ClanMainPageAsync as ClanMainPage} from "./ui/ClanMainPage/ClanMainPage.async";
+export { ClanMainPageAsync as ClanMainPage } from "./ui/ClanMainPage/ClanMainPage.async";
 
 
-export {default as ClanAllSubPage} from "./ui/ClanAllSubPage/ClanAllSubPage";
+export { default as ClanAllSubPage } from "./ui/ClanAllSubPage/ClanAllSubPage";
 
-export {default as ClanRoomSubPage} from "./ui/ClanRoomSubPage/ClanRoomSubPage";
+export { default as ClanRoomSubPage } from "./ui/ClanRoomSubPage/ClanRoomSubPage";
 
-export {default as ClanAddSubPage} from "./ui/ClanAddSubPage/ClanAddSubPage";
+export { default as ClanAddSubPage } from "./ui/ClanAddSubPage/ClanAddSubPage";
 
+export { default as MyClanPage } from "./ui/MyClanPage/MyClanPage";
