ae73e89f373d63e2dc8b06cbdaf17d9ad92c9188
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-29 14:42:52 +0300

    Refactor ComingSoon component to streamline translations
    Removed useEffect and state management for translations. Now the component reads the language parameter directly and retrieves translations using useClientTranslation, simplifying the code.

--- frontend-next-migration/src/widgets/ComingSoon/ui/ComingSoon.tsx
+++ frontend-next-migration/src/widgets/ComingSoon/ui/ComingSoon.tsx
@@ -2,48 +2,42 @@
 import cls from './ComingSoon.module.scss';
 import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
 import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
-import { useEffect, useState, memo } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useRouter } from 'next/navigation';
+import {useParams, useRouter} from 'next/navigation';
 import { classNames } from '@/shared/lib/classNames/classNames';
+import {memo} from "react";
+import {useClientTranslation} from "@/shared/i18n";
 
 export type Props = {
-  lng: string;
 };
 
-export const ComingSoon = memo(({ lng }: Props) => {
-  const { t } = useTranslation('coming');
-  const [title, setTitle] = useState(t('title'));
-  const [text, setText] = useState(t('text'));
-  const [backText, setBackText] = useState(t('backText'));
+export const ComingSoon = memo(() => {
+
+  const params = useParams();
+  const lng = params.lng as string;
+  const { t } = useClientTranslation(lng,'coming');
+
   const router = useRouter();
-
-  useEffect(() => {
-    setTitle(t('title'));
-    setText(t('text'));
-    setBackText(t('backText'));
-  }, [lng, t]);
-
   const handleGoBack = () => {
     router.back();
   };
 
   return (
-    <main className={cls.main}>
-      <div className={cls.container}>
-        <h1>{title}</h1>
-        <div className={cls.text}>
-          <h3>{text}</h3>
-          <div
-            className={classNames(cls.NavGoBackButton, {}, [])}
-            onClick={handleGoBack}>
-            <p>{backText}</p>
+      <main className={cls.main}>
+        <div className={cls.container}>
+          <h1>{t('title')}</h1>
+          <div className={cls.text}>
+            <h3>{t('text')}</h3>
+            <div
+                className={classNames(cls.NavGoBackButton, {}, [])}
+                onClick={handleGoBack}>
+              <p>{t('backText')}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 });
+
 
 ComingSoon.displayName = 'ComingSoon';
 
