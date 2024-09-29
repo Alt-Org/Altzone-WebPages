1b48e4bbec3e6ee459eb26051f4edbf7afc59f73
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-29 14:59:54 +0300

    Remove commented-out code in ComingSoon.tsx
    This commit cleans up the `ComingSoon.tsx` file by removing extensive commented-out code to improve readability and maintainability. Additionally, it streamlines the export statement by removing unnecessary type annotation.

--- frontend-next-migration/src/widgets/ComingSoon/ui/ComingSoon.tsx
+++ frontend-next-migration/src/widgets/ComingSoon/ui/ComingSoon.tsx
@@ -1,53 +1,4 @@
-// 'use client';
-// import cls from './ComingSoon.module.scss';
-// import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
-// import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
-// import {useParams, useRouter} from 'next/navigation';
-// import { classNames } from '@/shared/lib/classNames/classNames';
-// import {memo} from "react";
-// import {useClientTranslation} from "@/shared/i18n";
-//
-// export type Props = {
-// };
-//
-// export const ComingSoon = memo(() => {
-//
-//   const params = useParams();
-//   const lng = params.lng as string;
-//   const { t } = useClientTranslation(lng,'coming');
-//
-//   const router = useRouter();
-//   const handleGoBack = () => {
-//     router.back();
-//   };
-//
-//   return (
-//       <div className={cls.main}>
-//         <div className={cls.container}>
-//           <h1>{t('title')}</h1>
-//           <div className={cls.text}>
-//             <h3>{t('text')}</h3>
-//             <div
-//                 className={classNames(cls.NavGoBackButton)}
-//                 onClick={handleGoBack}>
-//               <p>{t('backText')}</p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//   );
-// });
-//
-//
-// ComingSoon.displayName = 'ComingSoon';
-//
-// export default withBackgroundImage<Props>({
-//   alt: 'Tile bg image',
-//   imagePath: bgPicture as unknown as string,
-// })(ComingSoon);
-
 'use client';
-
 import cls from './ComingSoon.module.scss';
 import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
 import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
@@ -81,7 +32,7 @@ export const ComingSoon = memo(() => {
 
 ComingSoon.displayName = 'ComingSoon';
 
-export default withBackgroundImage<Props>({
+export default withBackgroundImage({
   alt: 'Tile bg image',
   imagePath: bgPicture as unknown as string,
 })(ComingSoon);
