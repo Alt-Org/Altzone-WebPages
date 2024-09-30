6b7b1b25a0efa3e749854cc50786e8702563bd95
Joni Roine <joni.roine@outlook.com>
2024-09-30 09:58:54 +0300

    Comment out unwanted titles

--- frontend-next-migration/src/preparedPages/MainPage/ui/_components/sections/PlayWithUs/PlayWithUs.tsx
+++ frontend-next-migration/src/preparedPages/MainPage/ui/_components/sections/PlayWithUs/PlayWithUs.tsx
@@ -1,78 +1,77 @@
-import cls from "./PlayWithUs.module.scss"
-import Image from "next/image";
-import googlePLay from "@/shared/assets/images/media/googleplay.png";
-import sideImg from "@/shared/assets/images/heros/einstein/professori.webp";
-import Link from "next/link";
-import { AppExternalLinks } from "@/shared/appLinks/appExternalLinks";
-import {classNames} from "@/shared/lib/classNames/classNames";
-import {NavElement, NavItem} from "./NavElement/NavElement";
-
+import cls from './PlayWithUs.module.scss';
+import Image from 'next/image';
+import googlePLay from '@/shared/assets/images/media/googleplay.png';
+import sideImg from '@/shared/assets/images/heros/einstein/professori.webp';
+import Link from 'next/link';
+import { AppExternalLinks } from '@/shared/appLinks/appExternalLinks';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { NavElement, NavItem } from './NavElement/NavElement';
 
 type WebGl = {
-    title: string;
-    link: string;
-}
+  title: string;
+  link: string;
+};
 
 export type Props = {
-    title: string;
-    webGl: WebGl;
-    googlePLayLink: string;
-    belowNavs: NavItem[]
-}
+  title: string;
+  webGl: WebGl;
+  googlePLayLink: string;
+  belowNavs: NavItem[];
+};
 
 const PlayWithUs = (props: Props) => {
+  const {
+    title,
+    webGl,
+    googlePLayLink = AppExternalLinks.downloadAndroid,
+    belowNavs,
+  } = props;
 
-    const {
-        title,
-        webGl,
-        googlePLayLink= AppExternalLinks.downloadAndroid,
-        belowNavs
-    } = props;
-
-    return (
-        <section className={cls.SectionPlayWithUs}>
-
-            <h2 className={cls.title}>
+  return (
+    <section className={cls.SectionPlayWithUs}>
+      {/* <h2 className={cls.title}>
                 {title}
-            </h2>
-            <div className={cls.Content}>
-                <div className={cls.ContentWithNav}>
-                    <div className={cls.Buttons}>
+            </h2> */}
+      <div className={cls.Content}>
+        <div className={cls.ContentWithNav}>
+          <div className={cls.Buttons}>
+            <NavElement
+              className={cls.webGl}
+              navElem={{
+                isExternal: true,
+                title: webGl.title,
+                link: webGl.link,
+              }}
+              key={webGl.title}
+            />
 
-                        <NavElement
-                            className={cls.webGl}
-                            navElem={{
-                            isExternal: true,
-                            title: webGl.title,
-                            link: webGl.link,
-                        }} key={webGl.title}/>
+            <Link
+              href={googlePLayLink}
+              target={'_blank'}
+              className={cls.BtnDownloadWrapper}>
+              <Image
+                src={googlePLay}
+                alt={'google play button'}
+                className={cls.BtnDownload}
+              />
+            </Link>
+          </div>
 
-                        <Link href={googlePLayLink} target={"_blank"} className={cls.BtnDownloadWrapper}>
-                            <Image src={googlePLay} alt={"google play button"}
-                                   className={cls.BtnDownload}
-                            />
-                        </Link>
+          <div className={classNames(cls.NavElements, {}, [])}>
+            {belowNavs.map((item) => (
+              <NavElement navElem={item} key={item.title} />
+            ))}
+          </div>
+        </div>
 
-
-                    </div>
-
-
-                    <div className={classNames(cls.NavElements, {}, [])}>
-                        {belowNavs.map((item) => (
-                            <NavElement navElem={item} key={item.title}/>
-                        ))}
-                    </div>
-
-
-                </div>
-
-                <Image src={sideImg} alt={"Side image with hero"} className={cls.sideImg}/>
-
-            </div>
-
-        </section>
-
-    );
+        <Image
+          src={sideImg}
+          alt={'Side image with hero'}
+          className={cls.sideImg}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default PlayWithUs;
