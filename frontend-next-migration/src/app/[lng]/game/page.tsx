e294b13051f025588edce1e2af978cf856f877df
Joni Roine <joni.roine@outlook.com>
2024-10-03 15:30:35 +0300

    add MetaDataGenerator

--- frontend-next-migration/src/app/[lng]/game/page.tsx
+++ frontend-next-migration/src/app/[lng]/game/page.tsx
@@ -1,11 +1,5 @@
-import {GamePage} from "@/preparedPages/GamePage";
-import {withPageData ,
-    // createMetadataGenerator
-} from "@/app/_helpers";
-import {_getPage} from "./_getPage";
-
-// todo add after it will be ready
-// export const generateMetadata = createMetadataGenerator(_getPage);
+import { GamePage } from '@/preparedPages/GamePage';
+import { withPageData, createMetadataGenerator } from '@/app/_helpers';
+import { _getPage } from './_getPage';
+export const generateMetadata = createMetadataGenerator(_getPage);
 export default withPageData(GamePage, _getPage);
-
-
