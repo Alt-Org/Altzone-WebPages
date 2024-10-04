635b9065e96791e5d2610d731ec9097d84c03c1b
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-04 16:15:00 +0300

    Refactor `Intro` component to use `forwardRef`
    This change refactors the `Intro` component to use `forwardRef` for better handling of references. The classNames utility is also removed from the home layout file. The code is now more concise and easier to read.

--- frontend-next-migration/src/app/[lng]/(home)/_intro/Intro.tsx
+++ frontend-next-migration/src/app/[lng]/(home)/_intro/Intro.tsx
@@ -1,30 +1,31 @@
-import cls from "./Intro.module.scss";
+import {forwardRef} from "react";
 import {Button, ButtonTheme} from "@/shared/ui/Button";
+import cls from "./Intro.module.scss";
 
 type Props = {
     scrollToContent: () => void;
 };
 
-const Intro = (props: Props) => {
+const Intro = forwardRef<HTMLDivElement, Props>((props, ref) => {
     const { scrollToContent } = props;
     return (
-        <div className={cls.intro}>
-                <h1>
-                    Welcome to our website!
-                </h1>
-                <p>
-                    Scroll down to see more
-                </p>
-                <Button
-                    onClick={scrollToContent}
-                    theme={ButtonTheme.Graffiti}
-                    withScalableLink
-                >
-                    Scroll Down
-                </Button>
-            </div>
-            );
-};
+        <div ref={ref} className={cls.intro}>
+            <h1>
+                Welcome to our website!
+            </h1>
+            <p>
+                Scroll down to see more
+            </p>
+            <Button
+                onClick={scrollToContent}
+                theme={ButtonTheme.Graffiti}
+                withScalableLink
+            >
+                Scroll Down
+            </Button>
+        </div>
+    );
+});
 
 Intro.displayName = "IntroComponent";
 
