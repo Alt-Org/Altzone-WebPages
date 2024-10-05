d151cdba84468e7e88a88d5d5299b233c29bd3d9
Joni Roine <joni.roine@outlook.com>
2024-10-05 17:20:58 +0300

    add teamApi for stateSchema

--- frontend-next-migration/src/app/_providers/StoreProvider/config/StateSchema.ts
+++ frontend-next-migration/src/app/_providers/StoreProvider/config/StateSchema.ts
@@ -1,9 +1,9 @@
-import {AuthUserSchema} from "@/entities/Auth";
-import {gameApi} from "@/shared/api";
+import { AuthUserSchema } from '@/entities/Auth';
+import { gameApi } from '@/shared/api';
+import { teamApi } from '@/entities/Member/api/membersApi';
 
 export interface StateSchema {
-    authUser: AuthUserSchema;
-    [gameApi.reducerPath]: ReturnType<typeof gameApi.reducer>;
-    // todo add here strapiApi
+  authUser: AuthUserSchema;
+  [gameApi.reducerPath]: ReturnType<typeof gameApi.reducer>;
+  [teamApi.reducerPath]: ReturnType<typeof teamApi.reducer>;
 }
-
