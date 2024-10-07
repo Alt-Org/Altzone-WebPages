7c0088acd6a1fc83e84639dfabdbab77dee8411c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-07 19:00:51 +0300

    Add Providers component to Storybook decorators
    Incorporate the Providers component within Storybook's decorators to ensure consistent use of global providers across stories. This change improves the overall integration and testing of components wrapped in providers.

--- frontend-next-migration/.storybook/preview.tsx
+++ frontend-next-migration/.storybook/preview.tsx
@@ -1,6 +1,8 @@
 import type { Preview } from "@storybook/react";
 import {I18nextProvider} from "react-i18next";
 import i18n from "./mockI18n";
+import { Providers } from '../src/app/_providers';
+
 
 const preview: Preview = {
   parameters: {
@@ -18,7 +20,9 @@ const preview: Preview = {
   decorators: [
     (Story) => (
         <I18nextProvider i18n={i18n}>
+          <Providers>
           <Story />
+          </Providers>
         </I18nextProvider>
     ),
   ],
