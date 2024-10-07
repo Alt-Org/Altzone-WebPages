abf1dbfbd84dd9344bd7cc225cf6b524c728ba25
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-07 20:20:44 +0300

    Refactor FooterDesktop stories for better documentation
    Updated `FooterDesktop.stories.tsx` to enhance component and story documentation. Added controls and descriptions for props, and included additional Storybook parameters for improved clarity.

--- frontend-next-migration/src/widgets/Footer/ui/FooterDesktop/FooterDesktop.stories.tsx
+++ frontend-next-migration/src/widgets/Footer/ui/FooterDesktop/FooterDesktop.stories.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import FooterDesktop from './FooterDesktop';
 import { socialIconLinks } from '../../model/data/socialSectionMenu';
 
-const meta: Meta<typeof FooterDesktop> = {
+const meta = {
     title: 'widgets/Footer/Desktop',
     component: FooterDesktop,
-    args: {
-        title: 'Be part of our community 😊',
-        socialIconLinks: socialIconLinks,
-        texts: {
-            consent: 'Consent',
-            cookies: 'Cookies',
-            privacy: 'Privacy',
-            currentYear: new Date().getFullYear(),
-            companyName: "Psyche's Royale Gaming ry",
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: 'The `FooterDesktop` component is used to display the footer section for desktop screens, including social media links, company details, and consent information.',
+            },
         },
     },
-};
+    tags: ['autodocs'],
+    argTypes: {
+        title: {
+            control: 'text',
+            description: 'The title displayed in the footer, usually a message to engage users.',
+        },
+        socialIconLinks: {
+            control: 'object',
+            description: 'Array of social media links and icons displayed in the social section.',
+        },
+        texts: {
+            control: 'object',
+            description: 'An object containing text strings like consent message, cookies policy, privacy details, the current year, and the company name.',
+        },
+    },
+} satisfies Meta<typeof FooterDesktop>;
 
 export default meta;
-
-type Story = StoryObj<typeof FooterDesktop>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
@@ -33,6 +43,13 @@ export const Default: Story = {
             privacy: 'Privacy',
             currentYear: new Date().getFullYear(),
             companyName: "Psyche's Royale Gaming ry",
+        },
+    },
+    parameters: {
+        docs: {
+            description: {
+                story: 'Displays the default view of the `FooterDesktop` component with social links and privacy details.',
+            },
         },
     },
 };
