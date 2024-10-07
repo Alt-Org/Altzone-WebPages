97dfb84d59f52c46ed9dcd32850a3cfc815a5323
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-07 18:49:23 +0300

    Add i18n support to Storybook
    Introduce a mock i18n setup within Storybook using i18next and react-i18next. Renamed `preview.ts` to `preview.tsx` to include the I18nextProvider decorator for proper localization in stories.

--- frontend-next-migration/.storybook/preview.tsx
+++ frontend-next-migration/.storybook/preview.tsx
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import {I18nextProvider} from "react-i18next";
+import i18n from "./mockI18n";
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +11,17 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    nextjs: {
+      appDirectory: true,
+    },
   },
+  decorators: [
+    (Story) => (
+        <I18nextProvider i18n={i18n}>
+          <Story />
+        </I18nextProvider>
+    ),
+  ],
 };
 
 export default preview;
