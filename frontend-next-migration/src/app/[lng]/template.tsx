1e7713b76b483b9af1516cc9e67abb82e776bf4e
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-12 17:44:58 +0300

    Add dynamic Navbar type based on current route
    Updated the `template.tsx` file to include a dynamic Navbar configuration based on the current route. This change ensures the correct Navbar type is displayed for each specific path and omits the Navbar altogether for certain routes.

--- frontend-next-migration/src/app/[lng]/template.tsx
+++ frontend-next-migration/src/app/[lng]/template.tsx
@@ -1,23 +1,36 @@
 'use client'
 import {ReactNode} from "react";
 import {usePathname} from "next/navigation";
+import {Navbar, NavBarType} from '@/widgets/Navbar';
+import {RoutePaths} from "@/shared/appLinks/RoutePaths";
+
 
 interface Props {
     children: ReactNode;
 }
+
+export type NavBarTypeWithNone = NavBarType | 'None';
 
 export default function Template(props: Props) {
     const {children} = props;
 
     //todo add an external helper hook for this
     const pathname = usePathname();
-    const segments = pathname.split('/');
-    const pathAfterLang = segments.slice(2).join('/') || '/';
-
+    const segments = pathname.split('/').slice(2);
+    const pathAfterLang = segments.length > 0 ? `/${segments.join('/')}` : '/';
+    const navbarConfig: Partial<Record<keyof typeof RoutePaths, NavBarTypeWithNone>> = {
+        [RoutePaths.MAIN]: 'Default',
+        [RoutePaths.clan]: 'Clan',
+        [RoutePaths.GAME_ART]: 'GAME_ART',
+        [RoutePaths.cookies]: 'Cookies',
+        [RoutePaths.auth]: 'None',
+    };
+    const navBarType = navbarConfig[pathAfterLang as keyof typeof RoutePaths] || 'Default';
+    const shouldShowNavbar = navBarType !== 'None';
 
     return (
         <>
-            {/*{lng}*/}
+            {shouldShowNavbar && <Navbar navBarType={navBarType}/>}
             {children}
         </>
     )
