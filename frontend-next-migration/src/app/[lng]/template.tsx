6a513e1c9423b270f2120c5eb2c3974237925ae8
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-12 17:46:52 +0300

    Refactor navbar logic into a dedicated hook.
    Moved navbar configuration logic to a separate `useNavbarConfig` hook for better readability and maintainability. This change also cleans up the main template rendering by separating concerns.

--- frontend-next-migration/src/app/[lng]/template.tsx
+++ frontend-next-migration/src/app/[lng]/template.tsx
@@ -1,37 +1,46 @@
-'use client'
-import {ReactNode} from "react";
-import {usePathname} from "next/navigation";
-import {Navbar, NavBarType} from '@/widgets/Navbar';
-import {RoutePaths} from "@/shared/appLinks/RoutePaths";
-
+'use client';
+import { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
+import { Navbar, NavBarType } from '@/widgets/Navbar';
+import { RoutePaths } from "@/shared/appLinks/RoutePaths";
 
 interface Props {
     children: ReactNode;
 }
 
-export type NavBarTypeWithNone = NavBarType | 'None';
+type NavBarTypeWithNone = NavBarType | 'None';
+
 
 export default function Template(props: Props) {
-    const {children} = props;
+    const { children } = props;
 
-    //todo add an external helper hook for this
-    const pathname = usePathname();
-    const segments = pathname.split('/').slice(2);
-    const pathAfterLang = segments.length > 0 ? `/${segments.join('/')}` : '/';
-    const navbarConfig: Partial<Record<keyof typeof RoutePaths, NavBarTypeWithNone>> = {
-        [RoutePaths.MAIN]: 'Default',
-        [RoutePaths.clan]: 'Clan',
-        [RoutePaths.GAME_ART]: 'GAME_ART',
-        [RoutePaths.cookies]: 'Cookies',
-        [RoutePaths.auth]: 'None',
-    };
-    const navBarType = navbarConfig[pathAfterLang as keyof typeof RoutePaths] || 'Default';
-    const shouldShowNavbar = navBarType !== 'None';
+    const { navBarType, shouldShowNavbar } = useNavbarConfig();
 
     return (
         <>
-            {shouldShowNavbar && <Navbar navBarType={navBarType}/>}
+            {shouldShowNavbar && <Navbar navBarType={navBarType as NavBarType} />}
             {children}
         </>
-    )
+    );
+}
+
+function useNavbarConfig() {
+    const pathname = usePathname();
+
+    const segments = pathname.split('/').slice(2);
+    const pathAfterLang = segments.length > 0 ? `/${segments.join('/')}` : '/';
+
+    const navbarConfig: Partial<Record<keyof typeof RoutePaths, NavBarTypeWithNone>> = {
+        [RoutePaths.MAIN]: 'Default',
+        [RoutePaths.clan]: 'Clan',
+        [RoutePaths.GAME_ART]: 'GameArt',
+        [RoutePaths.cookies]: 'Cookies',
+        [RoutePaths.auth]: 'None',
+    };
+
+    const navBarType = navbarConfig[pathAfterLang as keyof typeof RoutePaths] || 'Default';
+
+    const shouldShowNavbar = navBarType !== 'None';
+
+    return { navBarType, shouldShowNavbar };
 }
