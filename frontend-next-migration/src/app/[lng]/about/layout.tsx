a1620b3afef9fe9f2928508d218d1ffc1c9d457c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-12 17:56:58 +0300

    Remove Navbar component from various layouts
    This commit removes the Navbar component from multiple layout files. This change streamlines the layout structure and eliminates redundancy. All affected layouts will no longer render the Navbar.

--- frontend-next-migration/src/app/[lng]/about/layout.tsx
+++ frontend-next-migration/src/app/[lng]/about/layout.tsx
@@ -9,7 +9,6 @@ type Props = {
 export default function AboutLayout({children}: Props) {
     return (
         <>
-            <Navbar/>
             {children}
             <Footer />
         </>
--- frontend-next-migration/src/app/[lng]/team/layout.tsx
+++ frontend-next-migration/src/app/[lng]/team/layout.tsx
@@ -10,7 +10,6 @@ type Props = {
 export default function TeamLayout({children}: Props) {
     return (
         <>
-            <Navbar />
             {children}
             <ScrollTop />
         </>
