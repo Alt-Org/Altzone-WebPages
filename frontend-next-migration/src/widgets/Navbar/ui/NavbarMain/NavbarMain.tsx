78aa9d103b8425438edff08c82233dc3fa0785c9
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-14 11:44:40 +0300

    Remove extensive commented-out code and redundant useEffect
    Removed large blocks of commented-out code that were no longer in use to improve readability. Also eliminated a useEffect that logged messages on component mount and unmount, which was unnecessary for the component's functionality.

--- frontend-next-migration/src/widgets/Navbar/ui/NavbarMain/NavbarMain.tsx
+++ frontend-next-migration/src/widgets/Navbar/ui/NavbarMain/NavbarMain.tsx
@@ -1,54 +1,4 @@
 'use client';
-// /* This code snippet is defining a React functional component called `NavbarMain`. It imports necessary
-// dependencies such as `memo` from React, and components like `NavbarDesktopV2` and `NavbarMobileV2`.
-// It also imports some types and functions related to the navbar. */
-// 'use client';
-// import { memo } from 'react';
-// import NavbarDesktopV2 from '../NavbarDesktopV2/NavbarDesktopV2';
-// import NavbarMobileV2 from '../NavbarMobileV2/NavbarMobileV2';
-// import { FixedProvider } from '@/widgets/Navbar/model/FixedProvider';
-// import { NavBarType } from '../../model/types';
-// import { getNavbarBuildByTypeAndSize } from '../../model/getNavbarBuildByTypeAndSize';
-// import useSizes from '@/shared/lib/hooks/useSizes';
-//
-// interface NavbarMainProps {
-//   marginTop?: number;
-//   className?: string;
-//   navBarType?: NavBarType;
-// }
-//
-// export const NavbarMain = memo((props: NavbarMainProps) => {
-//   const {marginTop, className, navBarType = 'Default' } = props;
-//   const { isMobileSize, isTabletSize } = useSizes();
-//   /* The line `const size = isMobileSize || isTabletSize ? 'mobile' : 'desktop';` is determining the
-//   size of the navbar based on the screen size. */
-//   const size = isMobileSize || isTabletSize ? 'mobile' : 'desktop';
-//   const navbarBuild = getNavbarBuildByTypeAndSize(navBarType, size);
-//
-//   return (
-//     <FixedProvider>
-//       {isMobileSize || isTabletSize ? (
-//         <NavbarMobileV2
-//           marginTop={marginTop}
-//           className={className}
-//           navbarBuild={navbarBuild}
-//           navBarType={navBarType}
-//         />
-//       ) : (
-//         <NavbarDesktopV2
-//           marginTop={marginTop}
-//           className={className}
-//           navbarBuild={navbarBuild}
-//           navBarType={navBarType}
-//         />
-//       )}
-//     </FixedProvider>
-//   );
-// });
-//
-// NavbarMain.displayName = 'NavbarMain';
-
-
 /* This code snippet is defining a React functional component called `NavbarMain`. It imports necessary
 dependencies such as `memo` from React, and components like `NavbarDesktopV2` and `NavbarMobileV2`.
 It also imports some types and functions related to the navbar. */
@@ -74,16 +24,6 @@ export const NavbarMain = memo((props: NavbarMainProps) => {
     className,
     navBarType = 'Default'
   } = props;
-
-  useEffect(() => {
-    // This will run on component mount (initial render)
-    console.log("hello");
-
-    return () => {
-      // This function will run on component unmount
-      console.log("goodbye");
-    };
-  }, []);
 
   const { isMobileSize, isTabletSize } = useSizes();
   const size = useMemo(() => (isMobileSize || isTabletSize ? 'mobile' : 'desktop'), [isMobileSize, isTabletSize]);
