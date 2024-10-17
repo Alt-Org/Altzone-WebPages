114fbed2af1e78c5b13213623c20a5ffcc5ec7a1
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-17 14:43:51 +0300

    Refactor gameApi to resolve circular dependencies
    Commented out StateSchema import and adjusted inline documentation to address FSD methodology violations. Modified type assertion for getState in prepareHeaders function to any to further mitigate dependency issues.

--- frontend-next-migration/src/shared/api/gameApi.ts
+++ frontend-next-migration/src/shared/api/gameApi.ts
@@ -1,8 +1,9 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { StateSchema } from "@/app/_providers/StoreProvider";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { HYDRATE } from "next-redux-wrapper";
+import { selectAccessTokenInfo } from "@/entities/Auth";
 import { envHelper } from "@/shared/const/envHelper";
-import {selectAccessTokenInfo} from "@/entities/Auth";
-import {HYDRATE} from "next-redux-wrapper";
+
+// import { StateSchema } from "@/app/_providers/StoreProvider";
 
 export enum GameApiCacheTags {
     AUTH = 'Auth',
@@ -21,9 +22,11 @@ export const gameApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: envHelper.apiLink,
         credentials: "include",
-        // todo violates fsd methodology, need to check if it is possible to add this logic via authMiddleware
+        /** todo violates fsd methodology, need to check if it is possible to add this logic via authMiddleware.
+         * This also causes circular dependencies.
+         * Make sure lower-level modules (like API setup) do not depend on higher-level modules (like state access or selectors) directly.*/
         prepareHeaders: (headers, { getState }) => {
-            const accessTokenInfo = selectAccessTokenInfo(getState() as StateSchema);
+            const accessTokenInfo = selectAccessTokenInfo(getState() as any);
             if (accessTokenInfo) {
                 headers.set('Authorization', `Bearer ${accessTokenInfo?.accessToken}`);
             }
