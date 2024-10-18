b28ab274e4ede729aa0253d5661960805dc03c3d
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-18 21:37:05 +0300

    test

--- frontend-next-migration/src/features/AddNewClan/ui/NewClanForm/NewClanForm.tsx
+++ frontend-next-migration/src/features/AddNewClan/ui/NewClanForm/NewClanForm.tsx
@@ -10,44 +10,52 @@ type Props = {
 
 export const NewClanForm = ({ onSuccess, className = "" }: Props) => {
 
-    const {
-        register,
-        handleSubmit,
-        onFormSubmit,
-        errors
-    } = useNewClanForm({ onSuccess });
+    const { register, handleSubmit, onFormSubmit, errors } = useNewClanForm({
+        onSuccess,
+    });
 
     return (
-        <CustomForm className={classNames(cls.Form, {}, [className])} onSubmit={handleSubmit(onFormSubmit)}>
-            <CustomForm.Header>
-                Luo Klaani
-            </CustomForm.Header>
+        <CustomForm
+            className={classNames(cls.Form, {}, [className])}
+            onSubmit={handleSubmit(onFormSubmit)}
+        >
+            <CustomForm.Header>Luo Klaani</CustomForm.Header>
 
             <CustomForm.InputField
                 key={"name"}
                 error={errors?.name?.message}
                 label={"Nimi"}
-                inputProps={{ ...register('name'), required: true }}
+                inputProps={{
+                    ...register("name"),
+                    required: true,
+                }}
             />
 
             <CustomForm.InputField
                 key={"tag"}
                 error={errors?.tag?.message}
                 label={"Tagi"}
-                inputProps={{ ...register('tag'), required: true }}
+                inputProps={{
+                    ...register("tag"),
+                    required: true,
+                }}
             />
 
-            {<CustomForm.InputField
-                key={"isOpen"}
-                error={errors?.isOpen?.message}
-                label={"Avoin klaani"}
-                inputProps={{ ...register('isOpen'), required: false, type: "checkbox" }}
-            />}
+            {
+                <CustomForm.InputField
+                    key={"isOpen"}
+                    error={errors?.isOpen?.message}
+                    label={"Avoin klaani"}
+                    inputProps={{
+                        ...register("isOpen"),
+                        required: false,
+                        type: "checkbox",
+                    }}
+                />
+            }
 
-            <CustomForm.Button type="submit">
-                Vahvista
-            </CustomForm.Button>
-
+            <CustomForm.Button type="submit">Vahvista</CustomForm.Button>
         </CustomForm>
     );
+
 };
