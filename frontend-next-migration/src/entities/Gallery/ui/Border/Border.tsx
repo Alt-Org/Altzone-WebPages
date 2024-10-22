86b0d15e8d06f8689fecea85f98b96b4389e302c
Kaisa Hakola <kaisahakola00@gmail.com>
2024-10-22 14:06:06 +0300

    Fix image borders to fit better
    Modifies the HeroCard and ClickableBoarder styling a bit and adds aspect ratio calculation to ImageWall's Border.

--- frontend-next-migration/src/entities/Gallery/ui/Border/Border.tsx
+++ frontend-next-migration/src/entities/Gallery/ui/Border/Border.tsx
@@ -17,9 +17,22 @@ export const Border = (props: BorderProps) => {
     const handleImageSizeUpdate: ResizeCallback<HTMLDivElement> = useCallback((refCurrent) => {
         const width = refCurrent.clientWidth;
         const height = refCurrent.clientHeight;
+        const aspectRatio = width / height
+
+        let paddingBasedOnAspectRatio;
+        if (aspectRatio >= 1.2) {
+            paddingBasedOnAspectRatio = 0.7
+        } else if (aspectRatio >= 1) {
+            paddingBasedOnAspectRatio =  1
+        } else if (aspectRatio >= 0.7) {
+            paddingBasedOnAspectRatio = 1.2
+        } else {
+            paddingBasedOnAspectRatio = 1.7
+        }
 
         refCurrent.style.setProperty('--cardWidthLocal', `${width}px`);
         refCurrent.style.setProperty('--cardHeightLocal', `${height}px`);
+        refCurrent.style.setProperty('--paddingBasedOnAspectRatio', `${paddingBasedOnAspectRatio}`)
     }, []);
 
     useResizeObserver({
