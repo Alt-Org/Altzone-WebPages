bda82e46499cfcc8279e9f3787c6b19380d0aca0
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-22 13:23:57 +0300

    Add tests for `useDeleteProfile` and fix deletion logic
    Introduce unit tests to `useDeleteProfile` to cover successful deletion, error handling, and cancellation scenarios. Additionally, correct the mutation call from `deleteClan` to `deleteProfile` and handle potential errors more robustly in the deletion logic.

--- frontend-next-migration/src/features/DeleteProfile/model/useDeleteProfile.ts
+++ frontend-next-migration/src/features/DeleteProfile/model/useDeleteProfile.ts
@@ -3,24 +3,28 @@ import { toast } from 'react-toastify';
 import { useDeleteProfileMutation } from '@/entities/Profile';
 
 const useDeleteProfile = () => {
-    const [deleteClan] = useDeleteProfileMutation();
+    const [deleteProfile] = useDeleteProfileMutation();
     const [isCancelled, setIsCancelled] = useState(false);
     const handleDelete = async (onSuccess?: () => void) => {
         //todo add i18n
         if (confirm('Oletko varma?')) {
-            try {
-                await deleteClan().unwrap(); // Call the mutation without arguments
-                toast.success('Profiili poistettiin onnistuneesti');
-                if (onSuccess) onSuccess();
-                setIsCancelled(false);
-            } catch (error) {
-                toast.error(`Virhe poistettaessa profiilia: ${error}`);
+            const result = await deleteProfile();
+            // @ts-ignore todo it works but ts for some reason doesnt recognise the type, figure our why and fix
+            if (result?.error) {
+                // @ts-ignore todo it works but ts for some reason doesnt recognise the type, figure our why and fix
+                toast.error(`Virhe poistettaessa profiilia: ${JSON.stringify(result?.error)}`);
                 setIsCancelled(true);
+                return;
             }
-        } else {
-            toast.info('Poisto peruutettiin', { autoClose: 1500 });
-            setIsCancelled(true);
+            toast.success('Profiili poistettiin onnistuneesti');
+            if (onSuccess) onSuccess();
+            setIsCancelled(false);
+            return;
         }
+
+        toast.info('Poisto peruutettiin', { autoClose: 1500 });
+        setIsCancelled(true);
+        return;
     };
 
     return { isCancelled, handleDelete };
