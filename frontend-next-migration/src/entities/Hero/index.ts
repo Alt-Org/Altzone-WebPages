ba89d9a79c6dfbb3fc82777ac88f46770bf2cd32
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-23 17:26:38 +0300

    Add new exports to Hero entity
    This commit adds exports for `HeroGroup`, `HeroManager`, and the types

--- frontend-next-migration/src/entities/Hero/index.ts
+++ frontend-next-migration/src/entities/Hero/index.ts
@@ -7,3 +7,7 @@ export { default as HeroContainer } from './ui/HeroContainerTempByLeo/HeroContai
 // export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV2/HeroContainer";
 
 export { default as heroes } from './model/heroes';
+
+export { HeroGroup } from './types/hero';
+export type { Hero, GroupInfo } from './types/hero';
+export { HeroManager } from './model/heroesv2';
