00c465bbc02e659badc4a7d76dafca4f4d62a02f
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-24 09:27:41 +0300

    Delete deprecated HeroContainer components and styles
    This commit removes unused HeroContainer components and their associated SCSS files from various versions (V1, V2, V3, and V4). This cleanup reduces code bloat and simplifies the component structure.

--- frontend-next-migration/src/entities/Hero/index.ts
+++ frontend-next-migration/src/entities/Hero/index.ts
@@ -1,10 +1,6 @@
 export { HeroCard } from './ui/HeroCard/HeroCard';
 
 export { default as HeroContainer } from './ui/HeroContainerTempByLeo/HeroContainer';
-// export { default as HeroContainer } from './ui/HeroContainerV4/HeroContainer';
-// export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV3/HeroContainer";
-// export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV1/HeroContainer";
-// export {default as HeroContainer} from "./~deprecated/ui/HeroContainerV2/HeroContainer";
 
 export { default as heroes } from './model/heroes';
 
