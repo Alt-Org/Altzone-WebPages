b0bc23b4420761720618a79fe2866b6ed6058117
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-24 19:24:10 +0300

    Refactor HeroesPage to accept dynamic title prop
    Introduced a new `Props` interface in HeroesPage to allow for dynamic title customization of the SectionHeroesBlocks component. This enhances the flexibility and reusability of the HeroesPage component by leveraging prop-based data.

--- frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroesPage.tsx
+++ frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroesPage.tsx
@@ -4,18 +4,22 @@ import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
 import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
 import cls from './HeroesPage.module.scss';
 
-const HeroesPage = () => {
-    const sameBg = undefined;
+interface Props {
+    SectionHeroesBlocksTitle: string;
+}
+
+const HeroesPage = (props: Props) => {
+    const { SectionHeroesBlocksTitle = 'Heroes' } = props;
 
     return (
         <main className={cls.main}>
             <FeedbackSideButton disableMobile={true} />
-            <SectionHeroesBlocks title={'Heroes'} />
+            <SectionHeroesBlocks title={SectionHeroesBlocksTitle} />
         </main>
     );
 };
 
-export default withBackgroundImage({
+export default withBackgroundImage<Props>({
     alt: 'ClassifiedHeroesPage underground style background',
     imagePath: bgPicture as unknown as string,
     className: cls.wholePageBG,
--- frontend-next-migration/src/widgets/SectionHeroesBlocks/index.ts
+++ frontend-next-migration/src/widgets/SectionHeroesBlocks/index.ts
@@ -1,4 +1,3 @@
-// export {default as SectionClassifiedHeroesBlocks} from "./~deprecated/ui/SectionClassifiedHeroesBlocks";
 export { default as SectionHeroesBlocks } from './ui';
 
 export type { Props as HeroesBlocksProps } from './ui';
