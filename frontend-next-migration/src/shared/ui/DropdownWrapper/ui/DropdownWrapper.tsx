bfeabc8e03c7f93c8a1c33673a13d15515f4c747
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-26 16:03:07 +0300

    Enhance DropdownWrapper with animations
    Refactored the DropdownWrapper component to add smooth opening and closing animations. Introduced states to manage rendering and animation sequences, ensuring a better user experience. Updated CSS to define the necessary keyframes and transition effects.

--- frontend-next-migration/src/shared/ui/DropdownWrapper/ui/DropdownWrapper.tsx
+++ frontend-next-migration/src/shared/ui/DropdownWrapper/ui/DropdownWrapper.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from 'react';
+'use client'
+import { useState, useEffect, MouseEvent } from 'react';
 import cls from './DropdownWrapper.module.scss';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { DropdownWrapperProps } from '../types';
@@ -6,157 +7,158 @@ import { AppLink } from '@/shared/ui/AppLink/AppLink';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faExternalLink } from '@fortawesome/free-solid-svg-icons';
 
-/**
- * A wrapper component that provides dropdown functionality.
- * @component
- *
- *
- * @example
- * const dropdownElements = [
- {
-            id: '1',
-            elementText: 'Option 1',
-            onClickCallback: () => {
-                console.log('Option 1 clicked!');
-            },
-        },
- ];
- * <DropdownWrapper elements={dropdownElements} > <button>press me<button/> <DropdownWrapper/>
- * @param {DropdownWrapperProps} props - The props for the DropdownWrapper component.
- * @returns {JSX.Element} The rendered DropdownWrapper component.
- */
-export const DropdownWrapper: FC<DropdownWrapperProps> = ({
-  contentAbsolute = false,
-  mouseOverLeaveMode = false,
-  className = '',
-  childrenWrapperClassName = '',
-  contentClassName = '',
-  contentItemClassName = '',
-  elements,
-  isDisabled,
-  children,
-  onOpen,
-  onClose,
-}: DropdownWrapperProps): JSX.Element => {
+export const DropdownWrapper = (props: DropdownWrapperProps)=> {
+
+  const { contentAbsolute = false,
+      mouseOverLeaveMode = false,
+      className = '',
+      childrenWrapperClassName = '',
+      contentClassName = '',
+      contentItemClassName = '',
+      elements,
+      isDisabled,
+      children,
+      onOpen,
+      onClose} = props;
+
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [shouldRender, setShouldRender] = useState<boolean>(false);
+  const [animationState, setAnimationState] = useState<'opening' | 'closing' | ''>('');
+
+  useEffect(() => {
+    if (isOpen) {
+      setShouldRender(true);
+      setAnimationState('opening');
+      if (onOpen) onOpen();
+    } else if (shouldRender) {
+      setAnimationState('closing');
+      if (onClose) onClose();
+    }
+  }, [isOpen]);
+
+  const handleAnimationEnd = () => {
+    if (animationState === 'closing') {
+      setShouldRender(false);
+    }
+    setAnimationState('');
+  };
 
   const handleMouseOver = () => {
     if (!mouseOverLeaveMode) return;
     setIsOpen(true);
-    if (onOpen) {
-      onOpen();
-    }
   };
 
-  const handleMouseLeave = () => {
+  const handleMouseOut = (event: MouseEvent<HTMLDivElement>) => {
     if (!mouseOverLeaveMode) return;
-    setIsOpen(false);
-    if (onClose) {
-      onClose();
+
+    const currentTarget = event.currentTarget;
+    const relatedTarget = event.relatedTarget as Node;
+
+    if (!currentTarget.contains(relatedTarget)) {
+      setIsOpen(false);
     }
   };
 
   const toggleDropdown = (): void => {
     setIsOpen(!isOpen);
-    if (isOpen && onOpen) {
-      onOpen();
-    }
-    if (!isOpen && onClose) {
-      onClose();
-    }
-  };
-
-  /**
-   * Handles the click event for a dropdown element.
-   *
-   * @param {Function} onClick - The onClick callback function for the element.
-   * @returns {void}
-   */
-  const handleElementClick = (onClick?: () => void): void => {
-    onClick && onClick();
   };
 
   const mods: Record<string, boolean> = {
     [cls.contentAbsolute]: contentAbsolute,
-  } as Record<string, boolean>;
+  };
 
-  const dropdownContentMods: Record<string, boolean> = {
-    [cls.closed]: !isOpen,
-  } as Record<string, boolean>;
+  const modsContent: Record<string, boolean> = {
+      [cls.open]: isOpen && animationState !== 'closing',
+      [cls.closed]: !isOpen && animationState === 'closing',
+      [cls.opening]: animationState === 'opening',
+      [cls.closing]: animationState === 'closing',
+  }
+
 
   const mainElementClass = isDisabled?.status ? cls.disabled : '';
 
   return (
-    <div
-      className={classNames(cls.DropdownWrapper, mods, [className])}
-      onMouseLeave={handleMouseLeave}>
       <div
-        onClick={!isDisabled?.status ? toggleDropdown : undefined}
-        onMouseOver={!isDisabled?.status ? handleMouseOver : undefined}
-        role='button'
-        title={isDisabled?.status ? isDisabled?.reason : ''}
-        tabIndex={0}
-        className={classNames(cls.childrenWrapper, {}, [
-          childrenWrapperClassName,
-          mainElementClass,
-        ])}>
-        {children}
-        <FontAwesomeIcon
-          size={'2xs'}
-          icon={faCaretDown}
-          style={{
-            transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-            transition: 'transform 0.4s ease-in-out',
-          }}
-        />
-      </div>
+          className={classNames(cls.DropdownWrapper, mods, [className])}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+      >
+        <div
+            onClick={!isDisabled?.status ? toggleDropdown : undefined}
+            role="button"
+            title={isDisabled?.status ? isDisabled?.reason : ''}
+            tabIndex={0}
+            className={classNames(cls.childrenWrapper, {}, [
+              childrenWrapperClassName,
+              mainElementClass,
+            ])}
+        >
+          {children}
+          <FontAwesomeIcon
+              size={'2xs'}
+              icon={faCaretDown}
+              style={{
+                transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+                transition: 'transform 0.4s ease-in-out',
+              }}
+          />
+        </div>
 
-      <div
-        className={classNames(cls.dropdownContent, dropdownContentMods, [
-          contentClassName,
-        ])}>
-        {elements.map((element, index) => (
-          <div
-            key={index}
-            className={
-              element.isDisabled && element.isDisabled.status
-                ? cls.disabled
-                : ''
-            }
-            title={
-              element.isDisabled?.status === true
-                ? element?.isDisabled?.reason
-                : ''
-            }>
-            {element.link ? (
-              <AppLink
-                to={element.link.path}
-                isExternal={element.link.isExternal}
-                className={contentItemClassName}>
-                {element.elementText}
-                {/* Show external link icon only if the link is external */}
-                {element.link.isExternal && (
-                  <FontAwesomeIcon
-                    className={cls.externalLinkIcon}
-                    size={'2xs'}
-                    icon={faExternalLink}
-                    style={{
-                      display: 'inline',
-                      verticalAlign: 'middle',
-                      marginLeft: '5px',
-                      color: 'var(--inverted-primary-color)',
-                    }}
-                  />
+        {shouldRender && (
+            <div
+                className={classNames(
+                    cls.dropdownContent,
+                    modsContent,
+                    [contentClassName]
                 )}
-              </AppLink>
-            ) : (
-              <span className={contentItemClassName}>
-                {element.elementText}
-              </span>
-            )}
-          </div>
-        ))}
+                onAnimationEnd={handleAnimationEnd}
+            >
+              {elements.map((element, index) => (
+                  <div
+                      key={index}
+                      className={
+                        element.isDisabled && element.isDisabled.status
+                            ? cls.disabled
+                            : ''
+                      }
+                      title={
+                        element.isDisabled?.status === true
+                            ? element?.isDisabled?.reason
+                            : ''
+                      }
+                  >
+                    {element.link ? (
+                        <AppLink
+                            to={element.link.path}
+                            isExternal={element.link.isExternal}
+                            className={contentItemClassName}
+                        >
+                          {element.elementText}
+                          {element.link.isExternal && (
+                              <FontAwesomeIcon
+                                  className={cls.externalLinkIcon}
+                                  size={'2xs'}
+                                  icon={faExternalLink}
+                                  style={{
+                                    display: 'inline',
+                                    verticalAlign: 'middle',
+                                    marginLeft: '5px',
+                                    color: 'var(--inverted-primary-color)',
+                                  }}
+                              />
+                          )}
+                        </AppLink>
+                    ) : (
+                        <span className={contentItemClassName}>
+                  {element.elementText}
+                </span>
+                    )}
+                  </div>
+              ))}
+            </div>
+        )}
       </div>
-    </div>
   );
 };
+
