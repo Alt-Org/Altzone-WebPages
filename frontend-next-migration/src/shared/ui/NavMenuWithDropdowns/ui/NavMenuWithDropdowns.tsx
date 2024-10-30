8adcb30d618d8cc1c6789c3f4ba8f3a1b518d72e
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-30 15:46:56 +0200

    Refactor NavMenuWithDropdowns component structure
    Reorganized imports and moved the docstring to the end of the file. These changes improve readability and maintain consistency in the codebase.

--- frontend-next-migration/src/shared/ui/NavMenuWithDropdowns/ui/NavMenuWithDropdowns.tsx
+++ frontend-next-migration/src/shared/ui/NavMenuWithDropdowns/ui/NavMenuWithDropdowns.tsx
@@ -1,17 +1,64 @@
 import { DropDownElement, DropdownWrapper } from '@/shared/ui/DropdownWrapper';
-import { DropDownElementASTextOrLink } from '@/shared/ui/DropdownWrapper/types';
 import { ReactNode } from 'react';
 import cls from './NavMenuWithDropdowns.module.scss';
 
 export interface NavMenuWithDropdownsProps {
     dropdownItems: {
         title: string;
-        elements: DropDownElementASTextOrLink[];
+        elements: DropDownElement[];
         openByDefault?: boolean;
     }[];
     openByDefault?: boolean;
     title: string;
     className?: string;
+}
+
+export function NavMenuWithDropdowns(props: NavMenuWithDropdownsProps): JSX.Element {
+    const { dropdownItems, className, title, openByDefault = false } = props;
+
+    return (
+        <div className={className}>
+            <DropdownWrapper
+                openByDefault={openByDefault}
+                elements={dropdownItems.map((item) => (
+                    <NestedDropDown
+                        key={item.title}
+                        openByDefault={item.openByDefault}
+                        elements={item.elements}
+                    >
+                        {item.title}
+                    </NestedDropDown>
+                ))}
+                className={cls.topDropDown}
+                childrenWrapperClassName={cls.topDropDownChildren}
+                contentClassName={cls.topDropDownContent}
+            >
+                {title}
+            </DropdownWrapper>
+        </div>
+    );
+}
+
+interface NestedDropDownProps {
+    openByDefault?: boolean;
+    elements: DropDownElement[];
+    children: ReactNode;
+}
+
+function NestedDropDown(props: NestedDropDownProps) {
+    const { openByDefault, elements, children } = props;
+
+    return (
+        <DropdownWrapper
+            openByDefault={openByDefault}
+            className={cls.subDropDown}
+            contentClassName={cls.subDropDownContent}
+            childrenWrapperClassName={cls.subDropDownChildren}
+            elements={elements}
+        >
+            {children}
+        </DropdownWrapper>
+    );
 }
 
 /**
@@ -61,50 +108,3 @@ export interface NavMenuWithDropdownsProps {
  * export default App;
  * ```
  */
-export function NavMenuWithDropdowns(props: NavMenuWithDropdownsProps): JSX.Element {
-    const { dropdownItems, className, title, openByDefault = false } = props;
-
-    return (
-        <div className={className}>
-            <DropdownWrapper
-                openByDefault={openByDefault}
-                elements={dropdownItems.map((item) => (
-                    <NestedDropDown
-                        key={item.title}
-                        openByDefault={item.openByDefault}
-                        elements={item.elements}
-                    >
-                        {item.title}
-                    </NestedDropDown>
-                ))}
-                className={cls.topDropDown}
-                childrenWrapperClassName={cls.topDropDownChildren}
-                contentClassName={cls.topDropDownContent}
-            >
-                {title}
-            </DropdownWrapper>
-        </div>
-    );
-}
-
-interface NestedDropDownProps {
-    openByDefault?: boolean;
-    elements: DropDownElement[];
-    children: ReactNode;
-}
-
-function NestedDropDown(props: NestedDropDownProps) {
-    const { openByDefault, elements, children } = props;
-
-    return (
-        <DropdownWrapper
-            openByDefault={openByDefault}
-            className={cls.subDropDown}
-            contentClassName={cls.subDropDownContent}
-            childrenWrapperClassName={cls.subDropDownChildren}
-            elements={elements}
-        >
-            {children}
-        </DropdownWrapper>
-    );
-}
