2e399c28cef53087dd8d9978b37c734f00c90af4
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-30 17:50:52 +0200

    Adjust media queries and update navigation imports
    Updated the breakpoints for responsive styles in `HeroPage.module.scss` to better accommodate smaller screens and added default min-height. Included `RoutePaths` and `NavMenuWithDropdowns` components in `HeroPage.tsx` but commented out their usage to retain future implementation flexibility.

--- frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroPage.tsx
+++ frontend-next-migration/src/preparedPages/HeroesPages/ui/HeroPage.tsx
@@ -1,8 +1,10 @@
 import { HeroContainer, HeroWithGroup } from '@/entities/Hero';
-import { getRouteAllHeroesPage } from '@/shared/appLinks/RoutePaths';
+import { getRouteAllHeroesPage, RoutePaths } from '@/shared/appLinks/RoutePaths';
 import { withBackgroundImage } from '@/shared/lib/hocs/withBackgroundImage';
 import bgPicture from '@/shared/assets/images/backgrounds/background.webp';
 import cls from './HeroPage.module.scss';
+import { LayoutWithSidebars } from '@/shared/ui/Layouts';
+import { NavMenuWithDropdowns, NavMenuWithDropdownsProps } from '@/shared/ui/NavMenuWithDropdowns';
 
 interface Props {
     newSelectedHero: HeroWithGroup;
@@ -10,10 +12,39 @@ interface Props {
     nextHeroLink: string;
 }
 
+const navMenuWithDropdownsProps: NavMenuWithDropdownsProps = {
+    title: 'Forum',
+    openByDefault: true,
+    dropdownItems: [
+        {
+            title: 'Heroes',
+            openByDefault: false,
+            elements: [
+                // links can be used as well, just add the "link" to object
+                {
+                    elementText: 'Hero 1',
+                    id: 'hero1',
+                    link: { path: RoutePaths.HEROES, isExternal: false },
+                },
+                { elementText: 'Hero 2', id: 'hero2' },
+            ],
+        },
+    ],
+};
+
 const HeroPage = (props: Props) => {
     const { prevHeroLink, nextHeroLink, newSelectedHero } = props;
 
     return (
+        // <LayoutWithSidebars
+        //     leftTopSidebar={{
+        //         component: (
+        //             <div style={{ width: '100%', maxWidth: '600px' }}>
+        //                 <NavMenuWithDropdowns {...navMenuWithDropdownsProps} />
+        //             </div>
+        //         ),
+        //     }}
+        // >
         <main className={cls.main}>
             <HeroContainer
                 groupLabel={newSelectedHero.groupLabel}
@@ -28,6 +59,7 @@ const HeroPage = (props: Props) => {
                 xLink={getRouteAllHeroesPage()}
             />
         </main>
+        // </LayoutWithSidebars>
     );
 };
 
