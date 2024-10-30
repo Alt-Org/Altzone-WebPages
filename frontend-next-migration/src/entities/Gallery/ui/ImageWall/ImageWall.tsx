169ecbff4ff8bb52e27ff9525bab970a89683f72
Kaisa Hakola <kaisahakola00@gmail.com>
2024-10-30 15:07:47 +0200

    Add masonry for ImageWall
    + fix prettier/eslint errors in ImageWall.tsx

--- frontend-next-migration/src/entities/Gallery/ui/ImageWall/ImageWall.tsx
+++ frontend-next-migration/src/entities/Gallery/ui/ImageWall/ImageWall.tsx
@@ -1,27 +1,27 @@
-'use client'; 
-import cls from "./ImageWall.module.scss"
-import Image from "next/image";
-import {Button, ButtonSize, ButtonTheme} from "@/shared/ui/Button";
-import {useInView} from "react-intersection-observer";
-import {classNames} from "@/shared/lib/classNames/classNames";
-import Link from "next/link";
-import { Version } from "../../types/gallery";
-import Fancybox from "@/shared/ui/Fancybox/Fancybox";
-import { ImageData } from "../../types/gallery";
-import React from "react";
-import { Border } from "../Border/Border";
+'use client';
+import cls from './ImageWall.module.scss';
+import Image from 'next/image';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button';
+import { useInView } from 'react-intersection-observer';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import Link from 'next/link';
+import { Version, ImageData } from '../../types/gallery';
+import Fancybox from '@/shared/ui/Fancybox/Fancybox';
+import React from 'react';
+import { Border } from '../Border/Border';
+import { MasonryWrapper } from '@/shared/ui/MasonryWrapper';
 
 export type ImageWallProps = {
-    version: Version
-    images: { [key: string]: ImageData }
+    version: Version;
+    images: { [key: string]: ImageData };
     seeMoreLink?: {
-        href: string,
-        text: string,
-    }
-}
+        href: string;
+        text: string;
+    };
+};
 
 export const ImageWall = (props: ImageWallProps) => {
-    const { version, images, seeMoreLink } = props
+    const { version, images, seeMoreLink } = props;
 
     const { ref, inView } = useInView({
         rootMargin: '-150px 0px',
@@ -29,29 +29,29 @@ export const ImageWall = (props: ImageWallProps) => {
     });
 
     const mods = {
-        [cls.inView]: inView
-    }
+        [cls.inView]: inView,
+    };
 
-    const borderImageSrc = "/images/hero-border3.png"
+    const borderImageSrc = '/images/hero-border3.png';
 
-    if (version === "full") {
+    if (version === 'full') {
         return (
             <Fancybox>
-                <div className={cls.Container}>
+                <MasonryWrapper>
                     {Object.keys(images).map((key: string) => (
-                        <Border 
+                        <Border
                             key={key}
-                            borderImageSrc={borderImageSrc} 
+                            borderImageSrc={borderImageSrc}
                         >
                             <div className={cls.Item}>
-                                <a 
+                                <a
                                     href={images[key].src}
                                     data-fancybox="gallery"
                                     className={cls.DataFancybox}
                                 >
                                     <Image
                                         className={cls.Image}
-                                        src={images[key].src} 
+                                        src={images[key].src}
                                         alt={`Image ${key}`}
                                         priority={true}
                                         width={images[key].width}
@@ -59,31 +59,31 @@ export const ImageWall = (props: ImageWallProps) => {
                                     />
                                 </a>
                             </div>
-                        </Border>  
+                        </Border>
                     ))}
-                </div>
+                </MasonryWrapper>
             </Fancybox>
-        )
-    } else if (version === "preview" && seeMoreLink) {
+        );
+    } else if (version === 'preview' && seeMoreLink) {
         return (
             <div>
                 <Fancybox>
-                    <div className={cls.Container}>
+                    <MasonryWrapper>
                         {Object.keys(images)
                             .slice(0, 8)
                             .map((key: string) => (
                                 <Border
                                     key={key}
-                                    borderImageSrc={borderImageSrc} 
+                                    borderImageSrc={borderImageSrc}
                                 >
                                     <div className={cls.Item}>
-                                        <a 
-                                            data-fancybox="gallery" 
+                                        <a
+                                            data-fancybox="gallery"
                                             href={images[key].src}
                                         >
                                             <Image
                                                 className={cls.Image}
-                                                src={images[key].src} 
+                                                src={images[key].src}
                                                 alt={`Image ${key}`}
                                                 width={images[key].width}
                                                 height={images[key].height}
@@ -91,25 +91,25 @@ export const ImageWall = (props: ImageWallProps) => {
                                         </a>
                                     </div>
                                 </Border>
-                            ))
-                        }
-                    </div>
+                            ))}
+                    </MasonryWrapper>
                 </Fancybox>
 
-                <div ref={ref} className={cls.buttonContainer}>
-                    <Button 
-                        withScalableLink={true} 
-                        theme={ButtonTheme.Graffiti} 
-                        className={classNames(cls.SeeMore, mods)} 
-                        size={ButtonSize.XL} 
-                        ref={ref} 
+                <div
+                    ref={ref}
+                    className={cls.buttonContainer}
+                >
+                    <Button
+                        withScalableLink={true}
+                        theme={ButtonTheme.Graffiti}
+                        className={classNames(cls.SeeMore, mods)}
+                        size={ButtonSize.XL}
+                        ref={ref}
                     >
-                        <Link href={seeMoreLink.href}>
-                            {seeMoreLink.text}
-                        </Link>
+                        <Link href={seeMoreLink.href}>{seeMoreLink.text}</Link>
                     </Button>
                 </div>
             </div>
-        )
+        );
     }
-}
+};
