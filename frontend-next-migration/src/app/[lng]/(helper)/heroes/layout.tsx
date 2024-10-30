57c6a9a385be2d665401220d1297221f1118fa71
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-30 17:42:43 +0200

    Remove unused CSS module in heroes layout
    Eliminated `Layout.module.scss` and corresponding class references in `layout.tsx`. This simplification removes redundant styles and cleans up the code.

--- frontend-next-migration/src/app/[lng]/(helper)/heroes/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/heroes/layout.tsx
@@ -1,7 +1,6 @@
 import { Footer } from '@/widgets/Footer';
 import { ReactNode } from 'react';
 import { HorizontalLines } from '@/shared/ui/HorizontalLines';
-import cls from './Layout.module.scss';
 
 interface Props {
     children?: ReactNode;
@@ -9,10 +8,10 @@ interface Props {
 
 export default function Layout({ children }: Props) {
     return (
-        <div className={cls.layout}>
-            <div className={cls.content}>{children}</div>
+        <>
+            {children}
             <HorizontalLines />
-            <Footer className={cls.footer} />
-        </div>
+            <Footer />
+        </>
     );
 }
