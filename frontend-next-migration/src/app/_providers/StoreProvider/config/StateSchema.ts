387fa499f1648a7e787a96c75c437c89dbcdb289
Skoivumaki <sakari.koivumaki@gmail.com>
2024-10-31 17:48:54 +0200

    add: declaring ProfileSchema in StateSchema

--- frontend-next-migration/src/app/_providers/StoreProvider/config/StateSchema.ts
+++ frontend-next-migration/src/app/_providers/StoreProvider/config/StateSchema.ts
@@ -1,7 +1,9 @@
 import { AuthUserSchema } from '@/entities/Auth';
+import { ProfileSchema } from '@/entities/Profile/types/profile';
 import { gameApi, strapiApi } from '@/shared/api';
 
 export interface StateSchema {
+    profile: ProfileSchema;
     authUser: AuthUserSchema;
     [gameApi.reducerPath]: ReturnType<typeof gameApi.reducer>;
     [strapiApi.reducerPath]: ReturnType<typeof strapiApi.reducer>;
