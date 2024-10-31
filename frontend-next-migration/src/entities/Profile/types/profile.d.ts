869be1d57cc4dec6012c080d5054f39e6fe69a6a
Skoivumaki <sakari.koivumaki@gmail.com>
2024-10-31 17:51:57 +0200

    fix: changed Profile types that it accepts null values

--- frontend-next-migration/src/entities/Profile/types/profile.d.ts
+++ frontend-next-migration/src/entities/Profile/types/profile.d.ts
@@ -1,5 +1,9 @@
 export type IProfile<Player> = {
-    _id: '650d422567e23912f940abe0';
-    username: string;
-    Player: Player;
+    _id: '650d422567e23912f940abe0' | null;
+    username: string | null;
+    Player: Player | null;
+};
+
+export type ProfileSchema = {
+    profile?: IProfile<IPlayer>;
 };
