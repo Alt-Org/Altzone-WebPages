1e28be2474da3c8ea3554d9a2e07c934b7ad8e31
Skoivumaki <sakari.koivumaki@gmail.com>
2024-10-31 17:54:41 +0200

    add: new key PROFILE to store profile information

--- frontend-next-migration/src/shared/const/LS_KEYS.ts
+++ frontend-next-migration/src/shared/const/LS_KEYS.ts
@@ -11,6 +11,12 @@ export enum LS_KEYS {
      * Key for saving language in local storage.
      */
     LOCAL_STORAGE_LANGUAGE_KEY = 'language',
-
+    /**
+     * Key for saving profile data in local storage
+     */
+    PROFILE = 'profile',
+    /**
+     * Key for storing the state of navbar being fixed or not.
+     */
     IsNavBarFixed = 'NavbarFixed',
 }
