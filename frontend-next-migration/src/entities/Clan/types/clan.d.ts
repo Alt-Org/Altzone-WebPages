1ba91e54bc9bcea210b8c47ea514422567eaf474
Skoivumaki <sakari.koivumaki@gmail.com>
2024-11-01 19:57:34 +0200

    add: new validations for phrase and labels

--- frontend-next-migration/src/entities/Clan/types/clan.d.ts
+++ frontend-next-migration/src/entities/Clan/types/clan.d.ts
@@ -4,11 +4,12 @@ export type GetClanResponse = ResponseShapeOne<'Clan', IClan>;
 export type IClan = {
     _id: string;
     name: string;
-    gameCoins: number;
     playerCount: number;
     stockCount: number;
     itemCount: number;
     tag: string;
+    phrase: string;
+    labels: ClanLabel[];
     admin_ids: string[];
     Player: Array[];
     isOpen: boolean;
@@ -32,8 +33,8 @@ export type IJoin = {
     join_message: string;
 };
 
-export type IClanCreateDto = Pick<IClan, 'name' | 'tag' | 'gameCoins' | 'isOpen'>;
+export type IClanCreateDto = Pick<IClan, 'name' | 'tag' | 'phrase' | 'labels' | 'isOpen'>;
 
-export type IClanUpdateDto = Pick<IClan, '_id' | 'name' | 'tag' | 'gameCoins' | 'isOpen'>;
+export type IClanUpdateDto = Pick<IClan, '_id' | 'name' | 'tag' | 'phrase' | 'labels' | 'isOpen'>;
 
 export type IJoinDto = Pick<IJoin, 'clan_id' | 'player_id' | 'join_message'>;
--- frontend-next-migration/src/features/AddNewClan/validations/index.ts
+++ frontend-next-migration/src/features/AddNewClan/validations/index.ts
@@ -8,11 +8,6 @@ export const ValidationAddNewClan = yup.object().shape({
         .min(3, 'Klaanin nimen on oltava vähintään 3 merkkiä pitkä')
         .max(15, 'Klaanin nimen on oltava enintään 15 merkkiä pitkä')
         .matches(/^[A-Za-z]+$/, 'Klaanin nimi saa sisältää vain kirjaimia'),
-    gameCoins: yup
-        .number()
-        .default(1)
-        .min(1, 'Pelikolikoiden on oltava vähintään 1')
-        .max(5000, 'Pelikolikoiden on oltava enintään 1500'),
 
     tag: yup
         .string()
@@ -20,6 +15,10 @@ export const ValidationAddNewClan = yup.object().shape({
         .min(3, 'Tagin on oltava vähintään 3 merkkiä pitkä')
         .max(10, 'Tagin on oltava enintään 10 merkkiä pitkä')
         .matches(/^[A-Za-z0-9_]+$/, 'Tagi voi sisältää vain kirjaimia, numeroita ja alaviivoja'),
+
+    phrase: yup.string(),
+
+    labels: yup.array(),
 
     isOpen: yup.boolean(),
 });
