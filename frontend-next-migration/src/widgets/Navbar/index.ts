cff47bbe8846f919f0a9a430308b29b9c3bbb3b2
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-02 17:39:29 +0200

    Add collapsing and collapsed styles in Navbar module
    Updated the NavbarDesktopV2 styles to include specific overflow handling for collapsing and collapsed states and removed user-select. Additionally, cleaned up imports in the Navbar index file for better organization.

--- frontend-next-migration/src/widgets/Navbar/index.ts
+++ frontend-next-migration/src/widgets/Navbar/index.ts
@@ -1,4 +1,3 @@
 export type { NavBarType } from './model/types';
 // export { NavbarMainV3 as Navbar } from './ui/NavbarMainV3/NavbarMainV3';
-// export { NavbarMainV31 as Navbar } from './ui/NavbarMainV3.1/NavbarMainV3.1';
 export { NavbarMain as Navbar } from './ui/NavbarMain/NavbarMain';
