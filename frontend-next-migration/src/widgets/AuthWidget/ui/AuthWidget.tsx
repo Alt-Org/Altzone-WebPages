87e7a2eb26bdc04a9dcb96f45f12d1d920b94006
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-04 16:36:20 +0200

    Add AuthWidget component
    Introduce AuthWidget component with login and register forms support. This widget dynamically selects and displays the appropriate form based on the passed prop. It also includes a redirect mechanism on successful login.

---
+++ frontend-next-migration/src/widgets/AuthWidget/ui/AuthWidget.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { redirect } from 'next/navigation';
+import { LoginForm, RegisterForm } from '@/features/AuthByUsername';
+import {
+    getRouteLoginPage,
+    getRouteMainPage,
+    getRouteRegisterPage,
+} from '@/shared/appLinks/RoutePaths';
+
+type AuthWidgetProps = {
+    formType: 'login' | 'register';
+    toOtherPage?: string;
+    redirectToOnSuccessLogin?: string;
+};
+
+const AuthWidget = (props: AuthWidgetProps) => {
+    const { formType, toOtherPage, redirectToOnSuccessLogin = getRouteMainPage() } = props;
+
+    const handleSuccessLogin = () => {
+        redirect(redirectToOnSuccessLogin);
+    };
+
+    const toOtherPageLocal =
+        toOtherPage || (formType === 'register' ? getRouteLoginPage() : getRouteRegisterPage());
+
+    const FormComponent =
+        formType === 'login'
+            ? () => (
+                  <LoginForm
+                      toRegisterPage={toOtherPageLocal}
+                      onSuccessLogin={handleSuccessLogin}
+                  />
+              )
+            : () => (
+                  <RegisterForm
+                      toLoginPage={toOtherPageLocal}
+                      // just sample how it could work in future, in this case we could create another feature e.g. authByGoogle
+                      // extraContent={<button onClick={handleRegisterLogin}>Register with Google</button>}
+                  />
+              );
+
+    return <FormComponent />;
+};
+
+export default AuthWidget;
