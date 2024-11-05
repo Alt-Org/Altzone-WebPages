46a40950e218973986f963916b0638f97e3b5dcf
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-05 15:23:40 +0200

    Simplify navbar menu data setup
    Removed redundant navbarBuilder instances and consolidated their usages into navbarBuilderV2. Standardized export objects to use a single navbarBuilder configuration.

--- frontend-next-migration/src/widgets/Navbar/model/data/navbarMenuDesktop.ts
+++ frontend-next-migration/src/widgets/Navbar/model/data/navbarMenuDesktop.ts
@@ -8,17 +8,6 @@ import {
 import { dropdowns } from './dropdowns';
 import { NavbarBuilder } from './NavbarBuilder';
 
-const navbarBuilder = new NavbarBuilder();
-navbarBuilder.addDropDown('community', false, dropdowns.community, 'left');
-navbarBuilder.addDropDown('game', false, dropdowns.game, 'left');
-navbarBuilder.addLogo('Nav logo', img as unknown as string, getRouteMainPage(), 'center');
-navbarBuilder.addDropDown('gameart', false, dropdowns.gameart, 'right');
-navbarBuilder.addDropDown('gallery', false, dropdowns.gallery, 'right');
-navbarBuilder.addLink('team', getRouteTeamPage(), false, 'right');
-navbarBuilder.addAuthProfile('profile', dropdowns.profile);
-navbarBuilder.addAuthLogin('login', getRouteLoginPage());
-export const navbarMenuDesktop = navbarBuilder.build();
-
 const navbarBuilderV2 = new NavbarBuilder();
 navbarBuilderV2.addLogo('Nav logo', img as unknown as string, getRouteMainPage());
 navbarBuilderV2.addLink('news', getRouteAllNewsPage(), true);
@@ -31,21 +20,6 @@ navbarBuilderV2.addAuthProfile('profile', dropdowns.profile);
 navbarBuilderV2.addAuthLogin('login', getRouteLoginPage());
 
 export const navbarMenuDesktop2 = navbarBuilderV2.build();
-
-const navbarBuilderV3 = new NavbarBuilder();
-navbarBuilderV3.addLink('news', getRouteAllNewsPage(), true);
-navbarBuilderV3.addDropDown('game', true, dropdowns.game);
-navbarBuilderV3.addDropDown('gallery', true, dropdowns.gallery);
-navbarBuilderV3.addDropDown('gameart', true, dropdowns.gameart);
-navbarBuilderV3.addDropDown('community', true, dropdowns.community);
-navbarBuilderV3.addLink('team', getRouteTeamPage(), true);
-navbarBuilderV3.addAuthProfile('profile', dropdowns.profile);
-navbarBuilderV3.addAuthLogin('login', getRouteLoginPage());
-
-export const navbarMenuDesktop3 = navbarBuilderV3.build();
-
-export const navbarGameArtDesktop = navbarBuilderV3.build();
-
-export const navbarCookiesDesktop = navbarBuilderV3.build();
-
-export const navbarPrivacyDesktop = navbarBuilderV3.build();
+export const navbarGameArtDesktop = navbarBuilderV2.build();
+export const navbarCookiesDesktop = navbarBuilderV2.build();
+export const navbarPrivacyDesktop = navbarBuilderV2.build();
