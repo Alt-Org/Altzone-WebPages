1265f530d21d8a343a8b60966852e84e69c31550
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-06 19:04:59 +0200

    Update news page component
    Replace ClanLeaderBoardPage with NewsPage component in the news page. Adjust imports accordingly to reflect the new component structure. This change ensures the correct page content is displayed.

--- frontend-next-migration/src/app/[lng]/(helper)/news/page.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/news/page.tsx
@@ -1,6 +1,6 @@
 import { withPageData, createMetadataGenerator } from '@/app/_helpers';
 import { _getPage } from './_getPage';
-import { ClanLeaderBoardPage } from '@/preparedPages/ClanPages';
+import { NewsPage } from '@/preparedPages/NewsPages';
 
 export const generateMetadata = createMetadataGenerator(_getPage);
-export default withPageData(ClanLeaderBoardPage, _getPage);
+export default withPageData(NewsPage, _getPage);
