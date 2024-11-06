af9f81b676e2e1c9d6cdabdc32459cb9e0169323
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-06 10:27:31 +0200

    Add WallIntroAnimation component
    Introduce WallIntroAnimation for an animated page load effect. Implement the component in the NewsPage to enhance the user experience with a loading animation.

---
+++ frontend-next-migration/src/shared/ui/WallIntroAnimation/ui/WallIntroAnimation.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useEffect, useState, ReactNode } from 'react';
+import cls from './WallIntroAnimation.module.scss';
+
+interface WallLoaderProps {
+    children: ReactNode;
+}
+
+export default function WallIntroAnimation({ children }: WallLoaderProps) {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsLoaded(true), 1500);
+        const hideTimer = setTimeout(() => setIsVisible(false), 2000);
+        return () => {
+            clearTimeout(timer);
+            clearTimeout(hideTimer);
+        };
+    }, []);
+
+    if (!isVisible) return children;
+
+    return (
+        <div className={cls.wrapper}>
+            <div className={`${cls.loader} ${isLoaded ? cls.loaded : ''}`}>
+                <div className={cls.wallLeft} />
+                <div className={cls.wallRight} />
+            </div>
+            <div
+                className={cls.content}
+                aria-hidden={!isLoaded}
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
