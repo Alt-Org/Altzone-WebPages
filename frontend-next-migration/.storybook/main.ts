7ff71a75f84f9d0d8b19e176fe074f9800cb7efc
Joni Roine <joni.roine@outlook.com>
2024-11-06 10:59:35 +0200

    Optimize Storybook configuration for SVG handling
    This commit refines the Storybook setup by enabling SVG support through custom webpack configuration. It ensures that SVG files are properly processed without conflicting with other image types, and introduces StoryStore version configuration for better performance.

--- frontend-next-migration/.storybook/main.ts
+++ frontend-next-migration/.storybook/main.ts
@@ -1,55 +1,48 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: [
-      // "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-
-  // stories: [
-  //   '../src/**/*.stories.@(js|jsx|ts|tsx)'
-  // ],
-
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: '@storybook/nextjs',
-    options: {
-      builder: {
-        useSWC: true, // Enables SWC support
-      },
+    stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-onboarding',
+        '@storybook/addon-interactions',
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {
+            builder: {
+                useSWC: true,
+            },
+        },
     },
-  },
+    features: {
+        storyStoreV7: false,
+    },
+    webpackFinal: async (config) => {
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
-  // features: {
-  //   storyStoreV7: false,
-  // },
+        if (fileLoaderRule) {
+            fileLoaderRule.exclude = /\.svg$/;
+        }
 
-  //todo if we enable this to work with svgs we will break other image types for some reason..., figure our why
-  // webpackFinal: async (config) => {
-  //
-  //   const fileLoaderRule = config.module.rules.find((rule) =>
-  //       // @ts-ignore
-  //       rule.test?.test?.('.svg')
-  //   );
-  //   // @ts-ignore
-  //   fileLoaderRule.exclude = /\.svg$/;
-  //
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['@svgr/webpack'],
-  //   });
-  //
-  //   return config;
-  // },
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: /\.(js|jsx|ts|tsx)$/,
+            use: ['@svgr/webpack'],
+        });
 
+        config.module.rules.push({
+            test: /\.svg$/,
+            type: 'asset',
+            resourceQuery: /url/,
+        });
 
-  docs: {
-    autodocs: "tag",
-  },
+        return config;
+    },
+    docs: {
+        autodocs: 'tag',
+    },
 };
+
 export default config;
