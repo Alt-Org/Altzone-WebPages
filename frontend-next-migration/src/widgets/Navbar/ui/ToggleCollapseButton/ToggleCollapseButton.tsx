2281968ba3bcfbec051aa3aba4bbf5618114c237
Skoivumaki <sakari.koivumaki@gmail.com>
2024-11-06 12:07:19 +0200

    refactor: removed unnecessary width from image

--- frontend-next-migration/src/widgets/Navbar/ui/ToggleCollapseButton/ToggleCollapseButton.tsx
+++ frontend-next-migration/src/widgets/Navbar/ui/ToggleCollapseButton/ToggleCollapseButton.tsx
@@ -1,8 +1,8 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './ToggleCollapseButton.module.scss';
 import Image from 'next/image';
-import visible from '@/shared/assets/images/visible.png';
-import invisible from '@/shared/assets/images/unvisible.png';
+import visible from '@/shared/assets/icons/visible.svg';
+import invisible from '@/shared/assets/icons/hidden.svg';
 
 type Props = {
     className?: string;
@@ -24,7 +24,6 @@ export function ToggleCollapseButton(props: Props) {
                 loading="eager"
                 alt="visibility"
                 src={isCollapsed ? invisible : visible}
-                width={42}
                 className={cls.buttonImage}
             />
         </button>
