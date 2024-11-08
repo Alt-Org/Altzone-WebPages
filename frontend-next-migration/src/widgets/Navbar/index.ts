606ed501a2f2c8c22d1a10c6acd47cd407d072ce
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-08 16:27:29 +0200

    Simplify Navbar import
    Removed commented

--- frontend-next-migration/src/widgets/Navbar/index.ts
+++ frontend-next-migration/src/widgets/Navbar/index.ts
@@ -1,3 +1,2 @@
 export type { NavBarType } from './model/types';
-// export { NavbarMainV3 as Navbar } from './ui/NavbarMainV3/NavbarMainV3';
 export { NavbarMain as Navbar } from './ui/NavbarMain/NavbarMain';
