fc8138aa9d1faa607ca3503d905842d159bbfffd
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-10 13:28:15 +0200

    Refactor ImageWall stories to use ImageData array
    Updated the mockImages from an object to an array of ImageData type. This change improves type consistency and aligns the mock data with the expected input format for the ImageWall component.

--- frontend-next-migration/src/entities/Gallery/ui/ImageWall/ImageWall.stories.tsx
+++ frontend-next-migration/src/entities/Gallery/ui/ImageWall/ImageWall.stories.tsx
@@ -1,5 +1,6 @@
 import { StoryObj } from '@storybook/react';
 import { ImageWall } from './ImageWall';
+import { ImageData } from '@/entities/Gallery';
 
 const meta = {
     title: 'entities/Gallery/ImageWall',
@@ -9,85 +10,77 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const mockImages = {
-    image1: {
+const mockImages: ImageData[] = [
+    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/PlaceholderLC.png',
         width: 275,
         height: 275,
     },
-    image2: {
+    {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOwRConBYl2t6L8QMOAQqa5FDmPB_bg7EnGA&s',
         width: 275,
         height: 283,
     },
-    image3: {
+    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/PlaceholderLC.png',
         width: 275,
         height: 275,
     },
-    image4: {
+    {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOwRConBYl2t6L8QMOAQqa5FDmPB_bg7EnGA&s',
         width: 275,
         height: 183,
     },
-    imgae5: {
+    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/PlaceholderLC.png',
         width: 275,
         height: 275,
     },
-    image6: {
+    {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOwRConBYl2t6L8QMOAQqa5FDmPB_bg7EnGA&s',
         width: 275,
         height: 283,
     },
-    image7: {
+    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/PlaceholderLC.png',
         width: 275,
         height: 275,
     },
-    image8: {
+    {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOwRConBYl2t6L8QMOAQqa5FDmPB_bg7EnGA&s',
         width: 275,
         height: 183,
     },
-    image9: {
+    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/PlaceholderLC.png',
         width: 275,
         height: 275,
     },
-    image10: {
+    {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOwRConBYl2t6L8QMOAQqa5FDmPB_bg7EnGA&s',
         width: 275,
         height: 283,
     },
-    image11: {
+    {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/PlaceholderLC.png',
         width: 275,
         height: 275,
     },
-    image12: {
+    {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOwRConBYl2t6L8QMOAQqa5FDmPB_bg7EnGA&s',
         width: 275,
         height: 183,
     },
-};
+];
 
 export const Full: Story = {
     args: {
-        version: 'full',
-        // @ts-ignore
         images: mockImages,
     },
 };
 
 export const Preview: Story = {
     args: {
-        version: 'preview',
-        // @ts-ignore
         images: mockImages,
-        seeMoreLink: {
-            href: '/picture-galleries',
-            text: 'See more',
-        },
     },
 };
