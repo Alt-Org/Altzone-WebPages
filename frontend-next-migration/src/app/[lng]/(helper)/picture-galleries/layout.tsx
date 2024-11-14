d12e5c66401a12e9f034f68811848e6afe4b1d04
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-14 18:53:14 +0200

    Set z-index for sidebar and update top indent logic
    Added a z-index of 1000 to the sidebar to ensure proper stacking order. Updated the top indent logic in PictureGalleryLayout to correctly handle the fixed and collapsed states using Redux state selectors.

--- frontend-next-migration/src/app/[lng]/(helper)/picture-galleries/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/picture-galleries/layout.tsx
@@ -3,15 +3,22 @@ import { ReactNode } from 'react';
 import { LayoutWithSidebars } from '@/shared/ui/Layouts';
 // import { HeroNavMenu } from '@/features/NavigateHeroes';
 import { GalleryNavMenu } from '@/features/NavigateGalleries';
+import { useSelector } from 'react-redux';
+import { selectIsCollapsed, selectIsFixed } from '@/widgets/Navbar';
 
 export default function PictureGalleryLayout({ children }: { children: ReactNode }) {
+    const isFixed = useSelector(selectIsFixed);
+    const isCollapsed = useSelector(selectIsCollapsed);
+
+    const isTopIndent = isFixed && !isCollapsed;
+
     return (
         <LayoutWithSidebars
             leftTopSidebar={{
                 component: <GalleryNavMenu />,
                 hideOnMobile: true,
             }}
-            // topIndent={getInitialFixedState()}
+            topIndent={isTopIndent}
             // rightBottomSidebar={{ component: <HeroNavMenu /> }}
         >
             {children}
