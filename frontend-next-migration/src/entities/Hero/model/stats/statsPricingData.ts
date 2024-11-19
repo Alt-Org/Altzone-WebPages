07ea82ec8c8627a7bf97e806fb4c98e8759020cc
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-11-19 19:33:04 +0200

    Add new pricing data model Prices2 for hero stats
    Introduced the Prices2 type and the statsPricingData2 constant to encapsulate detailed pricing information, including optional values. This addition allows for more flexible and enriched representation of pricing data for hero stats.

--- frontend-next-migration/src/entities/Hero/model/stats/statsPricingData.ts
+++ frontend-next-migration/src/entities/Hero/model/stats/statsPricingData.ts
@@ -13,3 +13,22 @@ export const statsPricingData: Prices = {
     10: [50, 100, 200, 300, 450, 600, 800, 1000, 1400],
     stepsPerLevel: [5, 5, 5, 10, 10, 10, 15, 15, 25],
 };
+
+export type Prices2 = {
+    [key: number]: {
+        prices: number[];
+        value?: number;
+    };
+    stepsPerLevel: number[];
+};
+
+export const statsPricingData2: Prices2 = {
+    1: { prices: [5, 10, 20, 30, 45, 60, 80, 100, 140], value: 100 },
+    3: { prices: [15, 30, 60, 90, 135, 180, 240, 300, 420], value: 300 },
+    5: { prices: [25, 50, 100, 150, 225, 300, 400, 500, 700], value: 500 },
+    6: { prices: [30, 60, 120, 180, 270, 360, 480, 600, 840], value: 600 },
+    7: { prices: [35, 70, 140, 210, 315, 420, 560, 700, 890], value: 700 },
+    8: { prices: [40, 80, 160, 240, 360, 480, 640, 800, 1120], value: 800 },
+    10: { prices: [50, 100, 200, 300, 450, 600, 800, 1000, 1400], value: 1000 },
+    stepsPerLevel: [5, 5, 5, 10, 10, 10, 15, 15, 25],
+};
