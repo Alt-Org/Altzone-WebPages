0572bba429a0c76fedd9f3d4297391c0478da98a
Joni Roine <joni.roine@outlook.com>
2024-12-08 01:44:44 +0200

    Refactor members display and organize by teams and departments.
    Updated the SectionMembers component to categorize and display members by teams and departments instead of a flat list. Implemented `organizeMembers` to process and structure the members data, enhancing readability and maintainability. Improved handling of loading and error states for a better user experience.

--- frontend-next-migration/src/widgets/SectionMembers/ui/SectionMembers.tsx
+++ frontend-next-migration/src/widgets/SectionMembers/ui/SectionMembers.tsx
@@ -7,7 +7,7 @@ import { Container } from '@/shared/ui/Container';
 import { useClientTranslation } from '@/shared/i18n';
 import { SkeletonLoaderWithHeader } from '@/shared/ui/SkeletonLoader';
 import cls from './SectionMembers.module.scss';
-import { Member } from '@/entities/Member/model/types/types';
+import { organizeMembers } from '@/entities/Member/api/mappers';
 
 interface WorkersSectionProps {
     className?: string;
@@ -22,9 +22,10 @@ export const SectionMembers: FC<WorkersSectionProps> = ({ className = '' }) => {
         data: members = [],
         isError,
         isLoading,
-    } = useGetMembersQuery(lng, {
+    } = useGetMembersQuery(undefined, {
         refetchOnMountOrArgChange: false,
     });
+    const { teamsMap, unmatchedDepartments } = organizeMembers(members, lng);
 
     return (
         <div className={classNames(cls.MembersSection, {}, [className])}>
@@ -34,24 +35,69 @@ export const SectionMembers: FC<WorkersSectionProps> = ({ className = '' }) => {
                 text={isError ? `${t('playButton')} 🚫` : t('playButton')}
             />
             <Container className={cls.membersListContainer}>
-                {isError && <p>Error fetching teams data</p>}
-
-                {isLoading || isError ? <SkeletonLoaderWithHeader sections={5} /> : null}
-
-                {!isLoading && !isError && members.length > 0 && (
-                    <div className={cls.membersList}>
-                        {members.map((member) => (
+                {isError && <p>Error fetching data</p>}
+                {isLoading ? <SkeletonLoaderWithHeader sections={5} /> : null}
+                {!isLoading && !isError && (
+                    <>
+                        {Array.from(teamsMap.values()).map((team) => (
                             <div
-                                key={member.id}
-                                className={cls.memberCard}
+                                key={team.id}
+                                className={cls.teamCard}
                             >
-                                <MemberItem
-                                    member={member}
-                                    language={lng}
-                                />
+                                <h1>{team.name}</h1>
+                                <ul className={cls.teamMembersList}>
+                                    {team.members.map((member) => (
+                                        <MemberItem
+                                            key={member.id}
+                                            member={member}
+                                            language={lng}
+                                        />
+                                    ))}
+                                </ul>
+                                {team.departments.map((department) => (
+                                    <div
+                                        key={department.id}
+                                        className={cls.departmentCard}
+                                    >
+                                        <h2>
+                                            {department.translations.find(
+                                                (t) => t.languages_code === lng,
+                                            )?.department ?? department.translations[0].department}
+                                        </h2>
+                                        <ul className={cls.departmentMembersList}>
+                                            {department.members.map((member) => (
+                                                <MemberItem
+                                                    key={member.id}
+                                                    member={member}
+                                                    language={lng}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                ))}
                             </div>
                         ))}
-                    </div>
+                        {unmatchedDepartments.map((department) => (
+                            <div
+                                key={department.id}
+                                className={cls.departmentCard}
+                            >
+                                <h2>
+                                    {department.translations.find((t) => t.languages_code === lng)
+                                        ?.department ?? department.translations[0].department}
+                                </h2>
+                                <ul className={cls.departmentMembersList}>
+                                    {department.members.map((member) => (
+                                        <MemberItem
+                                            key={member.id}
+                                            member={member}
+                                            language={lng}
+                                        />
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </>
                 )}
             </Container>
         </div>
