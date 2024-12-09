2f4cba2a5a5cbba348136b6162a5a9c09ea710c1
Joni Roine <joni.roine@outlook.com>
2024-12-09 09:55:58 +0200

    Refactor translation functions for default values
    Updated translation functions to use an empty string as a default value instead of specific strings like 'Task unavailable'. This change ensures consistency across translations and simplifies handling of untranslated values. Added documentation for improved code clarity and maintainability.

--- frontend-next-migration/src/entities/Member/api/translations.ts
+++ frontend-next-migration/src/entities/Member/api/translations.ts
@@ -1,12 +1,26 @@
 import { Translation, DepartmentTranslation, TeamTranslation } from '../model/types/types';
 
+/**
+ * Returns the language code in a standardized format.
+ *
+ * @param {string} language - The input language code in short format.
+ * @returns {string} - The standardized language code ('en-US' or 'fi-FI'), or 'default' if not recognized.
+ */
 export const getLanguageCode = (language: string): string => {
     return language === 'en' ? 'en-US' : language === 'fi' ? 'fi-FI' : 'default';
 };
 
 /**
- * Generic function to get a translation string for any key, with a fallback.
+ * Retrieves the translation value of a specified key for a given language code.
+ *
+ * @template T
+ * @param {T[]} translations - An array of translation objects each containing a language code.
+ * @param {string} languageCode - The language code to match translations against.
+ * @param {keyof T} key - The specific key to retrieve from the translation.
+ * @param {string} [defaultValue=''] - The default value returned if the key or language code doesn't match.
+ * @returns {string} - The translated value or the default value.
  */
+
 const getTranslation = <T extends { languages_code: string }>(
     translations: T[],
     languageCode: string,
@@ -17,29 +31,20 @@ const getTranslation = <T extends { languages_code: string }>(
     return translation && key in translation ? (translation[key] as string) : defaultValue;
 };
 
-/**
- * Get task translation.
- */
 export const getTaskTranslation = (translations: Translation[], languageCode: string): string => {
-    return getTranslation(translations, languageCode, 'task', 'Task unavailable');
+    return getTranslation(translations, languageCode, 'task', '');
 };
 
-/**
- * Get department name translation.
- */
 export const getDepartmentTranslation = (
     translations: DepartmentTranslation[],
     languageCode: string,
 ): string => {
-    return getTranslation(translations, languageCode, 'department', 'Unnamed Department');
+    return getTranslation(translations, languageCode, 'department', '');
 };
 
-/**
- * Get team name translation.
- */
 export const getTeamTranslation = (
     translations: TeamTranslation[],
     languageCode: string,
 ): string => {
-    return getTranslation(translations, languageCode, 'team', 'Unnamed Team');
+    return getTranslation(translations, languageCode, 'team', '');
 };
