135ef199c3a0b66bbcad6cbed4f4c650b21b83e9
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-09 17:30:20 +0200

    Refactor Team layout with LayoutWithIntro component
    Replaced the Team layout with the LayoutWithIntro to enhance consistency and leverage the existing layout components. Removed redundant margin styles from MembersPage.module.scss. Added new styles in Layout.module.scss to support the updated layout functionality.

--- frontend-next-migration/src/app/[lng]/(intro)/team/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(intro)/team/layout.tsx
@@ -1,14 +1,45 @@
 import { ReactNode } from 'react';
 import { ScrollTop } from '@/features/ScrollTop';
+import { LayoutWithIntro } from '@/preparedPages/Layouts';
+import introBg from '@/shared/assets/images/comics/comics5.webp';
+import cls from './Layout.module.scss';
+// import { ScrollToSectionButton } from "@/app/[lng]/(intro)/comics/_components/ScrollToSectionButton";
+import { useServerTranslation } from '@/shared/i18n';
 
 type Props = {
     children: ReactNode;
+    params: {
+        lng: string;
+    };
 };
 
-export default function TeamLayout({ children }: Props) {
+export default async function TeamLayout({ children, params }: Props) {
+    const { lng } = params;
+
+    const { t } = await useServerTranslation(lng, 'comics');
+
     return (
         <>
-            {children}
+            <LayoutWithIntro
+                key={'2'}
+                introHeight={'86vh'}
+                title={t('page-title')}
+                overlayColor={'rgba(7, 27, 30, 0.5'}
+                bgImage={introBg.src}
+                description={t('page-description')}
+                blurLineClass={cls.blurLine}
+                // bottomAdditional={
+                //     // <ScrollToSectionButton
+                //     //     className={cls.diveButton}
+                //     //     scrollToId={comicsSectionId}
+                //     // >
+                //     //     <b>{t('page-dive')}</b>
+                //     // </ScrollToSectionButton>
+                // }
+            >
+                <div>lol</div>
+                {children}
+            </LayoutWithIntro>
             <ScrollTop />
         </>
     );
