7ab4f8b76a014b064f5cd79173d7474683ad9923
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-09 19:48:23 +0200

    Add onBeforeClick callback to ScrollBottomButton
    Introduce an optional onBeforeClick callback to the ScrollBottomButton component, allowing execution of custom logic before the scroll action is initiated. This enhancement provides greater flexibility for users who need to trigger

--- frontend-next-migration/src/features/ScrollBottom/ui/ScrollBottomButton.tsx
+++ frontend-next-migration/src/features/ScrollBottom/ui/ScrollBottomButton.tsx
@@ -3,22 +3,32 @@ import { memo, useRef } from 'react';
 import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button/Button';
 import { scrollToBottom } from '../model/scrollToBottom/scrollToBottom';
 import { useBottomAnimationCancellation } from '../model/useBottomAnimationCancellation/useBottomAnimationCancellation';
-import { isDisabled } from '@testing-library/user-event/dist/types/utils';
 
 interface Props {
     speedInMs?: number;
     className?: string;
     text?: string;
     isDisabled?: boolean;
+    onBeforeClick?: () => void;
 }
 
 const ScrollBottomButtonComponent = (props: Props) => {
-    const { speedInMs = 25000, className = '', text = 'play', isDisabled = false } = props;
+    const {
+        speedInMs = 25000,
+        className = '',
+        text = 'play',
+        isDisabled = false,
+        onBeforeClick,
+    } = props;
 
     const ScrollButtonId = 'ScrollButton';
     const animationFrameIdRef = useRef<number>(0);
 
     const handleWatchClick = () => {
+        if (onBeforeClick) {
+            onBeforeClick();
+        }
+
         scrollToBottom(speedInMs, animationFrameIdRef);
     };
 
