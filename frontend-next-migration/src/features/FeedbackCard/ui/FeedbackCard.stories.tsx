57cb5e46f232d1d591f1b27502fae07501a3d2a5
SProkopios <119333647+SProkopios@users.noreply.github.com>
2024-12-11 15:44:31 +0200

    SProkopios/feature/293-add-feedback-card-component

---
+++ frontend-next-migration/src/features/FeedbackCard/ui/FeedbackCard.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+import { FeedbackCard } from './FeedbackCard';
+
+export default {
+    title: 'features/FeedbackCard',
+    component: FeedbackCard,
+    args: {
+        className: '',
+    },
+} as ComponentMeta<typeof FeedbackCard>;
+
+const Template: ComponentStory<typeof FeedbackCard> = () => {
+    return (
+        <>
+            {/*<div style={{ height: '2000px' , textAlign: 'center', color: "white" , fontSize: '36px' ,paddingTop: '1rem'}}></div>*/}
+            <FeedbackCard />
+        </>
+    );
+};
+
+export const Default = Template.bind({});
---
+++ frontend-next-migration/src/features/FeedbackCard/ui/FeedbackCard.tsx
@@ -0,0 +1,84 @@
+'use client';
+import { CustomForm } from '@/shared/ui/CustomForm';
+import cls from './FeedbackCard.module.scss';
+import AngerEmoji from '@/shared/assets/icons/Feedback/AngerChatEmoticon.png';
+import JoyEmoji from '@/shared/assets/icons/Feedback/JoyChatEmoticon.png';
+import LoveEmoji from '@/shared/assets/icons/Feedback/LoveChatEmoticon.png';
+import PlayfulEmoji from '@/shared/assets/icons/Feedback/PlayfulChatEmoticon.png';
+import SadEmoji from '@/shared/assets/icons/Feedback/SadnessChatEmoticon.png';
+import send from '@/shared/assets/icons/Feedback/Email Send.png';
+import { useState } from 'react';
+import { useClientTranslation } from '@/shared/i18n';
+
+/**
+ * FeedbackCard component for rendering a basic feedback card.
+ *
+ * @returns {JSX.Element} - The rendered feedback card component.
+ *
+ * @example
+ * <FeedbackCard/>
+ */
+
+export function FeedbackCard() {
+    const { t } = useClientTranslation('feedbackCard');
+
+    const [feedback, setFeedback] = useState<string>('');
+    const [feedbackEmoji, setFeedbackEmoji] = useState<number>();
+
+    const emojies = [
+        { src: AngerEmoji.src, value: 1 },
+        { src: SadEmoji.src, value: 2 },
+        { src: PlayfulEmoji.src, value: 3 },
+        { src: JoyEmoji.src, value: 4 },
+        { src: LoveEmoji.src, value: 5 },
+    ];
+
+    const submitFeedback = () => {
+        //Submit feedback here
+    };
+
+    return (
+        <CustomForm
+            className={cls.feedbackForm}
+            onSubmit={(event) => {
+                event.preventDefault();
+                submitFeedback();
+            }}
+        >
+            <h3 className={cls.feedbackTitle}>{t('title')}</h3>
+            <div className={cls.feedbackEmojies}>
+                {emojies?.map((image, index) => (
+                    <img
+                        src={image.src}
+                        alt={`emoji-${image.value}`}
+                        key={index}
+                        className={image.value === feedbackEmoji ? cls.selectedEmoji : cls.emoji}
+                        onClick={() => setFeedbackEmoji(image.value)}
+                    />
+                ))}
+            </div>
+            <CustomForm.InputField
+                label=""
+                className={cls.textInput}
+                inputProps={{
+                    placeholder: t('input-placeholder'),
+                    onChange: (e) => setFeedback(e.target.value),
+                }}
+            />
+            <CustomForm.Button
+                className={cls.feedbackButton}
+                type="submit"
+            >
+                {' '}
+                <img src={send.src} />
+                {t('send')}
+            </CustomForm.Button>
+            <a
+                className={cls.linkToForm}
+                href=""
+            >
+                {t('href-to-fullform')}
+            </a>
+        </CustomForm>
+    );
+}
