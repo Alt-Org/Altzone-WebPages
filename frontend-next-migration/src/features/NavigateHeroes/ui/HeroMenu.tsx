39c0a171088478a9c0def15b2ccf52cdbb08b354
juha <juhaj358@gmail.com>
2024-12-13 15:02:07 +0200

    added collapse/expand animation

--- frontend-next-migration/src/features/NavigateHeroes/ui/HeroMenu.tsx
+++ frontend-next-migration/src/features/NavigateHeroes/ui/HeroMenu.tsx
@@ -1,9 +1,9 @@
 'use client';
 import React, { useState } from 'react';
 import cls from './HeroMenu.module.scss';
-import { HeroManager, HeroSlug } from '@/entities/Hero';
+import { HeroManager, HeroSlug, Hero } from '@/entities/Hero';
 import { useClientTranslation } from '@/shared/i18n';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 
 interface HeroMenuProps {
     className?: string;
@@ -17,8 +17,50 @@ const HeroMenu: React.FC<HeroMenuProps> = ({ className, onClickCallback, selecte
     const heroManager = new HeroManager(t);
     const allHeroGroups = heroManager.getGroupsWithHeroesAsArray();
 
+    const mods: Mods = {
+        [cls.Hidden]: !visible,
+    };
+    const getHero = (hero: Hero, index: number) => {
+        if (visible) {
+            return selectedHero === hero.slug ? (
+                <div
+                    onClick={() => onClickCallback(hero.slug)}
+                    key={index}
+                    className={cls.Hero}
+                    style={{ color: 'var(--secondary-color)' }}
+                >
+                    {hero.title}
+                </div>
+            ) : (
+                <div
+                    onClick={() => onClickCallback(hero.slug)}
+                    key={index}
+                    className={cls.Hero}
+                >
+                    {hero.title}
+                </div>
+            );
+        } else {
+            return selectedHero === hero.slug ? (
+                <div
+                    key={index}
+                    className={classNames(cls.Hero, mods)}
+                    style={{ color: 'var(--secondary-color)' }}
+                >
+                    {hero.title}
+                </div>
+            ) : (
+                <div
+                    key={index}
+                    className={classNames(cls.Hero, mods)}
+                >
+                    {hero.title}
+                </div>
+            );
+        }
+    };
     const getList = (
-        <div className={classNames(cls.Text, undefined, [className || ''])}>
+        <div className={classNames(cls.Text, mods, [className || ''])}>
             {allHeroGroups.map((group, index) => (
                 <>
                     <div
@@ -27,54 +69,30 @@ const HeroMenu: React.FC<HeroMenuProps> = ({ className, onClickCallback, selecte
                     >
                         {group.name}
                     </div>
-                    {group.heroes.map((hero, index) =>
-                        selectedHero === hero.slug ? (
-                            <div
-                                onClick={() => onClickCallback(hero.slug)}
-                                key={index}
-                                className={cls.Hero}
-                                style={{ color: 'var(--secondary-color)' }}
-                            >
-                                {hero.title}
-                            </div>
-                        ) : (
-                            <div
-                                onClick={() => onClickCallback(hero.slug)}
-                                key={index}
-                                className={cls.Hero}
-                            >
-                                {hero.title}
-                            </div>
-                        ),
-                    )}
+                    {group.heroes.map((hero, index) => getHero(hero, index))}
                 </>
             ))}
         </div>
     );
 
     return (
-        <div className={cls.Box}>
+        <div className={classNames(cls.Box, mods)}>
             {visible ? (
-                <>
-                    <div
-                        className={cls.Arrow}
-                        onClick={() => setVisible(false)}
-                    >
-                        {'<'}
-                    </div>
-                    <div>{getList}</div>
-                </>
+                <div
+                    className={cls.Arrow}
+                    onClick={() => setVisible(false)}
+                >
+                    {'<'}
+                </div>
             ) : (
-                <>
-                    <div
-                        className={cls.Arrow}
-                        onClick={() => setVisible(true)}
-                    >
-                        {'>'}
-                    </div>
-                    <div style={{ width: '0', overflow: 'hidden' }}>{getList}</div>
-                </>
+                <div
+                    className={cls.Arrow}
+                    onClick={() => setVisible(true)}
+                >
+                    {'>'}
+                </div>
             )}
+            {getList}
         </div>
     );
 };
