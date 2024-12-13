575d60db8b8ff4227b7fa6197da1e5342ad37dea
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-13 20:24:18 +0200

    Add active state styling for Navbar navLink items
    Introduced an active state for the `navLink` in the Navbar, based on the current pathname. This includes logic within the component and corresponding styling updates to highlight active links.

--- frontend-next-migration/src/widgets/Navbar/ui/NavbarDesktop/NavItem.tsx
+++ frontend-next-migration/src/widgets/Navbar/ui/NavbarDesktop/NavItem.tsx
@@ -1,5 +1,109 @@
+// import Image from 'next/image';
+// import { memo } from 'react';
+// import { useUserPermissionsV2 } from '@/entities/Auth';
+// import { useClientTranslation } from '@/shared/i18n';
+// import { classNames } from '@/shared/lib/classNames/classNames';
+// import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink/AppLink';
+// import { DropdownWrapper } from '@/shared/ui/DropdownWrapper';
+// import { NavbarBuild, NavbarMenuItem } from '../../model/types';
+// import cls from './NavbarDesktop.module.scss';
+//
+// type NavItemProps = {
+//     item: NavbarMenuItem;
+//     className?: string;
+//     navbarBuild: NavbarBuild;
+// };
+//
+// const NavItem = memo((props: NavItemProps) => {
+//     const { item, className = '', navbarBuild } = props;
+//     const { type: itemType } = item;
+//
+//     const { t } = useClientTranslation('navbar');
+//     const { checkPermissionFor } = useUserPermissionsV2();
+//
+//     if (itemType === 'navLink') {
+//         return (
+//             <li
+//                 key={item.path}
+//                 className={classNames(cls.navItem, {}, [className])}
+//             >
+//                 <AppLink
+//                     theme={AppLinkTheme.PRIMARY}
+//                     to={item.path}
+//                     key={item.path}
+//                 >
+//                     <span>{t(`${item.name}`)}</span>
+//                 </AppLink>
+//             </li>
+//         );
+//     }
+//     if (itemType === 'navDropDown') {
+//         const canUserSeeOwnClan = checkPermissionFor('clan:seeOwn').isGranted;
+//         const localizedElements = item.elements
+//             .map((element) => {
+//                 // @ts-ignore todo add guard
+//                 if (element.elementText === 'clanpage' && !canUserSeeOwnClan) {
+//                     return null;
+//                 }
+//                 return {
+//                     // @ts-ignore todo add guard
+//                     ...element,
+//                     // @ts-ignore todo add guard
+//                     elementText: t(`${element.elementText}`),
+//                 };
+//             })
+//             .filter((element) => element !== null);
+//         return (
+//             <li
+//                 key={item.name}
+//                 className={classNames('', {}, [className])}
+//             >
+//                 <DropdownWrapper
+//                     elements={localizedElements}
+//                     contentAbsolute={true}
+//                     mouseOverLeaveMode={true}
+//                     contentClassName={cls.itemNavbarDropDownContent}
+//                 >
+//                     <div>{t(`${item.name}`)}</div>
+//                 </DropdownWrapper>
+//             </li>
+//         );
+//     }
+//
+//     if (itemType === 'navLogo') {
+//         return (
+//             <li
+//                 key={item.src}
+//                 className={className}
+//             >
+//                 <AppLink
+//                     theme={AppLinkTheme.PRIMARY}
+//                     to={item.path}
+//                     className={classNames(cls.appLink, {}, [cls.appLinkLogo])}
+//                 >
+//                     <Image
+//                         priority={true}
+//                         loading={'eager'}
+//                         alt={navbarBuild?.namedMenu?.navLogo?.name || ''}
+//                         src={navbarBuild?.namedMenu?.navLogo?.src || ''}
+//                         width={120}
+//                         height={0}
+//                         className={cls.itemLogoImg}
+//                     />
+//                 </AppLink>
+//             </li>
+//         );
+//     }
+// });
+//
+// NavItem.displayName = 'NavItem';
+//
+// export default NavItem;
+
+'use client';
 import Image from 'next/image';
 import { memo } from 'react';
+import { usePathname } from 'next/navigation';
 import { useUserPermissionsV2 } from '@/entities/Auth';
 import { useClientTranslation } from '@/shared/i18n';
 import { classNames } from '@/shared/lib/classNames/classNames';
@@ -17,15 +121,20 @@ type NavItemProps = {
 const NavItem = memo((props: NavItemProps) => {
     const { item, className = '', navbarBuild } = props;
     const { type: itemType } = item;
-
     const { t } = useClientTranslation('navbar');
     const { checkPermissionFor } = useUserPermissionsV2();
+    const pathname = usePathname();
+
+    const isActive = itemType === 'navLink' && pathname === item.path;
+
+    // console.log(pathname)
+    // console.log(item)
 
     if (itemType === 'navLink') {
         return (
             <li
                 key={item.path}
-                className={classNames(cls.navItem, {}, [className])}
+                className={classNames(cls.navItem, { [cls.active]: isActive }, [className])}
             >
                 <AppLink
                     theme={AppLinkTheme.PRIMARY}
@@ -37,18 +146,19 @@ const NavItem = memo((props: NavItemProps) => {
             </li>
         );
     }
+
     if (itemType === 'navDropDown') {
         const canUserSeeOwnClan = checkPermissionFor('clan:seeOwn').isGranted;
         const localizedElements = item.elements
             .map((element) => {
-                // @ts-ignore todo add guard
+                // @ts-ignore
                 if (element.elementText === 'clanpage' && !canUserSeeOwnClan) {
                     return null;
                 }
                 return {
-                    // @ts-ignore todo add guard
+                    // @ts-ignore
                     ...element,
-                    // @ts-ignore todo add guard
+                    // @ts-ignore
                     elementText: t(`${element.elementText}`),
                 };
             })
@@ -56,7 +166,7 @@ const NavItem = memo((props: NavItemProps) => {
         return (
             <li
                 key={item.name}
-                className={classNames('', {}, [className])}
+                className={classNames(cls.navItem, {}, [className])}
             >
                 <DropdownWrapper
                     elements={localizedElements}
