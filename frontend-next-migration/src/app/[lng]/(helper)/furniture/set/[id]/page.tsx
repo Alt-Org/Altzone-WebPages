2049097081bcc1bd98ec9ebb7a81c262823da77b
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-14 18:49:06 +0200

    Refactor metadata generation in furniture set page
    Replaces `createMetadataGenerator` with `withMetadataGenerator` for cleaner and more consistent metadata handling. Removes commented-out code to improve code readability and maintainability.

--- frontend-next-migration/src/app/[lng]/(helper)/furniture/set/[id]/page.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/furniture/set/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { withPageData, createMetadataGenerator } from '@/app/_helpers';
+import { withPageData } from '@/app/_helpers';
 import { _getPage } from './_getPage';
 import { FurnitureOneSetPage } from '@/preparedPages/FurniturePages';
 import { withMetadataGenerator } from '@/app/_helpers/createMetadataGenerator';
 
-// @ts-ignore
-// export const generateMetadata = createMetadataGenerator(_getPage);
 export const generateMetadata = withMetadataGenerator(_getPage);
 
 export default withPageData(FurnitureOneSetPage, _getPage);
