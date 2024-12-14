87b6c140f6a5a160c93e19848881556719cf4966
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-14 18:27:13 +0200

    Remove unused server translation import in NotFound component
    The `useServerTranslation` import was unused and has been removed to clean up the code. This improves maintainability and avoids confusion about unnecessary dependencies.

--- frontend-next-migration/src/app/[lng]/(helper)/furniture/set/[id]/not-found.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/furniture/set/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { AppLink } from '@/shared/ui/AppLink/AppLink';
 import { getRouteAllFurnitureSetsPage } from '@/shared/appLinks/RoutePaths';
-import { useClientTranslation, useServerTranslation } from '@/shared/i18n';
+import { useClientTranslation } from '@/shared/i18n';
 
 function NotFound() {
     const { t } = useClientTranslation('furnitureinfo');
