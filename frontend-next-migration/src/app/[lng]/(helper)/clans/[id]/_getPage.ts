7ffcc79bdbd1e86c2dacbff54408f22aec9b0431
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-14 19:32:05 +0200

    Refactor clan page to centralize translations in props
    Centralized translation strings into a unified `translations` prop for better organization and reusability. Updated `_getPage` to handle translation preparation and adjusted `ClanRoomSubPage` to consume the new prop structure. Simplified `page.tsx` by delegating translation logic to `_getPage`.

--- frontend-next-migration/src/app/[lng]/(helper)/clans/[id]/_getPage.ts
+++ frontend-next-migration/src/app/[lng]/(helper)/clans/[id]/_getPage.ts
@@ -1,11 +1,38 @@
 import { createPage } from '@/app/_helpers';
 import { useServerTranslation } from '@/shared/i18n';
+import { ClanRoomSubPageProps } from '@/preparedPages/ClanPages';
 
 export async function _getPage(lng: string, id: string) {
     const { t } = await useServerTranslation(lng, 'clan');
 
-    return createPage({
-        buildPage: () => ({}),
+    return createPage<ClanRoomSubPageProps>({
+        buildPage: () => ({
+            translations: {
+                toastMessages: {
+                    error: t('toast_error'),
+                    notLoggedIn: t('toast_notloggedin'),
+                    clanNotOpen: t('toast_clan_not_open'),
+                    editMode: t('toast_edit_mode'),
+                },
+                buttons: {
+                    joinClan: t('join_clan_btn'),
+                    leaveClan: t('leave_clan_btn'),
+                    editClan: t('edit_clan_btn'),
+                },
+                clanInfo: {
+                    memberListTitle: t('member_list'),
+                    mottoText: t('motto'),
+                    infoText: t('info'),
+                    assetsText: t('assets'),
+                    memberCountText: t('members_count'),
+                    languageText: t('language'),
+                    goalText: t('goal'),
+                    ageLimitText: t('age_limit'),
+                    winsText: t('wins'),
+                    lossesText: t('losses'),
+                },
+            },
+        }),
         buildSeo: () => ({
             title: t('head-title'),
             description: t('head-description'),
