9db2d67d6e8c76014fbfcb88c6312acba32c269c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-12-15 21:23:44 +0200

    "Remove unused import from home page component"
    Cleaned up an unused import of `createMetadataGenerator` in `page.tsx`. This improves code readability and avoids unnecessary dependencies in the file.

--- frontend-next-migration/src/app/[lng]/(home)/page.tsx
+++ frontend-next-migration/src/app/[lng]/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { MainPage } from '@/preparedPages/MainPage';
 import { _getPage } from './_getPage';
-import { withPageData, createMetadataGenerator, withMetadataGenerator } from '@/app/_helpers';
+import { withPageData, withMetadataGenerator } from '@/app/_helpers';
 
 export const generateMetadata = withMetadataGenerator(_getPage);
 export default withPageData(MainPage, _getPage);
