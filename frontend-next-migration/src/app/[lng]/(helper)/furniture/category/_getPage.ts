7cc17dc41745c4100bf0f34261d3ba7c1c294df3
juho236 <juho.toivola@edu.tampere.fi>
2024-12-19 10:26:21 +0200

    moved category page translations to server

--- frontend-next-migration/src/app/[lng]/(helper)/furniture/category/_getPage.ts
+++ frontend-next-migration/src/app/[lng]/(helper)/furniture/category/_getPage.ts
@@ -1,10 +1,24 @@
 import { createPage } from '@/app/_helpers';
 import { useServerTranslation } from '@/shared/i18n';
+import { categories } from '@/entities/Furniture';
 
 export async function _getPage(lng: string) {
     const { t } = await useServerTranslation(lng, 'furniturecategory');
+    const translations: Record<string, string> = {};
+    Object.entries(categories).map((index) => {
+        const name = index[1].name;
+        translations[name] = t(name);
+
+        return true;
+    });
+
     return createPage({
-        buildPage: () => ({}),
+        buildPage: () => ({
+            translations: translations,
+            textback: t('text-back'),
+            textresults: t('results'),
+            textnoresults: t('no-results'),
+        }),
         buildSeo: () => ({
             title: t('head-title'),
             description: t('head-description'),
--- frontend-next-migration/src/preparedPages/FurniturePages/ui/FurnitureCategoryPage/FurnitureCategoryPage.tsx
+++ frontend-next-migration/src/preparedPages/FurniturePages/ui/FurnitureCategoryPage/FurnitureCategoryPage.tsx
@@ -1,22 +1,23 @@
 'use client';
 import { useState, useMemo, useCallback } from 'react';
-import {
-    FurnitureManager,
-    categories,
-    PieceCard,
-    FurnitureCardsContainer,
-} from '@/entities/Furniture';
+import { FurnitureManager, categories, FurnitureCardsContainer } from '@/entities/Furniture';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Container } from '@/shared/ui/Container';
-import { useClientTranslation } from '@/shared/i18n';
 import { Button } from '@/shared/ui/Button';
 import { AppLink } from '@/shared/ui/AppLink/AppLink';
 import { getRouteAllFurnitureSetsPage } from '@/shared/appLinks/RoutePaths';
 import cls from './FurnitureCategoryPage.module.scss';
 
-const FurnitureCategoryPage = () => {
-    const { t } = useClientTranslation('furniturecategory');
+type Props = {
+    translations: Record<string, string>;
+    textback: string;
+    textresults: string;
+    textnoresults: string;
+};
+
+const FurnitureCategoryPage = (props: Props) => {
     const manager = useMemo(() => new FurnitureManager(), []);
+
+    const { translations, textback, textresults, textnoresults } = props;
 
     const [category, setCategory] = useState(categories.CHAIRS);
     const list = useMemo(() => manager.getPiecesByCategory(category), [manager, category]);
@@ -29,10 +30,10 @@ const FurnitureCategoryPage = () => {
                     className={classNames(cls.categoryButton, { [cls.active]: value === category })}
                     onClick={() => setCategory(value)}
                 >
-                    {t(cat)}
+                    {translations[cat]}
                 </Button>
             )),
-        [t, category, cats],
+        [translations, category, cats],
     );
 
     const renderPieceCards = useCallback(() => <FurnitureCardsContainer items={list} />, [list]);
@@ -40,16 +41,16 @@ const FurnitureCategoryPage = () => {
     const hasNoResults = list.length === 0;
 
     const renderContent = useMemo(
-        () => (hasNoResults ? <h3>{t('no-results')}</h3> : renderPieceCards()),
-        [renderPieceCards, t],
+        () => (hasNoResults ? <h3>{textnoresults}</h3> : renderPieceCards()),
+        [renderPieceCards, textnoresults],
     );
 
     return (
         <>
-            <h1 className={cls.title}>{t('results')}</h1>
+            <h1 className={cls.title}>{textresults}</h1>
             <div className={cls.Buttons}>{renderCategoryButtons()}</div>
             <div className={cls.Back}>
-                <AppLink to={getRouteAllFurnitureSetsPage()}>&lt;- {t('text-back')}</AppLink>
+                <AppLink to={getRouteAllFurnitureSetsPage()}>&lt;- {textback}</AppLink>
             </div>
             {renderContent}
         </>
