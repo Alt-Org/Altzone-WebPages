9e99fb6fe762f01206844fde02f237368292ca1c
Lauri <hiltulm@gmail.com>
2025-01-13 16:56:26 +0200

    started making the layout

--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
@@ -1,13 +1,32 @@
-import { ComingSoon } from '@/widgets/ComingSoon';
+'use client';
+import { LayoutWithSidebars } from '@/preparedPages/Layouts';
+// import { ComingSoon } from '@/widgets/ComingSoon';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import cls from './NewsPage.module.scss';
+import useSizes from '@/shared/lib/hooks/useSizes';
 import { WallIntroAnimation } from '@/shared/ui/WallIntroAnimation';
+import cls from './NewsPage.module.scss';
 
 const NewsPage = () => {
+    const { isMobileSize, isTabletSize, isDesktopSize, isWidescreenSize } = useSizes();
     return (
         <WallIntroAnimation>
             <main className={classNames(cls.NewsPage)}>
-                <ComingSoon />
+                <LayoutWithSidebars
+                    leftTopSidebar={{
+                        collapsed: false,
+                        component:
+                            isDesktopSize || isWidescreenSize ? (
+                                <div style={{ width: 'fit-content' }}>
+                                    <p>menu desktop</p>
+                                </div>
+                            ) : (
+                                <p>menu mobile</p>
+                            ),
+                    }}
+                >
+                    <p>Kortit</p>
+                </LayoutWithSidebars>
+                {/* <ComingSoon /> */}
             </main>
         </WallIntroAnimation>
     );
