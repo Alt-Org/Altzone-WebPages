6ad648745b1a7ad0430f884d8c31ef6fa99079ff
Lauri <hiltulm@gmail.com>
2025-01-14 13:04:15 +0200

    updated the layout

--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
@@ -1,30 +1,47 @@
 'use client';
 import { LayoutWithSidebars } from '@/preparedPages/Layouts';
 // import { ComingSoon } from '@/widgets/ComingSoon';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import useSizes from '@/shared/lib/hooks/useSizes';
 import { WallIntroAnimation } from '@/shared/ui/WallIntroAnimation';
+import { Container } from '@/shared/ui/Container';
 import cls from './NewsPage.module.scss';
 
 const NewsPage = () => {
+    const { title } = { title: 'NEWS' };
     const { isMobileSize, isTabletSize, isDesktopSize, isWidescreenSize } = useSizes();
+    const combinedModCss: Mods = {
+        [cls.isMobile]: isMobileSize,
+        [cls.isTablet]: isTabletSize,
+        [cls.isDesktop]: isDesktopSize,
+        [cls.isWidescreen]: isWidescreenSize,
+    };
     return (
         <WallIntroAnimation>
-            <main className={classNames(cls.NewsPage)}>
+            <main className={classNames(cls.NewsPage, combinedModCss)}>
+                <Container className={classNames(cls.TitleAndTabs, combinedModCss)}>
+                    <div style={{ display: 'flex' }}>
+                        <div style={{ minWidth: '220px', flexBasis: '20%', paddingLeft: '20px' }} />
+                        <div style={{ flex: '1 1' }}>
+                            <h1>{title}</h1>
+                        </div>
+                    </div>
+                </Container>
                 <LayoutWithSidebars
                     leftTopSidebar={{
                         collapsed: false,
                         component:
                             isDesktopSize || isWidescreenSize ? (
-                                <div style={{ width: 'fit-content' }}>
-                                    <p>menu desktop</p>
+                                <div style={{ border: '1px solid black', width: '220px' }}>
+                                    <p>categories desktop</p>
                                 </div>
                             ) : (
-                                <p>menu mobile</p>
+                                <p>categories mobile</p>
                             ),
                     }}
+                    className={classNames(cls.NewsAndSideBar, combinedModCss)}
                 >
-                    <p>Kortit</p>
+                    <p>cards</p>
                 </LayoutWithSidebars>
                 {/* <ComingSoon /> */}
             </main>
