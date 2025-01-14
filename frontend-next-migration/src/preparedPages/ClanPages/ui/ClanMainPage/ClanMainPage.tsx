e594b027429f808ffd5ba817b3fe017f06d653e0
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-01-14 21:58:07 +0200

    add ClanInfoTitle component to ClanMainPage for testing

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,12 +1,15 @@
 'use client';
 import { Container } from '@/shared/ui/Container';
 import cls from './ClanMainPage.module.scss';
+import ClanInfoTitle from './clanInfoTitle/ClanInfoTitle';
 
 const ClanMainPage = ({ children }: any) => {
     return (
         <div className={cls.Wrapper}>
             <Container className={cls.Container}>
                 <div className={cls.ClansViewMain}>{children}</div>
+            {/* just for test the component */}
+                <ClanInfoTitle />
             </Container>
         </div>
     );
