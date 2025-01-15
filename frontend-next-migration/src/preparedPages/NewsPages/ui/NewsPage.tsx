ab11e1fa3efbc40a0c23dcc7b2c6c63fc0ed12b9
Lauri <hiltulm@gmail.com>
2025-01-15 14:15:25 +0200

    added the layout

--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { LayoutWithSidebars } from '@/preparedPages/Layouts';
-// import { ComingSoon } from '@/widgets/ComingSoon';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import useSizes from '@/shared/lib/hooks/useSizes';
 import { WallIntroAnimation } from '@/shared/ui/WallIntroAnimation';
@@ -8,7 +7,33 @@ import { Container } from '@/shared/ui/Container';
 import cls from './NewsPage.module.scss';
 
 const NewsPage = () => {
-    const { title } = { title: 'NEWS' };
+    // Here could be the props, now this is just hardcoded data
+    const { title, news } = {
+        title: 'NEWS',
+        news: [
+            {
+                id: 1,
+                heading: '[Heading]',
+                content:
+                    '[body (primary text)] Some very long text. Some very long text. Some very long text. Some very long text. Some very long text. Some very long text.',
+                date: '[small-text/footnotes date and time]',
+            },
+            {
+                id: 2,
+                heading: '[Heading]',
+                content:
+                    '[body (primary text)] Some very long text. Some very long text. Some very long text. Some very long text. Some very long text. Some very long text.',
+                date: '[small-text/footnotes date and time]',
+            },
+            {
+                id: 3,
+                heading: '[Heading]',
+                content:
+                    '[body (primary text)] Some very long text. Some very long text. Some very long text. Some very long text. Some very long text. Some very long text.',
+                date: '[small-text/footnotes date and time]',
+            },
+        ],
+    };
     const { isMobileSize, isTabletSize, isDesktopSize, isWidescreenSize } = useSizes();
     const combinedModCss: Mods = {
         [cls.isMobile]: isMobileSize,
@@ -20,10 +45,38 @@ const NewsPage = () => {
         <WallIntroAnimation>
             <main className={classNames(cls.NewsPage, combinedModCss)}>
                 <Container className={classNames(cls.TitleAndTabs, combinedModCss)}>
-                    <div className={classNames(cls.TitleAlignBox, combinedModCss)}>
-                        <div style={{ minWidth: '220px', flexBasis: '20%' }} />
-                        <div style={{ flex: '1 1' }}>
-                            <h1>{title}</h1>
+                    <div className={classNames(cls.ContentAlignBox, combinedModCss)}>
+                        {!isMobileSize && !isTabletSize && (
+                            <div
+                                className={classNames(cls.ContentAlignBoxLeftBox, combinedModCss)}
+                            />
+                        )}
+                        <div className={classNames(cls.ContentAlignBoxRightBox, combinedModCss)}>
+                            <div>
+                                {/* Title */}
+                                <h1>{title}</h1>
+                            </div>
+                            <div className={classNames(cls.Tabs, combinedModCss)}>
+                                {!isMobileSize && !isTabletSize ? (
+                                    <div>
+                                        {/* Searchbox (desktop, widescreen) */}
+                                        <div>
+                                            <p style={{ border: '1px solid black' }}>Search box</p>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <>
+                                        {/* Categories (mobile, tablet) */}
+                                        <div style={{ border: '1px solid black', width: '100%' }}>
+                                            <p>categories mobile</p>
+                                        </div>
+                                        {/* Searchbox (mobile, tablet) */}
+                                        <div style={{ border: '1px solid black' }}>
+                                            <p>Search box</p>
+                                        </div>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </Container>
@@ -32,14 +85,16 @@ const NewsPage = () => {
                         collapsed: false,
                         component:
                             isDesktopSize || isWidescreenSize ? (
-                                <div
-                                    style={{
-                                        border: '1px solid black',
-                                        width: 'calc(100% - 20px)',
-                                        padding: '1em',
-                                    }}
-                                >
-                                    <div>
+                                // Categories-section (desktop)
+                                <div className={classNames(cls.CategoriesSection, combinedModCss)}>
+                                    {/* Categories (desktop, widescreen)*/}
+                                    <div
+                                        style={{
+                                            border: '1px solid black',
+                                            borderRadius: '12px',
+                                            padding: '1em',
+                                        }}
+                                    >
                                         <h2>Categories</h2>
                                         <p>Category</p>
                                         <p>Category</p>
@@ -48,65 +103,41 @@ const NewsPage = () => {
                                     </div>
                                 </div>
                             ) : (
-                                <p>categories mobile</p>
+                                <div />
                             ),
                     }}
                     className={classNames(cls.NewsAndSideBar, combinedModCss)}
                 >
-                    <div
-                        style={{
-                            display: 'flex',
-                            marginRight: !isMobileSize && !isTabletSize ? '100px' : '0',
-                            flexWrap: 'wrap',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        {/* Cards */}
-                        <div
-                            style={{
-                                border: '1px solid black',
-                                flex: '0 1 49%',
-                                marginBottom: '20px',
-                            }}
-                        >
-                            <div>
-                                <h2>Title</h2>
-                            </div>
-                            <div>
-                                <p>Some very long text. Some very long text.</p>
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                border: '1px solid black',
-                                flex: '0 1 49%',
-                                marginBottom: '20px',
-                            }}
-                        >
-                            <div>
-                                <h2>Title</h2>
-                            </div>
-                            <div>
-                                <p>Some very long text. Some very long text.</p>
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                border: '1px solid black',
-                                flex: '0 1 49%',
-                                marginBottom: '20px',
-                            }}
-                        >
-                            <div>
-                                <h2>Title</h2>
-                            </div>
-                            <div>
-                                <p>Some very long text. Some very long text.</p>
-                            </div>
-                        </div>
+                    {/* News-section (all devices) */}
+                    <div className={classNames(cls.News, combinedModCss)}>
+                        {news.map(
+                            (newsArticle: /* This could have it's own type when working with real data*/ {
+                                id: number;
+                                heading: string;
+                                content: string;
+                                date: string;
+                            }) => (
+                                <div
+                                    key={newsArticle.id}
+                                    className={classNames(cls.NewsArticle, combinedModCss)}
+                                >
+                                    {/* The card*/}
+                                    <div style={{ padding: '1em' }}>
+                                        <div>
+                                            <h2>{newsArticle.heading}</h2>
+                                        </div>
+                                        <div>
+                                            <p>{newsArticle.content}</p>
+                                        </div>
+                                        <div>
+                                            <p>{newsArticle.date}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ),
+                        )}
                     </div>
                 </LayoutWithSidebars>
-                {/* <ComingSoon /> */}
             </main>
         </WallIntroAnimation>
     );
