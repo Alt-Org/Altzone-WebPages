6a44d60380a89faa6cdbf09666666bbd663254b2
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-01-16 21:06:05 +0200

    fetch and display clan data in ClanMainPage using ClanInfoTitle component

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,15 +1,43 @@
+
 'use client';
+import ClanInfoTitle from './clanInfoTitle/ClanInfoTitle';
+import { useGetClansQuery } from '@/entities/Clan';
 import { Container } from '@/shared/ui/Container';
 import cls from './ClanMainPage.module.scss';
-import ClanInfoTitle from './clanInfoTitle/ClanInfoTitle';
 
 const ClanMainPage = ({ children }: any) => {
+    const { data, isLoading, error } = useGetClansQuery({});
+    
+    // Extracting the clans array from the data object this we will  be using to render the clan info later on.
+    // const clansArray = data?.data?.Clan || [];
+
+    //   skipping the first clan cause it's kind of long text and we don't need it for now
+      const clansArray = data?.data?.Clan?.slice(1) || [];
+
     return (
         <div className={cls.Wrapper}>
             <Container className={cls.Container}>
                 <div className={cls.ClansViewMain}>{children}</div>
-            {/* just for test the component */}
-                <ClanInfoTitle />
+                   {/* Loading the clan info component with the data we extracted from the query */}
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : error ? (
+                    <p>Error loading clans</p>
+                ) : clansArray && clansArray.length > 0 ? (
+                    clansArray.map((clan: any) => (
+                        <ClanInfoTitle
+                            key={clan._id}
+                            name={clan.name}
+                            ageRange={clan.ageRange}
+                            playerCount={clan.playerCount}
+                            maxPlayers={clan.maxPlayers}
+                            points={clan.points}
+                            labels={clan.labels}
+                        />
+                    ))
+                ) : (
+                    <p>No clans found</p>
+                )}
             </Container>
         </div>
     );
