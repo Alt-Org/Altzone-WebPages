b0e8b38b8b450326fd2c5ab85c8d7b3920f6de3d
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-01-17 22:59:44 +0200

    improve code formatting

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import ClanInfoTitle from './clanInfoTitle/ClanInfoTitle';
 import { useGetClansQuery } from '@/entities/Clan';
@@ -9,7 +8,7 @@ import cls from './ClanMainPage.module.scss';
  *
  * @param {object} props - The properties for the ClanMainPage component.
  * @param {React.ReactNode} props.children - The children components to be rendered inside the ClanMainPage.
- * 
+ *
  * @returns {JSX.Element} - The rendered ClanMainPage component.
  *
  * @example
@@ -20,18 +19,18 @@ import cls from './ClanMainPage.module.scss';
 
 const ClanMainPage = ({ children }: any) => {
     const { data, isLoading, error } = useGetClansQuery({});
-    
+
     // Extracting the clans array from the data object this we will  be using to render the clan info later on.
     // const clansArray = data?.data?.Clan || [];
 
     //   skipping the first clan cause it's kind of long text and we don't need it for now
-      const clansArray = data?.data?.Clan?.slice(1) || [];
+    const clansArray = data?.data?.Clan?.slice(1) || [];
 
     return (
         <div className={cls.Wrapper}>
             <Container className={cls.Container}>
                 <div className={cls.ClansViewMain}>{children}</div>
-                 {/* this just temporary to show the clan info in the ClanMain page for now when all the components are ready we will remove this. */}
+                {/* this just temporary to show the clan info in the ClanMain page for now when all the components are ready we will remove this. */}
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : error ? (
