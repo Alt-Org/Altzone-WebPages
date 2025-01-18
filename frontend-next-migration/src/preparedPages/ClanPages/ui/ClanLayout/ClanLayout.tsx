639bb3222702f829bf6ec0fb36581745901e741b
Skoivumaki <sakari.koivumaki@gmail.com>
2025-01-18 13:39:01 +0200

    initial testing of nav

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanLayout/ClanLayout.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanLayout/ClanLayout.tsx
@@ -1,7 +1,24 @@
+import { RoutePaths } from '@/shared/appLinks/RoutePaths';
 import cls from './ClanLayout.module.scss';
+import {
+    DropdownItem,
+    NavMenuWithDropdowns,
+    NavMenuWithDropdownsProps,
+} from '@/shared/ui/NavMenuWithDropdowns';
 
 type LayoutProps = {
     children: React.ReactNode;
+};
+
+const navMenuWithDropdownsProps: NavMenuWithDropdownsProps = {
+    title: 'Klaanit',
+    openByDefault: false,
+    dropdownItems: [
+        { elementText: 'Klaanit', link: { path: '/option1-1', isExternal: false } },
+        { elementText: 'Leaderboard', link: { path: '/option1-2', isExternal: false } },
+        { elementText: 'Klaanisivu', link: { path: '/option1-1', isExternal: false } },
+        { elementText: 'Kauppa', link: { path: '/option1-2', isExternal: false } },
+    ],
 };
 
 const ClanMainPageLayout: React.FC<LayoutProps> = ({ children }) => {
@@ -12,8 +29,12 @@ const ClanMainPageLayout: React.FC<LayoutProps> = ({ children }) => {
                     <header className={cls.header}>
                         <h1>Clans</h1>
                     </header>
-                    <nav className={cls.mobileNav}>Mobile Dropdown</nav>
-                    <aside className={cls.sidebar}>Sidebar</aside>
+                    <nav className={cls.mobileNav}>
+                        <NavMenuWithDropdowns {...navMenuWithDropdownsProps} />
+                    </nav>
+                    <aside className={cls.sidebar}>
+                        <NavMenuWithDropdowns {...navMenuWithDropdownsProps} />
+                    </aside>
                 </div>
                 <main className={cls.content}>{children}</main>
             </div>
