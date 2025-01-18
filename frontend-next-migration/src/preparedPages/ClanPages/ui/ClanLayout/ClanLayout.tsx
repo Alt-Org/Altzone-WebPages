6eba64fbb93e241aa32d2beffbce5a132fd6e968
Skoivumaki <sakari.koivumaki@gmail.com>
2025-01-18 16:07:53 +0200

    feat: test titleAsActive for navmenu

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanLayout/ClanLayout.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanLayout/ClanLayout.tsx
@@ -1,7 +1,7 @@
+'use client';
 import { RoutePaths } from '@/shared/appLinks/RoutePaths';
 import cls from './ClanLayout.module.scss';
 import {
-    DropdownItem,
     NavMenuWithDropdowns,
     NavMenuWithDropdownsProps,
 } from '@/shared/ui/NavMenuWithDropdownsV2';
@@ -13,11 +13,12 @@ type LayoutProps = {
 const navMenuWithDropdownsProps: NavMenuWithDropdownsProps = {
     title: 'Klaanit',
     openByDefault: false,
+    titleAsActive: true,
     dropdownItems: [
-        { elementText: 'Klaanit', link: { path: '/option1-1', isExternal: false } },
-        { elementText: 'Leaderboard', link: { path: '/option1-2', isExternal: false } },
-        { elementText: 'Klaanisivu', link: { path: '/option1-1', isExternal: false } },
-        { elementText: 'Kauppa', link: { path: '/option1-2', isExternal: false } },
+        { elementText: 'Selaa Klaaneja', link: { path: '/clans', isExternal: false } },
+        { elementText: 'Leaderboard', link: { path: '/clans/leaderboard', isExternal: false } },
+        { elementText: 'Klaanisivu', link: { path: '/clans/myclan', isExternal: false } },
+        { elementText: 'Kauppa', link: { path: '/store', isExternal: false } },
     ],
 };
 
@@ -26,9 +27,9 @@ const ClanMainPageLayout: React.FC<LayoutProps> = ({ children }) => {
         <div className={cls.container}>
             <div className={cls.layoutContainer}>
                 <div className={cls.headerSidebarContainer}>
-                    <header className={cls.header}>
+                    {/* <header className={cls.header}>
                         <h1>Clans</h1>
-                    </header>
+                    </header> */}
                     <nav className={cls.mobileNav}>
                         <NavMenuWithDropdowns {...navMenuWithDropdownsProps} />
                     </nav>
