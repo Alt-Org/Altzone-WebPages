9590f66a341f0b1181796cef6a3438b516e2be32
Skoivumaki <sakari.koivumaki@gmail.com>
2025-01-18 13:57:42 +0200

    fetched unmodified `NavMenuWithDropdowns` and `DropdownWrapper` files from dev

---
+++ frontend-next-migration/src/shared/ui/DropdownWrapper/types/index.ts
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+
+type Link = {
+    isExternal: boolean;
+    path: string;
+};
+
+export type DropDownElementASTextOrLink = {
+    id?: string;
+    elementText: string;
+    link?: Link;
+    active?: boolean;
+    onClickCallback?: () => void;
+    dataTestId?: string;
+    isDisabled?: {
+        status: boolean;
+        reason: string;
+    };
+    accessErrorMsg?: string;
+};
+
+export type DropDownElement = DropDownElementASTextOrLink | ReactNode;
+
+export type DropdownWrapperProps = {
+    className?: string;
+    contentClassName?: string;
+    contentItemClassName?: string;
+    childrenWrapperClassName?: string;
+    contentAbsolute?: boolean;
+    elements: Array<DropDownElement>;
+    onOpen?: () => void;
+    onClose?: () => void;
+    children: ReactNode;
+    dataTestId?: string;
+    mouseOverLeaveMode?: boolean;
+    isDisabled?: {
+        status: boolean;
+        reason: string;
+    };
+    openByDefault?: boolean;
+};
