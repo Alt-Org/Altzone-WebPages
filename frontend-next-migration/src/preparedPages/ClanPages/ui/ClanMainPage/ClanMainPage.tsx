b919154b4aaa24f1a28b7aa1f3c2f3339e8babc2
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-01-21 19:36:17 +0200

    fix import case sensitivity and clean up comments in ClanMainPage component

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,5 +1,5 @@
 'use client';
-import ClanInfoTitle from './clanInfoTitle/ClanInfoTitle';
+import ClanInfoTitle from './clanInfoTitle/clanInfoTitle';
 import { useGetClansQuery } from '@/entities/Clan';
 import { Container } from '@/shared/ui/Container';
 import cls from './ClanMainPage.module.scss';
@@ -20,11 +20,9 @@ import cls from './ClanMainPage.module.scss';
 const ClanMainPage = ({ children }: any) => {
     const { data, isLoading, error } = useGetClansQuery({});
 
-    // Extracting the clans array from the data object this we will  be using to render the clan info later on.
-    // const clansArray = data?.data?.Clan || [];
+    // Extracting the clans array from the data object.
+    const clansArray = data?.data?.Clan || [];
 
-    //   skipping the first clan cause it's kind of long text and we don't need it for now
-    const clansArray = data?.data?.Clan?.slice(1) || [];
 
     return (
         <div className={cls.Wrapper}>
