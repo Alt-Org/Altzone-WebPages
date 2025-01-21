9788f2c7be43557670b3436f724ed394866fbc42
Lauri <hiltulm@gmail.com>
2025-01-21 15:45:33 +0200

    updated modularcard

---
+++ frontend-next-migration/src/shared/ui/v2/ModularCard/ui/ModularCard.stories.tsx
@@ -0,0 +1,108 @@
+import { Meta, StoryObj } from '@storybook/react';
+//import hannu from '@/shared/assets/images/heros/hannu-hodari/hannu-hodari.png';
+import {
+    ModularCard,
+    ModularCardTheme,
+    ModularCardContent,
+    ModularCardContainer,
+    ModularCardTitle,
+    ModularCardBody,
+    ModularCardFooter,
+} from './ModularCard'; // Import your component
+
+const meta = {
+    title: 'shared/ui/v2/ModularCard',
+    component: ModularCard,
+    argTypes: {
+        className: {
+            control: 'text',
+            description: 'Additional CSS class names to apply for custom styling.',
+            defaultValue: '',
+        },
+        theme: {
+            control: { type: 'select', options: Object.values(ModularCardTheme) },
+            description: 'Theme for the Card.',
+            defaultValue: ModularCardTheme.PRIMARY,
+        },
+        children: {
+            control: 'text',
+            description: 'The content inside the Card.',
+        },
+    },
+    args: {
+        className: '',
+        theme: ModularCardTheme.PRIMARY,
+        children: 'Card Content',
+    },
+    tags: ['autodocs'],
+    parameters: {
+        backgrounds: {
+            default: 'dark',
+            values: [
+                { name: 'light', value: '#ffffff' },
+                { name: 'dark', value: '#333333' },
+                { name: 'gray', value: '#dddddd' },
+            ],
+        },
+        layout: 'centered',
+        docs: {
+            description: {
+                component:
+                    'The `Card` component is a versatile container with customizable subcomponents for Title, Body, Date, and ReadMoreLink.',
+            },
+        },
+    },
+} satisfies Meta<typeof ModularCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        className: 'customClass',
+        theme: ModularCardTheme.TITLEIMAGE,
+        children: [
+            <ModularCardContent
+                theme={ModularCardTheme.TITLEIMAGE}
+                key={0}
+            >
+                <ModularCardContainer theme={ModularCardTheme.TITLEIMAGE}>
+                    <ModularCardTitle theme={ModularCardTheme.TITLEIMAGE}>Otsikko</ModularCardTitle>
+                    <ModularCardBody theme={ModularCardTheme.TITLEIMAGE}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni assumenda,
+                        perspiciatis quidem neque minus, aperiam a reiciendis nesciunt ea
+                        perferendis quisquam explicabo reprehenderit consequatur? Eligendi vel
+                        tenetur molestias corporis! Ab!
+                    </ModularCardBody>
+                    <ModularCardFooter theme={ModularCardTheme.TITLEIMAGE}>
+                        Footer
+                    </ModularCardFooter>
+                </ModularCardContainer>
+                <ModularCardContainer theme={ModularCardTheme.TITLEIMAGE}>
+                    <p>s</p>
+                </ModularCardContainer>
+            </ModularCardContent>,
+        ],
+    },
+};
+
+// export const WithCustomLink: Story = {
+//     args: {
+//         className: 'customClass',
+//         theme: ModularCardTheme.PRIMARY,
+//         children: (
+//             <>
+//                 <ModularCard.Title>Another Card</ModularCard.Title>
+//                 <ModularCard.Body>This is another example of card content.</Card.Body>
+//                 <ModularCard.Date>2023-11-05</ModularCard.Date>
+//                 <ModularCard.ReadMoreLink
+//                     path="https://example.com"
+//                     isExternal
+//                 >
+//                     Visit External Link
+//                 </ModularCard.ReadMoreLink>
+//             </>
+//         ),
+//     },
+// };
---
+++ frontend-next-migration/src/shared/ui/v2/ModularCard/ui/ModularCard.tsx
@@ -0,0 +1,199 @@
+import { FC, memo, ReactNode } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { AppLink } from '@/shared/ui/AppLink/AppLink';
+import cls from './ModularCard.module.scss';
+
+/**
+ * Module containing a React CardTheme component with customizable themes, sizes, and square styling.
+ * @module Card
+ */
+export enum ModularCardTheme {
+    PRIMARY = '',
+    TITLEIMAGE = 'TitleImageCard',
+    NEWSIMAGE = 'NewsImageCard',
+}
+
+/**
+ * Props for the Card component.
+ * {Object} CardProps
+ * @property {string} [className=""] - Additional class name(s) for the Card.
+ * @property {CardTheme} [theme=CardTheme.PRIMARY] - Theme for the Card.
+ * @property {ReactNode} children - The content of the Card.
+ *
+ * @example
+ * <Card className="customClass" theme={CardTheme.PRIMARY}>
+ *    <Card.Title>Title</Card.Title>
+ *    <Card.Body>Body</Card.Body>
+ *    <Card.Date>2023-10-01</Card.Date>
+ *    <Card.ReadMoreLink path="/details" withScalableLink>
+ *        Read More
+ *    </Card.ReadMoreLink>
+ * </Card>
+ */
+interface CardProps {
+    className?: string;
+    theme?: ModularCardTheme;
+    children: ReactNode;
+}
+
+interface CardCompoundProps {
+    className?: string;
+    children: ReactNode;
+}
+
+interface ReadMoreLinkProps extends CardCompoundProps {
+    path: string;
+    isExternal?: boolean;
+    withScalableLink?: boolean;
+}
+
+/**
+ * Card component with composable subcomponents.
+ * @component
+ *
+ * @example
+ * <Card className="customClass" theme={CardTheme.PRIMARY}>
+ *    <Card.Title>Title</Card.Title>
+ *    <Card.Body>Body</Card.Body>
+ *    <Card.Date>2023-10-01</Card.Date>
+ *    <Card.ReadMoreLink path="/details" withScalableLink>
+ *        Read More
+ *    </Card.ReadMoreLink>
+ * </Card>
+ */
+interface CardComponent extends FC<CardProps> {
+    Title: FC<CardCompoundProps>;
+    Body: FC<CardCompoundProps>;
+    Date: FC<CardCompoundProps>;
+    ReadMoreLink: FC<ReadMoreLinkProps>;
+}
+
+/* 
+a
+a
+a
+a
+a
+a
+a
+a
+*/
+const ModularCard = (props: CardProps) => {
+    const { className = '', theme = ModularCardTheme.PRIMARY, children } = props;
+    return <div className={classNames(cls.Card, {}, [className, cls[theme]])}>{children}</div>;
+};
+
+const ModularCardContent = memo((props: CardProps) => {
+    const { className = '', theme = ModularCardTheme.PRIMARY, children } = props;
+    return (
+        <div className={classNames(cls.ModularCardBase, {}, [className, cls[theme]])}>
+            {children}
+        </div>
+    );
+});
+
+ModularCardContent.displayName = 'ModularCardContent';
+
+const ModularCardContainer = memo((props: CardProps) => {
+    const { className = '', theme = ModularCardTheme.PRIMARY, children } = props;
+    return (
+        <div className={classNames(cls.ModularCardContainer, {}, [className, cls[theme]])}>
+            {children}
+        </div>
+    );
+});
+
+ModularCardContainer.displayName = 'ModularCardContainer';
+
+const ModularCardTitle = memo((props: CardProps) => {
+    const { className = '', theme = ModularCardTheme.PRIMARY, children } = props;
+    return (
+        <div className={classNames(cls.ModularCardTitle, {}, [className, cls[theme]])}>
+            <h2>{children}</h2>
+        </div>
+    );
+});
+
+ModularCardTitle.displayName = 'ModularCardTitle';
+
+const ModularCardBody = memo((props: CardProps) => {
+    const { className = '', theme = ModularCardTheme.PRIMARY, children } = props;
+    return (
+        <div className={classNames(cls.ModularCardBody, {}, [className, cls[theme]])}>
+            <p>{children}</p>
+        </div>
+    );
+});
+
+ModularCardBody.displayName = 'ModularCardBody';
+
+const ModularCardFooter = memo((props: CardProps) => {
+    const { className = '', theme = ModularCardTheme.PRIMARY, children } = props;
+    return (
+        <div className={classNames(cls.ModularCardFooter, {}, [className, cls[theme]])}>
+            <p>{children}</p>
+        </div>
+    );
+});
+
+ModularCardFooter.displayName = 'ModularCardFooter';
+
+/* 
+a
+a
+a
+a
+a
+a
+a
+a
+*/
+export {
+    ModularCardContent,
+    ModularCardContainer,
+    ModularCardTitle,
+    ModularCardBody,
+    ModularCardFooter,
+};
+/**
+ * Card.Title component for the Card.
+ * @component
+ * @param {CardCompoundProps} props - Props for the component.
+ *
+ * @example
+ * <Card.Title className="customTitleClass">Card Title</Card.Title>
+ */
+
+/**
+ * Card.ReadMoreLink component for the Card.
+ * @component
+ * @param {ReadMoreLinkProps} props - Props for the component.
+ *
+ * @example
+ * <Card.ReadMoreLink path="/details" className="customLinkClass" withScalableLink>
+ *    Read More
+ * </Card.ReadMoreLink>
+ */
+const ReadMoreLink = memo((props: ReadMoreLinkProps) => {
+    const { children, className = '', path, isExternal = false, withScalableLink = false } = props;
+    const mods: Record<string, boolean> = {
+        [cls.withScalableLink]: withScalableLink,
+    } as Record<string, boolean>;
+    return (
+        <AppLink
+            to={path}
+            isExternal={isExternal}
+        >
+            <h2 className={classNames(cls.ReadMoreLink, mods, [className])}>{children}</h2>
+        </AppLink>
+    );
+});
+
+ReadMoreLink.displayName = 'card-ReadMoreLink';
+
+// Card.Title = CardTitle;
+// Card.Body = CardBody;
+// Card.Date = CardDate;
+// Card.ReadMoreLink = ReadMoreLink;
+
+export { ModularCard };
