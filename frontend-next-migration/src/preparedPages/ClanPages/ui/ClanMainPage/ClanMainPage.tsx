f316a10e4f1d3d8d9120ca9d283ec36d82efbac7
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-01-22 22:54:19 +0200

    Remove unused ClanInfoTile import and related loading logic from ClanMainPage component

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanMainPage/ClanMainPage.tsx
@@ -1,52 +1,12 @@
 'use client';
-import { useGetClansQuery } from '@/entities/Clan';
 import { Container } from '@/shared/ui/Container';
 import cls from './ClanMainPage.module.scss';
-import ClanInfoTile from './clanInfoTile/ClanInfoTile';
-/**
- * ClanMainPage component for rendering the main page with clan information.
- *
- * @param {object} props - The properties for the ClanMainPage component.
- * @param {React.ReactNode} props.children - The children components to be rendered inside the ClanMainPage.
- *
- * @returns {JSX.Element} - The rendered ClanMainPage component.
- *
- * @example
- * <ClanMainPage>
- *   <SomeChildComponent />
- * </ClanMainPage>
- */
 
 const ClanMainPage = ({ children }: any) => {
-    const { data, isLoading, error } = useGetClansQuery({});
-
-    // Extracting the clans array from the data object.
-    const clansArray = data?.data?.Clan || [];
-
     return (
         <div className={cls.Wrapper}>
             <Container className={cls.Container}>
                 <div className={cls.ClansViewMain}>{children}</div>
-                {/* this just temporary to show the clan info in the ClanMain page for now when all the components are ready we will remove this. */}
-                {isLoading ? (
-                    <p>Loading...</p>
-                ) : error ? (
-                    <p>Error loading clans</p>
-                ) : clansArray && clansArray.length > 0 ? (
-                    clansArray.map((clan: any) => (
-                        <ClanInfoTile
-                            key={clan._id}
-                            name={clan.name}
-                            ageRange={clan.ageRange}
-                            playerCount={clan.playerCount}
-                            points={clan.points}
-                            labels={clan.labels}
-                            id={clan._id}
-                        />
-                    ))
-                ) : (
-                    <p>No clans found</p>
-                )}
             </Container>
         </div>
     );
