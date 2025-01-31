c77d16383410275fefbaf0a5396e69d9039d6d5a
jartsanull <langinenj1@gmail.com>
2025-01-31 13:01:05 +0200

    Update Aapobot.tsx

--- frontend-next-migration/src/features/ChatBot/Aapobot.tsx
+++ frontend-next-migration/src/features/ChatBot/Aapobot.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 The central functional file of the bot that renders the whole thing
  */
 
-export default function Home() {
+export default function Aapobot() {
     return (
         <div>
 
