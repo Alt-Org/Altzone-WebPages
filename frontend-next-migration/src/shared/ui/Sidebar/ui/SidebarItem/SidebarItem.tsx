25028e20fb61573455d2133937bdf8c83af390af
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-02-08 02:40:30 +0200

    feat: enhance SidebarItem to manage dropdown state with openDropdown and setOpenDropdown props.

--- frontend-next-migration/src/shared/ui/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ frontend-next-migration/src/shared/ui/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -9,24 +9,28 @@ interface SidebarItemProps {
     item: ISidebarItem;
     collapsed: boolean;
     active?: boolean;
+    openDropdown: string | null;
+    setOpenDropdown: (name: string | null) => void;
 }
 
 /**
  * Renders a sidebar item, which can either be a basic item or a dropdown.
- *
  * @param {SidebarItemProps} props - The props for the component.
  * @param {ISidebarItem} props.item - The item to display in the sidebar.
  * @param {boolean} props.collapsed - Whether the sidebar is collapsed or not.
+ * @param {boolean} [props.active=false] - Whether the item is active or not.
+ * @param {string | null} props.openDropdown - The name of the currently open dropdown.
+ * @param {function} props.setOpenDropdown - Function to set the currently open dropdown.
  *
  * @example
- * // Rendering a basic sidebar item:
+ * Rendering a basic sidebar item:
  * <SidebarItem
  *    item={{ type: sidebarItemType.ISidebarItemBasic, path: '/home', name: 'Home' }}
  *    collapsed={false}
  * />
  *
  * @example
- * // Rendering a dropdown sidebar item:
+ * Rendering a dropdown sidebar item:
  * <SidebarItem
  *    item={{
  *        type: sidebarItemType.ISidebarItemDropDown,
@@ -34,38 +38,53 @@ interface SidebarItemProps {
  *        elements: [{ elementText: 'Profile', link: {path: '/profile'} }, { elementText: 'Logout', onClickCallback: handleLogout }]
  *    }}
  *    collapsed={true}
+ *    openDropdown={openDropdown}
+ *    setOpenDropdown={setOpenDropdown}
  * />
  */
-export const SidebarItem = memo(({ item, collapsed, active = false }: SidebarItemProps) => {
-    if (item.type === sidebarItemType.ISidebarItemBasic) {
-        return (
-            <AppLink
-                theme={AppLinkTheme.PRIMARY}
-                to={item.path}
-                className={classNames(
-                    cls.item,
-                    { [cls.collapsed]: collapsed, [cls.active]: active },
-                    [cls.itemBasic],
-                )}
-            >
-                <span className={cls.link}>{item.name}</span>
-            </AppLink>
-        );
-    }
 
-    if (item.type === sidebarItemType.ISidebarItemDropDown) {
-        return (
-            <DropdownWrapper
-                className={classNames(cls.itemDropDown, { [cls.active]: active })}
-                contentItemClassName={cls.item}
-                contentClassName={classNames(cls.link, { [cls.collapsed]: collapsed })}
-                elements={item.elements}
-            >
-                {item.name}
-            </DropdownWrapper>
-        );
-    }
-    return null;
-});
+export const SidebarItem = memo(
+    ({ item, collapsed, active = false, openDropdown, setOpenDropdown }: SidebarItemProps) => {
+        const handleDropdownClick = () => {
+            if (openDropdown === item.name) {
+                setOpenDropdown(null);
+            } else {
+                setOpenDropdown(item.name);
+            }
+        };
+
+        if (item.type === sidebarItemType.ISidebarItemBasic) {
+            return (
+                <AppLink
+                    theme={AppLinkTheme.PRIMARY}
+                    to={item.path}
+                    className={classNames(
+                        cls.item,
+                        { [cls.collapsed]: collapsed, [cls.active]: active },
+                        [cls.itemBasic],
+                    )}
+                >
+                    <span className={cls.link}>{item.name}</span>
+                </AppLink>
+            );
+        }
+
+        if (item.type === sidebarItemType.ISidebarItemDropDown) {
+            return (
+                <DropdownWrapper
+                    className={classNames(cls.itemDropDown, { [cls.active]: active })}
+                    contentItemClassName={cls.item}
+                    contentClassName={classNames(cls.link, { [cls.collapsed]: collapsed })}
+                    elements={item.elements}
+                    isOpen={openDropdown === item.name}
+                    onClick={handleDropdownClick}
+                >
+                    {item.name}
+                </DropdownWrapper>
+            );
+        }
+        return null;
+    },
+);
 
 SidebarItem.displayName = 'SidebarItem';
