3037f5c1fd330bd0a2ca3495ce46844ce86a3a6f
SProkopios <119333647+SProkopios@users.noreply.github.com>
2025-02-12 17:23:37 +0200

    Fixing styling and font

--- frontend-next-migration/src/shared/ui/v2/Feedback/ui/FeedbackCard/FeedbackCard.tsx
+++ frontend-next-migration/src/shared/ui/v2/Feedback/ui/FeedbackCard/FeedbackCard.tsx
@@ -12,12 +12,6 @@ import { useClientTranslation } from '@/shared/i18n';
 import { AppExternalLinks } from '@/shared/appLinks/appExternalLinks';
 import cls from './FeedbackCard.module.scss';
 import FeedbackEmoji from '../FeedbackEmoji/FeedbackEmoji';
-import { Sedgwick_Ave_Display } from 'next/font/google';
-
-const sedgwickFont = Sedgwick_Ave_Display({
-    subsets: ['latin'],
-    weight: '400',
-});
 
 export default function FeedbackCard() {
     const { t } = useClientTranslation('feedbackCard');
@@ -62,7 +56,7 @@ export default function FeedbackCard() {
                 await submitFeedback();
             }}
         >
-            <h3 className={`${cls.feedbackTitle} ${sedgwickFont.className}`}>{t('title')}</h3>
+            <h3 className={cls.feedbackTitle}>{t('title')}</h3>
             <FeedbackEmoji
                 listClassName={cls.emojiList}
                 value={feedbackEmoji}
