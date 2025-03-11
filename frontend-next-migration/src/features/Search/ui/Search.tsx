1be2205fc8619308f7fc7bb1ccfb63c33c34922f
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-11 13:26:48 +0200

    Add SearchInput component with custom styling and icon integration.

---
+++ frontend-next-migration/src/features/Search/ui/Search.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import search from '@/shared/assets/icons/search.png';
+import { CustomForm } from '@/shared/ui/CustomForm';
+import cls from './Search.module.scss';
+interface SearchInputProps {
+    placeholder?: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const SearchInput: React.FC<SearchInputProps> = ({ placeholder, value, onChange }) => {
+    return (
+        <div className={cls.searchContainer}>
+            <CustomForm.InputField
+                label=""
+                inputProps={{
+                    placeholder: 'Hea',
+                    className: cls.searchInput,
+                    value: value,
+                    onChange: onChange,
+                    style: {
+                        backgroundImage: `url(${search.src})`,
+                        backgroundPosition: 'left 10px center',
+                        backgroundRepeat: 'no-repeat',
+                        paddingLeft: '40px',
+                        backgroundSize: '20px',
+                    },
+                }}
+            />
+        </div>
+    );
+};
