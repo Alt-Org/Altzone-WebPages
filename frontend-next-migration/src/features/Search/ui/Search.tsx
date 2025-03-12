8403ea3db0bd796ca84ade8599e807ec184fc437
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-12 11:03:28 +0200

    Remove unused placeholder prop from SearchInput component.

--- frontend-next-migration/src/features/Search/ui/Search.tsx
+++ frontend-next-migration/src/features/Search/ui/Search.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import search from '@/shared/assets/icons/search.png';
 import { CustomForm } from '@/shared/ui/CustomForm';
 import cls from './Search.module.scss';
+
 interface SearchInputProps {
     placeholder?: string;
     value: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const SearchInput: React.FC<SearchInputProps> = ({ placeholder, value, onChange }) => {
+export const SearchInput: React.FC<SearchInputProps> = ({ value, onChange }) => {
     return (
         <div className={cls.searchContainer}>
             <CustomForm.InputField
