e599cffb4d7cadd98ba7dd04d43fe05ec377554f
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-12 11:07:53 +0200

    Refactor component for improved readability and structure.

--- frontend-next-migration/src/app/[lng]/(helper)/news/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/news/layout.tsx
@@ -1,34 +1,40 @@
 'use client';
 import { LayoutWithSidebars } from '@/preparedPages/Layouts';
-import { cls } from '@/preparedPages/PictureGalleryPages';
 import { NavMenuWithDropdowns } from '@/shared/ui/NavMenuWithDropdownsV2';
-
 
 export default function NewsLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <LayoutWithSidebars
                 leftTopSidebar={{
-                component: (
-                    <NavMenuWithDropdowns
-                        title="Categories"
-                        dropdownItems={[
-                            { elementText: 'All News', link: { path: '/news', isExternal: false } },
-                            { elementText: 'Updates', link: { path: '/news/', isExternal: false } },
-                            { elementText: 'Events', link: { path: '/news/', isExternal: false } },
-                            {
-                                elementText: 'Announcements',
-                                link: { path: '/news/', isExternal: false },
-                            },
-                        ]}
-                        openByDefault={false}
-                    />
-                ),
-            }}
-        >
-            {children}
-        </LayoutWithSidebars>
-    </div>
-
+                    component: (
+                        <NavMenuWithDropdowns
+                            title="Categories"
+                            dropdownItems={[
+                                {
+                                    elementText: 'All News',
+                                    link: { path: '/news', isExternal: false },
+                                },
+                                {
+                                    elementText: 'Updates',
+                                    link: { path: '/news/', isExternal: false },
+                                },
+                                {
+                                    elementText: 'Events',
+                                    link: { path: '/news/', isExternal: false },
+                                },
+                                {
+                                    elementText: 'Announcements',
+                                    link: { path: '/news/', isExternal: false },
+                                },
+                            ]}
+                            openByDefault={false}
+                        />
+                    ),
+                }}
+            >
+                {children}
+            </LayoutWithSidebars>
+        </div>
     );
 }
