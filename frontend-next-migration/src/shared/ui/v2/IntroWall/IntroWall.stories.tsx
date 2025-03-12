be0a9c68327c6e4dfcb367b32c9616f70ac93e34
SProkopios <119333647+SProkopios@users.noreply.github.com>
2025-03-12 10:21:09 +0200

    Adding test and stories

---
+++ frontend-next-migration/src/shared/ui/v2/IntroWall/IntroWall.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import IntroWall from './IntroWall';
+
+/**
+ * Storybook configuration for the `IntroWall` component.
+ * The `IntroWall` component renders animation of crumbling wall.
+ */
+const meta = {
+    title: 'features/v2/IntroWall',
+    component: IntroWall,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component:
+                    'The `IntroWall` component renders an opening animation of crumbling wall',
+            },
+        },
+    },
+    tags: ['autodocs'],
+    argTypes: {},
+} satisfies Meta<typeof IntroWall>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/**
+ * Default story for the `IntroWall` component.
+ * renders IntroWall animation
+ */
+export const Default: Story = {
+    args: {},
+    parameters: {
+        docs: {
+            description: {
+                story: 'Displays the `IntroWall` component',
+            },
+        },
+    },
+};
