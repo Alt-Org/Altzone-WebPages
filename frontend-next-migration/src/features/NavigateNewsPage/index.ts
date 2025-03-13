7c19ec8782298b214e6767a338c1271c298c0b12
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-13 21:57:06 +0200

    feat: add navigation components for news page

---
+++ frontend-next-migration/src/features/NavigateNewsPage/index.ts
@@ -0,0 +1,4 @@
+export { default as NewsPageNavMenuAsDropdown } from './ui/NewsPageNavMenuAsDropdown';
+export { default as NewsNavMenuAsDropdown } from './ui/NewsNavMenuAsDropdown';
+export { default as NewsNavMenu } from './ui/NewsNavMenu';
+export { default as NewsSidebar } from './ui/NewsSidebar';
