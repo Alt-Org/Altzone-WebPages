46b10d717e874f593009e2204af5fb34c347f096
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-13 21:46:57 +0200

    Refactor: replace NavMenuWithDropdowns with NewsPageNavMenuAsDropdown in NewsLayout component

--- frontend-next-migration/src/app/[lng]/(helper)/news/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/news/layout.tsx
@@ -1,36 +1,13 @@
 'use client';
 import { LayoutWithSidebars } from '@/preparedPages/Layouts';
-import { NavMenuWithDropdowns } from '@/shared/ui/NavMenuWithDropdownsV2';
+import { NewsPageNavMenuAsDropdown } from '@/features/NavigateNewsPage';
 
 export default function NewsLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <LayoutWithSidebars
                 leftTopSidebar={{
-                    component: (
-                        <NavMenuWithDropdowns
-                            title="Categories"
-                            dropdownItems={[
-                                {
-                                    elementText: 'All News',
-                                    link: { path: '/news', isExternal: false },
-                                },
-                                {
-                                    elementText: 'Updates',
-                                    link: { path: '/news/', isExternal: false },
-                                },
-                                {
-                                    elementText: 'Events',
-                                    link: { path: '/news/', isExternal: false },
-                                },
-                                {
-                                    elementText: 'Announcements',
-                                    link: { path: '/news/', isExternal: false },
-                                },
-                            ]}
-                            openByDefault={false}
-                        />
-                    ),
+                    component: <NewsPageNavMenuAsDropdown />,
                 }}
             >
                 {children}
