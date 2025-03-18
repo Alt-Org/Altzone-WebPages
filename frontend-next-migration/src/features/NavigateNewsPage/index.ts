a4abb5d0368399426e9b998bcf76db7ec2cf8f16
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-18 22:17:40 +0200

    fix: remove unused export for NewsNavMenuAsDropdown in NavigateNewsPage

--- frontend-next-migration/src/features/NavigateNewsPage/index.ts
+++ frontend-next-migration/src/features/NavigateNewsPage/index.ts
@@ -1,4 +1,4 @@
 export { default as NewsPageNavMenuAsDropdown } from './ui/NewsPageNavMenuAsDropdown';
-export { default as NewsNavMenuAsDropdown } from './ui/NewsNavMenuAsDropdown';
+// export { default as NewsNavMenuAsDropdown } from './ui/NewsNavMenuAsDropdown';
 export { default as NewsNavMenu } from './ui/NewsNavMenu';
 export { default as NewsSidebar } from './ui/NewsSidebar';
