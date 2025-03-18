48dbe9cbb8dc791639a50acf25c72f6e7f77feca
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-19 00:12:48 +0200

    feat: add NewsCard component for displaying news articles

---
+++ frontend-next-migration/src/widgets/NewsCard/ui/NewsCard.tsx
@@ -0,0 +1,47 @@
+import { memo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './NewsCard.module.scss';
+import Image from 'next/image';
+import hannu from '@/shared/assets/images/heros/hannu-hodari/hannu-hodari.png';
+
+interface NewsCardProps {
+    className?: string;
+    title: string;
+    content: string;
+    date: string;
+    id: number;
+    imageSrc?: string;
+}
+
+const NewsCard = (props: NewsCardProps) => {
+    const { title, date, id, content, imageSrc = hannu.src } = props;
+    return (
+        <div
+            // href={getRouteOneNewsPage(id.toString())}
+            className={classNames(cls.NewsCard, {})}
+        >
+            <div className={cls.content}>
+                <h3 className={cls.title}>{title}</h3>
+                <p className={cls.text}>{content}</p>
+                <span className={cls.date}>{date}</span>
+            </div>
+
+            <div className={cls.imageContainer}>
+                <div className={cls.imageWrapper}>
+                    <Image
+                        src={imageSrc}
+                        alt={title}
+                        className={cls.image}
+                        layout="responsive"
+                        width={200}
+                        height={554}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+NewsCard.displayName = 'NewsCard';
+
+export default memo(NewsCard);
