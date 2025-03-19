b1759fdfb2f1b1f32f8dd1da35df4e6b20f5c8ae
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-19 02:42:31 +0200

    refactor: remove unused NewsNavMenu exports from index file

--- frontend-next-migration/src/features/NavigateNewsPage/index.ts
+++ frontend-next-migration/src/features/NavigateNewsPage/index.ts
@@ -1,4 +1,2 @@
 export { default as NewsPageNavMenuAsDropdown } from './ui/NewsPageNavMenuAsDropdown';
-// export { default as NewsNavMenuAsDropdown } from './ui/NewsNavMenuAsDropdown';
-export { default as NewsNavMenu } from './ui/NewsNavMenu';
 export { default as NewsSidebar } from './ui/NewsSidebar';
