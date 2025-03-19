ccef9db8c65a6d09ffa9b0d23b3cbcbf7241f3d4
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-19 03:33:08 +0200

    fix: update import path for NewsElementPage component

--- frontend-next-migration/src/app/[lng]/(helper)/news/[id]/page.ts
+++ frontend-next-migration/src/app/[lng]/(helper)/news/[id]/page.ts
@@ -1,4 +1,4 @@
 // import { withMetadataGenerator } from "@/app/_helpers";
-export { NewsElementPage as default } from '@/preparedPages/NewsPages';
+export { NewsElementPage as default } from '@/widgets/NewsElementPage';
 
 // export const generateMetadata = withMetadataGenerator(_getPage);
