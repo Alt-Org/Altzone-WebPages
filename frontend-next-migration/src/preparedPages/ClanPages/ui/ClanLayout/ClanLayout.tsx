b4a68821970efa3d340312966e2dc4354c23370d
Skoivumaki <sakari.koivumaki@gmail.com>
2025-03-20 20:20:07 +0200

    refactor: moving pageTitle to its own component. Fixed bug with `NavMenuWithDropdown` props failing to keep language when redirecting.

--- frontend-next-migration/src/preparedPages/ClanPages/ui/ClanLayout/ClanLayout.tsx
+++ frontend-next-migration/src/preparedPages/ClanPages/ui/ClanLayout/ClanLayout.tsx
@@ -3,45 +3,20 @@ import {
     NavMenuWithDropdowns,
     NavMenuWithDropdownsProps,
 } from '@/shared/ui/NavMenuWithDropdownsV2';
-import { Container } from '@/shared/ui/Container';
 import cls from './ClanLayout.module.scss';
 import { useClientTranslation } from '@/shared/i18n';
-
-type PageTitleProps = {
-    titleText: string;
-    searchVisible?: boolean;
-};
+import { PageTitle } from '@/shared/ui/PageTitle';
+import { useParams } from 'next/navigation';
 
 type LayoutProps = {
     children: React.ReactNode;
 };
 
-/**
- * Displays h1 title and possibly a searchbar
- * @param param0 props
- * @returns ReactNode
- */
-
-const PageTitle = ({ titleText, searchVisible = false }: PageTitleProps) => {
-    return (
-        <Container className={cls.PageTitleContainer}>
-            <div className={cls.PageTitleLeft} />
-            <div className={cls.PageTitle}>
-                {/* Title */}
-                <h1>{titleText}</h1>
-            </div>
-            {searchVisible && (
-                <div className={cls.SearchContainer}>
-                    {/* placeholder for search */}
-                    <p>Search</p>
-                </div>
-            )}
-        </Container>
-    );
-};
-
 const ClanMainPageLayout: React.FC<LayoutProps> = ({ children }) => {
     const { t } = useClientTranslation('clan');
+
+    const params = useParams();
+    const lng = params.lng as string;
 
     const navMenuWithDropdownsDesktopProps: NavMenuWithDropdownsProps = {
         title: t('head-title'),
@@ -49,13 +24,16 @@ const ClanMainPageLayout: React.FC<LayoutProps> = ({ children }) => {
         titleAsActive: false,
         staticDropdown: true,
         dropdownItems: [
-            { elementText: t('browse-clans'), link: { path: '/clans', isExternal: false } },
+            { elementText: t('browse-clans'), link: { path: `/${lng}/clans`, isExternal: false } },
             {
                 elementText: t('leaderboard-title'),
-                link: { path: '/clans/leaderboard', isExternal: false },
+                link: { path: `/${lng}/clans/leaderboard`, isExternal: false },
             },
-            { elementText: t('my_clan'), link: { path: '/clans/myclan', isExternal: false } },
-            { elementText: t('store-title'), link: { path: '/store', isExternal: false } },
+            {
+                elementText: t('my_clan'),
+                link: { path: `/${lng}/clans/myclan`, isExternal: false },
+            },
+            { elementText: t('store-title'), link: { path: `/${lng}/store`, isExternal: false } },
         ],
     };
 
@@ -64,21 +42,26 @@ const ClanMainPageLayout: React.FC<LayoutProps> = ({ children }) => {
         openByDefault: false,
         titleAsActive: false,
         dropdownItems: [
-            { elementText: t('browse-clans'), link: { path: '/clans', isExternal: false } },
+            { elementText: t('browse-clans'), link: { path: `/${lng}/clans`, isExternal: false } },
             {
                 elementText: t('leaderboard-title'),
-                link: { path: '/clans/leaderboard', isExternal: false },
+                link: { path: `/${lng}/clans/leaderboard`, isExternal: false },
             },
-            { elementText: t('my_clan'), link: { path: '/clans/myclan', isExternal: false } },
-            { elementText: t('store-title'), link: { path: '/store', isExternal: false } },
+            {
+                elementText: t('my_clan'),
+                link: { path: `/${lng}/clans/myclan`, isExternal: false },
+            },
+            { elementText: t('store-title'), link: { path: `/${lng}/store`, isExternal: false } },
         ],
     };
+
     return (
         <div className={cls.container}>
             {
                 <PageTitle
-                    titleText="H1 Primary Heading"
+                    titleText={t('head-title')}
                     searchVisible={false}
+                    dynamicTitle="clan"
                 />
             }
             <div className={cls.layoutContainer}>
