4724b07bd1386b723412f2a798143f17c2fe6a55
jartsanull <langinenj1@gmail.com>
2025-03-21 13:20:07 +0200

    Changing the test file

--- frontend-next-migration/src/features/ScrollBottom/model/scrollToBottom/scrollToBottom.test.ts
+++ frontend-next-migration/src/features/ScrollBottom/model/scrollToBottom/scrollToBottom.test.ts
@@ -27,13 +27,15 @@ describe('scrollToBottom', () => {
     it('should scroll smoothly over the specified duration', async () => {
         const animationFrameIdRef: MutableRefObject<number> = { current: 0 };
         const speedInMs = 1000; // 1 second
+        const slower = 150; // Keep it the same as in your function
+        const totalDuration = speedInMs * slower; // Adjusted duration
 
         // Mocking requestAnimationFrame
         jest.useFakeTimers();
         scrollToBottom(speedInMs, animationFrameIdRef);
 
         // Advance timers to the end of the duration
-        jest.advanceTimersByTime(speedInMs);
+        jest.advanceTimersByTime(totalDuration);
 
         // Check that scrollTo was called at least once
         expect(window.scrollTo).toHaveBeenCalled();
@@ -46,6 +48,8 @@ describe('scrollToBottom', () => {
         expect(calls[calls.length - 1][0]).toBe(0); // x position should be 0
         expect(lastCallYPosition).toBeGreaterThan(900); // Should be close to 1000
         expect(lastCallYPosition).toBeLessThan(1100); // Should be close to 1000
+        expect(lastCallYPosition).toBeGreaterThan(1900); // Close to 2000
+        expect(lastCallYPosition).toBeLessThan(2100); // Within range
     });
 
     afterAll(() => {
