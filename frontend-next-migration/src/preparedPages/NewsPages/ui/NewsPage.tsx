1db72de7ab68c311b602d3edf15d11da064e037a
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-26 17:48:37 +0200

    fix: remove unused search handler and clean up code

--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsPage.tsx
@@ -1,7 +1,5 @@
 'use client';
 import { useState } from 'react';
-import { LayoutWithSidebars } from '@/preparedPages/Layouts';
-import useSizes from '@/shared/lib/hooks/use
 import { Container } from '@/shared/ui/Container';
 import { NewsCard } from '@/widgets/NewsCard';
 import cls from './NewsPage.module.scss';
