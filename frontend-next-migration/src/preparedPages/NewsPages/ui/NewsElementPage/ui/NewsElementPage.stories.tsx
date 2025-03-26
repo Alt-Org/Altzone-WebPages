71203292c68060a53321dcbfed06a4aa8a53872f
madinamacow <144919853+madinamacow@users.noreply.github.com>
2025-03-26 16:31:52 +0200

    feat: implement NewsElementPage component with dynamic import and styling

---
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsElementPage/ui/NewsElementPage.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import NewsElementPage from './NewsElementPage';
+
+export default {
+    title: 'pages/NewsPageElement',
+    component: NewsElementPage,
+} as ComponentMeta<typeof NewsElementPage>;
+
+// @ts-ignore
+const Template: ComponentStory<typeof NewsElementPage> = (args) => <NewsElementPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
