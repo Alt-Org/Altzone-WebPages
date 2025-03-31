e94fd2e2db457db25c2204763b8b518797351eed
Lauri <hiltulm@gmail.com>
2025-03-31 16:20:41 +0300

    updated NavigateHeroes

--- frontend-next-migration/src/features/NavigateHeroes/ui/HeroDevelopmentSidebar/HeroDevelopmentSidebar.tsx
+++ frontend-next-migration/src/features/NavigateHeroes/ui/HeroDevelopmentSidebar/HeroDevelopmentSidebar.tsx
@@ -1,27 +1,18 @@
 'use client';
 import React from 'react';
-import dynamic from 'next/dynamic';
 import useSizes from '@/shared/lib/hooks/useSizes';
-
-const HeroDevelopmentNavMenuAsDropdown = dynamic(
-    () => import('../HeroNavMenuAsDropdown/HeroNavMenuAsDropdown'),
-    {
-        ssr: false,
-    },
-);
-const HeroNavMenu = dynamic(() => import('../HeroNavMenu/HeroNavMenu'), {
-    ssr: false,
-});
+import HeroDevelopmentNavMenuAsDropdownAsync from '../HeroDevelopmentNavMenuAsDropdown/HeroDevelopmentNavMenuAsDropdown.async';
+import HeroNavMenuAsync from '../HeroNavMenu/HeroNavMenu.async';
 
 const HeroDevelopmentSidebar: React.FC = () => {
     const { isDesktopSize, isWidescreenSize } = useSizes();
 
     return isDesktopSize || isWidescreenSize ? (
         <div style={{ width: 'fit-content' }}>
-            <HeroNavMenu />
+            <HeroNavMenuAsync />
         </div>
     ) : (
-        <HeroDevelopmentNavMenuAsDropdown />
+        <HeroDevelopmentNavMenuAsDropdownAsync />
     );
 };
 export default HeroDevelopmentSidebar;
