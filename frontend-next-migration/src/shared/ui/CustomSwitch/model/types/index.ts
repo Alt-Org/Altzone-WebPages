98e04cdcdee757f27778fd1068ec359052806998
Lauri <hiltulm@gmail.com>
2025-04-04 16:07:48 +0300

    add: a shared/CustomSwitch component for the leaderboard pages switches

---
+++ frontend-next-migration/src/shared/ui/CustomSwitch/model/types/index.ts
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { CustomSwitchItems } from '../enum/CustomSwitch.enum';
+
+export type ToggleItem = {
+    type: CustomSwitchItems.ToggleItem;
+    isOpen: boolean;
+    onOpen: () => void;
+    identifier: number;
+    className?: string;
+    children: ReactNode;
+};
+
+export type ToggleLink = {
+    type: CustomSwitchItems.ToggleLink;
+    isOpen: boolean;
+    path: string;
+    className?: string;
+    children: ReactNode;
+};
--- frontend-next-migration/src/shared/ui/CustomSwitch/ui/CustomSwitch.tsx
+++ frontend-next-migration/src/shared/ui/CustomSwitch/ui/CustomSwitch.tsx
@@ -1,20 +1,49 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { AppLink } from '@/shared/ui/AppLink/AppLink';
+import { CustomSwitchItems } from '../model/enum/CustomSwitch.enum';
+import { ToggleItem, ToggleLink } from '../model/types/';
 import cls from './CustomSwitch.module.scss';
 
-interface ToggleItem {
-    isOpen: boolean;
-    onOpen?: () => void;
-    path?: string;
-    className?: string;
-    children: ReactNode;
-}
-
-interface CustomSwitchProps {
-    elements: ToggleItem[];
+export interface CustomSwitchProps {
+    elements: ToggleItem[] | ToggleLink[];
     className?: string;
 }
 
+/**
+ * CustomSwitch component is reusable customizable switch. Toggleable items can be links or with function.
+ * @param {CustomSwitchProps} props - props for the component
+ * @example
+ * // Example of the elements when using the non link version:
+ * import { CustomSwitch, CustomSwitchItems, ToggleItem } from '@/shared/ui/CustomSwitch';
+ * const [customSwitchOption, setCustomSwitchOption] = useState(0);
+ *    const CustomSwitchElements: ToggleItem[] = [
+ *       {
+ *           children: <p>Globaali</p>,
+ *       },
+ *       {
+ *           children: <p>Klaani</p>,
+ *       },
+ *       {
+ *           children: <p>Kaverit</p>,
+ *       },
+ *   ].map((elem, index) => {
+ *       return {
+ *           type: CustomSwitchItems.ToggleItem as CustomSwitchItems.ToggleItem,
+ *           identifier: index,
+ *           isOpen: customSwitchOption === index,
+ *           onOpen: () => {
+ *               if (customSwitchOption !== index) {
+ *                   setCustomSwitchOption(index);
+ *               }
+ *           },
+ *           ...elem,
+ *       };
+ *   });
+ * return <CustomSwitch elements={CustomSwitchElements} />;
+ * @example
+ * // Example of the elements when using the link version (not tested yet)
+ */
 const CustomSwitch = ({ elements, className }: CustomSwitchProps) => {
     const customSwitchClassName = classNames(
         cls.CustomSwitch,
@@ -24,13 +53,28 @@ const CustomSwitch = ({ elements, className }: CustomSwitchProps) => {
     return (
         <div className={customSwitchClassName}>
             {elements.map((element, index) => (
-                <div
-                    key={index}
-                    className={classNames('', { [cls.OpenToggleItem]: element.isOpen })}
-                    onClick={element.onOpen}
-                >
-                    <p>hello</p>
-                </div>
+                <React.Fragment key={index}>
+                    {element.type === CustomSwitchItems.ToggleLink && (
+                        <AppLink
+                            className={classNames(cls.ToggleItem, {
+                                [cls.OpenToggleItem]: element.isOpen,
+                            })}
+                            to={element.path}
+                        >
+                            {element.children}
+                        </AppLink>
+                    )}
+                    {element.type === CustomSwitchItems.ToggleItem && (
+                        <div
+                            className={classNames(cls.ToggleItem, {
+                                [cls.OpenToggleItem]: element.isOpen,
+                            })}
+                            onClick={element.onOpen}
+                        >
+                            {element.children}
+                        </div>
+                    )}
+                </React.Fragment>
             ))}
         </div>
     );
