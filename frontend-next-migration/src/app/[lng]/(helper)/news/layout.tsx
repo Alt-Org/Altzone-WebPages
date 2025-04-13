f09505bd455240d482f791888166bb0362d1f58e
Skoivumaki <sakari.koivumaki@gmail.com>
2025-04-13 22:07:36 +0300

    fix: mobile device variation for layout. PageTitle was in incorrect position in mobile. (below navigation)

--- frontend-next-migration/src/app/[lng]/(helper)/news/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/news/layout.tsx
@@ -3,23 +3,45 @@ import { LayoutWithSidebars } from '@/preparedPages/Layouts';
 import { NewsPageNavMenuAsDropdown } from '@/features/NavigateNewsPage';
 import { PageTitle } from '@/shared/ui/PageTitle';
 import { useClientTranslation } from '@/shared/i18n';
+import useSizes from '@/shared/lib/hooks/useSizes';
 
 export default function NewsLayout({ children }: { children: React.ReactNode }) {
     const { t } = useClientTranslation('search');
+    const { isMobileSize, isTabletSize } = useSizes();
+    const isTouchDevice = isMobileSize || isTabletSize;
     return (
         <div>
-            <LayoutWithSidebars
-                leftTopSidebar={{
-                    component: <NewsPageNavMenuAsDropdown />,
-                }}
-            >
-                <PageTitle
-                    titleText={t('newsTitle')}
-                    searchVisible={false}
-                    alternate={true}
-                />
-                {children}
-            </LayoutWithSidebars>
+            {isTouchDevice ? (
+                <div>
+                    <PageTitle
+                        titleText={t('newsTitle')}
+                        searchVisible={false}
+                        alternate={true}
+                    />
+                    <LayoutWithSidebars
+                        leftTopSidebar={{
+                            component: <NewsPageNavMenuAsDropdown />,
+                        }}
+                    >
+                        {children}
+                    </LayoutWithSidebars>
+                </div>
+            ) : (
+                <div>
+                    <LayoutWithSidebars
+                        leftTopSidebar={{
+                            component: <NewsPageNavMenuAsDropdown />,
+                        }}
+                    >
+                        <PageTitle
+                            titleText={t('newsTitle')}
+                            searchVisible={false}
+                            alternate={true}
+                        />
+                        {children}
+                    </LayoutWithSidebars>
+                </div>
+            )}
         </div>
     );
 }
