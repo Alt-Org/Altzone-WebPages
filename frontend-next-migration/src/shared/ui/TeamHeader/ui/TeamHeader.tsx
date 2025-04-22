f049e0186e3a11bfe1b632209d556aaee35e0eb3
SProkopios <119333647+SProkopios@users.noreply.github.com>
2025-04-22 14:00:27 +0300

    Enhancing teamheader

--- frontend-next-migration/src/shared/ui/TeamHeader/ui/TeamHeader.tsx
+++ frontend-next-migration/src/shared/ui/TeamHeader/ui/TeamHeader.tsx
@@ -8,89 +8,51 @@ import useSizes from '@/shared/lib/hooks/useSizes';
 
 type Props = {
     image: StaticImageData;
-    children?: ReactNode;
     dropdown?: ReactNode;
 };
 
 export function TeamHeader(props: Props) {
-    const { image, children, dropdown } = props;
+    const { image, dropdown } = props;
     const { t } = useClientTranslation('members');
     const { isMobileSize, isTabletSize } = useSizes();
     const isTouchDevice = isMobileSize || isTabletSize;
 
-    return isTouchDevice ? (
-        <div className={cls.HeaderMobile}>
-            <div className={cls.TitleContainer}>
-                <h2>{t('head-title')}</h2>
-            </div>
-            {dropdown}
-            <div className={cls.InputParent}>
-                <div className={cls.InputContainer}>
-                    <FontAwesomeIcon
-                        className={cls.textInputIcon}
-                        size={'lg'}
-                        icon={faMagnifyingGlass}
-                    />
-                    <input
-                        name="search"
-                        placeholder={t('search_placeholder')}
-                        type="text"
-                        id="search"
-                        className={cls.Searchfield}
-                    />
-                </div>
-            </div>
-            <div className={cls.HeaderImageContainer}>
-                <Image
-                    src={image}
-                    alt={'Header image'}
-                    width={0}
-                    height={0}
-                    quality={100}
-                    className={cls.headerImg}
-                    loading={'eager'}
-                />
-            </div>
-        </div>
-    ) : (
-        <>
-            <div className={cls.HeaderContainer}>
-                <div className={cls.emptyContainer} />
-                <div className={cls.dataContainer}>
-                    <div className={cls.DesktopTitleContainer}>
-                        <h2>{t('head-title')}</h2>
-                        <div className={cls.DesktopInputParent}>
-                            <div className={cls.InputContainer}>
-                                <FontAwesomeIcon
-                                    className={cls.textInputIcon}
-                                    size={'lg'}
-                                    icon={faMagnifyingGlass}
-                                />
-                                <input
-                                    name="search"
-                                    placeholder={t('search_placeholder')}
-                                    type="text"
-                                    id="search"
-                                    className={cls.Searchfield}
-                                />
-                            </div>
+    return (
+        <div className={cls.HeaderContainer}>
+            {!isTouchDevice && <div className={cls.emptyContainer} />}
+            <div className={cls.dataContainer}>
+                <div className={cls.TitleContainer}>
+                    <h2>{t('head-title')}</h2>
+                    {isTouchDevice && dropdown}
+                    <div className={cls.InputParent}>
+                        <div className={cls.InputContainer}>
+                            <FontAwesomeIcon
+                                className={cls.textInputIcon}
+                                size={'lg'}
+                                icon={faMagnifyingGlass}
+                            />
+                            <input
+                                name="search"
+                                placeholder={t('search_placeholder')}
+                                type="text"
+                                id="search"
+                                className={cls.Searchfield}
+                            />
                         </div>
                     </div>
-                    <div className={cls.HeaderImageContainer}>
-                        <Image
-                            src={image}
-                            alt={'Header image'}
-                            width={1000}
-                            height={1000}
-                            quality={100}
-                            className={cls.headerImg}
-                            loading={'eager'}
-                        />
-                    </div>
+                </div>
+                <div className={cls.ImageContainer}>
+                    <Image
+                        src={image}
+                        alt={'Header image'}
+                        width={0}
+                        height={0}
+                        quality={100}
+                        className={cls.headerImg}
+                        loading={'eager'}
+                    />
                 </div>
             </div>
-
-            {children}
-        </>
+        </div>
     );
 }
