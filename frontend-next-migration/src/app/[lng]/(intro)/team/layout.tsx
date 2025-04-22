31d9b63eadca4437d31aad73651ae073a247343b
SProkopios <119333647+SProkopios@users.noreply.github.com>
2025-04-22 12:04:04 +0300

    Refactoring to follow feature-sliced desing better

--- frontend-next-migration/src/app/[lng]/(intro)/team/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(intro)/team/layout.tsx
@@ -1,16 +1,50 @@
+'use client';
 import React, { ReactNode } from 'react';
 import { ScrollTop } from '@/features/ScrollTop';
 import cls from './Layout.module.scss';
+import useSizes from '@/shared/lib/hooks/useSizes';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import TeamHeader from '@/shared/ui/TeamHeader';
+import MembersNavMenu from '@/features/NavigateMembers';
+import { LayoutWithSidebars } from '@/preparedPages/Layouts';
+import headerImg from '@/shared/assets/images/members/members8.webp';
 
 type Props = {
     children: ReactNode;
 };
 
-export default async function TeamLayout({ children }: Props) {
-    return (
-        <div className={cls.MemberContainer}>
+export default function TeamLayout({ children }: Props) {
+    const { isMobileSize, isTabletSize } = useSizes();
+    const isTouchDevice = isMobileSize || isTabletSize;
+
+    return isTouchDevice ? (
+        <div
+            id={'members'}
+            className={classNames(cls.MembersPageMobile)}
+        >
+            <TeamHeader
+                image={headerImg}
+                dropdown={<MembersNavMenu />}
+            />
             {children}
             <ScrollTop />
+        </div>
+    ) : (
+        <div
+            id={'members'}
+            className={classNames(cls.MembersPage)}
+        >
+            <TeamHeader image={headerImg} />
+            <LayoutWithSidebars
+                className={cls.TeamPageSidebar}
+                leftTopSidebar={{
+                    component: <MembersNavMenu />,
+                    hideOnMobile: true,
+                }}
+            >
+                {children}
+                <ScrollTop />
+            </LayoutWithSidebars>
         </div>
     );
 }
--- frontend-next-migration/src/preparedPages/MembersPage/ui/MembersPage.tsx
+++ frontend-next-migration/src/preparedPages/MembersPage/ui/MembersPage.tsx
@@ -1,13 +1,7 @@
 'use client';
 import { SectionMembers } from '@/widgets/SectionMembers';
-import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './MembersPage.module.scss';
-import { LayoutWithSidebars } from '@/preparedPages/Layouts';
-import useSizes from '@/shared/lib/hooks/useSizes';
-import { TeamHeader } from '@/shared/ui/TeamHeader/ui/TeamHeader';
 import { FC } from 'react';
-import headerImg from '@/shared/assets/images/members/members8.webp';
-import { MembersNavMenu } from '@/features/NavigateMembers/ui/MemberNavMenuAsDropdown';
 
 /**
  * The above code defines a React functional component for a Members Page with a background image
@@ -16,35 +10,9 @@ import { MembersNavMenu } from '@/features/NavigateMembers/ui/MemberNavMenuAsDro
  */
 
 const MembersPage: FC = () => {
-    const { isMobileSize, isTabletSize } = useSizes();
-    const isTouchDevice = isMobileSize || isTabletSize;
-
-    return isTouchDevice ? (
-        <div
-            id={'members'}
-            className={classNames(cls.MembersPageMobile)}
-        >
-            <TeamHeader
-                image={headerImg}
-                dropdown={<MembersNavMenu />}
-            />
-            <SectionMembers className={cls.workersMobileSection} />
-        </div>
-    ) : (
-        <div
-            id={'members'}
-            className={classNames(cls.MembersPage)}
-        >
-            <TeamHeader image={headerImg} />
-            <LayoutWithSidebars
-                className={cls.TeamPageSidebar}
-                leftTopSidebar={{
-                    component: <MembersNavMenu />,
-                    hideOnMobile: true,
-                }}
-            >
-                <SectionMembers className={cls.workersSection} />
-            </LayoutWithSidebars>
+    return (
+        <div className={cls.Members}>
+            <SectionMembers className={cls.workersSection} />
         </div>
     );
 };
