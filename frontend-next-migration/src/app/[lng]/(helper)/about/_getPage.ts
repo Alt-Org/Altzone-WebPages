7f19f0da2d60d1a8c81d48a01c418572864b5936
Matias Kanerva <matias.kanerva2@live.com>
2025-04-23 12:55:53 +0300

    initial commit

--- frontend-next-migration/src/app/[lng]/(helper)/about/_getPage.ts
+++ frontend-next-migration/src/app/[lng]/(helper)/about/_getPage.ts
@@ -1,12 +1,21 @@
 import { useServerTranslation } from '@/shared/i18n';
 import { createPage } from '@/app/_helpers';
 import { AboutPageProps } from '@/preparedPages/AboutPage';
+import { object } from 'yup';
 
-export async function _getPage(lng: string) {
+export async function _getPage(lng: any) {
     const { t } = await useServerTranslation(lng, 'about');
     return createPage<AboutPageProps>({
         buildPage: () => ({
-            title: t('title'),
+            title: t('head-title'),
+            description: t('head-description'),
+            keywords: t('head-keywords'),
+            storytitle: t('story-title'),
+            project: t('project'),
+            locality: t('locality'),
+            nationality: t('nationality'),
+            behind: t('behind'),
+            story: t('V2019'),
         }),
         buildSeo: () => ({
             title: t('head-title'),
--- frontend-next-migration/src/preparedPages/AboutPage/ui/About.tsx
+++ frontend-next-migration/src/preparedPages/AboutPage/ui/About.tsx
@@ -1,9 +1,91 @@
-export type Props = {
+'use client';
+import { object, string } from 'yup';
+import cls from './About.module.scss';
+import { title } from 'process';
+
+export interface Props {
     title: string;
+    description: string;
+    keywords: string;
+    storytitle: string;
+    project: string;
+    locality: string;
+    nationality: string;
+    behind: string;
+    story: string;
+}
+
+interface section {}
+
+const About = (props: Props) => {
+    const {
+        title,
+        description,
+        keywords,
+        storytitle,
+        project,
+        locality,
+        nationality,
+        behind,
+        story,
+    } = props;
+
+    return (
+        <main className={cls.main}>
+            <div className={cls.container}>
+                <p className={cls.h1}>{title}</p>
+                <p className={cls.p}>{description}</p>
+                <p className={cls.h1}>{keywords}</p>
+                <div className={cls.headergrid}>
+                    <div>
+                        <p>10</p>
+                        <p className={cls.gridp}>{project}</p>
+                    </div>
+                    <div>
+                        <p>10</p>
+                        <p className={cls.gridp}>{locality}</p>
+                    </div>
+                    <div>
+                        <p>10</p>
+                        <p className={cls.gridp}>{nationality}</p>
+                    </div>
+                    <div>
+                        <p>10</p>
+                        <p className={cls.gridp}>{behind}</p>
+                    </div>
+                </div>
+            </div>
+            <div className={cls.container}>
+                <p className={cls.h1}>{storytitle}</p>
+                <div
+                    className={cls.storygrid}
+                    id={cls.line}
+                >
+                    <p className={cls.yearh1}>2019</p>
+                    <p className={cls.p}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book. It has survived not only five centuries, but also
+                        the leap into electronic typesetting, remaining essentially unchanged. It
+                        was popularised in the 1960s with the release of Letraset sheets containing
+                        Lorem Ipsum passages, and more recently with desktop publishing software
+                        like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <p className={cls.yearh1}>2020</p>
+                    <p className={cls.p}>{story}</p>
+                    <p className={cls.yearh1}>2021</p>
+                    <p className={cls.p}>{story}</p>
+                    <p className={cls.yearh1}>2022</p>
+                    <p className={cls.p}>{story}</p>
+                    <p className={cls.yearh1}>2023</p>
+                    <p className={cls.p}>{story}</p>
+                    <p className={cls.yearh1}>2024</p>
+                    <p className={cls.p}>{story}</p>
+                </div>
+            </div>
+        </main>
+    );
 };
 
-export default function About(props: Props) {
-    const { title } = props;
-
-    return <div>{title}</div>;
-}
+export default About;
