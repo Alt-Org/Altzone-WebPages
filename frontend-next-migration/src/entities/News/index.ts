c22122a55e5f5f9df102ac26b9da1d1719087c46
Skoivumaki <sakari.koivumaki@gmail.com>
2025-04-24 07:18:38 +0300

    add: implement formatNews and formatNewsSingle functions for news data processing

--- frontend-next-migration/src/entities/News/index.ts
+++ frontend-next-migration/src/entities/News/index.ts
@@ -1,4 +1,5 @@
 export { mockNews } from './model/data/mockNews';
 export { newsDataLocally } from './model/data/newsLocally';
-
+export { formatNews } from './model/data/formatNews';
+export { formatNewsSingle } from './model/data/formatNews';
 export type { INewsElement } from './model/types';
