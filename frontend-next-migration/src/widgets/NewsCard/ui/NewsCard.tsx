e4574e59d63a0b852c1ebfe86242d3527e4df45a
Skoivumaki <sakari.koivumaki@gmail.com>
2025-04-24 07:22:49 +0300

    fix: update date color to white and refactor NewsCard component for improved structure

--- frontend-next-migration/src/widgets/NewsCard/ui/NewsCard.tsx
+++ frontend-next-migration/src/widgets/NewsCard/ui/NewsCard.tsx
@@ -3,38 +3,44 @@ import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './NewsCard.module.scss';
 import Image from 'next/image';
 import hannu from '@/shared/assets/images/heros/hannu-hodari/hannu-hodari.png';
+import Link from 'next/link';
 
 interface NewsCardProps {
     className?: string;
     title: string;
-    content: string;
+    previewText: string;
     date: string;
     id: number;
     imageSrc?: string;
 }
 
 const NewsCard = (props: NewsCardProps) => {
-    const { title, date, id, content, imageSrc = hannu.src } = props;
+    const { title, date, id, previewText, imageSrc = hannu.src } = props;
     return (
-        <div className={classNames(cls.NewsCard, {})}>
-            <div className={cls.content}>
-                <h2 className={cls.title}>{title}</h2>
-                <p className={cls.text}>{content}</p>
-                <span className={cls.date}>Date:{date}</span>
-                <div className={cls.imageContainer}>
-                    <div className={cls.imageWrapper}>
-                        <Image
-                            src={imageSrc}
-                            alt={title}
-                            className={cls.image}
-                            layout="responsive"
-                            width={100}
-                            height={600}
-                        />
+        <Link
+            rel="id"
+            href={`/news/${id}`}
+        >
+            <div className={classNames(cls.NewsCard, {})}>
+                <div className={cls.content}>
+                    <h2 className={cls.title}>{title}</h2>
+                    <p className={cls.text}>{previewText}</p>
+                    <span className={cls.date}>{date}</span>
+                    <div className={cls.imageContainer}>
+                        <div className={cls.imageWrapper}>
+                            <Image
+                                src={imageSrc}
+                                alt={title}
+                                className={cls.image}
+                                layout="responsive"
+                                width={100}
+                                height={600}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
