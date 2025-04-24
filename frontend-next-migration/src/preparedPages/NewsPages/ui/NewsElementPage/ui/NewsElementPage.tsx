5a1e2e47776748fd25071f87edbd219742d71ac3
Skoivumaki <sakari.koivumaki@gmail.com>
2025-04-24 07:22:19 +0300

    feat: enhance NewsElementPage with dynamic image loading and improved layout

--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsElementPage/ui/NewsElementPage.tsx
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsElementPage/ui/NewsElementPage.tsx
@@ -1,26 +1,59 @@
 'use client';
 import { useParams } from 'next/navigation';
-import { Navbar } from '@/widgets/Navbar';
 import { NavGoBackButton } from '@/features/NavGoBack';
-import { newsDataLocally } from '@/entities/News';
-import { getPostDataById, Post } from '@/shared/ui/Post';
+import { formatNewsSingle } from '@/entities/News';
+import hannu from '@/shared/assets/images/heros/hannu-hodari/hannu-hodari.png';
 import { Container } from '@/shared/ui/Container';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useScrollToTop } from '@/shared/lib/hooks';
 import cls from './NewsElementPage.module.scss';
+import Image from 'next/image';
+import { useGetNewsByIdQuery } from '@/entities/NewsV2/Api/newsApi';
+import { envHelper } from '@/shared/const/envHelper';
 
 const NewsElementPage = () => {
     const { id } = useParams();
 
     useScrollToTop();
-    const postData = getPostDataById(id as string, newsDataLocally);
+
+    const { data, isLoading } = useGetNewsByIdQuery(id as string);
+    const params = useParams();
+    const lng = params.lng as string;
+    const lngCode = lng === 'en' ? 'en-US' : lng === 'fi' ? 'fi-FI' : lng;
+
+    if (isLoading || !data) {
+        return <div>Loading...</div>;
+    }
+    const directusBaseUrl = envHelper.directusHost;
+    const post = formatNewsSingle(data, lngCode || 'fi-FI');
+    const picture = post.titlePicture?.id
+        ? `${directusBaseUrl}/assets/${post.titlePicture.id}`
+        : hannu.src;
 
     return (
         <Container>
+            <NavGoBackButton />
             <div className={classNames(cls.NewsElementPage)}>
-                <Navbar className={cls.navbar} />
-                <NavGoBackButton />
-                {postData ? <Post postData={postData} /> : 'Not found'}
+                <div className={cls.imageContainer}>
+                    <Image
+                        src={picture}
+                        alt={''}
+                        className={cls.imageBlur}
+                        width={100}
+                        height={600}
+                    />
+                    <Image
+                        src={picture}
+                        alt={''}
+                        className={cls.image}
+                        width={100}
+                        height={600}
+                    />
+                </div>
+                <h1 className={cls.title}>{post?.title}</h1>
+                <h3 className={cls.subtitle}>{post?.previewText}</h3>
+                <p className={cls.text}>{post?.bodyText}</p>
+                <span className={cls.date}>{post?.date}</span>
             </div>
         </Container>
     );
