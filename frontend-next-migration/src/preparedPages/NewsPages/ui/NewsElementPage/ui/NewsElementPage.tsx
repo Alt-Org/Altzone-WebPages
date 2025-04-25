ad994efa5dd465d43e2ac78ee14276063a9b3b66
Skoivumaki <sakari.koivumaki@gmail.com>
2025-04-25 14:13:44 +0300

    feat: enhance NewsElementPage with navigation buttons and improved layout for related news

--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsElementPage/ui/NewsElementPage.tsx
+++ frontend-next-migration/src/preparedPages/NewsPages/ui/NewsElementPage/ui/NewsElementPage.tsx
@@ -1,38 +1,84 @@
 'use client';
-import { useParams } from 'next/navigation';
-import { NavGoBackButton } from '@/features/NavGoBack';
-import { formatNewsSingle } from '@/entities/News';
+import { formatNews, formatNewsSingle } from '@/entities/News';
 import hannu from '@/shared/assets/images/heros/hannu-hodari/hannu-hodari.png';
 import { Container } from '@/shared/ui/Container';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useScrollToTop } from '@/shared/lib/hooks';
 import cls from './NewsElementPage.module.scss';
 import Image from 'next/image';
-import { useGetNewsByIdQuery } from '@/entities/NewsV2/Api/newsApi';
+import { useGetNewsByIdQuery, useGetNewsQuery } from '@/entities/NewsV2';
 import { envHelper } from '@/shared/const/envHelper';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button';
+import chevronleft from '@/shared/assets/icons/ChevronLeftBlack.svg';
+import chevronright from '@/shared/assets/icons/ChevronRightBlack.svg';
+import { useRouter, useParams } from 'next/navigation';
+import { useClientTranslation } from '@/shared/i18n';
+import { NewsCard } from '@/widgets/NewsCard';
 
 const NewsElementPage = () => {
-    const { id } = useParams();
-
     useScrollToTop();
-
-    const { data, isLoading } = useGetNewsByIdQuery(id as string);
     const params = useParams();
+    const id = params.id;
     const lng = params.lng as string;
+
+    const { t } = useClientTranslation('news');
+    const { data: moreNews } = useGetNewsQuery(2);
+    const { data, isLoading } = useGetNewsByIdQuery(id as string);
+    const directusBaseUrl = envHelper.directusHost;
+    const router = useRouter();
     const lngCode = lng === 'en' ? 'en-US' : lng === 'fi' ? 'fi-FI' : lng;
+
+    const handleNextNews = (newsId: string) => {
+        if (newsId) router.push(`/news/${newsId}`);
+    };
 
     if (isLoading || !data) {
         return <div>Loading...</div>;
     }
-    const directusBaseUrl = envHelper.directusHost;
+
     const post = formatNewsSingle(data, lngCode || 'fi-FI');
+    const groupedNews = formatNews(moreNews, lngCode || 'fi-FI');
     const picture = post.titlePicture?.id
         ? `${directusBaseUrl}/assets/${post.titlePicture.id}`
         : hannu.src;
-
     return (
         <Container>
-            <NavGoBackButton />
+            <div className={cls.navButtons}>
+                <Button
+                    disabled={data.prevId === null}
+                    onClick={() => handleNextNews(data.prevId)}
+                    theme={ButtonTheme.PRIMARY}
+                    size={ButtonSize.M}
+                    className={classNames(cls.ButtonNewsNavigation)}
+                >
+                    {
+                        <Image
+                            loading="eager"
+                            alt={'Pin'}
+                            src={chevronleft}
+                            className={cls.buttonImage}
+                        />
+                    }
+                    {'Edellinen'}
+                </Button>
+                <Button
+                    disabled={data.nextId === null}
+                    onClick={() => handleNextNews(data.nextId)}
+                    theme={ButtonTheme.PRIMARY}
+                    size={ButtonSize.M}
+                    className={classNames(cls.ButtonNewsNavigation)}
+                >
+                    {'Seuraava'}
+                    {
+                        <Image
+                            loading="eager"
+                            alt={'Pin'}
+                            src={chevronright}
+                            className={cls.buttonImage}
+                        />
+                    }
+                </Button>
+            </div>
             <div className={classNames(cls.NewsElementPage)}>
                 <div className={cls.imageContainer}>
                     <Image
@@ -54,6 +100,26 @@ const NewsElementPage = () => {
                 <h3 className={cls.subtitle}>{post?.previewText}</h3>
                 <p className={cls.text}>{post?.bodyText}</p>
                 <span className={cls.date}>{post?.date}</span>
+            </div>
+            <div className={cls.readmoreText}>
+                <h1 className={cls.titleReadmore}>{t('read-more')}</h1>
+            </div>
+            <div className={cls.newsGrid}>
+                {groupedNews.map((news) => {
+                    const imageSrc = news.titlePicture?.id
+                        ? `${directusBaseUrl}/assets/${news.titlePicture.id}`
+                        : hannu.src;
+                    return (
+                        <NewsCard
+                            key={news.id}
+                            titlePicture={imageSrc}
+                            title={news.title}
+                            previewText={news.previewText}
+                            date={news.date}
+                            id={news.id}
+                        />
+                    );
+                })}
             </div>
         </Container>
     );
