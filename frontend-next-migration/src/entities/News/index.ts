92a215e394235a99f962e4be5ee752831657c504
Skoivumaki <sakari.koivumaki@gmail.com>
2025-05-06 16:58:17 +0300

    refactor: remove formatNews functions and update exports in News index

--- frontend-next-migration/src/entities/News/index.ts
+++ frontend-next-migration/src/entities/News/index.ts
@@ -1,4 +1,2 @@
 export { newsDataLocally } from './model/data/newsLocally';
-export { formatNews } from './model/data/formatNews';
-export { formatNewsSingle } from './model/data/formatNews';
 export type { INewsElement } from './model/types';
