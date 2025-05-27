111cb776a5e9cef37e89ba82b0c9ebba5baec5c9
Joni Roine <joni.roine@outlook.com>
2025-05-27 13:58:21 +0300

    refactor: add directusApi to StateSchema for enhanced state management

--- frontend-next-migration/src/app/_providers/StoreProvider/config/StateSchema.ts
+++ frontend-next-migration/src/app/_providers/StoreProvider/config/StateSchema.ts
@@ -1,6 +1,6 @@
 import { AuthUserSchema } from '@/entities/Auth';
 import { ProfileSchema } from '@/entities/Profile/types/profile';
-import { gameApi, strapiApi } from '@/shared/api';
+import { gameApi, strapiApi, directusApi } from '@/shared/api';
 import { NavBarSchema } from '@/widgets/Navbar';
 
 export interface StateSchema {
@@ -9,4 +9,5 @@ export interface StateSchema {
     navbar: NavBarSchema;
     [gameApi.reducerPath]: ReturnType<typeof gameApi.reducer>;
     [strapiApi.reducerPath]: ReturnType<typeof strapiApi.reducer>;
+    [directusApi.reducerPath]: ReturnType<typeof directusApi.reducer>;
 }
