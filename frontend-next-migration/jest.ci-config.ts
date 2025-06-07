78deb4c9f25ad4df4a34322a563c69a366b0037a
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-06-07 15:42:57 +0300

    add initial jenkins setup

---
+++ frontend-next-migration/jest.ci-config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+import dotenv from 'dotenv';
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+});
+
+dotenv.config({ path: '.env.local' });
+
+/**
+ * Config for CI / CD pipeline
+ */
+const config: Config = {
+    clearMocks: true,
+    moduleDirectories: ['node_modules', __dirname],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
+    testEnvironment: 'jsdom',
+
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    coverageReporters: ['cobertura'],
+    reporters: ['jest-junit'],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
