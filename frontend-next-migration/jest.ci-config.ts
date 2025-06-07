8ea5075990bbb179d8ed3054641a906e24de6ead
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-06-07 16:29:43 +0300

    change jest config for ci

--- frontend-next-migration/jest.ci-config.ts
+++ frontend-next-migration/jest.ci-config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 const createJestConfig = nextJest({
     // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-    dir: './',
+    dir: './frontend-next-migration',
 });
 
 dotenv.config({ path: '.env.local' });
