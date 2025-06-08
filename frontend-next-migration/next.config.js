cf349a42e0f17a99a7c427db3f6e433dbfada7a0
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-06-08 15:35:09 +0300

    fix failing build due to next config

--- frontend-next-migration/next.config.js
+++ frontend-next-migration/next.config.js
@@ -1,36 +1,37 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: process.env.NEXT_PUBLIC_API_LINK
-            ? new URL(process.env.NEXT_PUBLIC_API_LINK).hostname
-            : ""
-      },
-      { hostname: 'hips.hearstapps.com' },
-      { hostname: 'www.thesprucepets.com' },
-      { hostname: 'heroes.ts.unsplash.com' },
-      { hostname: 'raw.githubusercontent.com' },
-      { hostname: 'raw.githubusercontent.com' },
-      { hostname: "localhost" },
-      {
-        hostname: process.env.NEXT_PUBLIC_STRAPI_HOST
-            ? new URL(process.env.NEXT_PUBLIC_STRAPI_HOST).hostname
-            : ""
-      },
-    ],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    // prependData: `@import "src/app/styles/variables/_mixins.scss";`,
-    prependData: `@import "src/app/_styles/variables/_mixins.scss";`,
-    // prependData: `@import "src/preparedApp/styles/variables/_mixins.scss";`,
-  },
-  output: 'standalone',
-  // experimental: {
-  //     webpackBuildWorker: true
-  // },
+    images: {
+        remotePatterns: [
+            {
+                hostname: process.env.NEXT_PUBLIC_API_LINK
+                    ? new URL(process.env.NEXT_PUBLIC_API_LINK).hostname
+                    : 'http://localhost:1234',
+            },
+            { hostname: 'hips.hearstapps.com' },
+            { hostname: 'www.thesprucepets.com' },
+            { hostname: 'heroes.ts.unsplash.com' },
+            { hostname: 'raw.githubusercontent.com' },
+            { hostname: 'raw.githubusercontent.com' },
+            { hostname: 'localhost' },
+            {
+                hostname: process.env.NEXT_PUBLIC_STRAPI_HOST
+                    ? new URL(process.env.NEXT_PUBLIC_STRAPI_HOST).hostname
+                    : 'http://localhost:2345',
+            },
+        ],
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        // prependData: `@import "src/app/styles/variables/_mixins.scss";`,
+        prependData: `@import "src/app/_styles/variables/_mixins.scss";`,
+        // prependData: `@import "src/preparedApp/styles/variables/_mixins.scss";`,
+    },
+    output: 'standalone',
+    // experimental: {
+    //     webpackBuildWorker: true
+    // },
 };
 
 module.exports = nextConfig;
