10d42c0a130ca205745411d8c2275a97ba25029c
Idrees Yaser <edrees003@gmail.com>
2025-06-24 22:57:26 +0300

    Refactor picture gallery layout and update fi localization

--- frontend-next-migration/src/app/[lng]/(helper)/picture-galleries/layout.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/picture-galleries/layout.tsx
@@ -12,17 +12,24 @@ export default function PictureGalleryLayout({ children }: { children: ReactNode
     const { t } = useClientTranslation('picture-galleries');
 
     return (
-        <LayoutWithSidebars className='test'
-            leftTopSidebar={{
-                component: <GalleryNavMenuAsDropdown openByDefault={true} />,
-                hideOnMobile: true,
-                className: cls.LeftSidebar,
-            }}
-        >
-            <h1 className={cls.Title}>{t('picture-galleries')}</h1>
-            <p className={cls.InfoText}>{t('info-text')}</p>
-            {isTouchDevice ? <GalleryNavMenuAsDropdown openByDefault={false} /> : null}
-            {children}
-        </LayoutWithSidebars>
+        <>
+            <div >
+                <h1 className={cls.mainTitle}>{t('picture-galleries-title')}</h1>
+            </div>
+            <LayoutWithSidebars className='test'
+                leftTopSidebar={{
+                    component: <GalleryNavMenuAsDropdown openByDefault={true} />,
+                    hideOnMobile: true,
+                    className: cls.LeftSidebar,
+                }}
+            >
+                <div className={cls.Header}>
+                <h1 className={cls.Title}>{t('picture-galleries')}</h1>
+                <p className={cls.InfoText}>{t('info-text')}</p>
+                </div>
+                {isTouchDevice ? <GalleryNavMenuAsDropdown openByDefault={false} /> : null}
+                {children}
+            </LayoutWithSidebars>
+        </>
     );
 }
