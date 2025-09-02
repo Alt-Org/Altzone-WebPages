876bf3f447c4fdaf5f7a6f6b2e15b7d6540785f0
eleino <emilia.leino@proton.me>
2025-09-02 16:22:02 +0300

    Started adding info boxes to collections page

--- frontend-next-migration/src/preparedPages/CollectionsPage/ui/CollectionsPage.tsx
+++ frontend-next-migration/src/preparedPages/CollectionsPage/ui/CollectionsPage.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import styles from './CollectionsPage.module.scss';
+import { DescriptionCard, DescriptionCardTheme } from '@/shared/ui/v2/DescriptionCard';
 
 const CollectionsPage = () => {
     return (
         <div>
-            <h1 className={styles.title}>Collections Page</h1>
+            <h1 className={styles.title}>Kokoelmat</h1>
+            <DescriptionCard
+                theme={DescriptionCardTheme.DEFENSEGALLERY}
+                path="/collections/avatar"
+            >
+                <DescriptionCard.Texts>
+                    <DescriptionCard.Texts.Title>Avatar</DescriptionCard.Texts.Title>
+                    <DescriptionCard.Texts.Body>Avatar-kokoelmat.</DescriptionCard.Texts.Body>
+                </DescriptionCard.Texts>
+            </DescriptionCard>
+
+            <DescriptionCard
+                theme={DescriptionCardTheme.DEFENSEGALLERY}
+                path="/collections/furniture"
+            >
+                <DescriptionCard.Texts>
+                    <DescriptionCard.Texts.Title>Huonekalut</DescriptionCard.Texts.Title>
+                    <DescriptionCard.Texts.Body>Huonekalu-kokoelmat.</DescriptionCard.Texts.Body>
+                </DescriptionCard.Texts>
+            </DescriptionCard>
+
+            <DescriptionCard
+                theme={DescriptionCardTheme.DEFENSEGALLERY}
+                path="/collections/music"
+            >
+                <DescriptionCard.Texts>
+                    <DescriptionCard.Texts.Title>Musiikki</DescriptionCard.Texts.Title>
+                    <DescriptionCard.Texts.Body>Musiikki-kokoelmat.</DescriptionCard.Texts.Body>
+                </DescriptionCard.Texts>
+            </DescriptionCard>
         </div>
     );
 };
