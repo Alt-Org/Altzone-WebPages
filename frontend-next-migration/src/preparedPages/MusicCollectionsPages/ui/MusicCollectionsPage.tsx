1eee126eff166c8d5faa81985508ffdb4c288b6f
emintt <60625745+emintt@users.noreply.github.com>
2025-09-17 09:45:26 +0300

    add search functionality and search bar styles

--- frontend-next-migration/src/preparedPages/MusicCollectionsPages/ui/MusicCollectionsPage.tsx
+++ frontend-next-migration/src/preparedPages/MusicCollectionsPages/ui/MusicCollectionsPage.tsx
@@ -1,51 +1,65 @@
 'use client';
-import { NavigationRow } from '@/features/NavigateFurniture/ui/NavigatioRow/NavigationRow';
-// import SearchIcon from '@/shared/assets/icons/Search.svg';
-import { SearchBar } from '@/preparedPages/FurnitureCollectionsPages/ui/SingleFurnitureCollectionPage';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import cls from './MusicCollectionsPage.module.scss';
+import SearchIcon from '@/shared/assets/icons/Search.svg';
 import { useClientTranslation } from '@/shared/i18n';
 import { PageTitle } from '@/shared/ui/PageTitle';
-import cls from './MusicCollectionsPage.module.scss';
-import { useState } from 'react';
+import { NavigationRow } from '@/features/NavigateFurniture/ui/NavigatioRow/NavigationRow';
 import { YoutubeVideoCard } from '@/shared/ui/v2/YoutubeVideoCard';
 import { MusicManager } from '@/entities/Music/model/MusicCollectionsManager';
 import useSizes from '@/shared/lib/hooks/useSizes';
+import { classNames } from '@/shared/lib/classNames/classNames';
 
-// export interface SearchBarProps {
-//     className: string;
-//     value: string;
-//     onChange: (value: string) => void;
-//     placeholder: string;
-// }
+interface SearchBarProps {
+    className: string;
+    value: string;
+    onChange: (value: string) => void;
+    placeholder: string;
+}
 
-// export const SearchBar = (props: SearchBarProps) => {
-//     const { className, value, onChange, placeholder } = props;
-//     return (
-//         // <div className={classNames(cls.SearchBar, undefined, [className])}>
-//         <div>
-//             <Image
-//                 src={SearchIcon}
-//                 alt="search icon"
-//                 height={20}
-//             />
-//             <input
-//                 type="text"
-//                 value={value}
-//                 onChange={(event) => onChange(event.target.value)}
-//                 placeholder={placeholder}
-//                 // className={cls.Input}
-//             />
-//         </div>
-//     );
-// };
+const SearchBar = (props: SearchBarProps) => {
+    const { className, value, onChange, placeholder } = props;
+    return (
+        <div className={classNames(cls.SearchBar, undefined, [className])}>
+            <Image
+                src={SearchIcon}
+                alt="search icon"
+                height={20}
+            />
+            <input
+                type="text"
+                value={value}
+                name="search"
+                onChange={(event) => onChange(event.target.value)}
+                placeholder={placeholder}
+                className={cls.Input}
+            />
+        </div>
+    );
+};
 
 const MusicCollectionsPage = () => {
     const { t } = useClientTranslation('music');
     const manager = new MusicManager();
     const { isMobileSize, isTabletSize } = useSizes();
-
     const [searchQuery, setSearchQuery] = useState('');
 
     const allItems = manager.getAllCollectionsItems();
+    const filteredItems = React.useMemo(() => {
+        if (!allItems) return [];
+
+        if (searchQuery.trim() === '') return allItems;
+
+        return allItems.filter((item) => {
+            // filter by music title and artist name
+            return (
+                item.musicTitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                item.artistName.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+        });
+    }, [allItems, searchQuery]);
+
     return (
         <div className={cls.Container}>
             {isMobileSize || isTabletSize ? (
@@ -61,7 +75,6 @@ const MusicCollectionsPage = () => {
                         titleText={t('music-collections-title')}
                         alternate={true}
                         searchVisible={false}
-                        className={cls.PageTitle}
                     />
                     <SearchBar
                         className={cls.SearchBarDesktop}
@@ -73,10 +86,10 @@ const MusicCollectionsPage = () => {
             )}
             <NavigationRow />
             <div className={cls.DesktopCardContainer}>
-                {allItems && allItems.length === 0 && <div>No music items found</div>}
-                {allItems &&
-                    allItems.length > 0 &&
-                    allItems.map((item, index) => {
+                {filteredItems && filteredItems.length === 0 && <div>No music items found</div>}
+                {filteredItems &&
+                    filteredItems.length > 0 &&
+                    filteredItems.map((item, index) => {
                         return (
                             <YoutubeVideoCard
                                 key={index}
