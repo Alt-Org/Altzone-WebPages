e325610c03a246e837b0ebcc17afeaa8f7ea8513
juha <juhaj358@gmail.com>
2025-10-07 12:14:12 +0300

    BarIndicator component with a fixed width

--- frontend-next-migration/src/app/[lng]/(helper)/admin/_components/BarIndicatorBlock.tsx
+++ frontend-next-migration/src/app/[lng]/(helper)/admin/_components/BarIndicatorBlock.tsx
@@ -2,9 +2,56 @@ import { BarIndicator } from '@/shared/ui/v2/BarIndicator';
 
 const BarIndicatorBlock = () => {
     return (
-        <div style={{ background: 'var(--base-card-background)', paddingBottom: '1.5em' }}>
-            <div style={{ width: '33%', height: '2em', padding: '2em' }}>
-                <BarIndicator value={5} />
+        <div
+            style={{
+                background: 'var(--base-card-background)',
+                paddingBottom: '1.5em',
+                marginTop: '1em',
+            }}
+        >
+            <div
+                style={{
+                    width: '100%',
+                    height: 'fit-content',
+                    padding: '2em',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    gap: '2rem',
+                }}
+            >
+                <BarIndicator
+                    label={'Speed'}
+                    value={5}
+                    maxValue={20}
+                    developmentThreshold={'Kokenut'}
+                />
+                <BarIndicator
+                    label={'HP'}
+                    value={'-'}
+                    developmentThreshold={'Harjoittelija'}
+                />
+                <BarIndicator
+                    label={'strike'}
+                    value={12}
+                    maxValue={14}
+                    developmentThreshold={'Konkari'}
+                />
+                <BarIndicator
+                    label={'size'}
+                    value={8}
+                    developmentThreshold={'Kokenut'}
+                />
+                <BarIndicator
+                    label={'resistance'}
+                    value={16}
+                    maxValue={23}
+                    developmentThreshold={'Aloittelija'}
+                />
+                <BarIndicator
+                    value={14}
+                    maxValue={20}
+                />
             </div>
         </div>
     );
