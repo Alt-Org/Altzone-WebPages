b9a233b264db3f545986bc03998231297f5809c4
Sara Salim <ssalim@student.uef.fi>
2025-10-23 01:39:01 +0300

    Adjusted layout

--- frontend-next-migration/src/preparedPages/GameArtPage/ui/GameArtPage.tsx
+++ frontend-next-migration/src/preparedPages/GameArtPage/ui/GameArtPage.tsx
@@ -1,11 +1,11 @@
 'use client';
 import cls from './GameArtPage.module.scss';
 import { LayoutWithSidebars } from '@/preparedPages/Layouts';
-import WikiContent from '@/shared/ui/WikiContent/ui/WikiContent';
 import { ModularCard, ModularCardTheme } from '@/shared/ui/v2/ModularCard';
 import GameArtNavMenu from '@/features/NavigateGameArt';
 import { PageTitle } from '@/shared/ui/PageTitle';
 import { Container } from '@/shared/ui/Container';
+import useSizes from '@/shared/lib/hooks/useSizes';
 
 interface Section {
     id: string;
@@ -24,56 +24,66 @@ export type Props = {
 
 const GameArtPackagePage = (props: Props) => {
     const { sections = [], title } = props;
+    const { isMobileSize, isTabletSize } = useSizes();
 
     return (
         <main>
-            <LayoutWithSidebars
-                leftTopSidebar={{
-                    component: <GameArtNavMenu sections={sections} />,
-                    hideOnMobile: false,
-                }}
-            >
+            {(isTabletSize || isMobileSize) && (
                 <Container className={cls.Title}>
                     <PageTitle
                         titleText={title}
                         alternate={true}
                     />
                 </Container>
-                <div className={cls.SectionGrid}>
-                    {sections.map((section) => {
-                        return (
-                            <ModularCard
-                                key={section.id}
-                                theme={ModularCardTheme.SECTIONCARD}
-                                className={cls.SectionCard}
-                            >
-                                <ModularCard.Texts>
-                                    <ModularCard.Texts.Title>
-                                        {section.label}
-                                    </ModularCard.Texts.Title>
-                                    <ModularCard.Texts.Body>
-                                        <div
-                                            dangerouslySetInnerHTML={{
-                                                __html: section.description,
-                                            }}
-                                        />
-                                    </ModularCard.Texts.Body>
-                                </ModularCard.Texts>
+            )}
 
-                                {section.image && (
-                                    <ModularCard.Image className={cls.SectionCardImage}>
-                                        <ModularCard.Image.Triangle />
-                                        <ModularCard.Image.Image
-                                            src={section.image}
-                                            alt={section.imageAlt}
-                                            width={600}
-                                            height={400}
-                                        />
-                                    </ModularCard.Image>
-                                )}
-                            </ModularCard>
-                        );
-                    })}
+            <LayoutWithSidebars
+                className={cls.SideBar}
+                leftTopSidebar={{
+                    component: <GameArtNavMenu sections={sections} />,
+                    hideOnMobile: false,
+                }}
+            >
+                {!isTabletSize && !isMobileSize && (
+                    <Container className={cls.Title}>
+                        <PageTitle
+                            titleText={title}
+                            alternate
+                        />
+                    </Container>
+                )}
+
+                <div className={cls.SectionGrid}>
+                    {sections.map((section) => (
+                        <ModularCard
+                            key={section.id}
+                            theme={ModularCardTheme.SECTIONCARD}
+                            className={cls.SectionCard}
+                        >
+                            <ModularCard.Texts>
+                                <ModularCard.Texts.Title>{section.label}</ModularCard.Texts.Title>
+                                <ModularCard.Texts.Body>
+                                    <div
+                                        dangerouslySetInnerHTML={{
+                                            __html: section.description,
+                                        }}
+                                    />
+                                </ModularCard.Texts.Body>
+                            </ModularCard.Texts>
+
+                            {section.image && (
+                                <ModularCard.Image className={cls.SectionCardImage}>
+                                    <ModularCard.Image.Triangle />
+                                    <ModularCard.Image.Image
+                                        src={section.image}
+                                        alt={section.imageAlt}
+                                        width={600}
+                                        height={400}
+                                    />
+                                </ModularCard.Image>
+                            )}
+                        </ModularCard>
+                    ))}
                 </div>
             </LayoutWithSidebars>
         </main>
