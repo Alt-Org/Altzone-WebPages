7ebee5a2fe62c744bf19dc16ad5f59b8ca1488c5
eleino <emilia.leino@proton.me>
2025-11-08 11:49:52 +0200

    Updated TeachersPage to use Container component, updated css to always use global font styles

--- frontend-next-migration/src/preparedPages/TeachersPage/ui/TeachersPage.tsx
+++ frontend-next-migration/src/preparedPages/TeachersPage/ui/TeachersPage.tsx
@@ -5,48 +5,51 @@ import { DescriptionCard, DescriptionCardTheme } from '@/shared/ui/v2/Descriptio
 import LessonList from './LessonList/LessonList';
 import { Button } from '@/shared/ui/v2/Button';
 import { useClientTranslation } from '@/shared/i18n';
+import { Container } from '@/shared/ui/Container';
 
 const TeachersPage = () => {
     const { t } = useClientTranslation('teachers');
     return (
-        <div className={cls.container}>
-            <PageTitle
-                titleText={t('head-title')}
-                alternate={true}
-                searchVisible={false}
-            />
-            <DescriptionCard theme={DescriptionCardTheme.TEACHERS}>
-                <DescriptionCard.Texts>
-                    <DescriptionCard.Texts.Title>{t('welcome')}!</DescriptionCard.Texts.Title>
-                    <h3>{t('instruction-title')}</h3>
-                    <p dangerouslySetInnerHTML={{ __html: t('instruction-text') }} />
-                </DescriptionCard.Texts>
-            </DescriptionCard>
-            <DescriptionCard theme={DescriptionCardTheme.TEACHERS}>
-                <LessonList />
-            </DescriptionCard>
-            <DescriptionCard theme={DescriptionCardTheme.TEACHERS}>
-                <DescriptionCard.Texts>
-                    <DescriptionCard.Texts.Title>
-                        {t('preparation-title')}
-                    </DescriptionCard.Texts.Title>
-                    <h3>{t('managing-title')}</h3>
-                    <p>{t('managing-text')}</p>
-                    <h3>{t('implementation-title')}</h3>
-                    <p dangerouslySetInnerHTML={{ __html: t('implementation-text') }} />
-                    <h3>{t('preparatory-materials-title')}</h3>
-                    <p>{t('preparatory-materials-text')}</p>
-                    <h3>{t('game-literacy-tasks-title')}</h3>
-                    <p>{t('game-literacy-tasks-text')}</p>
-                    <Button
-                        path="/teachers/tasks"
-                        className={cls.taskButton}
-                    >
-                        {t('game-literacy-tasks-button')}
-                    </Button>
-                </DescriptionCard.Texts>
-            </DescriptionCard>
-        </div>
+        <Container>
+            <div className={cls.container}>
+                <PageTitle
+                    titleText={t('head-title')}
+                    alternate={true}
+                    searchVisible={false}
+                />
+                <DescriptionCard theme={DescriptionCardTheme.TEACHERS}>
+                    <DescriptionCard.Texts>
+                        <DescriptionCard.Texts.Title>{t('welcome')}!</DescriptionCard.Texts.Title>
+                        <h3>{t('instruction-title')}</h3>
+                        <p dangerouslySetInnerHTML={{ __html: t('instruction-text') }} />
+                    </DescriptionCard.Texts>
+                </DescriptionCard>
+                <DescriptionCard theme={DescriptionCardTheme.TEACHERS}>
+                    <LessonList />
+                </DescriptionCard>
+                <DescriptionCard theme={DescriptionCardTheme.TEACHERS}>
+                    <DescriptionCard.Texts>
+                        <DescriptionCard.Texts.Title>
+                            {t('preparation-title')}
+                        </DescriptionCard.Texts.Title>
+                        <h3>{t('managing-title')}</h3>
+                        <p>{t('managing-text')}</p>
+                        <h3>{t('implementation-title')}</h3>
+                        <p dangerouslySetInnerHTML={{ __html: t('implementation-text') }} />
+                        <h3>{t('preparatory-materials-title')}</h3>
+                        <p>{t('preparatory-materials-text')}</p>
+                        <h3>{t('game-literacy-tasks-title')}</h3>
+                        <p>{t('game-literacy-tasks-text')}</p>
+                        <Button
+                            path="/teachers/tasks"
+                            className={cls.taskButton}
+                        >
+                            {t('game-literacy-tasks-button')}
+                        </Button>
+                    </DescriptionCard.Texts>
+                </DescriptionCard>
+            </div>
+        </Container>
     );
 };
 
