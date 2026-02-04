f0809e28bb51f7e4e8679d42154ba1c315d22873
patinen <juho.patinen@gmail.com>
2026-02-04 11:53:57 +0200

    refactor news page to use standard routes and remove admin bench variants

--- frontend-next-migration/src/app/[lng]/(helper)/admin/news/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { withMetadataGenerator, withPageData } from '@/app/_helpers';
-import AdminNewsElementPage from '@/preparedPages/NewsPages/ui/NewsElementPage/ui/AdminNewsElementPage.async';
-import { _getPage } from './_getPage';
-
-export const generateMetadata = withMetadataGenerator(_getPage);
-export default withPageData(AdminNewsElementPage, _getPage);
--- frontend-next-migration/src/preparedPages/NewsPages/ui/NewsElementPage/ui/AdminNewsElementPage.async.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const AdminNewsElementPageAsync = dynamic(() => import('./AdminNewsElementPage'));
-
-export default AdminNewsElementPageAsync;
