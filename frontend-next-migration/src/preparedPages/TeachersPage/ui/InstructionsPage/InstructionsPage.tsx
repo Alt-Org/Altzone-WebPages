b76e09e62388b395e8ae3a9049202a2f1b8f1a9a
Liisa Davydov <liisamuttonen@gmail.com>
2026-07-15 16:27:25 +0300

    Add instruction card to instructions page
    Add instruction description card template to instruction page

--- frontend-next-migration/src/preparedPages/TeachersPage/ui/InstructionsPage/InstructionsPage.tsx
+++ frontend-next-migration/src/preparedPages/TeachersPage/ui/InstructionsPage/InstructionsPage.tsx
@@ -3,28 +3,33 @@ import React from 'react';
 import { NavigateTeachers } from '@/features/NavigateTeachers';
 import { SectionLessonFlow } from '@/widgets/SectionLessonFlow';
 import { Container } from '@/shared/ui/Container';
+import { DescriptionCard, DescriptionCardTheme } from '@/shared/ui/v2/DescriptionCard';
 import { useClientTranslation } from '@/shared/i18n';
 import { PageTitle } from '@/shared/ui/PageTitle';
 import cls from './InstructionPage.module.scss';
 
-export interface Props {
-    title: string;
-}
-
 const InstructionsPage = () => {
+    //watch and flow are not defined in english version
     const { t } = useClientTranslation('teachers');
     return (
-        <div>
-            <PageTitle
-                titleText={t('lesson')}
-                alternate={true}
-                searchVisible={false}
-            />
-            <div className={cls.Container}>
+        <Container>
+            <div className={cls.container}>
+                <PageTitle
+                    titleText={t('lesson')}
+                    alternate={true}
+                    searchVisible={false}
+                />
+                <DescriptionCard theme={DescriptionCardTheme.INSTRUCTION}>
+                    <DescriptionCard.Texts width="100%">
+                        <DescriptionCard.Texts.Title>{t('flow')}</DescriptionCard.Texts.Title>
+
+                        <DescriptionCard.Texts.Body>{t('watch')}</DescriptionCard.Texts.Body>
+                    </DescriptionCard.Texts>
+                </DescriptionCard>
                 <NavigateTeachers />
                 <SectionLessonFlow />
             </div>
-        </div>
+        </Container>
     );
 };
 
